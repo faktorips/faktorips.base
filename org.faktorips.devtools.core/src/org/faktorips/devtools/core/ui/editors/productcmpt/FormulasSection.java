3ea2f45a691e6f9bfffea78090e204d1098a5771
guenther <guenther>
2006-02-08 12:56:08 +0000

    *** empty log message ***

--- org.faktorips.devtools.core/src/org/faktorips/devtools/core/ui/editors/productcmpt/FormulasSection.java
+++ org.faktorips.devtools.core/src/org/faktorips/devtools/core/ui/editors/productcmpt/FormulasSection.java
@@ -1,6 +1,8 @@
 package org.faktorips.devtools.core.ui.editors.productcmpt;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
 import org.eclipse.core.runtime.CoreException;
@@ -8,6 +10,7 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.ui.forms.widgets.Section;
 import org.faktorips.devtools.core.IpsPlugin;
@@ -25,39 +28,62 @@ import org.faktorips.devtools.core.ui.forms.IpsSection;
 import org.faktorips.util.ArgumentCheck;
 
 /**
+ * Section to display and edit the formulas of a product
  * 
+ * @author Thorsten Guenther
  */
 public class FormulasSection extends IpsSection {
 	
+	/**
+	 * Generation which holds the informations to display
+	 */
 	private IProductCmptGeneration generation;
-	private Composite workArea;
+
+	/**
+	 * Pane which serves as parent for all controlls created inside this section.
+	 */
+	private Composite rootPane;
+	
+	/**
+	 * Toolkit to handle common ui-operations
+	 */
 	private UIToolkit toolkit;
-	private CompositeUIController uiController;
-	private boolean fGenerationDirty;
+
+	/**
+	 * List of controls displaying data (needed to enable/disable).
+	 */
+	private List editControls = new ArrayList();
+
+	/**
+	 * Controller to handle update of ui and model automatically.
+	 */
+	private CompositeUIController uiMasterController;
+	
+	/**
+	 * Label which is displayed if no formulas are defined.
+	 */
 	private Label noFormulasLabel;
-	private ArrayList labels;
 	
 	public FormulasSection(IProductCmptGeneration generation, Composite parent, UIToolkit toolkit) {
 		super(parent, Section.TITLE_BAR, GridData.FILL_BOTH, toolkit);
 		ArgumentCheck.notNull(generation);
 		
 		this.generation = generation;
-		fGenerationDirty = true;
 		initControls();
 		setText(Messages.FormulasSection_calculationFormulas);
 	}
 	
 	/**
-	 * Overridden
+	 * {@inheritDoc}
 	 */
 	protected void initClientComposite(Composite client, UIToolkit toolkit) {
     	GridLayout layout = new GridLayout(1, true);
     	layout.marginHeight = 2;
     	layout.marginWidth = 1;
     	client.setLayout(layout);
-    	workArea = toolkit.createLabelEditColumnComposite(client);
-    	workArea.setLayoutData(new GridData(GridData.FILL_BOTH));
-    	GridLayout workAreaLayout = (GridLayout) workArea.getLayout();
+    	rootPane = toolkit.createLabelEditColumnComposite(client);
+    	rootPane.setLayoutData(new GridData(GridData.FILL_BOTH));
+    	GridLayout workAreaLayout = (GridLayout) rootPane.getLayout();
     	workAreaLayout.marginHeight = 5;
     	workAreaLayout.marginWidth = 5;
     	this.toolkit = toolkit;
@@ -66,39 +92,30 @@ public class FormulasSection extends IpsSection {
     	// around
     	// the text control. Can only be understood by looking at the
     	// FormToolkit.PaintBorder class.
-    	workArea.setData(FormToolkit.KEY_DRAW_BORDER, FormToolkit.TREE_BORDER);
-    	toolkit.getFormToolkit().paintBordersFor(workArea);
+    	rootPane.setData(FormToolkit.KEY_DRAW_BORDER, FormToolkit.TREE_BORDER);
+    	toolkit.getFormToolkit().paintBordersFor(rootPane);
+		createEditControls();
 	}
 	
 	/**
-	 * Overridden method.
+	 * {@inheritDoc}
 	 */
 	protected void performRefresh() {
-		if (fGenerationDirty || structureChanged()) {
-			createEditControls();
-		}
+		uiMasterController.updateUI();
 	}
 	
 	/**
-	 * Determines whether the structure of the underlying data has changed
+	 * Create the ui-elements
 	 */
-	private boolean structureChanged() {
-		return true;
-	}
-	
     private void createEditControls() {
-    	if (labels == null) {
-    		labels = new ArrayList();
-    	}
-    	
-    	uiController = new CompositeUIController();
+    	uiMasterController = new CompositeUIController();
     	IpsObjectUIController ctrl = new IpsObjectUIController(generation.getIpsObject());
-    	uiController.add(ctrl);
+    	uiMasterController.add(ctrl);
     	
     	IConfigElement[] elements = generation.getConfigElements(ConfigElementType.FORMULA);
     	
     	if (elements.length == 0 && noFormulasLabel == null) {
-    		noFormulasLabel = toolkit.createLabel(workArea, Messages.FormulasSection_noFormulasDefined);
+    		noFormulasLabel = toolkit.createLabel(rootPane, Messages.FormulasSection_noFormulasDefined);
     	}
     	else if (elements.length > 0 && noFormulasLabel != null) {
 			noFormulasLabel.dispose();
@@ -107,34 +124,38 @@ public class FormulasSection extends IpsSection {
     		    	
     	for (int i = 0; i < elements.length; i++) {
     		
-    		if (i < labels.size()) {
-    			((Label)labels.get(i)).setText(StringUtils.capitalise(elements[i].getName()));
-    		}
-    		else {
-    			Label label = toolkit.createFormLabel(workArea, StringUtils.capitalise(elements[i].getName()));
-    			labels.add(label);
-    			FormulaEditControl evc = new FormulaEditControl(workArea, toolkit, elements[i], this.getShell());
-    			evc.setText(elements[i].getValue());
-    			ctrl.add(evc.getTextControl(), elements[i], ConfigElement.PROPERTY_VALUE);
-    			try {
-    				FormulaCompletionProcessor completionProcessor = new FormulaCompletionProcessor(elements[i].getIpsProject(), elements[i].getExprCompiler());
-    				ContentAssistHandler.createHandlerForText(evc.getTextControl(), CompletionUtil.createContentAssistant(completionProcessor));
-    			} catch (CoreException e) {
-    				IpsPlugin.logAndShowErrorDialog(e);
-    			}
-    		}    		
+			toolkit.createFormLabel(rootPane, StringUtils.capitalise(elements[i].getName()));
+			FormulaEditControl evc = new FormulaEditControl(rootPane, toolkit, elements[i], this.getShell());
+			evc.setText(elements[i].getValue());
+			ctrl.add(evc.getTextControl(), elements[i], ConfigElement.PROPERTY_VALUE);
+			this.editControls.add(evc.getTextControl());
+			
+			try {
+				FormulaCompletionProcessor completionProcessor = new FormulaCompletionProcessor(elements[i].getIpsProject(), elements[i].getExprCompiler());
+				ContentAssistHandler.createHandlerForText(evc.getTextControl(), CompletionUtil.createContentAssistant(completionProcessor));
+			} catch (CoreException e) {
+				IpsPlugin.logAndShowErrorDialog(e);
+			}
     	}
-    }
+    	
+		rootPane.layout(true);
+		rootPane.redraw();
+   }
 
-    public void setActiveGeneration(IProductCmptGeneration generation) {
-		if (this.generation.equals(generation)) {
-			return;
-		}
+	/**
+	 * {@inheritDoc}
+	 */
+	public void setEnabled(boolean enabled) {
+		super.setEnabled(enabled);
 		
-		if (generation instanceof IProductCmptGeneration) {
-			this.generation = (IProductCmptGeneration)generation;
-			fGenerationDirty = true;
-			performRefresh();
+		// to get the disabled look, we have to disable all the input-fields manually :-(
+		for (Iterator iter = editControls.iterator(); iter.hasNext();) {
+			Text element = (Text) iter.next();
+			element.setEnabled(enabled);
+			
 		}
+		rootPane.layout(true);
+		rootPane.redraw();
 	}
+
 }
