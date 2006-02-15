9b76698bf456fe1a09a8bca9e7a0f11ffe02ee99
erzberger <erzberger>
2006-02-15 09:44:08 +0000

    *** empty log message ***

--- org.faktorips.devtools.core/src/org/faktorips/devtools/core/builder/BuilderHelper.java
+++ org.faktorips.devtools.core/src/org/faktorips/devtools/core/builder/BuilderHelper.java
@@ -1,23 +1,11 @@
 package org.faktorips.devtools.core.builder;
 
-import java.io.InputStream;
 import java.util.ArrayList;
 
 import org.apache.commons.lang.StringUtils;
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.MultiStatus;
-import org.eclipse.core.runtime.Path;
-import org.eclipse.jdt.core.IPackageFragment;
 import org.faktorips.datatype.Datatype;
-import org.faktorips.devtools.core.IpsStatus;
-import org.faktorips.devtools.core.model.IIpsObject;
-import org.faktorips.devtools.core.model.IIpsPackageFragment;
 import org.faktorips.devtools.core.model.IIpsProject;
-import org.faktorips.devtools.core.model.IIpsSrcFolderEntry;
-import org.faktorips.devtools.core.model.pctype.IAttribute;
 import org.faktorips.devtools.core.model.pctype.Parameter;
 
 /**
@@ -107,75 +95,4 @@ public class BuilderHelper {
     private BuilderHelper() {
         super();
     }
-
-    /**
-     * find the datatype for the attribute in the project configuration
-     */
-    public final static Datatype findAttributeDatatype(IAttribute a) 
-    throws CoreException {
-        Datatype datatype = a.getIpsProject().findDatatype(a.getDatatype());
-        if (datatype == null) {
-            throw new CoreException(new IpsStatus(
-                    "No datatype found for attribute " + a.getName() + "and datatype " + a.getDatatype()));                       
-        }
-        return datatype;
-    }
-    
-    /**
-     * Returns the name of the provided file without its extension.
-     */
-    public final static String getFileNameWithoutExtension(IFile file){
-        String name = file.getName(); 
-        return file.getFileExtension() == null ? name : 
-            name.substring(0, name.length() - 1 - file.getFileExtension().length());
-    }
-    
-    /**
-     * Copies the file of the provided IpsObject to the corresponding folder of the generated files.
-     * In addition to that it changes the file extension to .xml. 
-     */
-    public final static void copyIpsObjectResource(IIpsObject ipsObject, MultiStatus buildStatus){
-        IFile file = (IFile)ipsObject.getEnclosingResource();
-        InputStream is;
-        try {
-            is = file.getContents(true);
-        } catch (Exception e) {
-            buildStatus.add(new IpsStatus("Can't read xml file contents for " + file, e));
-            return;
-        }
-        IFolder folder = null;
-        try {
-            IIpsSrcFolderEntry srcEntry = (IIpsSrcFolderEntry)ipsObject.getIpsPackageFragment().getRoot().getIpsObjectPathEntry();
-            IPackageFragment javaPack = ipsObject.getIpsPackageFragment().getJavaPackageFragment(IIpsPackageFragment.JAVA_PACK_PUBLISHED_INTERFACE);
-            IPath pathToPack = new Path(javaPack.getElementName().replace('.', '/'));
-            folder = srcEntry.getOutputFolderForGeneratedJavaFiles().getFolder(pathToPack);
-            if (!folder.exists()) {
-                createFolder(folder);
-            }
-        } catch (Exception e) {
-            buildStatus.add(new IpsStatus("Can't create folder " + folder, e));
-            return;
-        }
-        IFile copy = null;
-        try {
-            copy = folder.getFile(getFileNameWithoutExtension(file) + ".xml");
-            if (copy.exists()) {
-                copy.setContents(is, true, true, null);
-            } else {
-                copy.create(is, true, null);
-            }
-        } catch (Exception e) {
-            buildStatus.add(new IpsStatus("Error copying contents of file " + file + " to " + copy, e));
-            return;
-        }
-    }
-    
-    private final static void createFolder(IFolder folder) throws CoreException {
-        while (!folder.getParent().exists()) {
-            createFolder((IFolder)folder.getParent());
-        }
-        folder.create(true, true, null);
-    }
-
-
 }
