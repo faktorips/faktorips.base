c915f4860843259b6d2b78125b27cad7c55e4f9e
guenther <guenther>
2006-03-01 09:52:42 +0000

    Properties page for ips-properties on folders

---
+++ org.faktorips.devtools.core/src/org/faktorips/devtools/core/ui/FolderPropertiesPage.java
@@ -0,0 +1,74 @@
+package org.faktorips.devtools.core.ui;
+
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.QualifiedName;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.IWorkbenchPropertyPage;
+import org.eclipse.ui.dialogs.PropertyPage;
+import org.faktorips.devtools.core.IpsPlugin;
+
+/**
+ * Page to display and modify properties for FaktorIPS applied on folders.
+ * 
+ * @author Thorsten Guenther
+ */
+public class FolderPropertiesPage extends PropertyPage implements IWorkbenchPropertyPage, ModifyListener {
+
+	/**
+	 * The input field for the order value
+	 */
+	private Text orderValue; 
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	protected Control createContents(Composite parent) {
+		Composite root = new Composite(parent, SWT.NONE);
+		
+		root.setLayout(new GridLayout(2, false));
+		root.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		Label label = new Label(root, SWT.LEFT);
+		label.setText("Sorting order number: ");
+		orderValue = new Text(root, SWT.BORDER);
+		orderValue.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+		QualifiedName name = new QualifiedName(IpsPlugin.PLUGIN_ID, "FolderOrderNumber");
+		String value = null;;
+		try {
+			value = ((IFolder)super.getElement()).getPersistentProperty(name);
+		} catch (CoreException e) {
+			IpsPlugin.log(e);
+		}
+		
+		if (value == null) {
+			value = "0";
+		}
+		
+		orderValue.setText(value);
+		
+		orderValue.addModifyListener(this);
+		return root;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void modifyText(ModifyEvent e) {
+		try {
+			Integer.valueOf(orderValue.getText());
+			super.setMessage(null);
+		} catch (NumberFormatException e1) {
+			super.setMessage("The sorting oder number has to be a valid integer.", ERROR);
+		}
+		
+	}
+
+}
