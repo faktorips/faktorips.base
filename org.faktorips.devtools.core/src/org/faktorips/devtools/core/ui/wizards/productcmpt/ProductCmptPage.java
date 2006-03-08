302a99d7b151d47b8e739f6d2aefeff63dd8ee54
guenther <guenther>
2006-03-08 08:49:32 +0000

    undo version 1.4 to 1.3

--- org.faktorips.devtools.core/src/org/faktorips/devtools/core/ui/wizards/productcmpt/ProductCmptPage.java
+++ org.faktorips.devtools.core/src/org/faktorips/devtools/core/ui/wizards/productcmpt/ProductCmptPage.java
@@ -1,12 +1,15 @@
 package org.faktorips.devtools.core.ui.wizards.productcmpt;
 
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.widgets.Composite;
 import org.faktorips.devtools.core.model.IIpsPackageFragmentRoot;
+import org.faktorips.devtools.core.model.pctype.IPolicyCmptType;
 import org.faktorips.devtools.core.ui.UIToolkit;
 import org.faktorips.devtools.core.ui.controller.fields.TextButtonField;
-import org.faktorips.devtools.core.ui.controls.ProductCmptRefControl;
+import org.faktorips.devtools.core.ui.controls.PcTypeRefControl;
 import org.faktorips.devtools.core.ui.wizards.IpsObjectPage;
 
 
@@ -15,7 +18,7 @@ import org.faktorips.devtools.core.ui.wizards.IpsObjectPage;
  */
 public class ProductCmptPage extends IpsObjectPage {
     
-    private ProductCmptRefControl productCmptRefControl;
+    private PcTypeRefControl pcTypeControl;
     
     /**
      * @param pageName
@@ -33,14 +36,13 @@ public class ProductCmptPage extends IpsObjectPage {
     protected void fillNameComposite(Composite nameComposite, UIToolkit toolkit) {
         addNameLabelField(toolkit);
         toolkit.createFormLabel(nameComposite, Messages.ProductCmptPage_labelName);
-        
-        productCmptRefControl = new ProductCmptRefControl(null, nameComposite, toolkit);
-        TextButtonField pcTypeField = new TextButtonField(productCmptRefControl);
+        pcTypeControl = toolkit.createPcTypeRefControl(null, nameComposite);
+        TextButtonField pcTypeField = new TextButtonField(pcTypeControl);
         pcTypeField.addChangeListener(this);
     }
     
     String getPolicyCmptType() {
-        return productCmptRefControl.getText();
+        return pcTypeControl.getText();
     }
     
     /** 
@@ -51,9 +53,27 @@ public class ProductCmptPage extends IpsObjectPage {
         super.sourceFolderChanged();
         IIpsPackageFragmentRoot root = getPdPackageFragmentRoot();
         if (root!=null) {
-        	productCmptRefControl.setPdProject(root.getIpsProject());
+            pcTypeControl.setPdProject(root.getIpsProject());
         } else {
-        	productCmptRefControl.setPdProject(null);
+            pcTypeControl.setPdProject(null);
         }
-    }    
+    }
+    
+    /**
+     * Overridden IMethod.
+     *
+     * @see org.faktorips.devtools.core.ui.wizards.IpsObjectPage#validatePage()
+     */
+    protected void validatePage() throws CoreException {
+        super.validatePage();
+        if (getErrorMessage()!=null) {
+            return;
+        }
+        IPolicyCmptType pcType = pcTypeControl.findPcType();
+        if (pcType==null) {
+			setErrorMessage(NLS.bind(Messages.ProductCmptPage_msgPolicyClassMissing, pcTypeControl.getText()));
+			return;
+        }
+        updatePageComplete();
+    }
 }
