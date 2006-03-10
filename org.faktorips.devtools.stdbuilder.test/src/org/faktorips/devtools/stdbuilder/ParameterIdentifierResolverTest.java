fe317d24df354c186a379d2639e4a511f0c38452
senf <senf>
2006-03-10 15:56:54 +0000

    *** empty log message ***

---
+++ org.faktorips.devtools.stdbuilder.test/src/org/faktorips/devtools/stdbuilder/ParameterIdentifierResolverTest.java
@@ -0,0 +1,147 @@
+/*******************************************************************************
+ * Copyright (c) 2005,2006 Faktor Zehn GmbH und andere.
+ *
+ * Alle Rechte vorbehalten.
+ *
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele,
+ * Konfigurationen, etc.) duerfen nur unter den Bedingungen der 
+ * Faktor-Zehn-Community Lizenzvereinbarung - Version 0.1 (vor Gruendung Community) 
+ * genutzt werden, die Bestandteil der Auslieferung ist und auch unter
+ *   http://www.faktorips.org/legal/cl-v01.html
+ * eingesehen werden kann.
+ *
+ * Mitwirkende:
+ *   Faktor Zehn GmbH - initial API and implementation - http://www.faktorzehn.de
+ *
+ *******************************************************************************/
+
+package org.faktorips.devtools.stdbuilder;
+
+import java.util.Locale;
+
+import org.faktorips.datatype.Datatype;
+import org.faktorips.devtools.core.IpsPluginTest;
+import org.faktorips.devtools.core.builder.JavaSourceFileBuilder;
+import org.faktorips.devtools.core.internal.model.pctype.PolicyCmptType;
+import org.faktorips.devtools.core.model.IIpsArtefactBuilder;
+import org.faktorips.devtools.core.model.IIpsPackageFragment;
+import org.faktorips.devtools.core.model.IIpsPackageFragmentRoot;
+import org.faktorips.devtools.core.model.IIpsProject;
+import org.faktorips.devtools.core.model.IIpsSrcFile;
+import org.faktorips.devtools.core.model.IParameterIdentifierResolver;
+import org.faktorips.devtools.core.model.IpsObjectType;
+import org.faktorips.devtools.core.model.pctype.IAttribute;
+import org.faktorips.devtools.core.model.pctype.Parameter;
+import org.faktorips.devtools.stdbuilder.policycmpttype.PolicyCmptInterfaceBuilder;
+import org.faktorips.fl.CompilationResult;
+import org.faktorips.fl.ExprCompiler;
+
+/**
+ * 
+ */
+public class ParameterIdentifierResolverTest extends IpsPluginTest {
+
+    private IIpsPackageFragmentRoot pdRootFolder;
+    private IIpsPackageFragment pdFolder;
+    private IIpsSrcFile pdSrcFile;
+    private PolicyCmptType pcType;
+    private IAttribute attribute;
+    private IParameterIdentifierResolver resolver;
+    private IIpsProject pdProject;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        pdProject = this.newIpsProject("TestProject");
+        pdProject.setGeneratedJavaSourcecodeDocumentationLanguage(Locale.GERMAN);
+        pdRootFolder = pdProject.getIpsPackageFragmentRoots()[0];
+        pdFolder = pdRootFolder.createPackageFragment("products.folder", true, null);
+        pdSrcFile = pdFolder.createIpsFile(IpsObjectType.POLICY_CMPT_TYPE, "TestPolicy", true, null);
+        pcType = (PolicyCmptType)pdSrcFile.getIpsObject();
+        attribute = pcType.newAttribute();
+        attribute.setName("tax");
+        attribute.setDatatype(Datatype.DECIMAL.getQualifiedName());
+        pdProject.setCurrentArtefactBuilderSet(StdBuilderPlugin.STANDARD_BUILDER_EXTENSION_ID);
+        resolver = pdProject.getCurrentArtefactBuilderSet().getFlParameterIdentifierResolver();
+        resolver.setIpsProject(pdProject);
+    }
+
+    private PolicyCmptInterfaceBuilder getPolicyCmptInterfaceBuilder() throws Exception {
+        IIpsArtefactBuilder[] builders = pdProject.getCurrentArtefactBuilderSet().getArtefactBuilders();
+        for (int i = 0; i < builders.length; i++) {
+            if (((JavaSourceFileBuilder)builders[i]).getKindId().equals(StandardBuilderSet.KIND_POLICY_CMPT_INTERFACE)) {
+                return (PolicyCmptInterfaceBuilder)builders[i];
+            }
+        }
+        return null;
+    }
+
+    public void testCompile() throws Exception {
+        Locale locale = Locale.GERMAN;
+
+        // no parameter registered => undefined identifier
+        CompilationResult result = resolver.compile("identifier", locale);
+        assertTrue(result.failed());
+        assertEquals(1, result.getMessages().getNoOfMessages());
+        assertEquals(ExprCompiler.UNDEFINED_IDENTIFIER, result.getMessages().getMessage(0).getCode());
+
+        // parameter with a value datatype
+        Parameter p1 = new Parameter(0, "rate", Datatype.MONEY.getQualifiedName());
+        resolver.setParameters(new Parameter[] { p1 });
+        result = resolver.compile("rate", locale);
+        assertTrue(result.successfull());
+        assertEquals(Datatype.MONEY, result.getDatatype());
+        assertEquals("rate", result.getCodeFragment().getSourcecode());
+
+        // parameter with the datatype being a policy component type
+        // => resolver can resolve identifiers with form paramName.attributeName
+        // with attributeName is the name of one of the type's attributes
+        Parameter p2 = new Parameter(1, "policy", pcType.getQualifiedName());
+        resolver.setParameters(new Parameter[] { p2 });
+        result = resolver.compile("policy.tax", locale);
+        assertTrue(result.successfull());
+        assertEquals(Datatype.DECIMAL, result.getDatatype());
+        String expected = "policy."
+                + getPolicyCmptInterfaceBuilder().getMethodNameGetPropertyValue(attribute, result.getDatatype()) + "()";
+        assertEquals(expected, result.getCodeFragment().getSourcecode());
+
+        // unkown parameter
+        result = resolver.compile("unkownParameter", locale);
+        assertTrue(result.failed());
+        assertEquals(1, result.getMessages().getNoOfMessages());
+        assertEquals(ExprCompiler.UNDEFINED_IDENTIFIER, result.getMessages().getMessage(0).getCode());
+
+        // parameter with unkown datatype
+        Parameter p3 = new Parameter(0, "p3", "UnkownDatatype");
+        resolver.setParameters(new Parameter[] { p3 });
+        result = resolver.compile("p3", locale);
+        assertTrue(result.failed());
+        assertEquals(1, result.getMessages().getNoOfMessages());
+        assertEquals(ExprCompiler.UNDEFINED_IDENTIFIER, result.getMessages().getMessage(0).getCode());
+
+        // unkown attribute
+        result = resolver.compile("policy.unkownAttribute", locale);
+        assertTrue(result.failed());
+        assertEquals(1, result.getMessages().getNoOfMessages());
+        assertEquals(ExprCompiler.UNDEFINED_IDENTIFIER, result.getMessages().getMessage(0).getCode());
+
+        // attribute with unkown datatype
+        attribute.setDatatype("UnknownDatatype");
+        result = resolver.compile("policy.tax", locale);
+        assertTrue(result.failed());
+        assertEquals(1, result.getMessages().getNoOfMessages());
+        assertEquals(ExprCompiler.UNDEFINED_IDENTIFIER, result.getMessages().getMessage(0).getCode());
+
+        // no attributename given
+        result = resolver.compile("policy.", locale);
+        assertTrue(result.failed());
+        assertEquals(1, result.getMessages().getNoOfMessages());
+        assertEquals(ExprCompiler.UNDEFINED_IDENTIFIER, result.getMessages().getMessage(0).getCode());
+
+        // unkown policy component type
+        result = resolver.compile("unkownType.tax", locale);
+        assertTrue(result.failed());
+        assertEquals(1, result.getMessages().getNoOfMessages());
+        assertEquals(ExprCompiler.UNDEFINED_IDENTIFIER, result.getMessages().getMessage(0).getCode());
+    }
+
+}
---
+++ org.faktorips.devtools.stdbuilder.test/src/org/faktorips/devtools/stdbuilder/TocFileBuilderTest.java
@@ -0,0 +1,164 @@
+/*******************************************************************************
+ * Copyright (c) 2005,2006 Faktor Zehn GmbH und andere.
+ *
+ * Alle Rechte vorbehalten.
+ *
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele,
+ * Konfigurationen, etc.) duerfen nur unter den Bedingungen der 
+ * Faktor-Zehn-Community Lizenzvereinbarung - Version 0.1 (vor Gruendung Community) 
+ * genutzt werden, die Bestandteil der Auslieferung ist und auch unter
+ *   http://www.faktorips.org/legal/cl-v01.html
+ * eingesehen werden kann.
+ *
+ * Mitwirkende:
+ *   Faktor Zehn GmbH - initial API and implementation - http://www.faktorzehn.de
+ *
+ *******************************************************************************/
+
+package org.faktorips.devtools.stdbuilder;
+
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
+import org.eclipse.core.runtime.CoreException;
+import org.faktorips.devtools.core.IpsPluginTest;
+import org.faktorips.devtools.core.model.IIpsPackageFragmentRoot;
+import org.faktorips.devtools.core.model.IIpsProject;
+import org.faktorips.devtools.core.model.IpsObjectType;
+import org.faktorips.devtools.core.model.pctype.IPolicyCmptType;
+import org.faktorips.devtools.core.model.product.IProductCmpt;
+import org.faktorips.devtools.core.model.tablecontents.ITableContents;
+import org.faktorips.devtools.core.model.tablecontents.ITableContentsGeneration;
+import org.faktorips.devtools.core.model.tablestructure.ITableStructure;
+import org.faktorips.devtools.stdbuilder.table.TableImplBuilder;
+import org.faktorips.runtime.ReadonlyTableOfContentsImpl;
+import org.faktorips.runtime.TocEntryGeneration;
+import org.faktorips.runtime.TocEntryObject;
+import org.w3c.dom.Document;
+
+/**
+ * 
+ * @author Jan Ortmann
+ */
+public class TocFileBuilderTest extends IpsPluginTest {
+
+    private IIpsProject project;
+    private StandardBuilderSet builderSet;
+    private TableImplBuilder tableImplBuilder;
+    private TocFileBuilder tocFileBuilder;
+    
+    /*
+     * @see IpsPluginTest#setUp()
+     */
+    protected void setUp() throws Exception {
+        super.setUp();
+        project = newIpsProject("TestProject");
+        project.setGeneratedJavaSourcecodeDocumentationLanguage(Locale.GERMAN);
+        builderSet = (StandardBuilderSet)project.getCurrentArtefactBuilderSet();
+        tableImplBuilder = (TableImplBuilder)builderSet.getBuilder(TableImplBuilder.class);
+        tocFileBuilder = (TocFileBuilder)builderSet.getBuilder(TocFileBuilder.class);
+    }
+
+    public void testGetToc() throws CoreException {
+        
+        IPolicyCmptType type = (IPolicyCmptType)newIpsObject(project, IpsObjectType.POLICY_CMPT_TYPE, "motor.MotorPolicy");
+        IProductCmpt motorProduct = (IProductCmpt)newIpsObject(project, IpsObjectType.PRODUCT_CMPT, "motor.MotorProduct");
+        motorProduct.setPolicyCmptType(type.getQualifiedName());
+        GregorianCalendar validFrom = new GregorianCalendar(2006, 0, 1);
+        motorProduct.newGeneration().setValidFrom(validFrom);
+        type.getIpsSrcFile().save(true, null);
+        
+        // toc should be empty as long as the project hasn't been builder
+        IIpsPackageFragmentRoot root = project.getIpsPackageFragmentRoots()[0];
+        MutableClRuntimeRepositoryToc toc = tocFileBuilder.getToc(root);
+        assertEquals(0, toc.getProductCmptTocEntries().length);
+        
+        project.getProject().build(IncrementalProjectBuilder.FULL_BUILD, null);
+        toc = tocFileBuilder.getToc(root);
+        assertEquals(1, toc.getProductCmptTocEntries().length);
+    }
+    
+    public void testCreateTocEntryTable() throws CoreException {
+        GregorianCalendar validFrom = new GregorianCalendar(2006, 0, 1);
+        ITableStructure structure = (ITableStructure)newIpsObject(project, IpsObjectType.TABLE_STRUCTURE, "motor.RateTableStructure");
+        ITableContents table = (ITableContents)newIpsObject(project, IpsObjectType.TABLE_CONTENTS, "motor.RateTable");
+        ITableContentsGeneration tableGen = (ITableContentsGeneration)table.newGeneration();
+        tableGen.setValidFrom(validFrom);
+        table.setTableStructure(structure.getQualifiedName());
+        structure.getIpsSrcFile().save(true, null);
+        table.getIpsSrcFile().save(true, null);
+        
+        TocEntryObject entry = tocFileBuilder.createTocEntry(table);
+        assertEquals("motor.RateTable", entry.getIpsObjectName());
+        assertEquals(tableImplBuilder.getQualifiedClassName(structure), entry.getImplementationClassName());
+        assertTrue(entry.isTableTocEntry());
+        assertNull(entry.getPcTypeClassName());
+    }
+    
+    public void test() throws Exception {
+        
+        // create a product component
+        IPolicyCmptType type = (IPolicyCmptType)newIpsObject(project, IpsObjectType.POLICY_CMPT_TYPE, "motor.MotorPolicy");
+        IProductCmpt motorProduct = (IProductCmpt)newIpsObject(project, IpsObjectType.PRODUCT_CMPT, "motor.MotorProduct");
+        motorProduct.setPolicyCmptType(type.getQualifiedName());
+        GregorianCalendar validFrom = new GregorianCalendar(2006, 0, 1);
+        motorProduct.newGeneration().setValidFrom(validFrom);
+        type.getIpsSrcFile().save(true, null);
+        motorProduct.getIpsSrcFile().save(true, null);
+        
+        // create a table content
+        ITableStructure structure = (ITableStructure)newIpsObject(project, IpsObjectType.TABLE_STRUCTURE, "motor.RateTableStructure");
+        ITableContents table = (ITableContents)newIpsObject(project, IpsObjectType.TABLE_CONTENTS, "motor.RateTable");
+        ITableContentsGeneration tableGen = (ITableContentsGeneration)table.newGeneration();
+        tableGen.setValidFrom(validFrom);
+        table.setTableStructure(structure.getQualifiedName());
+        structure.getIpsSrcFile().save(true, null);
+        table.getIpsSrcFile().save(true, null);
+        
+        // now build
+        project.getProject().build(IncrementalProjectBuilder.FULL_BUILD, null);
+        IIpsPackageFragmentRoot root = project.getIpsPackageFragmentRoots()[0];
+        IFile tocFile = project.getCurrentArtefactBuilderSet().getRuntimeRepositoryTocFile(root);
+        assertTrue(tocFile.exists());
+        assertEquals(project.getXmlFileCharset(), tocFile.getCharset());
+        
+        Document doc = getDocumentBuilder().parse(tocFile.getContents());
+        ReadonlyTableOfContentsImpl toc = new ReadonlyTableOfContentsImpl();
+        toc.initFromXml(doc.getDocumentElement());
+
+        // asserts for product cmpt entry
+        TocEntryObject[] entries = toc.getProductCmptTocEntries();
+        assertEquals(1, entries.length);
+        
+        TocEntryObject entry0 = toc.getProductCmptTocEntry("motor.MotorProduct");
+        assertNotNull(entry0);
+        TocEntryGeneration genEntry0 = entry0.getGenerationEntry(validFrom);
+        assertNotNull(genEntry0);
+        
+        // asserts for table entry
+        TocEntryObject entry1 = toc.getTableTocEntryByClassname(tableImplBuilder.getQualifiedClassName(structure));
+        assertNotNull(entry1);
+        
+        // no changes => toc file should remain untouched.
+        long stamp = tocFile.getModificationStamp();
+        project.getProject().build(IncrementalProjectBuilder.INCREMENTAL_BUILD, null);
+        assertEquals(stamp, tocFile.getModificationStamp());
+        
+        // delete the product cmpt => should be removed from toc
+        motorProduct.getIpsSrcFile().getCorrespondingFile().delete(true, false, null);
+        project.getProject().build(IncrementalProjectBuilder.INCREMENTAL_BUILD, null);
+        MutableClRuntimeRepositoryToc toc2 = tocFileBuilder.getToc(root);
+        assertEquals(0, toc2.getProductCmptTocEntries().length);
+
+        // delete the table => should be removed from toc
+        table.getIpsSrcFile().getCorrespondingFile().delete(true, false, null);
+        project.getProject().build(IncrementalProjectBuilder.INCREMENTAL_BUILD, null);
+        toc2 = tocFileBuilder.getToc(root);
+        assertNull(toc2.getTableTocEntryByClassname(tableImplBuilder.getQualifiedClassName(structure)));
+    }
+    
+
+
+}
