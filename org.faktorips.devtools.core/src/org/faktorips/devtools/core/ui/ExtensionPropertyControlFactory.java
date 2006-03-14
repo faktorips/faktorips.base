f04fab4706d49d26747a383306e28c50e28fce27
eidenschink <eidenschink>
2006-03-14 08:29:51 +0000

    Aenderung code style

--- org.faktorips.devtools.core/src/org/faktorips/devtools/core/ui/ExtensionPropertyControlFactory.java
+++ org.faktorips.devtools.core/src/org/faktorips/devtools/core/ui/ExtensionPropertyControlFactory.java
@@ -30,69 +30,68 @@ import org.faktorips.devtools.core.ui.controller.IpsObjectPartContainerUIControl
  * Factory to create the specific Controls for the extensions of a type
  * 
  */
-public class ExtensionPropertyControlFactory   {
+public class ExtensionPropertyControlFactory {
 
 	private IExtensionPropertyDefinition[] extensionProperties;
+
 	private EditField[] extensionEditFields;
-	
+
 	public ExtensionPropertyControlFactory(Class extensionClass) {
-	    extensionProperties = IpsPlugin.getDefault().getIpsModel().getExtensionPropertyDefinitions(extensionClass,true);
-	    extensionEditFields = new EditField[extensionProperties.length];
-	}
-	
-	
-	/*
-	 * erzeugt die <code>EditFields</code> zu einer Extension zu einer bestimmten Position <code>where</code>
-	 */
-	public void createControls(Composite workArea, UIToolkit uiToolkit, IpsObjectPartContainer ipsObjectPart, String where) 
-	{
-		for(int i=0; i< extensionProperties.length; i++)
-	    {
-	    	if(extensionProperties[i].getEditedInStandardTextArea().equals(where)
-	    			&&(extensionEditFields[i]==null))
-	    	{
-	            uiToolkit.createFormLabel(workArea, extensionProperties[i].getDisplayName() + ":"); //$NON-NLS-1$
-	            extensionEditFields[i]=extensionProperties[i].newEditField(ipsObjectPart, workArea, uiToolkit);    		
-	    	}
-	    }
-		
-	}
-	
-	
-	
-	/*
-	 * erzeugt alle noch nicht explizit  erzeugten EditFields zu einer Extension ausser
-	 * denen, die im plugin.xml mit <code>false</code> gekennzeichnet sind
-	 */
-	public void createControls(Composite workArea, UIToolkit uiToolkit, IpsObjectPartContainer ipsObjectPart)
-	{
-		for(int i=0; i< extensionProperties.length; i++)
-	    {
-	    	if((!extensionProperties[i].getEditedInStandardTextArea().equals("false"))  //$NON-NLS-1$
-	    		&&(extensionEditFields[i] == null))
-	    	{
-	            uiToolkit.createFormLabel(workArea, extensionProperties[i].getDisplayName() + ":"); //$NON-NLS-1$
-	            extensionEditFields[i]=extensionProperties[i].newEditField(ipsObjectPart, workArea, uiToolkit);    		
-	    	}
-	    }
-		
+		extensionProperties = IpsPlugin.getDefault().getIpsModel()
+				.getExtensionPropertyDefinitions(extensionClass, true);
+		extensionEditFields = new EditField[extensionProperties.length];
 	}
 
-	
+	/*
+	 * creates the <code>EditFields</code> of extension at <code>where</code> position
+	 */
+	public void createControls(Composite workArea, UIToolkit uiToolkit,
+			IpsObjectPartContainer ipsObjectPart, String where) {
+		for (int i = 0; i < extensionProperties.length; i++) {
+			if (extensionProperties[i].getEditedInStandardTextArea().equals(
+					where)
+					&& (extensionEditFields[i] == null)) {
+				uiToolkit.createFormLabel(workArea, extensionProperties[i]
+						.getDisplayName()
+						+ ":"); //$NON-NLS-1$
+				extensionEditFields[i] = extensionProperties[i].newEditField(
+						ipsObjectPart, workArea, uiToolkit);
+			}
+		}
+
+	}
 
 	/*
-	 * alle erzeugten EditFields mit dem Modell verbinden
+	 * creates all not yet explicitely created EditFields of an extension not including
+	 * the extensions tagged with <code>false</code> 
+	 */
+	public void createControls(Composite workArea, UIToolkit uiToolkit,
+			IpsObjectPartContainer ipsObjectPart) {
+		for (int i = 0; i < extensionProperties.length; i++) {
+			if ((!extensionProperties[i].getEditedInStandardTextArea().equals(
+					"false")) //$NON-NLS-1$
+					&& (extensionEditFields[i] == null)) {
+				uiToolkit.createFormLabel(workArea, extensionProperties[i]
+						.getDisplayName()
+						+ ":"); //$NON-NLS-1$
+				extensionEditFields[i] = extensionProperties[i].newEditField(
+						ipsObjectPart, workArea, uiToolkit);
+			}
+		}
+
+	}
+
+	/*
+	 * connect all created EditFields with the model
 	 */
 	public void connectToModel(IpsObjectPartContainerUIController uiController) {
-		for(int i=0; i<extensionEditFields.length; i++)
-        {
-        	if(extensionEditFields[i]!=null)
-        	{
-        		uiController.add(extensionEditFields[i],extensionProperties[i].getPropertyId());
-        	}
-        }
+		for (int i = 0; i < extensionEditFields.length; i++) {
+			if (extensionEditFields[i] != null) {
+				uiController.add(extensionEditFields[i], extensionProperties[i]
+						.getPropertyId());
+			}
+		}
 
 	}
-
 
 }
