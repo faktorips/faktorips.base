9a60c875844f3c1dc5b92324c0c76b4307910cb0
senf <senf>
2006-04-21 15:20:50 +0000

    *** empty log message ***

--- org.faktorips.devtools.stdbuilder.test/src/org/faktorips/devtools/stdbuilder/test/AutomatedSuite.java
+++ org.faktorips.devtools.stdbuilder.test/src/org/faktorips/devtools/stdbuilder/test/AutomatedSuite.java
@@ -1,8 +1,14 @@
 package org.faktorips.devtools.stdbuilder.test;
-import org.faktorips.devtools.stdbuilder.ParameterIdentifierResolverTest;
-
 import junit.framework.Test;
 import junit.framework.TestSuite;
+
+import org.faktorips.devtools.stdbuilder.MutableClProductCmptRegistryTocTest;
+import org.faktorips.devtools.stdbuilder.ParameterIdentifierResolverTest;
+import org.faktorips.devtools.stdbuilder.StandardBuilderSetTest;
+import org.faktorips.devtools.stdbuilder.TocFileBuilderTest;
+import org.faktorips.devtools.stdbuilder.XmlContentFileCopyBuilderTest;
+import org.faktorips.devtools.stdbuilder.productcmpt.ProductCmptBuilderTest;
+import org.faktorips.devtools.stdbuilder.table.TableImplBuilderTest;
 
 public class AutomatedSuite extends TestSuite {
 
@@ -10,6 +16,12 @@ public class AutomatedSuite extends TestSuite {
 		
 		TestSuite suite = new TestSuite();
 		suite.addTestSuite(ParameterIdentifierResolverTest.class);
+		suite.addTestSuite(TocFileBuilderTest.class             );
+		suite.addTestSuite(StandardBuilderSetTest.class         );
+		suite.addTestSuite(MutableClProductCmptRegistryTocTest.class );
+		suite.addTestSuite(XmlContentFileCopyBuilderTest.class       );
+		suite.addTestSuite(ProductCmptBuilderTest.class              );
+		suite.addTestSuite(TableImplBuilderTest.class                );
 		
 
 		return suite;
