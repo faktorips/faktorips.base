b01f9de9fd7787553ca2fc2a1c681ba0839b9631
senf <senf>
2006-04-21 13:49:42 +0000

    *** empty log message ***

---
+++ org.faktorips.devtools.stdbuilder.test/src/AutomatedSuite.java
@@ -0,0 +1,17 @@
+import org.faktorips.devtools.stdbuilder.ParameterIdentifierResolverTest;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AutomatedSuite extends TestSuite {
+
+	public static Test suite(){
+		
+		TestSuite suite = new TestSuite();
+		suite.addTestSuite(ParameterIdentifierResolverTest.class);
+		
+
+		return suite;
+	}
+	
+}
