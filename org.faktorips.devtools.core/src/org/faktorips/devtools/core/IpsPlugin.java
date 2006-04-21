bc713b62a092bc4999c02fb82fe0703fab3b0d3f
senf <senf>
2006-04-21 14:06:21 +0000

    *** empty log message ***

--- org.faktorips.devtools.core/src/org/faktorips/devtools/core/IpsPlugin.java
+++ org.faktorips.devtools.core/src/org/faktorips/devtools/core/IpsPlugin.java
@@ -132,7 +132,7 @@ public class IpsPlugin extends AbstractUIPlugin {
      * Reinits the model (so all data in the cache is cleared). Should only be called in test cases to ensure
      * a clean environment.
      */
-    public void reinitModel() {
+    void reinitModel() {
         ((IpsModel)getIpsModel()).stopListeningToResourceChanges();
         manager = new IpsModelManager();
         ((IpsModel)getIpsModel()).startListeningToResourceChanges();
