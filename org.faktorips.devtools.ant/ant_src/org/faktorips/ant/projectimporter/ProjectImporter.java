397af07f3b0dc65c0f5fd2c554ebdbb421f18543
senf <senf>
2006-09-12 18:36:23 +0000

    *** empty log message ***

--- org.faktorips.devtools.ant/ant_src/org/faktorips/ant/projectimporter/ProjectImporter.java
+++ org.faktorips.devtools.ant/ant_src/org/faktorips/ant/projectimporter/ProjectImporter.java
@@ -1,25 +1,32 @@
-/*******************************************************************************
+/***************************************************************************************************
  * Copyright (c) 2005,2006 Faktor Zehn GmbH und andere.
- *
+ * 
  * Alle Rechte vorbehalten.
- *
- * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele,
- * Konfigurationen, etc.) dürfen nur unter den Bedingungen der 
- * Faktor-Zehn-Community Lizenzvereinbarung – Version 0.1 (vor Gründung Community) 
- * genutzt werden, die Bestandteil der Auslieferung ist und auch unter
- *   http://www.faktorips.org/legal/cl-v01.html
- * eingesehen werden kann.
- *
- * Mitwirkende:
- *   Faktor Zehn GmbH - initial API and implementation 
- *
- *******************************************************************************/
+ * 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
+ * etc.) dürfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung – Version 0.1
+ * (vor Gründung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
+ * http://www.faktorips.org/legal/cl-v01.html eingesehen werden kann.
+ * 
+ * Mitwirkende: Faktor Zehn GmbH - initial API and implementation
+ * 
+ **************************************************************************************************/
 
 package org.faktorips.ant.projectimporter;
 
+import java.io.File;
+import java.io.FileInputStream;
+
 import org.apache.tools.ant.BuildException;
+import org.eclipse.ant.core.AntCorePlugin;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.faktorips.ant.util.Copy;
+
 /**
  * Implements a custom Ant-Task, which imports a given Directory to a running Eclipse Workspace as
  * Project.
@@ -28,38 +35,62 @@ import org.eclipse.core.resources.ResourcesPlugin;
  */
 public class ProjectImporter extends org.apache.tools.ant.Task {
 
-    /** Directory from which to import Data */
-    private String baseDir = "";
-    /** Name of the Project */
-    private String projectName = "";
+    /** path to project dir file */
+    private String projectDir = "";
+
+    public void setDir(String dir) {
+        System.out.println(dir);
+        this.projectDir = dir;
+    }
+
+    public String getDir() {
+        return this.projectDir;
+    }
+
+    private File getProjectFile() {
+        return new File(this.projectDir + "/.project");
+
+    }
 
     public ProjectImporter() {
         super();
     }
 
-    public void setDir(String dir) {
-        this.baseDir = dir;
-    }
-
-    public void setName(String name) {
-        this.projectName = name;
-    }
-
     public void execute() throws BuildException {
 
-
-        
-        // check if properties are set
-        if (this.baseDir == "" || this.projectName == "") {
-            throw new BuildException("Please provide both attributes, 'dir' AND 'name'");
-        }
-
-
+        // Fetch Workspace
         IWorkspace workspace = ResourcesPlugin.getWorkspace();
 
-        System.out.println("Hello World. Workspace: " + workspace.toString());
-        
-//        IProgressMonitor monitor = (IProgressMonitor) getProject().getReferences().get(AntCorePlugin.ECLIPSE_PROGRESS_MONITOR);
-//        System.out.println(monitor.toString());
+        // Fetch ProgressMonitor
+        IProgressMonitor monitor = (IProgressMonitor)getProject().getReferences().get(
+                AntCorePlugin.ECLIPSE_PROGRESS_MONITOR);
+
+        try {
+            // get description provieded in .project File
+            IProjectDescription description = workspace.loadProjectDescription(new FileInputStream(this
+                    .getProjectFile()));
+
+            // create new project with name provided in description
+            IProject project = workspace.getRoot().getProject(description.getName());
+            
+            // check if project already exists in current workspace
+            if (project.exists()){
+                throw new BuildException("Project "+project.getName()+" already exists.");
+            }
+            project.create(description, monitor);
+
+            // copy files
+            Copy.copyDir(this.getDir(), project.getLocation().toString());
+
+            project.open(monitor);
+            project.refreshLocal(IResource.DEPTH_INFINITE, monitor);
+
+        }
+        catch (Exception e) {
+            throw new BuildException(e);
+        }
+
     }
+
+
 }
