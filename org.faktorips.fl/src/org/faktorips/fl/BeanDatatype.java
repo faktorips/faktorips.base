6b4ff840a377309f33a9f6980cc74901ed168a63
erzberger <erzberger>
2006-10-25 16:01:13 +0000

    *** empty log message ***

--- org.faktorips.fl/src/org/faktorips/fl/BeanDatatype.java
+++ org.faktorips.fl/src/org/faktorips/fl/BeanDatatype.java
@@ -29,6 +29,7 @@ public interface BeanDatatype extends Datatype {
     /**
      * Returns the property datatype representing the property with the given name.
      */
+    
     public PropertyDatatype getProperty(String name);
 
 }
