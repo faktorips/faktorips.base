54c44110c13e817b6f9cf15269f7a63dd83e15db
schmidt <schmidt>
2008-02-04 09:32:18 +0000

    fixed speling of assocation.

--- org.faktorips.devtools.core/src/org/faktorips/devtools/core/model/productcmpt/DeltaType.java
+++ org.faktorips.devtools.core/src/org/faktorips/devtools/core/model/productcmpt/DeltaType.java
@@ -45,7 +45,7 @@ public class DeltaType extends DefaultEnumValue {
         VALUE_WITHOUT_PROPERTY = new DeltaType(enumType, "ValueWithoutProperty", Messages.DeltaType_propertiesNotFoundInTheModel, "DeltaTypeValueWithoutProperty.gif"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
         PROPERTY_TYPE_MISMATCH = new DeltaType(enumType, "PropertyTypeMismatch", Messages.DeltaType_propertiesWithTypeMismatch, "DeltaTypePropertyTypeMismatch.gif"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
         VALUE_SET_MISMATCH = new DeltaType(enumType, "ValueSetMismatch", Messages.DeltaType_ValueSetMismatches, "DeltaTypeValueSetMismatch.gif"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-        LINK_WITHOUT_ASSOCIATION = new DeltaType(enumType, "LinkWithoutAssocation", Messages.DeltaType_LinksNotFoundInTheModel, "DeltaTypeLinkWithoutAssociation.gif"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+        LINK_WITHOUT_ASSOCIATION = new DeltaType(enumType, "LinkWithoutAssociation", Messages.DeltaType_LinksNotFoundInTheModel, "DeltaTypeLinkWithoutAssociation.gif"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
         WRONG_ORDER_ENTRY = new DeltaType(enumType, "WrongOrderEntry", "Wrong sort order", "DeltaTypeWrongOrder.gif"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
         
         ALL_TYPES = new DeltaType[]{MISSING_PROPERTY_VALUE, VALUE_WITHOUT_PROPERTY, PROPERTY_TYPE_MISMATCH, VALUE_SET_MISMATCH, LINK_WITHOUT_ASSOCIATION, WRONG_ORDER_ENTRY};
