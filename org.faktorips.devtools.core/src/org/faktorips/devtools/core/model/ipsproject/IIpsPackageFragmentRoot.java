b5c8989b8627783a6523575024dd7b9b0d405487
schmidt <schmidt>
2008-02-07 10:22:35 +0000

    typo.

--- org.faktorips.devtools.core/src/org/faktorips/devtools/core/model/ipsproject/IIpsPackageFragmentRoot.java
+++ org.faktorips.devtools.core/src/org/faktorips/devtools/core/model/ipsproject/IIpsPackageFragmentRoot.java
@@ -74,8 +74,8 @@ public interface IIpsPackageFragmentRoot extends IIpsElement {
     public IIpsPackageFragment[] getIpsPackageFragments() throws CoreException;
 
     /**
-     * Returns the package fragments contained in this root folder in the order defined by the sort defintions.
-     * Returns an empty array if this root folder does not contain any fragments
+     * Returns the package fragments contained in this root folder in the order defined by the sort definitions.
+     * Returns an empty array if this root folder does not contain any fragments.
      *
      * @see IIpsPackageFragmentSortDefinition
      */
