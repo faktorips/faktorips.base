5b52adfa7605a1b688d8a54b9b8f7f94df21b562
ortmann <ortmann>
2008-03-13 13:39:47 +0000

    *** empty log message ***

--- org.faktorips.devtools.core/src/org/faktorips/devtools/core/builder/DefaultJavaGeneratorForIpsPart.java
+++ org.faktorips.devtools.core/src/org/faktorips/devtools/core/builder/DefaultJavaGeneratorForIpsPart.java
@@ -17,6 +17,7 @@
 
 package org.faktorips.devtools.core.builder;
 
+import org.eclipse.core.runtime.CoreException;
 import org.faktorips.codegen.JavaCodeFragmentBuilder;
 import org.faktorips.devtools.core.model.ipsobject.IIpsObjectPartContainer;
 
@@ -27,33 +28,116 @@ import org.faktorips.devtools.core.model.ipsobject.IIpsObjectPartContainer;
  * 
  * @author Jan Ortmann
  */
-public class DefaultJavaGeneratorForIpsPart extends JavaGeneratorForIpsPart {
+public abstract class DefaultJavaGeneratorForIpsPart extends JavaGeneratorForIpsPart {
 
+    
     private DefaultJavaSourceFileBuilder javaSourceFileBuilder;
     
-    public DefaultJavaGeneratorForIpsPart(IIpsObjectPartContainer part, DefaultJavaSourceFileBuilder builder) {
+    // true if the generation should generate part of the implementation class, false if it should generate part
+    // of the interface.
+    private boolean generateImplementation;
+    
+    public DefaultJavaGeneratorForIpsPart(IIpsObjectPartContainer part, DefaultJavaSourceFileBuilder builder, boolean generateImplementation) throws CoreException {
         super(part, builder);
-        javaSourceFileBuilder = builder;
+        this.javaSourceFileBuilder = builder;
+        this.generateImplementation = generateImplementation;
+        init();
     }
+    
+    /**
+     * Hook for subclasses to initialize member variables before the code generation starts.
+     * 
+     * @throws CoreException if an error occurs while initializing the generator
+     */
+    protected void init() throws CoreException {
+        
+    }
+    
+    /**
+     * Returns <code>true</code> if the generator should generate part of the implementation class, 
+     * <code>false</code> if it should generate part of the interface.
+     */
+    public boolean isGeneratingImplementationClass() {
+        return generateImplementation;
+    }
+    
+    /**
+     * Returns <code>true</code> if the generator should generate part of the interface, 
+     * <code>false</code> if it should generate part of the implementation class.
+     */
+    public boolean isGeneratingInterface() {
+        return !generateImplementation;
+    }
+    
+    /**
+     * Generates the source code for the ips object part this is a generator for.
+     */
+    public void generate() throws CoreException {
+        generateConstants(getConstantBuilder());
+        if (isGeneratingImplementationClass()) {
+            generateMemberVariables(getMemberVarBuilder());
+        }
+        generateMethods(getMethodBuilder());
+    }
+    
+    /**
+     * Subclasses have to implement generation of the methods here. Whether the generator is used for generating the
+     * implementation class or the interface can be queried via {@link #isGeneratingImplementationClass()} and 
+     * {@link #isGeneratingInterface()}.
+     * 
+     * @param builder The builder for the type's method section.
+     * 
+     * @throws CoreException if an error occurs while generating the member variables
+     * 
+     * @see #isGeneratingImplementationClass()
+     * @see #isGeneratingInterface()
+     */
+    protected abstract void generateMethods(JavaCodeFragmentBuilder builder) throws CoreException;
+
+    /**
+     * Subclasses have to implement generation of the member variables here. This method is only called if the
+     * generator is generating an implementation class.
+     * 
+     * @param builder The builder for the type's member variables section.
+     * 
+     * @throws CoreException if an error occurs while generating the member variables
+     * 
+     * @see #isGeneratingImplementationClass()
+     */
+    protected abstract void generateMemberVariables(JavaCodeFragmentBuilder builder) throws CoreException;
+    
+    /**
+     * Subclasses have to implement generation of the constants (final statics) here. Whether the generator is used for generating the
+     * implementation class or the interface can be queried via {@link #isGeneratingImplementationClass()} and 
+     * {@link #isGeneratingInterface()}.
+     * 
+     * @param builder The builder for the type's constants section.
+     * 
+     * @throws CoreException if an error occurs while generating the member variables
+     * 
+     * @see #isGeneratingImplementationClass()
+     * @see #isGeneratingInterface()
+     */
+    protected abstract void generateConstants(JavaCodeFragmentBuilder builder) throws CoreException;
     
     /**
      * Returns the {@link <code>JavaCodeFragmentBuilder</code>} that assembles the code for the constant definitions.
      */
-    public JavaCodeFragmentBuilder getConstantBuilder() {
+    protected JavaCodeFragmentBuilder getConstantBuilder() {
         return javaSourceFileBuilder.getMainTypeSection().getConstantBuilder();
     }
 
     /**
      * Returns the {@link <code>JavaCodeFragmentBuilder</code>} that assembles the code for the member variable definitions.
      */
-    public JavaCodeFragmentBuilder getMemberVarBuilder() {
+    protected JavaCodeFragmentBuilder getMemberVarBuilder() {
         return javaSourceFileBuilder.getMainTypeSection().getMemberVarBuilder();
     }
 
     /**
      * Returns the {@link <code>JavaCodeFragmentBuilder</code>} that assembles the code for the method definitions.
      */
-    public JavaCodeFragmentBuilder getMethodBuilder() {
+    protected JavaCodeFragmentBuilder getMethodBuilder() {
         return javaSourceFileBuilder.getMainTypeSection().getMethodBuilder();
     }    
 
