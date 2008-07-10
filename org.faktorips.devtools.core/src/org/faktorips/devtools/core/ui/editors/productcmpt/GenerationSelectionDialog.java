b3d45c02f128860e9946437f871b4cf772ae7ca1
ortmann2 <ortmann2>
2008-07-10 15:27:58 +0000

    FS#1229 - GenerationSelectionDialog missing cancel button

--- org.faktorips.devtools.core/src/org/faktorips/devtools/core/ui/editors/productcmpt/GenerationSelectionDialog.java
+++ org.faktorips.devtools.core/src/org/faktorips/devtools/core/ui/editors/productcmpt/GenerationSelectionDialog.java
@@ -251,8 +251,8 @@ public class GenerationSelectionDialog extends TitleAreaDialog {
      */
     protected void createButtonsForButtonBar(Composite parent) {
         // create OK and Cancel buttons by default
-        createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL,
-                true);
+        createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL, true);
+        createButton(parent, IDialogConstants.CANCEL_ID, IDialogConstants.CANCEL_LABEL, true);
     }
     
     private void createChoiceForSwitchWorkingDate(Composite selectPane) {
