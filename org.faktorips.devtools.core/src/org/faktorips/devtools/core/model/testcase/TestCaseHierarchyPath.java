16cc9abdbdade84144e0d576b87fefbfd7d9133c
ortmann <ortmann>
2009-02-01 12:33:01 +0000

    Umlaute aus Copyright note entfernt

--- org.faktorips.devtools.core/src/org/faktorips/devtools/core/model/testcase/TestCaseHierarchyPath.java
+++ org.faktorips.devtools.core/src/org/faktorips/devtools/core/model/testcase/TestCaseHierarchyPath.java
@@ -19,8 +19,8 @@ package org.faktorips.devtools.core.model.testcase;
  * Alle Rechte vorbehalten.
  *
  * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele,
- * Konfigurationen, etc.) dürfen nur unter den Bedingungen der 
- * Faktor-Zehn-Community Lizenzvereinbarung – Version 0.1 (vor Gründung Community) 
+ * Konfigurationen, etc.) duerfen nur unter den Bedingungen der 
+ * Faktor-Zehn-Community Lizenzvereinbarung (Version 0.1 (vor Gruendung Community) 
  * genutzt werden, die Bestandteil der Auslieferung ist und auch unter
  *   http://www.faktorips.org/legal/cl-v01.html
  * eingesehen werden kann.
