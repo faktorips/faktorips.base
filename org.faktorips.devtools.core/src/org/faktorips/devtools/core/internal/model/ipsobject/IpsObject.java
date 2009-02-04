bec2656f2b141d916cb3644e1a336b60fa04b40b
weickmann <weickmann>
2009-02-04 09:45:59 +0000

    javadoc ergänzt

--- org.faktorips.devtools.core/src/org/faktorips/devtools/core/internal/model/ipsobject/IpsObject.java
+++ org.faktorips.devtools.core/src/org/faktorips/devtools/core/internal/model/ipsobject/IpsObject.java
@@ -3,7 +3,7 @@
  * 
  * Alle Rechte vorbehalten.
  * 
- * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen, 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
  * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
  * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
  * http://www.faktorzehn.org/f10-org:lizenzen:community eingesehen werden kann.
@@ -32,26 +32,33 @@ import org.faktorips.util.message.MessageList;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
-
 /**
- *
+ * This is the abstract super type that all ips objects should extend.
  */
 public abstract class IpsObject extends IpsObjectPartContainer implements IIpsObject {
-    
+
+    // The description currently attached to this ips object
     private String description = ""; //$NON-NLS-1$
-    
+
+    // Flag indicating whether this object was created from a parsable file content
     private boolean fromParsableFile = false;
 
+    /**
+     * Creates a new ips object.
+     * 
+     * @param file The ips source file in which this ips object will be stored in.
+     */
     protected IpsObject(IIpsSrcFile file) {
         super(file, ""); //$NON-NLS-1$
     }
-    
+
     /**
      * Constructor for testing purposes.
      */
     protected IpsObject() {
+        
     }
-    
+
     /**
      * {@inheritDoc}
      */
@@ -60,15 +67,12 @@ public abstract class IpsObject extends IpsObjectPartContainer implements IIpsOb
     }
 
     /**
-     * Marks the ips object as originating from an ips src file with an invalid file
-     * format.
+     * Marks the ips object as originating from an ips src file with an invalid file format.
      */
     void markAsFromUnparsableFile() {
         fromParsableFile = false;
         this.reinitPartCollections();
     }
-    
-    
 
     /**
      * {@inheritDoc}
@@ -99,6 +103,7 @@ public abstract class IpsObject extends IpsObjectPartContainer implements IIpsOb
         if (folderName.equals("")) { //$NON-NLS-1$
             return getName();
         }
+        
         return folderName + '.' + getName();
     }
 
@@ -108,9 +113,10 @@ public abstract class IpsObject extends IpsObjectPartContainer implements IIpsOb
     public String getName() {
         String filename = getParent().getName();
         int index = filename.indexOf('.');
-        if (index==-1) {
+        if (index == -1) {
             throw new RuntimeException("filename has no extension: " + filename); //$NON-NLS-1$
         }
+        
         return filename.substring(0, index);
     }
 
@@ -120,25 +126,25 @@ public abstract class IpsObject extends IpsObjectPartContainer implements IIpsOb
     public IResource getCorrespondingResource() {
         return null;
     }
-    
+
     /**
      * {@inheritDoc}
      */
     public IIpsSrcFile getIpsSrcFile() {
         if (getParent() instanceof IIpsSrcFile) {
-            return (IIpsSrcFile)getParent();    
+            return (IIpsSrcFile)getParent();
         }
         return null;
     }
-    
+
     /**
      * {@inheritDoc}
      */
     public Image getImage() {
-        if (getIpsSrcFile().exists()){
+        if (getIpsSrcFile().exists()) {
             return getIpsObjectType().getEnabledImage();
         } else {
-            // the ips source file doesn't exists, thus the ips object couldn't be linked to 
+            // the ips source file doesn't exists, thus the ips object couldn't be linked to
             // an ips src file in the workspace, return the image of the ips source file to
             // decide between valid and invalid ips objects
             return IpsObjectType.IPS_SOURCE_FILE.getEnabledImage();
@@ -159,28 +165,28 @@ public abstract class IpsObject extends IpsObjectPartContainer implements IIpsOb
     public String getDescription() {
         return description;
     }
-    
+
     /**
-     * Notifies the model about the change.  
+     * Notifies the model that the object has changed.
      */
     protected void objectHasChanged() {
         ContentChangeEvent event = ContentChangeEvent.newWholeContentChangedEvent(getIpsSrcFile());
         objectHasChanged(event);
     }
-    
+
     /**
-	 * {@inheritDoc}
-	 */
-	public boolean isValid() throws CoreException {
-		return getValidationResultSeverity()!=Message.ERROR;
-	}
-	
-	/**
-	 * {@inheritDoc}
-	 */
-	public int getValidationResultSeverity() throws CoreException {
-		return validate(getIpsProject()).getSeverity();
-	}
+     * {@inheritDoc}
+     */
+    public boolean isValid() throws CoreException {
+        return getValidationResultSeverity() != Message.ERROR;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public int getValidationResultSeverity() throws CoreException {
+        return validate(getIpsProject()).getSeverity();
+    }
 
     /**
      * {@inheritDoc}
@@ -188,21 +194,21 @@ public abstract class IpsObject extends IpsObjectPartContainer implements IIpsOb
     public IDependency[] dependsOn() throws CoreException {
         return new IDependency[0];
     }
-    
+
     /**
      * {@inheritDoc}
      */
     protected final Element createElement(Document doc) {
         return doc.createElement(getIpsObjectType().getXmlElementName());
     }
-    
+
     /**
      * {@inheritDoc}
      */
     protected void propertiesToXml(Element element) {
         DescriptionHelper.setDescription(element, description);
     }
-    
+
     /**
      * {@inheritDoc}
      */
@@ -217,49 +223,66 @@ public abstract class IpsObject extends IpsObjectPartContainer implements IIpsOb
     protected void initPropertiesFromXml(Element element, Integer id) {
         description = DescriptionHelper.getDescription(element);
     }
-    
+
     public String toString() {
-        if (getParent()==null) {
+        if (getParent() == null) {
             return "unnamed object"; // can only happen in test cases.  //$NON-NLS-1$
         }
-        return getParent().toString(); // ips object's name is the same as the file name, so use the parent's to string method
+
+        // ips object's name is the same as the file name, so use the
+        // parent's to string method
+        return getParent().toString();
     }
 
     /**
      * {@inheritDoc}
+     * 
+     * <p>
+     * This operation is extended by <code>IpsObject</code> to perform validations on the name
+     * property.
+     * </p>
+     * 
+     * @see #validateNamingConventions(MessageList, String, String)
      */
     protected void validateThis(MessageList list, IIpsProject ipsProject) throws CoreException {
         super.validateThis(list, ipsProject);
+        
         validateNamingConventions(list, getName(), PROPERTY_NAME);
         validateSecondIpsObjectWithSameNameTypeInIpsObjectPath(list, ipsProject);
     }
-    
+
+    // Validates whether there is another type in the object path with the same name
     private void validateSecondIpsObjectWithSameNameTypeInIpsObjectPath(MessageList list, IIpsProject ipsProject)
             throws CoreException {
+
         IIpsObject otherIpsObject = ipsProject.findIpsObject(getQualifiedNameType());
         if (otherIpsObject != this) {
-            list.add(new Message(MSGCODE_SAME_IPSOBJECT_IN_IPSOBEJECTPATH_AHEAD, 
-                    NLS.bind(Messages.IpsObject_msg_OtherIpsObjectAlreadyInPathAhead,
-                            getIpsProject()), Message.WARNING, this));
+            list.add(new Message(MSGCODE_SAME_IPSOBJECT_IN_IPSOBEJECTPATH_AHEAD, NLS.bind(
+                    Messages.IpsObject_msg_OtherIpsObjectAlreadyInPathAhead, getIpsProject()), Message.WARNING, this));
         }
     }
-    
+
     /**
-     *  Validate the naming conventions for the given name.
-     *  
-     *  @param list The list the message will be added if the name is invalid
-     *  @param nameToValidate The name which will be validated against the naming conventions
-     *  @param property The property which contains the name, the message will be related to this property
+     * Validate the naming conventions for the given name.
+     * 
+     * @param list The list the message will be added if the name is invalid.
+     * @param nameToValidate The name which will be validated against the naming conventions.
+     * @param property The property which contains the name, the message will be related to this
+     *            property.
      */
-    protected void validateNamingConventions(MessageList list, String nameToValidate, String property) throws CoreException {
+    @SuppressWarnings("unchecked")
+    protected void validateNamingConventions(MessageList list, String nameToValidate, String property)
+            throws CoreException {
+
         MessageList mlForNameValidation = new MessageList();
-        mlForNameValidation.add(getIpsProject().getNamingConventions().validateUnqualifiedIpsObjectName(getIpsObjectType(), nameToValidate));
-        for (Iterator iter = mlForNameValidation.iterator(); iter.hasNext();) {
-            // create new messages related to this object and the given property
+        mlForNameValidation.add(getIpsProject().getNamingConventions().validateUnqualifiedIpsObjectName(
+                getIpsObjectType(), nameToValidate));
+        for (Iterator<Message> iter = mlForNameValidation.iterator(); iter.hasNext();) {
+            // Create new messages related to this object and the given property
             Message msg = (Message)iter.next();
             Message newMsg = new Message(msg.getCode(), msg.getText(), msg.getSeverity(), this, property);
             list.add(newMsg);
         }
     }
-    
+
 }
