c7eeb50110eaab5ccdfe1614a7fa44f25a449265
weickmann <weickmann>
2009-02-05 10:57:51 +0000

    Generics parametrisiert, javadoc ergaenzt

--- org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/wizards/type/TypePage.java
+++ org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/wizards/type/TypePage.java
@@ -3,7 +3,7 @@
  * 
  * Alle Rechte vorbehalten.
  * 
- * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen, 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
  * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
  * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
  * http://www.faktorzehn.org/f10-org:lizenzen:community eingesehen werden kann.
@@ -33,87 +33,130 @@ import org.faktorips.devtools.core.ui.controls.IpsObjectRefControl;
 import org.faktorips.devtools.core.ui.wizards.IpsObjectPage;
 
 /**
- * A abstract superclass that implements common behaviour of the policy and product component type pages. 
+ * An abstract superclass that implements common behaviour of the policy and product component type
+ * pages.
  */
 public abstract class TypePage extends IpsObjectPage {
-    
-    private TextButtonField supertypeField;
+
+    /** The checkbox field to set the abstract property. */
     protected CheckboxField abstractField;
+
+    /** The wizard type page of the associated (product or policy) type. */
     protected TypePage pageOfAssociatedType;
-    private boolean alreadyBeenEntered = false;
-    
+
+    // The text field to choose the supertype
+    private TextButtonField supertypeField;
+
+    // Flag inidcating whether this type page has already been entered
+    private boolean alreadyBeenEntered;
+
     /**
      * @param pageName
      * @param selection
      * @throws JavaModelException
      */
-    public TypePage(IpsObjectType ipsObjectType, IStructuredSelection selection, String pageName) throws JavaModelException {
+    public TypePage(IpsObjectType ipsObjectType, IStructuredSelection selection, String pageName)
+            throws JavaModelException {
+
         super(ipsObjectType, selection, pageName);
+        alreadyBeenEntered = false;
         setTitle(pageName);
     }
-    
+
     /**
      * {@inheritDoc}
      */
+    @Override
     protected void fillNameComposite(Composite nameComposite, UIToolkit toolkit) {
         super.fillNameComposite(nameComposite, toolkit);
+
         toolkit.createFormLabel(nameComposite, Messages.TypePage_superclass);
         IpsObjectRefControl superTypeControl = createSupertypeControl(nameComposite, toolkit);
         supertypeField = new TextButtonField(superTypeControl);
         supertypeField.addChangeListener(this);
     }
 
-    protected void addAbstractField(Composite nameComposite, UIToolkit toolkit){
+    /**
+     * 
+     * 
+     * @param nameComposite
+     * @param toolkit
+     */
+    protected void addAbstractField(Composite nameComposite, UIToolkit toolkit) {
         toolkit.createLabel(nameComposite, ""); //$NON-NLS-1$
         abstractField = new CheckboxField(toolkit.createCheckbox(nameComposite, "Abstract"));
         abstractField.addChangeListener(this);
     }
-    
+
     /**
-     * Delegates to supertypeChanged() if the supertype control has been edited.
+     * Delegates to <code>supertypeChanged()</code> if the supertype control has been edited.
      */
-    protected void valueChangedExtension(FieldValueChangedEvent e) throws CoreException{
-        if(e.field == supertypeField){
+    @Override
+    protected void valueChangedExtension(FieldValueChangedEvent e) throws CoreException {
+        if (e.field == supertypeField) {
             supertypeChanged(supertypeField);
         }
     }
 
     /**
-     * Empty by default. Subclasses can override it to react input changes of the super type control.
+     * Empty by default. Subclasses can override it to react input changes of the super type
+     * control.
      * 
-     * @throws CoreException are logged
+     * @throws CoreException Subclasses may throw at any time.
      */
     protected void supertypeChanged(TextButtonField supertypeField) throws CoreException {
+
     }
-    
+
     /**
-     * The type specific control must be instantiation and returned by this method. 
+     * The type specific control must be instantiated and returned by this method.
+     * 
+     * @param container
+     * @param toolkit
+     * 
+     * @return
      */
     protected abstract IpsObjectRefControl createSupertypeControl(Composite container, UIToolkit toolkit);
 
-    /** 
+    /**
      * {@inheritDoc}
      */
+    @Override
     protected void sourceFolderChanged() {
         super.sourceFolderChanged();
+
         IIpsPackageFragmentRoot root = getIpsPackageFragmentRoot();
-        if (root!=null) {
+        if (root != null) {
             ((IpsObjectRefControl)supertypeField.getControl()).setIpsProject(root.getIpsProject());
         } else {
             ((IpsObjectRefControl)supertypeField.getControl()).setIpsProject(null);
         }
     }
-    
-    public boolean getAbstract(){
+
+    /**
+     * Returns the value of the abstract checkbox field.
+     * 
+     * @return A flag indicating whether the abstract checkbox field is checked (<code>true</code>)
+     *         or not ( <code>false</code>).
+     */
+    public boolean getAbstract() {
         return ((Boolean)abstractField.getValue()).booleanValue();
     }
-    
-    public void setAbstract(boolean value){
+
+    /**
+     * Sets the value of the abstract checkbox field.
+     * 
+     * @param value If <code>true</code> the abstract checkbox field will be checked, if
+     *            <code>false</code> it won't be checked.
+     */
+    public void setAbstract(boolean value) {
         abstractField.setValue(new Boolean(value));
     }
-    
+
     /**
      * Returns the value of the super type field.
+     * 
+     * @return A <code>String</code> representing the current value of the super type field.
      */
     public String getSuperType() {
         return (String)supertypeField.getValue();
@@ -121,54 +164,67 @@ public abstract class TypePage extends IpsObjectPage {
 
     /**
      * Sets the value of the super type field.
+     * 
+     * @param superTypeQualifiedName The qualified name of the super type that will be written into
+     *            the super type field.
      */
-    public void setSuperType(String superTypeQualifiedName){
+    public void setSuperType(String superTypeQualifiedName) {
         supertypeField.setValue(superTypeQualifiedName);
     }
-    
-    /** 
+
+    /**
      * {@inheritDoc}
      */
-    protected void finishIpsObjects(IIpsObject newIpsObject, List modifiedIpsObjects) throws CoreException {
+    protected void finishIpsObjects(IIpsObject newIpsObject, List<IIpsObject> modifiedIpsObjects) throws CoreException {
         IType type = (IType)newIpsObject;
-        String supertypeName = getSuperType(); 
+        String supertypeName = getSuperType();
         type.setSupertype(supertypeName);
         type.setAbstract(getAbstract());
     }
-    
+
     /**
-     * Checks if the name of the policy component type and product component type of the associated product component type page
-     * are not equal. 
      * {@inheritDoc}
+     * 
+     * <p>
+     * Checks if the name of the policy component type and product component type of the associated
+     * product component type page are not equal.
+     * </p>
      */
+    @Override
     protected void validatePageExtension() throws CoreException {
-        //check for name conflicts of the product and policy component type name
-        if (pageOfAssociatedType != null && 
-            getIpsObjectName() != null && 
-            pageOfAssociatedType.getIpsObjectName() != null && 
-            !StringUtils.isEmpty(getIpsObjectName()) && 
-            !StringUtils.isEmpty(pageOfAssociatedType.getIpsObjectName()) && 
-            getIpsObjectName().equals(pageOfAssociatedType.getIpsObjectName())) {
-                setErrorMessage(Messages.TypePage_msgNameConflicts);
+        // Check for name conflicts of the product and policy component type name
+        if (pageOfAssociatedType != null && getIpsObjectName() != null
+                && pageOfAssociatedType.getIpsObjectName() != null && !StringUtils.isEmpty(getIpsObjectName())
+                && !StringUtils.isEmpty(pageOfAssociatedType.getIpsObjectName())
+                && getIpsObjectName().equals(pageOfAssociatedType.getIpsObjectName())) {
+            setErrorMessage(Messages.TypePage_msgNameConflicts);
         }
-        //check if selected super type exists
-        if(!StringUtils.isEmpty(getSuperType())){
+
+        // Check if selected super type exists
+        if (!StringUtils.isEmpty(getSuperType())) {
             IIpsSrcFile ipsSrcFile = getIpsProject().findIpsSrcFile(getIpsObjectType(), getSuperType());
-            if(ipsSrcFile == null){
+            if (ipsSrcFile == null) {
                 setErrorMessage(Messages.TypePage_msgSupertypeDoesNotExist);
             }
         }
     }
-    
+
     /**
      * {@inheritDoc}
      */
-    public void pageEntered() throws CoreException{
+    @Override
+    public void pageEntered() throws CoreException {
         super.pageEntered();
+
         validatePage();
         alreadyBeenEntered = true;
     }
 
+    /**
+     * Returns whether this wizard page has already been entered.
+     * 
+     * @return A flag indicating whether this wizard page has already been entered.
+     */
     public boolean isAlreadyBeenEntered() {
         return alreadyBeenEntered;
     }
