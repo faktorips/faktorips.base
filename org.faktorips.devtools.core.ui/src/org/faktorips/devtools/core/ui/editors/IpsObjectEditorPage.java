8284308dce00bd5afa3705a561c6b083aeced308
weickmann <weickmann>
2009-02-05 12:41:32 +0000

    Generics parametrisiert, javadoc ergaenzt

--- org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/editors/IpsObjectEditorPage.java
+++ org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/editors/IpsObjectEditorPage.java
@@ -3,7 +3,7 @@
  * 
  * Alle Rechte vorbehalten.
  * 
- * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen, 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
  * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
  * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
  * http://www.faktorzehn.org/f10-org:lizenzen:community eingesehen werden kann.
@@ -32,178 +32,212 @@ import org.faktorips.devtools.core.ui.IDataChangeableStateChangeListener;
 import org.faktorips.devtools.core.ui.UIToolkit;
 import org.faktorips.devtools.core.ui.forms.IpsSection;
 
-
 /**
  * Abstract base class for pages of an ips object editor.
  */
 public abstract class IpsObjectEditorPage extends FormPage implements IDataChangeableReadAccessWithListenerSupport {
-    
-    // the space between two sections 
+
+    // The horizontal space between two sections
     public final static int HORIZONTAL_SECTION_SPACE = 15;
+
+    // The vertical space between two sections
     public final static int VERTICAL_SECTION_SPACE = 10;
 
     private UIToolkit uiToolkit;
     private boolean dataChangeable = true;
 
-    private ArrayList dataChangeableStateChangeListeners;
-    
+    private ArrayList<IDataChangeableStateChangeListener> dataChangeableStateChangeListeners;
+
     /**
-     * @param editor	The editor the page belongs to.
-     * @param id		Page id used to identify the page.
-     * @param title		The title shown at the top of the page when the page is selected.
-     * @param title	The page name shown at the bottom of the editor as tab page. 
+     * Creates a new <code>IpsObjectEditorPage</code>.
+     * 
+     * @param editor The editor the page belongs to.
+     * @param id Page id used to identify the page.
+     * @param title The title shown at the top of the page when the page is selected.
+     * @param tabPageName The page name shown at the bottom of the editor as tab page.
      */
-    public IpsObjectEditorPage(
-            IpsObjectEditor editor, 
-            String id, 
-            String tabPageName) {
+    public IpsObjectEditorPage(IpsObjectEditor editor, String id, String tabPageName) {
         super(editor, id, tabPageName);
         uiToolkit = new UIToolkit(new FormToolkit(Display.getCurrent()));
     }
-    
+
+    /**
+     * Returns the <code>IpsObjectEditor</code> this page belongs to.
+     */
     protected IpsObjectEditor getIpsObjectEditor() {
         return (IpsObjectEditor)getEditor();
     }
-    
+
     /**
-     * Return the ips object of the ips src file beeing edited.
-     * Returns <code>null</code> if the src file couldn't determine the ips object
-     * (e.g. if the src file is stored outside an ips package)
+     * Returns the ips object of the ips src file beeing edited. Returns <code>null</code> if the
+     * src file couldn't determine the ips object (e.g. if the src file is stored outside an ips
+     * package).
      */
     protected IIpsObject getIpsObject() {
-        //null checking is necessary since in might be the case that the ips source file cannot be determined. E.g. in the special case
-        //that one tries to open a ips source file which is not in a ips package.
-        if (getIpsObjectEditor() != null && getIpsObjectEditor().getIpsSrcFile() != null && getIpsObjectEditor().getIpsSrcFile().exists()){
+        /*
+         * null checking is necessary since in might be the case that the ips source file cannot be
+         * determined. E.g. in the special case that one tries to open a ips source file which is
+         * not in a ips package.
+         */
+        if (getIpsObjectEditor() != null && getIpsObjectEditor().getIpsSrcFile() != null
+                && getIpsObjectEditor().getIpsSrcFile().exists()) {
             return getIpsObjectEditor().getIpsObject();
         } else {
             return null;
         }
     }
-    
-	protected void createFormContent(IManagedForm managedForm) {
-		super.createFormContent(managedForm);
-		ScrolledForm form = managedForm.getForm();
-        if (getIpsObject() == null){
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void createFormContent(IManagedForm managedForm) {
+        super.createFormContent(managedForm);
+
+        ScrolledForm form = managedForm.getForm();
+        if (getIpsObject() == null) {
             // no valid ips src file, create nothing
             return;
         }
-		form.setText(getIpsObjectEditor().getUniformPageTitle());
-		FormToolkit toolkit = managedForm.getToolkit();
-		createPageContent(form.getBody(), new UIToolkit(toolkit));
-		form.setExpandHorizontal(true);
-		form.setExpandVertical(true);
-		form.reflow(true);
+
+        form.setText(getIpsObjectEditor().getUniformPageTitle());
+        FormToolkit toolkit = managedForm.getToolkit();
+        createPageContent(form.getBody(), new UIToolkit(toolkit));
+        form.setExpandHorizontal(true);
+        form.setExpandVertical(true);
+        form.reflow(true);
+
         registerSelectionProviderActivation(getPartControl());
-	}
-	
-    protected final void registerSelectionProviderActivation(Control container){
-        if(container instanceof ISelectionProviderActivation){
-            getIpsObjectEditor().getSelectionProviderDispatcher().addSelectionProviderActivation((ISelectionProviderActivation)container);
+    }
+
+    protected final void registerSelectionProviderActivation(Control container) {
+        if (container instanceof ISelectionProviderActivation) {
+            getIpsObjectEditor().getSelectionProviderDispatcher().addSelectionProviderActivation(
+                    (ISelectionProviderActivation)container);
         }
-        if(!(container instanceof Composite)){
+
+        if (!(container instanceof Composite)) {
             return;
         }
+
         Control[] childs = ((Composite)container).getChildren();
         for (int i = 0; i < childs.length; i++) {
-            if(childs[i] instanceof Composite){
+            if (childs[i] instanceof Composite) {
                 registerSelectionProviderActivation(childs[i]);
             }
         }
-        
+
     }
 
     /**
-     * Subclasses might implement this method to provide the visiable content of this page.
+     * Subclasses might implement this method to provide the visible content of this page.
      * 
-     * @param formBody the root composite where the content of this page needs to be added to 
-     * @param toolkit the layout conform toolkit to create widgets with
+     * @param formBody The root composite where the content of this page needs to be added to.
+     * @param toolkit The layout conform toolkit to create widgets with.
      */
-	protected void createPageContent(Composite formBody, UIToolkit toolkit){
-	}
-	
-	/**
-	 * Creates a grid layout for the page with the indicated number of columns
-	 * and the default margins.
-	 * 
-	 * @param numOfColumns	Number of columns in the grid.
-	 * @param equalSize		True if the columns should have the same size
-	 */
-	protected GridLayout createPageLayout(int numOfColumns, boolean equalSize) {
-	    GridLayout layout = new GridLayout(numOfColumns, equalSize);
-	    layout.marginHeight = 10;
-	    layout.marginWidth = 10;
-	    layout.verticalSpacing = VERTICAL_SECTION_SPACE;
-	    layout.horizontalSpacing = HORIZONTAL_SECTION_SPACE;
-	    return layout;
-	}
-	
-	/**
-	 * Creates a grid composite for the inner page structure. The composite
-	 * has no margins but the default spacing settings.
-	 * 
-	 * @param numOfColumns	Number of columns in the grid.
-	 * @param equalSize		True if the columns should have the same size
-	 */
-	protected Composite createGridComposite(
-	        UIToolkit toolkit, 
-	        Composite parent, 
-	        int numOfColumns, 
-	        boolean equalSize,
-	        int gridData) {
-	    Composite composite = toolkit.getFormToolkit().createComposite(parent);
-	    GridLayout layout = new GridLayout(numOfColumns, equalSize);
-	    layout.marginHeight = 0;
-	    layout.marginWidth = 0;
-	    layout.verticalSpacing = VERTICAL_SECTION_SPACE;
-	    layout.horizontalSpacing = HORIZONTAL_SECTION_SPACE;
-	    composite.setLayout(layout);
-	    composite.setLayoutData(new GridData(gridData));
-	    return composite;
-	}
-	
+    protected void createPageContent(Composite formBody, UIToolkit toolkit) {
+
+    }
+
     /**
-     * Refreshes the page with the data from the model.
+     * Creates a grid layout for the page with the indicated number of columns and the default
+     * margins.
      * 
-     * Default implementation refreshs all ancestors that are instances
-     * of <code>IpsSection</code>. By ancestors we mean the children of the
-     * composite that represents this page and their children.
+     * @param numOfColumns Number of columns in the grid.
+     * @param equalSize Set to <code>true</code> if the columns should have the same size.
+     */
+    protected GridLayout createPageLayout(int numOfColumns, boolean equalSize) {
+        GridLayout layout = new GridLayout(numOfColumns, equalSize);
+        layout.marginHeight = 10;
+        layout.marginWidth = 10;
+        layout.verticalSpacing = VERTICAL_SECTION_SPACE;
+        layout.horizontalSpacing = HORIZONTAL_SECTION_SPACE;
+
+        return layout;
+    }
+
+    /**
+     * Creates a grid composite for the inner page structure. The composite has no margins but the
+     * default spacing settings.
+     * 
+     * @param toolkit
+     * @param parent
+     * @param numOfColumns Number of columns in the grid.
+     * @param equalSize Set to <code>true</code> if the columns should have the same size.
+     * @param gridData
+     */
+    protected Composite createGridComposite(UIToolkit toolkit,
+            Composite parent,
+            int numOfColumns,
+            boolean equalSize,
+            int gridData) {
+
+        Composite composite = toolkit.getFormToolkit().createComposite(parent);
+
+        GridLayout layout = new GridLayout(numOfColumns, equalSize);
+        layout.marginHeight = 0;
+        layout.marginWidth = 0;
+        layout.verticalSpacing = VERTICAL_SECTION_SPACE;
+        layout.horizontalSpacing = HORIZONTAL_SECTION_SPACE;
+
+        composite.setLayout(layout);
+        composite.setLayoutData(new GridData(gridData));
+
+        return composite;
+    }
+
+    /**
+     * <p>
+     * Refreshes the page with the data from the model.
+     * </p>
+     * <p>
+     * The default implementation refreshs all ancestors that are instances of
+     * <code>IpsSection</code>. By ancestors we mean the children of the composite that represents
+     * this page and its children.
+     * </p>
      */
     protected void refresh() {
         if (!(getPartControl() instanceof Composite)) {
             return;
         }
+
         refresh((Composite)getPartControl());
     }
-    
+
     private void refresh(Composite composite) {
-        Control[] children =composite.getChildren();
-        for (int i=0; i<children.length; i++) {
+        Control[] children = composite.getChildren();
+        for (int i = 0; i < children.length; i++) {
             if (children[i] instanceof IpsSection) {
                 ((IpsSection)children[i]).refresh();
-            }
-            else if (children[i] instanceof Composite) {
+            } else if (children[i] instanceof Composite) {
                 refresh((Composite)children[i]);
             }
         }
     }
-    
+
     /**
-     * Returns <code>true</code> if the content shown on this page is changeable,
-     * otherwise false.
+     * Returns <code>true</code> if the content shown on this page is changeable, otherwise
+     * <code>false</code>.
      */
     public boolean isDataChangeable() {
         return dataChangeable;
     }
-    
+
     /**
+     * <p>
      * Evaluates the new data changeable state and updates it, if it has changed.
-     * If the user can't change the data shown in the editor's at all, he also can't change the
-     * data shown on this page. If the user can change editor's data in general,
-     * the computeContentChangeableState() is called to evaluate if the data shown on
-     * this page can be changed. 
+     * </p>
+     * <p>
+     * If the user can't change the data shown in the editor's at all, he also can't change the data
+     * shown on this page. If the user can change editor's data in general, the
+     * <code>computeDataChangeableState()</code> is called to evaluate if the data shown on this
+     * page can be changed.
+     * </p>
+     * 
+     * @see #computeDataChangeableState()
      */
     public void updateDataChangeableState() {
-        if (getIpsObjectEditor().isDataChangeable()==null || !getIpsObjectEditor().isDataChangeable().booleanValue()) {
+        if (getIpsObjectEditor().isDataChangeable() == null || !getIpsObjectEditor().isDataChangeable().booleanValue()) {
             setDataChangeable(false);
         } else {
             setDataChangeable(computeDataChangeableState());
@@ -211,35 +245,41 @@ public abstract class IpsObjectEditorPage extends FormPage implements IDataChang
     }
 
     /**
-     * Evaluates if if the data shown on this page is changeable by the user. 
-     * This method does not consider the state of the ips object editor. 
-     * 
+     * <p>
+     * Evaluates whether the data shown on this page is changeable by the user. This method does not
+     * consider the state of the ips object editor.
+     * </p>
+     * <p>
      * The default implementation returns <code>true</code>, subclasses may override.
+     * </p>
      */
     protected boolean computeDataChangeableState() {
         return true;
     }
-    
+
     /**
-     * Resets the data changeable state to it's default, which is <code>true</code>,
-     * so that it machtes the initial state of controls which are by default enabled / editable.
+     * Resets the data changeable state to it's default, which is <code>true</code>, so that it
+     * matches the initial state of controls which are by default enabled / editable.
      */
     protected void resetDataChangeableState() {
         dataChangeable = true;
     }
-        
+
     private void setDataChangeable(boolean changeable) {
-        if (changeable==this.dataChangeable) {
+        if (changeable == this.dataChangeable) {
             return;
         }
+
         this.dataChangeable = changeable;
         uiToolkit.setDataChangeable(getPartControl(), changeable);
-        if (dataChangeableStateChangeListeners==null) {
+        if (dataChangeableStateChangeListeners == null) {
             return;
         }
-        List listeners = new ArrayList(dataChangeableStateChangeListeners);
-        for (Iterator it = listeners.iterator(); it.hasNext();) {
-            ((IDataChangeableStateChangeListener)it.next()).dataChangeableStateHasChanged(this);
+
+        List<IDataChangeableStateChangeListener> listeners = new ArrayList<IDataChangeableStateChangeListener>(
+                dataChangeableStateChangeListeners);
+        for (Iterator<IDataChangeableStateChangeListener> it = listeners.iterator(); it.hasNext();) {
+            it.next().dataChangeableStateHasChanged(this);
         }
     }
 
@@ -247,12 +287,14 @@ public abstract class IpsObjectEditorPage extends FormPage implements IDataChang
      * {@inheritDoc}
      */
     public void addDataChangeableStateChangeListener(IDataChangeableStateChangeListener listener) {
-        if (listener==null) {
+        if (listener == null) {
             return;
         }
-        if (dataChangeableStateChangeListeners==null) {
-            dataChangeableStateChangeListeners = new ArrayList(1);
+
+        if (dataChangeableStateChangeListeners == null) {
+            dataChangeableStateChangeListeners = new ArrayList<IDataChangeableStateChangeListener>(1);
         }
+
         this.dataChangeableStateChangeListeners.add(listener);
     }
 
@@ -260,15 +302,17 @@ public abstract class IpsObjectEditorPage extends FormPage implements IDataChang
      * {@inheritDoc}
      */
     public void removeDataChangeableStateChangeListener(IDataChangeableStateChangeListener listener) {
-        if (dataChangeableStateChangeListeners==null) {
+        if (dataChangeableStateChangeListeners == null) {
             return;
         }
+
         dataChangeableStateChangeListeners.remove(listener);
     }
 
     /**
      * {@inheritDoc}
      */
+    @Override
     public void dispose() {
         uiToolkit.dispose();
         super.dispose();
