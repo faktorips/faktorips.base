9d4d53182989a580ba5e4be206bfc864ca759b9c
weickmann <weickmann>
2009-02-20 14:57:21 +0000

    *** empty log message ***

--- org.faktorips.devtools.stdbuilder/src/org/faktorips/devtools/stdbuilder/enumtype/EnumTypeInterfaceBuilder.java
+++ org.faktorips.devtools.stdbuilder/src/org/faktorips/devtools/stdbuilder/enumtype/EnumTypeInterfaceBuilder.java
@@ -24,14 +24,23 @@ import org.faktorips.devtools.core.model.ipsobject.IpsObjectType;
 import org.faktorips.devtools.core.model.ipsproject.IIpsArtefactBuilderSet;
 import org.faktorips.util.LocalizedStringsSet;
 
+/**
+ * Builder that generates the published interface java source for <code>EnumType</code> ips objects.
+ * 
+ * @author Alexander Weickmann
+ * 
+ * @since 2.3
+ */
 public class EnumTypeInterfaceBuilder extends DefaultJavaSourceFileBuilder {
 
+    /** The package id identifiying the builder */
     public final static String PACKAGE_STRUCTURE_KIND_ID = "EnumTypeInterfaceBuilder.enumtype.stdbuilder.devtools.faktorips.org"; //$NON-NLS-1$
 
     /**
+     * Creates a new <code>EnumTypeInterfaceBuilder</code> that will belong to the given ips
+     * artefact builder set.
      * 
-     * 
-     * @param builderSet
+     * @param builderSet The ips artefact builder set this builder shall be a part of.
      */
     public EnumTypeInterfaceBuilder(IIpsArtefactBuilderSet builderSet) {
         super(builderSet, PACKAGE_STRUCTURE_KIND_ID, new LocalizedStringsSet(EnumTypeInterfaceBuilder.class));
@@ -54,6 +63,11 @@ public class EnumTypeInterfaceBuilder extends DefaultJavaSourceFileBuilder {
      */
     @Override
     protected void generateCodeForJavatype() throws CoreException {
+        generateMainTypeSection();
+    }
+
+    // Generates the code for the main type section
+    private void generateMainTypeSection() {
         TypeSection mainSection = getMainTypeSection();
         mainSection.setUnqualifiedName(getEnumType().getName());
         mainSection.setClassModifier(Modifier.PUBLIC);
