786d6c04ee459a22f43aa9749beabffc8030da93
ortmann <ortmann>
2009-03-13 14:52:14 +0000

    Codegenerator-Version auf 2.2.1 gesetzt.

--- org.faktorips.devtools.stdbuilder/src/org/faktorips/devtools/stdbuilder/StandardBuilderSet.java
+++ org.faktorips.devtools.stdbuilder/src/org/faktorips/devtools/stdbuilder/StandardBuilderSet.java
@@ -140,7 +140,7 @@ public class StandardBuilderSet extends DefaultBuilderSet {
     }
 
     private void initVersion() {
-        version = "2.2.0";
+        version = "2.2.1";
         // Version versionObj =
         // Version.parseVersion((String)StdBuilderPlugin.getDefault().getBundle().getHeaders().get(org.osgi.framework.Constants.BUNDLE_VERSION));
         // StringBuffer buf = new StringBuffer();
