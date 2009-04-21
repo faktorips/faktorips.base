f76ded3d0f6b3bcf25763917ed27f8208b547181
weickmann <weickmann>
2009-04-21 18:57:41 +0000

    *** empty log message ***

--- org.faktorips.devtools.core.test/src/org/faktorips/devtools/core/model/CreateIpsArchiveOperationTest.java
+++ org.faktorips.devtools.core.test/src/org/faktorips/devtools/core/model/CreateIpsArchiveOperationTest.java
@@ -3,7 +3,7 @@
  * 
  * Alle Rechte vorbehalten.
  * 
- * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen, 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
  * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
  * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
  * http://www.faktorzehn.org/f10-org:lizenzen:community eingesehen werden kann.
@@ -32,9 +32,9 @@ import org.faktorips.devtools.core.model.ipsproject.IIpsProject;
  */
 public class CreateIpsArchiveOperationTest extends AbstractIpsPluginTest {
 
-
     /*
-     * Test method for 'org.faktorips.devtools.core.model.CreateIpsArchiveOperation.run(IProgressMonitor)'
+     * Test method for
+     * 'org.faktorips.devtools.core.model.CreateIpsArchiveOperation.run(IProgressMonitor)'
      */
     public void testRun() throws CoreException {
         IIpsProject project = newIpsProject();
@@ -43,7 +43,7 @@ public class CreateIpsArchiveOperationTest extends AbstractIpsPluginTest {
         newPolicyAndProductCmptType(project, "mycompany.home.HomePolicy", "mycompany.home.HomeProduct");
         IFile archiveFile = project.getProject().getFile("test.ipsar");
         archiveFile.getWorkspace().build(IncrementalProjectBuilder.FULL_BUILD, null);
-        
+
         File file = archiveFile.getLocation().toFile();
         CreateIpsArchiveOperation operation = new CreateIpsArchiveOperation(project.getIpsPackageFragmentRoots(), file);
         operation.setInclJavaBinaries(true);
@@ -52,19 +52,18 @@ public class CreateIpsArchiveOperationTest extends AbstractIpsPluginTest {
         createLinkIfNecessary(archiveFile, file);
 
         assertTrue(archiveFile.exists());
-        
+
         IIpsArchive archive = new IpsArchive(project, archiveFile.getLocation());
         String[] packs = archive.getNonEmptyPackages();
         assertEquals(2, packs.length);
         assertEquals("mycompany.home", packs[0]);
         assertEquals("mycompany.motor", packs[1]);
-        
-        Set qnt = archive.getQNameTypes();
+
+        Set<QualifiedNameType> qnt = archive.getQNameTypes();
         assertEquals(6, qnt.size());
         assertTrue(qnt.contains(new QualifiedNameType("mycompany.motor.MotorPolicy", IpsObjectType.POLICY_CMPT_TYPE)));
         assertTrue(qnt.contains(new QualifiedNameType("mycompany.motor.MotorCoverage", IpsObjectType.POLICY_CMPT_TYPE)));
         assertTrue(qnt.contains(new QualifiedNameType("mycompany.home.HomePolicy", IpsObjectType.POLICY_CMPT_TYPE)));
     }
-    
 
 }
