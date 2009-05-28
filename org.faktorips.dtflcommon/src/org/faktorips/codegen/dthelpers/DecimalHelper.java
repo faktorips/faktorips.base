18b3b3a2a20bd6a2a3beff59664fcf30723e449b
ortmann2 <ortmann2>
2009-05-28 10:36:25 +0000

    fix unmappable character for encoding UTF-8

--- org.faktorips.dtflcommon/src/org/faktorips/codegen/dthelpers/DecimalHelper.java
+++ org.faktorips.dtflcommon/src/org/faktorips/codegen/dthelpers/DecimalHelper.java
@@ -74,7 +74,7 @@ public class DecimalHelper extends AbstractDatatypeHelper {
 	
 	/**
 	 * Methode der Oberklasse wird ueberschrieben, weil bei diesem Datentyp
-	 * valueOf-Methode selbst Null-Expression zurückgeben kann
+	 * valueOf-Methode selbst Null-Expression zurueckgeben kann
 	 */
 	public JavaCodeFragment newInstanceFromExpression(String expression) {	
 	    return valueOfExpression(expression);
