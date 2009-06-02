b515cf8e0b346504f33dedb8364496f9d9c3f1fc
weickmann <weickmann>
2009-06-02 14:28:34 +0000

    auto-format

--- org.faktorips.devtools.core/src/org/faktorips/devtools/core/model/ipsobject/IFixDifferencesToModelSupport.java
+++ org.faktorips.devtools.core/src/org/faktorips/devtools/core/model/ipsobject/IFixDifferencesToModelSupport.java
@@ -3,7 +3,7 @@
  * 
  * Alle Rechte vorbehalten.
  * 
- * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen, 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
  * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
  * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
  * http://www.faktorzehn.org/f10-org:lizenzen:community eingesehen werden kann.
@@ -18,10 +18,10 @@ import org.faktorips.devtools.core.model.ipsproject.IIpsProject;
 
 /**
  * Interface that marks an ips object as beeing able to fix difference between it's own assumed
- * state of the model and the real state of the model. E.g. a product component might contain a value
- * for an attribute named "maxSumInsured", but the model does not contain the attribute any longer.
- * In this case the call of {@link #fixAllDifferencesToModel(IIpsProject)} would remove the attribute value
- * from the product component.
+ * state of the model and the real state of the model. E.g. a product component might contain a
+ * value for an attribute named "maxSumInsured", but the model does not contain the attribute any
+ * longer. In this case the call of {@link #fixAllDifferencesToModel(IIpsProject)} would remove the
+ * attribute value from the product component.
  * 
  * @author Daniel Hohenberger
  */
@@ -31,8 +31,8 @@ public interface IFixDifferencesToModelSupport {
      * Returns <code>true</code> if this element contains structural differences to its
      * corresponding model element.
      * 
-     * @param ipsProject The ips project which ips object path is used to search for ips objects needed
-     * during the fix. 
+     * @param ipsProject The ips project which ips object path is used to search for ips objects
+     *            needed during the fix.
      * 
      * @throws CoreException if an error occurs while checking.
      */
@@ -41,16 +41,17 @@ public interface IFixDifferencesToModelSupport {
     /**
      * Fixes all differences between this element and its corresponding model element.
      * 
-     * @param ipsProject The ips project which ips object path is used to search for ips objects needed
-     * during the fix. 
+     * @param ipsProject The ips project which ips object path is used to search for ips objects
+     *            needed during the fix.
      * 
      * @throws CoreException if an error occurs while fixing.
      */
     public void fixAllDifferencesToModel(IIpsProject ipsProject) throws CoreException;
-    
+
     /**
-     * Returns the IPS source file this object is stored in.
-     * Duplicated here for cleaner inheritance.
+     * Returns the IPS source file this object is stored in. Duplicated here for cleaner
+     * inheritance.
+     * 
      * @see org.faktorips.devtools.core.model.ipsobject.IIpsObject#getIpsSrcFile()
      */
     public IIpsSrcFile getIpsSrcFile();
