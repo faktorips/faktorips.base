2a7acbf846d5ba80f42edf4307475ff6bf48a341
weickmann <weickmann>
2009-06-10 10:45:35 +0000

    *** empty log message ***

--- org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/editors/pctype/ValidationDecorator.java
+++ org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/editors/pctype/ValidationDecorator.java
@@ -3,7 +3,7 @@
  * 
  * Alle Rechte vorbehalten.
  * 
- * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen, 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
  * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
  * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
  * http://www.faktorzehn.org/f10-org:lizenzen:community eingesehen werden kann.
@@ -25,61 +25,78 @@ import org.faktorips.devtools.core.IpsPlugin;
 import org.faktorips.util.message.Message;
 import org.faktorips.util.message.MessageList;
 
-
 /**
- *
+ * The <tt>ValidationDecorator</tt> is responsible for drawing markers to ui composites. This
+ * mechanism is used to show little images to ui controls when the ips object being edited by an
+ * editor or a dialog does not pass the validation because of invalid input.
+ * <p>
+ * The used pattern is a GoF pattern called DecoratorPattern. In this structural pattern a so called
+ * decorator object extends an object that is to decorate (in our case a composite) so it has the
+ * same interface. If then a method from the composite is called by the client the decorator may
+ * have overwritten this method and in this way changes what happens. In this way it is possible to
+ * change the implementation of methods of the object to decorate without the need to change the
+ * client (by calling another method or something like that).
  */
+// FIXME aw: This class seems not to be used at all.
 public class ValidationDecorator extends Composite {
-    
-	protected final static Point IMAGE_SIZE = new Point(8, 8);
 
+    /** The size of the marker to decorate the ui composite with. */
+    protected final static Point IMAGE_SIZE = new Point(8, 8);
+
+    /** The ui label that is used to show the validation image. */
     private Label imageLabel;
+
+    // TODO aw: This field seems not to be used at all.
     private Control control;
 
     /**
-     * @param parent
-     * @param style
+     * Creates an instance of the <tt>ValidationDecorator</tt>.
+     * 
+     * @param parent The parent ui composite.
      */
     public ValidationDecorator(Composite parent) {
         super(parent, SWT.NONE);
+
         GridLayout layout = new GridLayout(2, false);
         layout.horizontalSpacing = 2;
         layout.verticalSpacing = 0;
         layout.marginHeight = 0;
         layout.marginWidth = 0;
         setLayout(layout);
+
         Label imageLabel = new Label(this, SWT.CENTER);
-		imageLabel.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_BEGINNING | GridData.VERTICAL_ALIGN_BEGINNING));
+        imageLabel.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_BEGINNING | GridData.VERTICAL_ALIGN_BEGINNING));
     }
-    
-	/**
-	 * @see org.eclipse.swt.widgets.Control#setBackground(org.eclipse.swt.graphics.Color)
-	 */
-	public void setBackground(Color color) {
-		super.setBackground(color);
-		imageLabel.setBackground(color);
-		control.setBackground(color);
-	}
-	
-	public void setMessageList(MessageList list) {
-	    String imageName;
-	    switch (list.getSeverity()) {
-	    	case Message.ERROR: {
-	    	    imageName = "size8/Error.gif"; //$NON-NLS-1$
-	    	    break;
-	    	}
-	    	case Message.WARNING: {
-	    	    imageName = "size8/Warning.gif"; //$NON-NLS-1$
-	    	    break;
-	    	}
-	    	case Message.INFO: {
-	    	    imageName = "info.gif"; //$NON-NLS-1$
-	    	    break;
-	    	}
-	    	default:
-	    	    imageName = "empty.gif"; //$NON-NLS-1$
-	    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setBackground(Color color) {
+        super.setBackground(color);
+        imageLabel.setBackground(color);
+        control.setBackground(color);
+    }
+
+    public void setMessageList(MessageList list) {
+        String imageName;
+        switch (list.getSeverity()) {
+            case Message.ERROR: {
+                imageName = "size8/Error.gif"; //$NON-NLS-1$
+                break;
+            }
+            case Message.WARNING: {
+                imageName = "size8/Warning.gif"; //$NON-NLS-1$
+                break;
+            }
+            case Message.INFO: {
+                imageName = "info.gif"; //$NON-NLS-1$
+                break;
+            }
+            default:
+                imageName = "empty.gif"; //$NON-NLS-1$
+        }
         imageLabel.setImage(IpsPlugin.getDefault().getImage(imageName));
-	}
-	
+    }
+
 }
