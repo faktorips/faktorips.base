87d735621d42bd71f102d5ebd87f1775ba59f4f6
kuntz <kuntz>
2009-08-28 12:11:09 +0000

    *** empty log message ***

--- org.faktorips.devtools.stdbuilder/src/org/faktorips/devtools/stdbuilder/table/TableImplBuilder.java
+++ org.faktorips.devtools.stdbuilder/src/org/faktorips/devtools/stdbuilder/table/TableImplBuilder.java
@@ -437,10 +437,14 @@ public class TableImplBuilder extends DefaultJavaSourceFileBuilder {
         methodBody.append("));");
 
         methodBody.addImport(List.class.getName());
+        codeBuilder.javaDoc(getLocalizedText(getIpsObject(), ADD_ROW_JAVADOC), ANNOTATION_GENERATED);
         appendOverrideAnnotation(codeBuilder, false);
-        codeBuilder.method(Modifier.PROTECTED, "void", "addRow", new String[] { "values", "productRepository" }, new String[] { List.class
-                .getName(), IRuntimeRepository.class.getName() }, methodBody, getLocalizedText(getIpsObject(), ADD_ROW_JAVADOC), ANNOTATION_GENERATED,
-                isUseTypesafeCollections() ? new String[] { ANNOTATION_SUPPRESS_WARNINGS_UNCHECKED } : null);
+        if(isUseTypesafeCollections()){
+            codeBuilder.annotation(new String[] { ANNOTATION_SUPPRESS_WARNINGS_UNCHECKED });
+        }
+        codeBuilder.methodBegin(Modifier.PROTECTED, Void.TYPE, "addRow", new String[] { "values", "productRepository" }, new Class[] { List.class, IRuntimeRepository.class});
+        codeBuilder.append(methodBody);
+        codeBuilder.methodEnd();
     }
 
     /**
@@ -528,9 +532,14 @@ public class TableImplBuilder extends DefaultJavaSourceFileBuilder {
             methodBody = createInitKeyMapsMethodBody(keys);
         }
         appendOverrideAnnotation(codeBuilder, false);
-        codeBuilder.method(Modifier.PROTECTED, Void.TYPE, "initKeyMaps", new String[0], new Class[0], methodBody,
-                getLocalizedText(getIpsObject(), INIT_KEY_MAPS_JAVADOC), ANNOTATION_GENERATED,
-                isUseTypesafeCollections()&&methodBody.getSourcecode().indexOf("<")>0 ? new String[] { ANNOTATION_SUPPRESS_WARNINGS_UNCHECKED } : null);
+        //TODO pk 12-08-2009: why is here a check agains <???
+        if(isUseTypesafeCollections()&&methodBody.getSourcecode().indexOf("<")>0){
+            codeBuilder.annotation(new String[] { ANNOTATION_SUPPRESS_WARNINGS_UNCHECKED });
+        }
+        codeBuilder.javaDoc(getLocalizedText(getIpsObject(), INIT_KEY_MAPS_JAVADOC), ANNOTATION_GENERATED);
+        codeBuilder.methodBegin(Modifier.PROTECTED, Void.TYPE, "initKeyMaps", new String[0], new Class[0]);
+        codeBuilder.append(methodBody);
+        codeBuilder.methodEnd();
     }
 
     private JavaCodeFragment createInitKeyMapsMethodBody(IUniqueKey[] keys) throws CoreException {
@@ -850,8 +859,10 @@ public class TableImplBuilder extends DefaultJavaSourceFileBuilder {
         methodBody.append("return false;");
 
         appendOverrideAnnotation(codeBuilder, false);
-        codeBuilder.method(Modifier.PUBLIC, Boolean.TYPE, "equals", new String[] { "o" }, new Class[] { Object.class },
-                methodBody, getLocalizedText(getIpsObject(), KEY_CLASS_EQUALS_JAVADOC), ANNOTATION_GENERATED);
+        codeBuilder.javaDoc(getLocalizedText(getIpsObject(), KEY_CLASS_EQUALS_JAVADOC), ANNOTATION_GENERATED);
+        codeBuilder.methodBegin(Modifier.PUBLIC, Boolean.TYPE, "equals", new String[] { "o" }, new Class[] { Object.class });
+        codeBuilder.append(methodBody);
+        codeBuilder.methodEnd();
     }
 
     private void createKeyClassHashCodeMethod(String[] combineKeyNames, JavaCodeFragmentBuilder codeBuilder)
@@ -861,6 +872,10 @@ public class TableImplBuilder extends DefaultJavaSourceFileBuilder {
         methodBody.append("return hashCode;");
 
         appendOverrideAnnotation(codeBuilder, false);
+        codeBuilder.annotation(ANNOTATION_GENERATED);
+        codeBuilder.methodBegin(Modifier.PUBLIC, Integer.TYPE, "hashCode", new String[0], new Class[0]);
+        codeBuilder.append(methodBody);
+        codeBuilder.methodEnd();
         codeBuilder.method(Modifier.PUBLIC, Integer.TYPE, "hashCode", new String[0], new Class[0], methodBody,
                 getLocalizedText(getIpsObject(), KEY_CLASS_HASHCODE_JAVADOC), ANNOTATION_GENERATED);
     }
