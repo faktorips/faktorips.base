c47c0b627b8c5e8e4fa3b4f7417377d9a49d19fd
dirmeier <dirmeier>
2009-09-07 13:31:24 +0000

    +parameter firstUp in Methode toCamelCase + viel Formatiert

--- org.faktorips.util/src/org/faktorips/util/StringUtil.java
+++ org.faktorips.util/src/org/faktorips/util/StringUtil.java
@@ -3,7 +3,7 @@
  * 
  * Alle Rechte vorbehalten.
  * 
- * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen, 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
  * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
  * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
  * http://www.faktorzehn.org/f10-org:lizenzen:community eingesehen werden kann.
@@ -23,69 +23,64 @@ import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
-
 /**
  * A collection of util methods for Strings.
- *  
+ * 
  * @author Jan Ortmann
  */
-public class StringUtil
-{
+public class StringUtil {
     public final static String CHARSET_UTF8 = "UTF-8";
-    
+
     public final static String CHARSET_ISO_8859_1 = "ISO-8859-1";
-    
-    
+
     /**
-     * Reads the available bytes from the input stream and returns them as a string
-     * using the given {@link java.nio.charset.Charset </code>charset<code>}. The method does not close the stream.
+     * Reads the available bytes from the input stream and returns them as a string using the given
+     * {@link java.nio.charset.Charset </code>charset<code>}. The method does not close the stream.
      * 
      * @throws IOException
      */
     public final static String readFromInputStream(InputStream is, Charset charset) throws IOException {
         return readFromInputStream(is, charset.name());
     }
-	
+
     /**
-     * Reads the available bytes from the input stream and returns them as a string
-     * using the given {@link java.nio.charset.Charset </code>charset<code>}.
-     * <p> 
-     * This method closes the input stream before returning!
+     * Reads the available bytes from the input stream and returns them as a string using the given
+     * {@link java.nio.charset.Charset </code>charset<code>}. <p> This method closes the input
+     * stream before returning!
      * 
      * @throws IOException
      */
     public final static String readFromInputStream(InputStream is, String charsetName) throws IOException {
-        
+
         StringBuffer buf = new StringBuffer(is.available());
         BufferedReader in = new BufferedReader(new InputStreamReader(is, charsetName));
-        
+
         try {
             int charValue = 0;
-            while(true){
+            while (true) {
                 charValue = in.read();
-                if(charValue == -1){
+                if (charValue == -1) {
                     break;
                 }
                 buf.append((char)charValue);
             }
-            
+
             return buf.toString();
         } finally {
             in.close();
         }
     }
-    
+
     /**
      * Returns the passed String enclosed in double quotes.
      */
     public static String quote(String s) {
-        if (s==null)
-        {
+        if (s == null) {
             return null;
         }
         return "\"" + s + "\"";
     }
-    
+
     /**
      * Returns the passed List of String enclosing every entry in double quotes
      */
@@ -96,81 +91,75 @@ public class StringUtil
         }
         return result;
     }
-    
+
     /**
      * Takes a name like a class name and removes the package information from the beginning.
      */
     public final static String unqualifiedName(String qualifiedName) {
         int index = qualifiedName.lastIndexOf(".");
-        if (index == -1)
-        {
+        if (index == -1) {
             return qualifiedName;
         }
-        return qualifiedName.substring(index+1);
+        return qualifiedName.substring(index + 1);
     }
-    
+
     /**
-     * Returns the qualified name for the given package name and unqualified name.
-     * If packageName is <code>null</code> or the empty String the unqualified name
-     * is returned.
+     * Returns the qualified name for the given package name and unqualified name. If packageName is
+     * <code>null</code> or the empty String the unqualified name is returned.
      * 
      * @throws NullPointerException if unqualifiedName is <code>null</code>.
      */
     public final static String qualifiedName(String packageName, String unqualifiedName) {
         ArgumentCheck.notNull(unqualifiedName);
-        if (packageName==null || packageName.equals("")) {
+        if (packageName == null || packageName.equals("")) {
             return unqualifiedName;
         }
         return packageName + '.' + unqualifiedName;
     }
-    
+
     /**
-     * Returns the package name for a given class name. Returns an empty String
-     * if the class name does not contain a package name.
+     * Returns the package name for a given class name. Returns an empty String if the class name
+     * does not contain a package name.
      * 
      * @throws NullPointerException if the qualifiedClassName is null.
      */
-    public final static String getPackageName(String qualifiedClassName)
-    {
+    public final static String getPackageName(String qualifiedClassName) {
         int index = qualifiedClassName.lastIndexOf(".");
-        if (index == -1)
-        {
+        if (index == -1) {
             return "";
         }
         return qualifiedClassName.substring(0, index);
     }
-    
-	/**
-	 * Returns the filename without extension.
-	 */
-	public static String getFilenameWithoutExtension(String filename) {
-		int index = filename.lastIndexOf('.');
-		if (index==-1)
-		{
-			return filename;
-		}
-		return filename.substring(0, index);
-	}
-	
-    public static String getFileExtension(String fileName){
+
+    /**
+     * Returns the filename without extension.
+     */
+    public static String getFilenameWithoutExtension(String filename) {
+        int index = filename.lastIndexOf('.');
+        if (index == -1) {
+            return filename;
+        }
+        return filename.substring(0, index);
+    }
+
+    public static String getFileExtension(String fileName) {
         int index = fileName.lastIndexOf('.');
-        if (index==-1)
-        {
+        if (index == -1) {
             return null;
         }
         return fileName.substring(index + 1, fileName.length());
-        
+
     }
-    
-	/**
-	 * Returns the lines of the given text as array. Each array item represents
-	 * one line. The lines don't contains the line separator.
-	 */
+
+    /**
+     * Returns the lines of the given text as array. Each array item represents one line. The lines
+     * don't contains the line separator.
+     */
     public final static String[] getLines(String text, String lineSeparator) {
         List<String> lines = new ArrayList<String>();
         int start = 0;
         int end = text.indexOf(lineSeparator);
-        while (end>0) {
+        while (end > 0) {
             lines.add(text.substring(start, end));
             start = end + lineSeparator.length();
             end = text.indexOf(lineSeparator, start);
@@ -178,30 +167,26 @@ public class StringUtil
         lines.add(text.substring(start));
         return lines.toArray(new String[lines.size()]);
     }
-    
+
     /**
-     * Returns the line in the text that starts at the given position and
-     * ends at the next line separator. The line separator itself is not returned
-     * as part of the line. 
+     * Returns the line in the text that starts at the given position and ends at the next line
+     * separator. The line separator itself is not returned as part of the line.
      */
-    public final static String getLine(
-            String text, 
-            int startPos, 
-            String lineSeparator) {
+    public final static String getLine(String text, int startPos, String lineSeparator) {
         int pos = text.indexOf(lineSeparator, startPos);
-        if (pos==-1) {
+        if (pos == -1) {
             return text.substring(startPos);
         }
         return text.substring(startPos, pos);
     }
-    
+
     /**
      * Returns the line separator provided by System.getProperty("line.separator").
      */
-    public static String getSystemLineSeparator(){
+    public static String getSystemLineSeparator() {
         return System.getProperty("line.separator");
     }
-    
+
     /**
      * Returns the input stream to read the given data.
      * 
@@ -212,8 +197,8 @@ public class StringUtil
     public static InputStream getInputStreamForString(String data, String charset) throws UnsupportedEncodingException {
         return new ByteArrayInputStream(data.getBytes(charset));
     }
-    
-    public static String toCamelCase(String text) {
+
+    public static String toCamelCase(String text, boolean firstUp) {
         if (text == null || text.equals("")) {
             return "";
         }
@@ -221,10 +206,15 @@ public class StringUtil
         String result = "";
         for (String word : words) {
             char firstChar = word.charAt(0);
-            result += Character.toUpperCase(firstChar) + word.substring(1).toLowerCase();
+            if (firstUp) {
+                firstChar = Character.toUpperCase(firstChar);
+            } else {
+                firstChar = Character.toLowerCase(firstChar);
+            }
+            result += firstChar + word.substring(1).toLowerCase();
+            firstUp = true;
         }
         return result;
     }
 
-    
 }
--- org.faktorips.util/test/org/faktorips/util/StringUtilTest.java
+++ org.faktorips.util/test/org/faktorips/util/StringUtilTest.java
@@ -3,7 +3,7 @@
  * 
  * Alle Rechte vorbehalten.
  * 
- * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen, 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
  * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
  * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
  * http://www.faktorzehn.org/f10-org:lizenzen:community eingesehen werden kann.
@@ -12,81 +12,129 @@
  *******************************************************************************/
 
 package org.faktorips.util;
+
 import junit.framework.TestCase;
 
 import org.apache.commons.lang.SystemUtils;
-import org.faktorips.util.StringUtil;
 
 /**
  * @author Jan Ortmann
  */
 public class StringUtilTest extends TestCase {
 
-	/**
-	 * Constructor for StringUtilTest.
-	 * @param name
-	 */
-	public StringUtilTest(String name) {
-		super(name);
-	}
+    /**
+     * Constructor for StringUtilTest.
+     * 
+     * @param name
+     */
+    public StringUtilTest(String name) {
+        super(name);
+    }
 
-	public void testQuote() {
-		assertEquals("\"hello\"", StringUtil.quote("hello"));
-		assertNull(StringUtil.quote(null));
-	}
+    public void testQuote() {
+        assertEquals("\"hello\"", StringUtil.quote("hello"));
+        assertNull(StringUtil.quote(null));
+    }
 
-	public void testUnqualifiedClassName() {
-		assertEquals("Test", StringUtil.unqualifiedName("com.Test"));
-		assertEquals("Test", StringUtil.unqualifiedName("com.ips.Test"));
-		assertEquals("Test", StringUtil.unqualifiedName("Test"));
-	}
+    public void testUnqualifiedClassName() {
+        assertEquals("Test", StringUtil.unqualifiedName("com.Test"));
+        assertEquals("Test", StringUtil.unqualifiedName("com.ips.Test"));
+        assertEquals("Test", StringUtil.unqualifiedName("Test"));
+    }
 
-	public void testGetLine() {
-	    String lineSeparator = SystemUtils.LINE_SEPARATOR;
-	    String text = "blabla";
-	    assertEquals("blabla", StringUtil.getLine(text, 0, lineSeparator));
-	    
-	    text = "blabla" + lineSeparator
-	    	 + "2.line";
-        
-	    assertEquals("blabla", StringUtil.getLine(text, 0, lineSeparator));
-	    assertEquals("2.line", StringUtil.getLine(text, 6 + SystemUtils.LINE_SEPARATOR.getBytes().length, lineSeparator));
-	}
-	
-	public void testGetLines() {
-		String[] result;
-		
-		result = StringUtil.getLines("blabla", SystemUtils.LINE_SEPARATOR);
-		assertEquals(1, result.length);
-	    assertEquals("blabla", result[0]);
-	    
-		result = StringUtil.getLines("blabla" + SystemUtils.LINE_SEPARATOR, SystemUtils.LINE_SEPARATOR);
-		assertEquals(2, result.length);
-	    assertEquals("blabla", result[0]);
-	    assertEquals("", result[1]);
-	    
-		result = StringUtil.getLines("blabla" + SystemUtils.LINE_SEPARATOR + "2.line", SystemUtils.LINE_SEPARATOR);
-		assertEquals(2, result.length);
-	    assertEquals("blabla", result[0]);
-	    assertEquals("2.line", result[1]);
+    public void testGetLine() {
+        String lineSeparator = SystemUtils.LINE_SEPARATOR;
+        String text = "blabla";
+        assertEquals("blabla", StringUtil.getLine(text, 0, lineSeparator));
 
-		result = StringUtil.getLines("blabla" + SystemUtils.LINE_SEPARATOR + "2.line" + SystemUtils.LINE_SEPARATOR, SystemUtils.LINE_SEPARATOR);
-		assertEquals(3, result.length);
-	    assertEquals("blabla", result[0]);
-	    assertEquals("2.line", result[1]);
-	    assertEquals("", result[2]);
-	}
-	
-	public void testGetSystemLineseparator(){
-	    assertEquals(System.getProperty("line.separator"), StringUtil.getSystemLineSeparator());
-	}
-    
-    public void testGetFileExtension(){
+        text = "blabla" + lineSeparator + "2.line";
+
+        assertEquals("blabla", StringUtil.getLine(text, 0, lineSeparator));
+        assertEquals("2.line", StringUtil
+                .getLine(text, 6 + SystemUtils.LINE_SEPARATOR.getBytes().length, lineSeparator));
+    }
+
+    public void testGetLines() {
+        String[] result;
+
+        result = StringUtil.getLines("blabla", SystemUtils.LINE_SEPARATOR);
+        assertEquals(1, result.length);
+        assertEquals("blabla", result[0]);
+
+        result = StringUtil.getLines("blabla" + SystemUtils.LINE_SEPARATOR, SystemUtils.LINE_SEPARATOR);
+        assertEquals(2, result.length);
+        assertEquals("blabla", result[0]);
+        assertEquals("", result[1]);
+
+        result = StringUtil.getLines("blabla" + SystemUtils.LINE_SEPARATOR + "2.line", SystemUtils.LINE_SEPARATOR);
+        assertEquals(2, result.length);
+        assertEquals("blabla", result[0]);
+        assertEquals("2.line", result[1]);
+
+        result = StringUtil.getLines("blabla" + SystemUtils.LINE_SEPARATOR + "2.line" + SystemUtils.LINE_SEPARATOR,
+                SystemUtils.LINE_SEPARATOR);
+        assertEquals(3, result.length);
+        assertEquals("blabla", result[0]);
+        assertEquals("2.line", result[1]);
+        assertEquals("", result[2]);
+    }
+
+    public void testGetSystemLineseparator() {
+        assertEquals(System.getProperty("line.separator"), StringUtil.getSystemLineSeparator());
+    }
+
+    public void testGetFileExtension() {
         String ext = StringUtil.getFileExtension("readme.txt");
         assertEquals("txt", ext);
 
         ext = StringUtil.getFileExtension("readme");
         assertNull(ext);
 
+    }
+
+    public void testToCamlCase() {
+        String testString = null;
+        assertEquals("", StringUtil.toCamelCase(testString, true));
+        assertEquals("", StringUtil.toCamelCase(testString, false));
+
+        testString = "";
+        assertEquals("", StringUtil.toCamelCase(testString, true));
+        assertEquals("", StringUtil.toCamelCase(testString, false));
+
+        testString = "asd";
+        assertEquals("Asd", StringUtil.toCamelCase(testString, true));
+        assertEquals("asd", StringUtil.toCamelCase(testString, false));
+
+        testString = "ASD";
+        assertEquals("Asd", StringUtil.toCamelCase(testString, true));
+        assertEquals("asd", StringUtil.toCamelCase(testString, false));
+
+        testString = "asd-def";
+        assertEquals("AsdDef", StringUtil.toCamelCase(testString, true));
+        assertEquals("asdDef", StringUtil.toCamelCase(testString, false));
+
+        testString = "asd_def";
+        assertEquals("AsdDef", StringUtil.toCamelCase(testString, true));
+        assertEquals("asdDef", StringUtil.toCamelCase(testString, false));
+
+        testString = "asd.def";
+        assertEquals("AsdDef", StringUtil.toCamelCase(testString, true));
+        assertEquals("asdDef", StringUtil.toCamelCase(testString, false));
+
+        testString = "asd,def";
+        assertEquals("AsdDef", StringUtil.toCamelCase(testString, true));
+        assertEquals("asdDef", StringUtil.toCamelCase(testString, false));
+
+        testString = "asd def";
+        assertEquals("AsdDef", StringUtil.toCamelCase(testString, true));
+        assertEquals("asdDef", StringUtil.toCamelCase(testString, false));
+
+        testString = "asd-def qwer";
+        assertEquals("AsdDefQwer", StringUtil.toCamelCase(testString, true));
+        assertEquals("asdDefQwer", StringUtil.toCamelCase(testString, false));
+
+        testString = "asd-1234";
+        assertEquals("Asd1234", StringUtil.toCamelCase(testString, true));
+        assertEquals("asd1234", StringUtil.toCamelCase(testString, false));
     }
 }
