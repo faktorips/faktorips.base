3dfffef23d466e19716e6e2a8f43dbc5d1ecd3fd
ortmann <ortmann>
2009-09-15 19:14:25 +0000

    verwendung generics

--- org.faktorips.devtools.core/src/org/faktorips/devtools/core/builder/AbstractProductCmptTypeBuilder.java
+++ org.faktorips.devtools.core/src/org/faktorips/devtools/core/builder/AbstractProductCmptTypeBuilder.java
@@ -3,7 +3,7 @@
  * 
  * Alle Rechte vorbehalten.
  * 
- * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen, 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
  * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
  * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
  * http://www.faktorzehn.org/f10-org:lizenzen:community eingesehen werden kann.
@@ -62,31 +62,36 @@ public abstract class AbstractProductCmptTypeBuilder extends AbstractTypeBuilder
     /**
      * Returns the product component type this builder builds an artefact for.
      * 
-     * @throws CoreException 
+     * @throws CoreException
      */
+    @Override
     public IProductCmptType getProductCmptType() {
-        return (IProductCmptType)getIpsObject();    }
+        return (IProductCmptType)getIpsObject();
+    }
 
+    @Override
     protected IPolicyCmptType getPcType() throws CoreException {
         return getProductCmptType().findPolicyCmptType(getIpsProject());
     }
 
-    
     /*
      * Generates the sourcecode of the generated Java class or interface.
      */
+    @Override
     protected final void generateCodeForJavatype(TypeSection mainSection) throws CoreException {
         generateCodeForTableUsages(mainSection.getMemberVarBuilder(), mainSection.getMethodBuilder());
     }
 
     /*
-     * Loops over the attributes and generates code for an attribute if it is valid.
-     * Takes care of proper exception handling.
+     * Loops over the attributes and generates code for an attribute if it is valid. Takes care of
+     * proper exception handling.
      */
+    @Override
     protected final void generateCodeForPolicyCmptTypeAttributes(TypeSection typeSection) throws CoreException {
-        
+
         IPolicyCmptType policyCmptType = getPcType();
-        IPolicyCmptTypeAttribute[] attributes = policyCmptType == null ? new IPolicyCmptTypeAttribute[0] : policyCmptType.getPolicyCmptTypeAttributes();
+        IPolicyCmptTypeAttribute[] attributes = policyCmptType == null ? new IPolicyCmptTypeAttribute[0]
+                : policyCmptType.getPolicyCmptTypeAttributes();
         for (int i = 0; i < attributes.length; i++) {
             IPolicyCmptTypeAttribute a = attributes[i];
             if (!a.isProductRelevant() || !a.isChangeable() || !a.isValid()) {
@@ -96,24 +101,24 @@ public abstract class AbstractProductCmptTypeBuilder extends AbstractTypeBuilder
                 Datatype datatype = a.findDatatype(getIpsProject());
                 DatatypeHelper helper = a.getIpsProject().getDatatypeHelper(datatype);
                 if (helper == null) {
-                    throw new CoreException(new IpsStatus("No datatype helper found for datatype " + datatype));            
+                    throw new CoreException(new IpsStatus("No datatype helper found for datatype " + datatype));
                 }
-                generateCodeForPolicyCmptTypeAttribute(a, helper, typeSection.getMemberVarBuilder(), typeSection.getMethodBuilder());
+                generateCodeForPolicyCmptTypeAttribute(a, helper, typeSection.getMemberVarBuilder(), typeSection
+                        .getMethodBuilder());
             } catch (Exception e) {
-                throw new CoreException(new IpsStatus(IStatus.ERROR,
-                        "Error building attribute " + attributes[i].getName() + " of "
-                                + getQualifiedClassName(getIpsObject().getIpsSrcFile()), e));
+                throw new CoreException(new IpsStatus(IStatus.ERROR, "Error building attribute "
+                        + attributes[i].getName() + " of " + getQualifiedClassName(getIpsObject().getIpsSrcFile()), e));
             }
         }
     }
-        
+
     /*
      * Loops over all table structure usages and generates code for the table content access method.
      */
     private void generateCodeForTableUsages(JavaCodeFragmentBuilder fieldCodeBuilder,
             JavaCodeFragmentBuilder methodCodeBuilder) throws CoreException {
         IProductCmptType type = getProductCmptType();
-        if (type==null) {
+        if (type == null) {
             return;
         }
         ITableStructureUsage[] tsus = type.getTableStructureUsages();
@@ -123,18 +128,18 @@ public abstract class AbstractProductCmptTypeBuilder extends AbstractTypeBuilder
             }
         }
     }
-    
+
     /**
-     * This method is called from the abstract builder if the policy component attribute is valid and
-     * therefore code can be generated.
+     * This method is called from the abstract builder if the policy component attribute is valid
+     * and therefore code can be generated.
      * <p>
+     * 
      * @param attribute The attribute sourcecode should be generated for.
      * @param datatypeHelper The datatype code generation helper for the attribute's datatype.
      * @param fieldsBuilder The code fragment builder to build the member variabales section.
      * @param methodsBuilder The code fragment builder to build the method section.
      */
-    protected abstract void generateCodeForPolicyCmptTypeAttribute(
-            IPolicyCmptTypeAttribute a, 
+    protected abstract void generateCodeForPolicyCmptTypeAttribute(IPolicyCmptTypeAttribute a,
             DatatypeHelper datatypeHelper,
             JavaCodeFragmentBuilder fieldsBuilder,
             JavaCodeFragmentBuilder methodsBuilder) throws CoreException;
@@ -142,50 +147,53 @@ public abstract class AbstractProductCmptTypeBuilder extends AbstractTypeBuilder
     protected abstract void generateCodeForTableUsage(ITableStructureUsage tsu,
             JavaCodeFragmentBuilder fieldsBuilder,
             JavaCodeFragmentBuilder methodsBuilder) throws CoreException;
-    
+
     /*
-     * Loops over the associations and generates code for a association if it is valid.
-     * Takes care of proper exception handling.
+     * Loops over the associations and generates code for a association if it is valid. Takes care
+     * of proper exception handling.
      */
+    @Override
     protected final void generateCodeForAssociations(JavaCodeFragmentBuilder fieldsBuilder,
             JavaCodeFragmentBuilder methodsBuilder) throws CoreException {
-        
-        HashMap containerAssociations = new HashMap();
+
+        HashMap<IAssociation, List<IAssociation>> derivedUnionAssociations = new HashMap<IAssociation, List<IAssociation>>();
         IAssociation[] associations = getProductCmptType().getAssociations();
         for (int i = 0; i < associations.length; i++) {
             IProductCmptTypeAssociation association = (IProductCmptTypeAssociation)associations[i];
             try {
-                if (associations[i].validate(getIpsProject()).containsErrorMsg()) {
+                if (association.validate(getIpsProject()).containsErrorMsg()) {
                     continue;
                 }
-                if (associations[i].isDerivedUnion()) {
+                if (association.isDerivedUnion()) {
                     generateCodeForDerivedUnionAssociationDefinition(association, fieldsBuilder, methodsBuilder);
                 } else {
-                    generateCodeForNoneDerivedUnionAssociation(association, fieldsBuilder, methodsBuilder);                
+                    generateCodeForNoneDerivedUnionAssociation(association, fieldsBuilder, methodsBuilder);
                 }
-                if (associations[i].isSubsetOfADerivedUnion()) {
-                    IAssociation containerRel = associations[i].findSubsettedDerivedUnion(getIpsSrcFile().getIpsProject());
-                    List implementationAssociations = (List)containerAssociations.get(containerRel);
-                    if (implementationAssociations==null) {
-                        implementationAssociations = new ArrayList();
-                        containerAssociations.put(containerRel, implementationAssociations);
+                if (association.isSubsetOfADerivedUnion()) {
+                    IAssociation derivedUnion = associations[i].findSubsettedDerivedUnion(getIpsSrcFile()
+                            .getIpsProject());
+                    List<IAssociation> implementationAssociations = derivedUnionAssociations.get(derivedUnion);
+                    if (implementationAssociations == null) {
+                        implementationAssociations = new ArrayList<IAssociation>();
+                        derivedUnionAssociations.put(derivedUnion, implementationAssociations);
                     }
                     implementationAssociations.add(associations[i]);
                 }
             } catch (Exception e) {
-                throw new CoreException(new IpsStatus(IStatus.ERROR, "Error building association "
-                        + associations[i].getName() + " of "
-                        + getQualifiedClassName(getIpsObject().getIpsSrcFile()), e));
+                throw new CoreException(
+                        new IpsStatus(IStatus.ERROR, "Error building association " + associations[i].getName() + " of "
+                                + getQualifiedClassName(getIpsObject().getIpsSrcFile()), e));
             }
         }
-        CodeGeneratorForContainerAssosImpl generator = new CodeGeneratorForContainerAssosImpl(
-                getIpsProject(), containerAssociations, fieldsBuilder, methodsBuilder);
+        CodeGeneratorForDerivedUnionSubsets generator = new CodeGeneratorForDerivedUnionSubsets(getIpsProject(),
+                derivedUnionAssociations, fieldsBuilder, methodsBuilder);
         generator.start(getProductCmptType());
     }
-    
+
     /**
-     * Generates the code for a none-container association definition. The method is called for every 
-     * valid none-container association defined in the product component type we currently build sourcecode for.
+     * Generates the code for a none-container association definition. The method is called for
+     * every valid none-container association defined in the product component type we currently
+     * build sourcecode for.
      * 
      * @param association the association source code should be generated for
      * @param fieldsBuilder the code fragment builder to build the memeber variabales section.
@@ -196,7 +204,7 @@ public abstract class AbstractProductCmptTypeBuilder extends AbstractTypeBuilder
      *             current build cycle of this builder. Alternatively it is possible to catch an
      *             exception and log it by means of the addToBuildStatus() method of the super
      *             class.
-     *             
+     * 
      * @see JavaSourceFileBuilder#addToBuildStatus(CoreException)
      * @see JavaSourceFileBuilder#addToBuildStatus(IStatus)
      */
@@ -204,72 +212,66 @@ public abstract class AbstractProductCmptTypeBuilder extends AbstractTypeBuilder
             JavaCodeFragmentBuilder fieldsBuilder,
             JavaCodeFragmentBuilder methodsBuilder) throws Exception;
 
-
     /**
-     * Generates the code for a container association definition. The method is called for every 
-     * valid container association defined in the product component type we currently build sourcecode for.
+     * Generates the code for a container association definition. The method is called for every
+     * valid container association defined in the product component type we currently build
+     * sourcecode for.
      * 
      * @param containerAssociation the container association source code should be generated for.
      * @param fieldsBuilder the code fragment builder to build the memeber variabales section.
      * @param fieldsBuilder the code fragment builder to build the method section.
      */
-    protected abstract void generateCodeForDerivedUnionAssociationDefinition(
-            IProductCmptTypeAssociation containerAssociation,
+    protected abstract void generateCodeForDerivedUnionAssociationDefinition(IProductCmptTypeAssociation containerAssociation,
             JavaCodeFragmentBuilder fieldsBuilder,
             JavaCodeFragmentBuilder methodsBuilder) throws Exception;
 
     /**
-     * Generates code for a container association implementation. 
-     * The method is called for every valid container association in the product 
-     * component type we currently build sourcecode for and for each valid container association
-     * in one of it's supertypes.
+     * Generates code for a container association implementation. The method is called for every
+     * valid container association in the product component type we currently build sourcecode for
+     * and for each valid container association in one of it's supertypes.
      * 
      * @param containerAssociation the container association source code should be generated for.
      * @param implementationAssociations the association implementing the container association.
      * @param fieldsBuilder the code fragment builder to build the memeber variabales section.
      * @param methodsBuilder the code fragment builder to build the method section.
      */
-    protected abstract void generateCodeForDerivedUnionAssociationImplementation(
-            IProductCmptTypeAssociation containerAssociation,
-            List implementationAssociations, 
+    protected abstract void generateCodeForDerivedUnionAssociationImplementation(IProductCmptTypeAssociation containerAssociation,
+            List<?> implementationAssociations,
             JavaCodeFragmentBuilder fieldsBuilder,
             JavaCodeFragmentBuilder methodsBuilder) throws Exception;
 
-    class CodeGeneratorForContainerAssosImpl extends ProductCmptTypeHierarchyCodeGenerator {
+    class CodeGeneratorForDerivedUnionSubsets extends ProductCmptTypeHierarchyCodeGenerator {
 
-        private HashMap containerImplMap; 
+        private HashMap<IAssociation, List<IAssociation>> derivedUnionMap;
 
-        public CodeGeneratorForContainerAssosImpl(
-                IIpsProject ipsProject,
-                HashMap containerImplMap,
-                JavaCodeFragmentBuilder fieldsBuilder, 
+        public CodeGeneratorForDerivedUnionSubsets(IIpsProject ipsProject,
+                HashMap<IAssociation, List<IAssociation>> derivedUnionMap, JavaCodeFragmentBuilder fieldsBuilder,
                 JavaCodeFragmentBuilder methodsBuilder) {
             super(ipsProject, fieldsBuilder, methodsBuilder);
-            this.containerImplMap = containerImplMap;
+            this.derivedUnionMap = derivedUnionMap;
         }
 
-        /**
-         * {@inheritDoc}
-         * @throws CoreException 
-         */
+        @Override
         protected boolean visit(IProductCmptType type) throws CoreException {
             IAssociation[] associations = type.getAssociations();
             for (int i = 0; i < associations.length; i++) {
                 if (associations[i].isDerivedUnion() && associations[i].isValid()) {
                     try {
-                        List implAssociations = (List)containerImplMap.get(associations[i]);
-                        if (implAssociations!=null) {
-                            generateCodeForDerivedUnionAssociationImplementation((IProductCmptTypeAssociation)associations[i], implAssociations, fieldsBuilder, methodsBuilder);
+                        List<IAssociation> implAssociations = derivedUnionMap.get(associations[i]);
+                        if (implAssociations != null) {
+                            generateCodeForDerivedUnionAssociationImplementation(
+                                    (IProductCmptTypeAssociation)associations[i], implAssociations, fieldsBuilder,
+                                    methodsBuilder);
                         }
                     } catch (Exception e) {
                         addToBuildStatus(new IpsStatus("Error building container association implementation. "
-                            + "DerivedUnionAssociation: " + associations[i]
-                            + "Implementing Type: " + getProductCmptType(), e));
+                                + "DerivedUnionAssociation: " + associations[i] + "Implementing Type: "
+                                + getProductCmptType(), e));
                     }
                 }
             }
             return true;
         }
-        
+
     }
 }
