4caeb16ba90f55859c4b7ac09ae68b3a97a40d64
ortmann <ortmann>
2009-09-16 11:39:17 +0000

    verwendung generics

--- org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/actions/WrapperAction.java
+++ org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/actions/WrapperAction.java
@@ -3,7 +3,7 @@
  * 
  * Alle Rechte vorbehalten.
  * 
- * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen, 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
  * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
  * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
  * http://www.faktorzehn.org/f10-org:lizenzen:community eingesehen werden kann.
@@ -31,58 +31,64 @@ import org.faktorips.devtools.core.ui.IpsUIPlugin;
  */
 public class WrapperAction extends IpsAction {
 
-	/**
-	 * The action delegate this action wrapps.
-	 */
-	private IActionDelegate wrappedActionDelegate = null;
+    /**
+     * The action delegate this action wrapps.
+     */
+    private IActionDelegate wrappedActionDelegate = null;
 
     /**
-     * Creates a new wrapper action for the given ids. The WrapperAction therefor searches action definitions
-     * of extensions/plugins to retrieve the classname of the actual action.
+     * Creates a new wrapper action for the given ids. The WrapperAction therefor searches action
+     * definitions of extensions/plugins to retrieve the classname of the actual action.
      * <p>
-     * First all actionset definitions are searched for the given actionset id and action id. Secondly all
-     * popup menu definitions are searched for the given action id, the actionset id is ignored in this case.
+     * First all actionset definitions are searched for the given actionset id and action id.
+     * Secondly all popup menu definitions are searched for the given action id, the actionset id is
+     * ignored in this case.
      * <p>
-     * The found classname is used to instanciate the requested delegate action. If no action delegate
-     * can be instanciated this wrapperaction does nothing when run.
+     * The found classname is used to instanciate the requested delegate action. If no action
+     * delegate can be instanciated this wrapperaction does nothing when run.
      * 
-     * @param selectionProvider The provider to get the selection from to let the wrapped action work on.
+     * @param selectionProvider The provider to get the selection from to let the wrapped action
+     *            work on.
      * @param label The label of this action in the GUI.
      * @param tooltip The tooltip of this action.
      * @param actionSetId The id of the action set to get the action from.
      * @param actionId The id of the action to wrap.
      */
-    public WrapperAction(ISelectionProvider selectionProvider, String label, String tooltip, String actionSetId, String actionId) {
+    public WrapperAction(ISelectionProvider selectionProvider, String label, String tooltip, String actionSetId,
+            String actionId) {
         super(selectionProvider);
         setText(label);
         setToolTipText(tooltip);
-        
+
         initDelegate(actionSetId, actionId);
     }
-    
+
     /**
      * The wrapperaction is created with the image given by imageName. If no image with this name
      * can be found, the action will be created without an icon.
      * 
-     * @param selectionProvider The provider to get the selection from to let the wrapped action work on.
+     * @param selectionProvider The provider to get the selection from to let the wrapped action
+     *            work on.
      * @param label The label of this action in the GUI.
      * @param tooltip The tooltip of this action.
-     * @param imageName The name of the icon/image that should be used for this action in the GUI. If no 
-     * image with the given name can be found or if imageName ist null this action is created without an image.
+     * @param imageName The name of the icon/image that should be used for this action in the GUI.
+     *            If no image with the given name can be found or if imageName ist null this action
+     *            is created without an image.
      * @param actionSetId The id of the action set to get the action from.
      * @param actionId The id of the action to wrap.
      */
-    public WrapperAction(ISelectionProvider selectionProvider, String label, String tooltip, String imageName, String actionSetId, String actionId) {
+    public WrapperAction(ISelectionProvider selectionProvider, String label, String tooltip, String imageName,
+            String actionSetId, String actionId) {
         this(selectionProvider, label, tooltip, actionSetId, actionId);
-        if(imageName!=null){
-            ImageDescriptor imageDescriptor= IpsUIPlugin.getDefault().getImageDescriptor(imageName);
-            if(imageDescriptor!=null){
+        if (imageName != null) {
+            ImageDescriptor imageDescriptor = IpsUIPlugin.getDefault().getImageDescriptor(imageName);
+            if (imageDescriptor != null) {
                 setImageDescriptor(imageDescriptor);
             }
         }
     }
 
-	private void initDelegate(String actionSetId, String actionId) {
+    private void initDelegate(String actionSetId, String actionId) {
         String className = null;
         IExtensionRegistry registry = Platform.getExtensionRegistry();
         // search actionsets for action definitions
@@ -97,12 +103,12 @@ public class WrapperAction extends IpsAction {
                     }
                 }
             }
-            if(className!=null){
+            if (className != null) {
                 break;
             }
         }
-        
-        if(className==null){
+
+        if (className == null) {
             // search popupmenu defs for action definitions
             IConfigurationElement[] popupElements = registry.getConfigurationElementsFor("org.eclipse.ui.popupMenus"); //$NON-NLS-1$
             for (int i = 0; i < popupElements.length; i++) {
@@ -113,16 +119,16 @@ public class WrapperAction extends IpsAction {
                         break;
                     }
                 }
-                if(className!=null){
+                if (className != null) {
                     break;
                 }
             }
         }
-        
+
         if (className != null) {
             try {
-                Class tempClass= Class.forName(className);
-                wrappedActionDelegate = (IActionDelegate) tempClass.newInstance();
+                Class<?> tempClass = Class.forName(className);
+                wrappedActionDelegate = (IActionDelegate)tempClass.newInstance();
             } catch (InstantiationException e) {
                 IpsPlugin.log(e);
             } catch (IllegalAccessException e) {
@@ -136,15 +142,15 @@ public class WrapperAction extends IpsAction {
         }
     }
 
-    /** 
-     * Sets the selection of the wrapped action and runs it. Does nothing if
-     * no action delegate could be instanciated when this action was initialized.
-	 * {@inheritDoc}
-	 */
-	public void run(IStructuredSelection selection) {
-		if (wrappedActionDelegate != null) {
-			wrappedActionDelegate.selectionChanged(this, selection);
-			wrappedActionDelegate.run(this);
-		}
-	}
+    /**
+     * Sets the selection of the wrapped action and runs it. Does nothing if no action delegate
+     * could be instanciated when this action was initialized. {@inheritDoc}
+     */
+    @Override
+    public void run(IStructuredSelection selection) {
+        if (wrappedActionDelegate != null) {
+            wrappedActionDelegate.selectionChanged(this, selection);
+            wrappedActionDelegate.run(this);
+        }
+    }
 }
