fe7e7f3eea9c68ccdb30933b017be061ee0f1328
ortmann <ortmann>
2009-09-17 11:49:17 +0000

    Zugriff auf deprecated Eclipse API ausgebaut

--- org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/table/BeanTableCellModifier.java
+++ org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/table/BeanTableCellModifier.java
@@ -3,7 +3,7 @@
  * 
  * Alle Rechte vorbehalten.
  * 
- * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen, 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
  * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
  * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
  * http://www.faktorzehn.org/f10-org:lizenzen:community eingesehen werden kann.
@@ -24,7 +24,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import org.eclipse.jface.util.Assert;
+import org.eclipse.core.runtime.Assert;
 import org.eclipse.jface.viewers.CellEditor;
 import org.eclipse.jface.viewers.ICellModifier;
 import org.eclipse.jface.viewers.TableViewer;
@@ -51,48 +51,47 @@ public class BeanTableCellModifier implements ICellModifier {
     // The parent control
     IDataChangeableReadWriteAccess parentControl;
 
-    //the ips project in the context of which this object has been instantiated
+    // the ips project in the context of which this object has been instantiated
     private IIpsProject ipsProject;
-    
+
     // Listeners for the changes inside the cell
     private List<ColumnChangeListener> columnChangeListeners = new ArrayList<ColumnChangeListener>(1);
-    
+
     // Cache
     private Map<Object, Object> propertyDescriptors = new HashMap<Object, Object>(4);
     private Map<Object, Object> columnIdentifers = new HashMap<Object, Object>(4);
-    
-    // Contains the delegate cell editors for each column, 
+
+    // Contains the delegate cell editors for each column,
     // only if at least one column specifies different cell editor for each row
     // otherwise the list is empty
-    private Map<Integer, DelegateCellEditor> delegateCellEditor= new HashMap<Integer, DelegateCellEditor>(0);
+    private Map<Integer, DelegateCellEditor> delegateCellEditor = new HashMap<Integer, DelegateCellEditor>(0);
 
-    
-    
-    public BeanTableCellModifier(TableViewer tableViewer, IDataChangeableReadWriteAccess parentControl, IIpsProject ipsProject) {
+    public BeanTableCellModifier(TableViewer tableViewer, IDataChangeableReadWriteAccess parentControl,
+            IIpsProject ipsProject) {
         ArgumentCheck.notNull(tableViewer);
         this.tableViewer = tableViewer;
         this.parentControl = parentControl;
         ArgumentCheck.notNull(ipsProject, this);
         this.ipsProject = ipsProject;
     }
-    
+
     /**
      * Inits (create and store) the cell editors for each row. The given value datatypes specifies
      * the type of the cell editor in the row.
      */
     public void initRowModifier(int column, ValueDatatype[] datatypesRows) {
         List<CellEditor> rowCellEditors = new ArrayList<CellEditor>(datatypesRows.length);
-        DelegateCellEditor dm = (DelegateCellEditor) delegateCellEditor.get(new Integer(column));
-        if (dm == null){
+        DelegateCellEditor dm = delegateCellEditor.get(new Integer(column));
+        if (dm == null) {
             // error wrong initializing of the delegate cell editor
-            throw new RuntimeException("Wrong index of delegate cell editor!"); //$NON-NLS-1$
+            throw new RuntimeException("Wrong index of delegate cell editor!");
         }
         for (int i = 0; i < datatypesRows.length; i++) {
             rowCellEditors.add(createCellEditor(uiToolkit, datatypesRows[i], dm.getColumn()));
         }
-        dm.setCellEditors((CellEditor[])rowCellEditors.toArray(new CellEditor[rowCellEditors.size()]));
+        dm.setCellEditors(rowCellEditors.toArray(new CellEditor[rowCellEditors.size()]));
     }
-    
+
     /**
      * Initialize the modifiers with the given properties and datatypes. The datatypes specifies the
      * type of the cell editor which will adapt to the corresponding column. The cell editors will
@@ -103,29 +102,30 @@ public class BeanTableCellModifier implements ICellModifier {
         ArgumentCheck.isTrue(properties.length == datatypes.length);
         this.uiToolkit = uiToolkit;
         ArrayList<CellEditor> cellEditors = new ArrayList<CellEditor>(datatypes.length);
-        
+
         // create column identifier and cell editors
         for (int i = 0; i < properties.length; i++) {
             ColumnIdentifier ci = new ColumnIdentifier(properties[i], datatypes[i], i);
             // assert that a property could only be assigned to one column
-            Assert.isTrue(columnIdentifers.get(properties[i]) == null, "A column modifier for property " + properties[i] + " already exist!"); //$NON-NLS-1$ //$NON-NLS-2$
+            Assert.isTrue(columnIdentifers.get(properties[i]) == null, "A column modifier for property "
+                    + properties[i] + " already exist!");
             columnIdentifers.put(properties[i], ci);
             // create cell modifier if enabled
-            
-            if (datatypes[i] != null){
+
+            if (datatypes[i] != null) {
                 CellEditor cellEditor = createCellEditor(uiToolkit, datatypes[i], i);
                 cellEditors.add(cellEditor);
             } else {
                 cellEditors.add(null);
             }
         }
-        
+
         // connect to table viewer
         tableViewer.setColumnProperties(properties);
-        tableViewer.setCellEditors((CellEditor[])cellEditors.toArray(new CellEditor[cellEditors.size()]));
+        tableViewer.setCellEditors(cellEditors.toArray(new CellEditor[cellEditors.size()]));
         tableViewer.setCellModifier(this);
     }
-    
+
     /*
      * Returns a new cell editor for the given datatype.
      */
@@ -140,18 +140,19 @@ public class BeanTableCellModifier implements ICellModifier {
             delegateCellEditor.put(new Integer(columnIndex), dm);
             return dm;
         } else {
-            ValueDatatypeControlFactory factory = IpsUIPlugin.getDefault().getValueDatatypeControlFactory(valueDatatype);
+            ValueDatatypeControlFactory factory = IpsUIPlugin.getDefault()
+                    .getValueDatatypeControlFactory(valueDatatype);
             return factory.createCellEditor(uiToolkit, valueDatatype, null, tableViewer, columnIndex, ipsProject);
         }
     }
-    
+
     /**
      * Adds a column change listener to the cell modifier.
      */
-    public void addListener(ColumnChangeListener changeListener){
+    public void addListener(ColumnChangeListener changeListener) {
         columnChangeListeners.add(changeListener);
     }
-    
+
     /**
      * {@inheritDoc}
      */
@@ -165,13 +166,13 @@ public class BeanTableCellModifier implements ICellModifier {
     public String getValue(Object element, String property) {
         try {
             PropertyDescriptor pd = getPropertyDescriptor(element, property);
-            if (pd != null){
-                return (String) getPropertyValue(element, pd);
+            if (pd != null) {
+                return (String)getPropertyValue(element, pd);
             }
         } catch (Exception e) {
-            throw new RuntimeException("Error resolving property methods for element " + element.getClass().getName()); //$NON-NLS-1$
+            throw new RuntimeException("Error resolving property methods for element " + element.getClass().getName());
         }
-        throw new RuntimeException("Error resolving property method " + property); //$NON-NLS-1$
+        throw new RuntimeException("Error resolving property method " + property);
     }
 
     /**
@@ -182,7 +183,7 @@ public class BeanTableCellModifier implements ICellModifier {
             ArgumentCheck.isInstanceOf(element, TableItem.class);
             element = ((TableItem)element).getData();
             PropertyDescriptor pd = getPropertyDescriptor(element, property);
-            ColumnIdentifier ci = (ColumnIdentifier) columnIdentifers.get(property);
+            ColumnIdentifier ci = (ColumnIdentifier)columnIdentifers.get(property);
             if (pd != null && ci != null) {
                 if (value == null || !value.equals(getPropertyValue(element, pd))) {
                     setPropertyValue(element, pd, value);
@@ -192,17 +193,17 @@ public class BeanTableCellModifier implements ICellModifier {
                 return;
             }
         } catch (Exception e) {
-            throw new RuntimeException("Error resolving property methods for element " + element.getClass().getName()); //$NON-NLS-1$
+            throw new RuntimeException("Error resolving property methods for element " + element.getClass().getName());
         }
-        throw new RuntimeException("Error resolving property method " + property); //$NON-NLS-1$
+        throw new RuntimeException("Error resolving property method " + property);
     }
-    
+
     /*
-     * Returns the property descriptor of the given property and element 
+     * Returns the property descriptor of the given property and element
      */
-    private PropertyDescriptor getPropertyDescriptor(Object element, String property ) throws IntrospectionException{
-        PropertyDescriptor pd = (PropertyDescriptor ) propertyDescriptors.get(element.getClass().getName() + property);
-        if (pd != null){
+    private PropertyDescriptor getPropertyDescriptor(Object element, String property) throws IntrospectionException {
+        PropertyDescriptor pd = (PropertyDescriptor)propertyDescriptors.get(element.getClass().getName() + property);
+        if (pd != null) {
             return pd;
         }
         BeanInfo beanInfo = Introspector.getBeanInfo(element.getClass());
@@ -215,33 +216,33 @@ public class BeanTableCellModifier implements ICellModifier {
         }
         return null;
     }
-    
+
     /*
      * Returns the value of the given property and element
      */
     private Object getPropertyValue(Object element, PropertyDescriptor property) {
         try {
             Method getter = property.getReadMethod();
-             Object value = getter.invoke(element, new Object[0]);
-             return value;
+            Object value = getter.invoke(element, new Object[0]);
+            return value;
         } catch (Exception e) {
-            throw new RuntimeException("Error getting property value " + property.getName()); //$NON-NLS-1$
+            throw new RuntimeException("Error getting property value " + property.getName());
         }
     }
-    
+
     /*
      * Sets the value of the given property and element
      */
     private void setPropertyValue(Object element, PropertyDescriptor property, Object value) {
         try {
             Method setter = property.getWriteMethod();
-            setter.invoke(element, new Object[]{value});
+            setter.invoke(element, new Object[] { value });
         } catch (Exception e) {
-            throw new RuntimeException("Error setting property value " + property.getName(), e); //$NON-NLS-1$
+            throw new RuntimeException("Error setting property value " + property.getName(), e);
         }
     }
-    
-    private void notifyColumnChangeListener(ColumnIdentifier columnIdentifier, Object value){
+
+    private void notifyColumnChangeListener(ColumnIdentifier columnIdentifier, Object value) {
         for (Iterator<ColumnChangeListener> iter = columnChangeListeners.iterator(); iter.hasNext();) {
             iter.next().valueChanged(columnIdentifier, value);
         }
