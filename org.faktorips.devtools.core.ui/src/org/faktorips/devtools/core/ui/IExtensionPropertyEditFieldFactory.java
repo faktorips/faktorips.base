92f3099375779c585fbd17b76d14609b1d256c8e
niedernhuber <niedernhuber>
2009-10-12 15:35:16 +0000

    javadoc

--- org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/IExtensionPropertyEditFieldFactory.java
+++ org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/IExtensionPropertyEditFieldFactory.java
@@ -3,7 +3,7 @@
  * 
  * Alle Rechte vorbehalten.
  * 
- * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen, 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
  * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
  * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
  * http://www.faktorzehn.org/f10-org:lizenzen:community eingesehen werden kann.
@@ -20,14 +20,16 @@ import org.faktorips.devtools.core.ui.controller.EditField;
 public interface IExtensionPropertyEditFieldFactory {
 
     /**
-     * Returns a new EditField that allows to edit the property value. Can return <code>null</code> if 
-     * <code>isEditedInStandardExtensionArea()</code> returns <code>false</code>.
+     * Returns a new EditField that allows to edit the property value. Can return <code>null</code>
+     * if <code>isEditedInStandardExtensionArea()</code> returns <code>false</code>.
      * 
-     * @param ipsObjectPart The ips object part which extension property an edit field is created for.
-     * @param extensionArea The standard extension area composite. A new control that allows editing the property
-     * value has to be added to this composite in subclasses. The EditField has to be constructed based on
-     * the control. 
-     * @param toolkit The ui toolkit to be used to create the control to ensure a consistent user interface.
+     * @param ipsObjectPart The ips object part which extension property an edit field is created
+     *            for.
+     * @param extensionArea The standard extension area composite. A new control that allows editing
+     *            the property value has to be added to this composite in subclasses. The EditField
+     *            has to be constructed based on the control.
+     * @param toolkit The ui toolkit to be used to create the control to ensure a consistent user
+     *            interface.
      */
     public EditField newEditField(IIpsObjectPartContainer ipsObjectPart, Composite extensionArea, UIToolkit toolkit);
 
--- org.faktorips.devtools.core/src/org/faktorips/devtools/core/model/productcmpt/IPropertyValue.java
+++ org.faktorips.devtools.core/src/org/faktorips/devtools/core/model/productcmpt/IPropertyValue.java
@@ -3,7 +3,7 @@
  * 
  * Alle Rechte vorbehalten.
  * 
- * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen, 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
  * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
  * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
  * http://www.faktorzehn.org/f10-org:lizenzen:community eingesehen werden kann.
@@ -19,8 +19,8 @@ import org.faktorips.devtools.core.model.productcmpttype.IProdDefProperty;
 import org.faktorips.devtools.core.model.productcmpttype.ProdDefPropertyType;
 
 /**
- * Base interface for properties stored in product component generartions like 
- * formulas, table content usages, product component attributes and config elements.
+ * Base interface for properties stored in product component generartions like formulas, table
+ * content usages, product component attributes and config elements.
  * 
  * @author Jan Ortmann
  */
@@ -30,33 +30,45 @@ public interface IPropertyValue {
      * Returns the product component generation this value belongs to.
      */
     public IProductCmptGeneration getProductCmptGeneration();
-    
+
     /**
      * Returns the name of the product definition property, this is a value of.
      * 
-     * @see IProdDefProperty 
+     * @see IProdDefProperty
      */
     public String getPropertyName();
 
     /**
-     * Returns the property this object provides a value for. Returns <code>null</code> if the property can't be found.
+     * Returns the property this object provides a value for. Returns <code>null</code> if the
+     * property can't be found.
      * 
      * @param ispProject The ips project which search path is used.
      * 
-     * @throws CoreException if an error occurs 
+     * @throws CoreException if an error occurs
      */
     public IProdDefProperty findProperty(IIpsProject ipsProject) throws CoreException;
-    
+
     /**
-     * Returns the type of the property. 
+     * Returns the type of the product definition property.
+     * <p>
+     * If type is {@link ProdDefPropertyType#VALUE} the property can be casted to
+     * {@link IAttributeValue}.
+     * <p>
+     * If type is {@link ProdDefPropertyType#FORMULA} the property can be casted to {@link IFormula}.
+     * <p>
+     * If type is {@link ProdDefPropertyType#TABLE_CONTENT_USAGE} the property can be casted to
+     * {@link ITableContentUsage}.
+     * <p>
+     * If type is {@link ProdDefPropertyType#DEFAULT_VALUE_AND_VALUESET} the property can be casted
+     * to {@link IConfigElement}.
      */
     public ProdDefPropertyType getPropertyType();
-    
+
     /**
      * Returns the value.
      */
     public String getPropertyValue();
-    
+
     /**
      * Removes the part from the parent.
      */
