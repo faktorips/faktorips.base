597154ddbdf56462317d6e83c4f268b2902776a3
hohenberger <hohenberger>
2009-10-29 08:39:21 +0000

    added NPEs to constructor and constants for common cardinalities

--- org.faktorips.runtime.java5/src/org/faktorips/runtime/internal/ProductComponentLink.java
+++ org.faktorips.runtime.java5/src/org/faktorips/runtime/internal/ProductComponentLink.java
@@ -3,7 +3,7 @@
  * 
  * Alle Rechte vorbehalten.
  * 
- * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen, 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
  * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
  * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
  * http://www.faktorzehn.org/f10-org:lizenzen:community eingesehen werden kann.
@@ -23,12 +23,26 @@ import org.w3c.dom.Element;
  * 
  * @author Daniel Hohenberger
  */
-public class ProductComponentLink<E extends IProductComponent> extends RuntimeObject implements IProductComponentLink<E> {
+public class ProductComponentLink<E extends IProductComponent> extends RuntimeObject implements
+        IProductComponentLink<E> {
 
     private final IProductComponentGeneration source;
     private IntegerRange cardinality;
     private String targetId;
     private String associationName;
+
+    /**
+     * Cardinality (0,&#42;)
+     */
+    public static final IntegerRange CARDINALITY_FULL_RANGE = new IntegerRange(0, Integer.MAX_VALUE);
+    /**
+     * Cardinality (0,1)
+     */
+    public static final IntegerRange CARDINALITY_OPTIONAL = new IntegerRange(0, 1);
+    /**
+     * Cardinality (1,1)
+     */
+    public static final IntegerRange CARDINALITY_OBLIGATORY = new IntegerRange(1, 1);
 
     /**
      * Creates a new link for the given product component generation. Target and cardinality must be
@@ -41,20 +55,31 @@ public class ProductComponentLink<E extends IProductComponent> extends RuntimeOb
     /**
      * Creates a new link to the given target for the given product component generation using the
      * cardinality (0,*).
+     * 
+     * @throws NullPointerException if any of the parameters is {@code null}.
      */
     public ProductComponentLink(IProductComponentGeneration source, E target) {
-        this.source = source;
-        this.targetId = target.getId();
-        cardinality = new IntegerRange(0, Integer.MAX_VALUE);
+        this(source, target, CARDINALITY_FULL_RANGE);
     }
 
     /**
      * Creates a new link with the given cardinality to the given target for the given product
      * component generation.
+     * 
+     * @throws NullPointerException if any of the parameters is {@code null}.
      */
     public ProductComponentLink(IProductComponentGeneration source, E target, IntegerRange cardinality) {
+        if (source == null) {
+            throw new NullPointerException("The source for the ProductComponentLink may not be null.");
+        }
         this.source = source;
+        if (targetId == null) {
+            throw new NullPointerException("The targetId for the ProductComponentLink may not be null.");
+        }
         this.targetId = target.getId();
+        if (cardinality == null) {
+            throw new NullPointerException("The cardinality for the ProductComponentLink may not be null.");
+        }
         this.cardinality = cardinality;
     }
 
@@ -119,7 +144,8 @@ public class ProductComponentLink<E extends IProductComponent> extends RuntimeOb
         sb.append('(');
         sb.append(cardinality.getLowerBound());
         sb.append("..");
-        sb.append(new Integer(Integer.MAX_VALUE).equals(cardinality.getUpperBound()) ? "*" : cardinality.getUpperBound());
+        sb.append(new Integer(Integer.MAX_VALUE).equals(cardinality.getUpperBound()) ? "*" : cardinality
+                .getUpperBound());
         sb.append(')');
         return sb.toString();
     }
