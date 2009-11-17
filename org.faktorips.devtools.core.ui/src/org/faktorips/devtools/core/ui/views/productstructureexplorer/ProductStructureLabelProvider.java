8ee31a909326c42b9b152338674954508ffd53bd
dirmeier <dirmeier>
2009-11-17 12:35:01 +0000

    refactored: statt 'implements ILabelProvider' jetzt 'extends LabelProvider'

--- org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/views/productstructureexplorer/ProductStructureLabelProvider.java
+++ org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/views/productstructureexplorer/ProductStructureLabelProvider.java
@@ -3,7 +3,7 @@
  * 
  * Alle Rechte vorbehalten.
  * 
- * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen, 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
  * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
  * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
  * http://www.faktorzehn.org/f10-org:lizenzen:community eingesehen werden kann.
@@ -13,12 +13,8 @@
 
 package org.faktorips.devtools.core.ui.views.productstructureexplorer;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.commons.lang.StringUtils;
-import org.eclipse.jface.viewers.ILabelProvider;
-import org.eclipse.jface.viewers.ILabelProviderListener;
+import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.ViewerLabel;
 import org.eclipse.swt.graphics.Image;
 import org.faktorips.devtools.core.IpsPlugin;
@@ -30,96 +26,65 @@ import org.faktorips.devtools.core.model.productcmpttype.IProductCmptTypeAssocia
 import org.faktorips.devtools.core.ui.views.productstructureexplorer.ProductStructureExplorer.GenerationRootNode;
 import org.faktorips.util.StringUtil;
 
-public class ProductStructureLabelProvider implements ILabelProvider {
+public class ProductStructureLabelProvider extends LabelProvider {
 
-	private List listeners = new ArrayList();
-	
     private boolean showTableStructureUsageName = false;
-    
+
     private GenerationRootNode generationRootNode;
-    
-	/**
-	 * {@inheritDoc}
-	 */
-	public void addListener(ILabelProviderListener listener) {
-		listeners.add(listener);
-	}
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public void dispose() {
-		this.listeners = null;
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public boolean isLabelProperty(Object element, String property) {
-		return true; 
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public void removeListener(ILabelProviderListener listener) {
-		listeners.remove(listener);
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public Image getImage(Object element) {
-		if (element instanceof IProductCmptReference) {
-			return ((IProductCmptReference)element).getProductCmpt().getImage();
-		}
-		else if (element instanceof IProductCmptTypeRelationReference) {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Image getImage(Object element) {
+        if (element instanceof IProductCmptReference) {
+            return ((IProductCmptReference)element).getProductCmpt().getImage();
+        } else if (element instanceof IProductCmptTypeRelationReference) {
             return ((IProductCmptTypeRelationReference)element).getRelation().getImage();
-        }
-        else if (element instanceof IProductCmptStructureTblUsageReference) {
+        } else if (element instanceof IProductCmptStructureTblUsageReference) {
             return ((IProductCmptStructureTblUsageReference)element).getTableContentUsage().getImage();
-        }   
-        else if (element instanceof ViewerLabel){
+        } else if (element instanceof ViewerLabel) {
             return ((ViewerLabel)element).getImage();
         }
-	    return IpsPlugin.getDefault().getImage(Messages.ProductStructureLabelProvider_undefined);
-	}
+        return IpsPlugin.getDefault().getImage(Messages.ProductStructureLabelProvider_undefined);
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public String getText(Object element) {
-		if (element instanceof IProductCmptReference) {
-            return generationRootNode.getProductCmptNoGenerationLabel(((IProductCmptReference)element).getProductCmpt());
-		}
-		else if (element instanceof IProductCmptTypeRelationReference) {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getText(Object element) {
+        if (element instanceof IProductCmptReference) {
+            return generationRootNode
+                    .getProductCmptNoGenerationLabel(((IProductCmptReference)element).getProductCmpt());
+        } else if (element instanceof IProductCmptTypeRelationReference) {
             IProductCmptTypeAssociation association = ((IProductCmptTypeRelationReference)element).getRelation();
-            // if the cardinality of the association is "toMany" then show the name (target role name) in plural
+            // if the cardinality of the association is "toMany" then show the name (target role
+            // name) in plural
             // otherwise show the default name, which normally is the singular target role name
-            if (association.is1ToMany()){
+            if (association.is1ToMany()) {
                 return association.getTargetRolePlural();
             } else {
                 return association.getName();
             }
-        }
-        else if (element instanceof IProductCmptStructureTblUsageReference) {
+        } else if (element instanceof IProductCmptStructureTblUsageReference) {
             ITableContentUsage tcu = ((IProductCmptStructureTblUsageReference)element).getTableContentUsage();
-            String tableUsageLabelText = showTableStructureUsageName?tcu.getStructureUsage() + ": ":""; //$NON-NLS-1$ //$NON-NLS-2$
-            return  StringUtils.capitalize(tableUsageLabelText) + StringUtil.unqualifiedName(tcu.getTableContentName()); //$NON-NLS-1$
-        }
-        else if (element instanceof ViewerLabel){
+            String tableUsageLabelText = showTableStructureUsageName ? tcu.getStructureUsage() + ": " : ""; //$NON-NLS-1$ //$NON-NLS-2$
+            return StringUtils.capitalize(tableUsageLabelText) + StringUtil.unqualifiedName(tcu.getTableContentName());
+        } else if (element instanceof ViewerLabel) {
             return ((ViewerLabel)element).getText();
         }
-		return Messages.ProductStructureLabelProvider_undefined;
-	}
+        return Messages.ProductStructureLabelProvider_undefined;
+    }
 
     public void setGenerationRootNode(GenerationRootNode generationRootNode) {
         this.generationRootNode = generationRootNode;
     }
 
     /**
-     * Definines if the table content usage role name will be displayed beside the referenced table content (<code>true</code>),
-     * or if the corresponding table structure usage name will be hidden (<code>false</code>).
+     * Definines if the table content usage role name will be displayed beside the referenced table
+     * content (<code>true</code>), or if the corresponding table structure usage name will be
+     * hidden (<code>false</code>).
      */
     public void setShowTableStructureUsageName(boolean showTableStructureUsageName) {
         this.showTableStructureUsageName = showTableStructureUsageName;
@@ -130,5 +95,5 @@ public class ProductStructureLabelProvider implements ILabelProvider {
      */
     public boolean isShowTableStructureUsageName() {
         return showTableStructureUsageName;
-    }    
+    }
 }
