79acb69f0b6b8f757cd7ddcb429cc6a2501d490b
ortmann2 <ortmann2>
2009-11-22 17:00:58 +0000

    *** empty log message ***

--- org.faktorips.build/cruisecontrol_config/build_crontab.sh
+++ org.faktorips.build/cruisecontrol_config/build_crontab.sh
@@ -6,6 +6,9 @@ CVS_ROOT=/usr/local/cvsroot
 CVS_CHECKOUT=/opt/cc/work/skel/
 CVS_CRUISE_CONTROL_SOURCE_LOCATION=org.faktorips.build/cruisecontrol_config/
 
+INPUT_FILES=(faktorips.projects.xml mailmapping.txt)
+OUTPUT_FILE=config.xml
+
 JAVA_HOME=/usr/lib/jvm/java-1.5.0-sun
 ANT_HOME=/opt/cc/apache-ant-1.6.5
 
@@ -16,10 +19,24 @@ cvsUpdate()
 
 generateCruiseControlConfig()
 {
-  $ANT_HOME/bin/ant -buildfile $CVS_CHECKOUT/generate.cc.config.xml generate.config.xml -Dskip.pretty.print=true -Dconfig.xml=config.xml
+  $ANT_HOME/bin/ant -buildfile $CVS_CHECKOUT/generate.cc.config.xml generate.config.xml -Dskip.pretty.print=true -Dconfig.xml=$OUTPUT_FILE
 }
 
 #the update of this folder will be done using crontab
 #cvsUpdate
 
-generateCruiseControlConfig
+CHANGE_FOUND="false"
+for file in ${INPUT_FILES[*]} ; do
+  echo "date check: $file"
+  if [ $CVS_CHECKOUT/$OUTPUT_FILE -ot $CVS_CHECKOUT/$file ] ; then
+    echo " -> change found"
+    CHANGE_FOUND=true
+  fi
+done
+
+if [ $CHANGE_FOUND = "true" ] ; then
+  generateCruiseControlConfig
+else
+  echo "no change found"
+fi
+
