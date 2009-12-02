99dbbec9c742bd89a97f9de5609fd3fc92f00c31
dirmeier <dirmeier>
2009-12-02 09:44:25 +0000

    FS#1701

--- org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/editors/enums/DeleteEnumValueAction.java
+++ org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/editors/enums/DeleteEnumValueAction.java
@@ -38,7 +38,7 @@ import org.faktorips.util.ArgumentCheck;
 public class DeleteEnumValueAction extends Action {
 
     /** The name of the image for the action. */
-    private final String IMAGE_NAME = "Delete.gif";
+    private final String IMAGE_NAME = "DeleteRow.gif";
 
     /** The table viewer linking the enumeration values UI table widget with the model data. */
     private TableViewer enumValuesTableViewer;
--- org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/editors/tablecontents/DeleteRowAction.java
+++ org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/editors/tablecontents/DeleteRowAction.java
@@ -3,7 +3,7 @@
  * 
  * Alle Rechte vorbehalten.
  * 
- * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen, 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
  * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
  * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
  * http://www.faktorzehn.org/f10-org:lizenzen:community eingesehen werden kann.
@@ -26,32 +26,32 @@ import org.faktorips.devtools.core.ui.actions.Messages;
  * @author Stefan Widmaier
  */
 public class DeleteRowAction extends IpsAction {
-    
+
     /**
      * The TableViewer this action operates in.
      */
     private TableViewer tableViewer;
-    
+
     /**
      * Creates an action that, when run, deletes the (first) selected row in the given
      * <code>TableViewer</code>.
      */
     public DeleteRowAction(TableViewer tableViewer, ContentPage page) {
         super(tableViewer);
-        this.tableViewer= tableViewer;
+        this.tableViewer = tableViewer;
         setControlWithDataChangeableSupport(page);
         setText(Messages.DeleteRowAction_Label);
         setToolTipText(Messages.DeleteRowAction_Tooltip);
-        setImageDescriptor(IpsUIPlugin.getDefault().getImageDescriptor("Delete.gif")); //$NON-NLS-1$
+        setImageDescriptor(IpsUIPlugin.getDefault().getImageDescriptor("DeleteRow.gif")); //$NON-NLS-1$
     }
-    
+
     /**
-     * Deletes the first selected row in the tableviewer and refreshes thereafter.
-     * {@inheritDoc}
+     * Deletes the first selected row in the tableviewer and refreshes thereafter. {@inheritDoc}
      */
+    @Override
     public void run(IStructuredSelection selection) {
-        Object selected= selection.getFirstElement();
-        if(selected instanceof IRow){
+        Object selected = selection.getFirstElement();
+        if (selected instanceof IRow) {
             ((IRow)selected).delete();
         }
         tableViewer.refresh(false);
