5977ad2bcef9e1dfa94bf70f67225a633dae646e
gitconvert <gitconvert>
2009-12-14 18:01:28 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.19.2'.

--- org.faktorips.devtools.htmlexport/src/org/faktorips/devtools/htmlexport/helper/filter/IpsObjectFilter.java
+++
@@ -1,7 +0,0 @@
-package org.faktorips.devtools.htmlexport.helper.filter;
-
-import org.faktorips.devtools.core.model.ipsobject.IIpsObject;
-
-public interface IpsObjectFilter {
-    public boolean accept(IIpsObject object);
-}
