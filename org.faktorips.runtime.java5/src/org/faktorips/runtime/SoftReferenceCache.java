f6512c6cafa911d230c9efcb8a1344ead526ff14
ortmann <ortmann>
2009-12-21 13:01:22 +0000

    fix for bug #1722, due to a bug in the SUN Compiler, see http://forums.sun.com/thread.jspa?threadID=5336141

--- org.faktorips.runtime.java5/src/org/faktorips/runtime/SoftReferenceCache.java
+++ org.faktorips.runtime.java5/src/org/faktorips/runtime/SoftReferenceCache.java
@@ -3,7 +3,7 @@
  * 
  * Alle Rechte vorbehalten.
  * 
- * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen, 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
  * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
  * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
  * http://www.faktorzehn.org/f10-org:lizenzen:community eingesehen werden kann.
@@ -21,31 +21,33 @@ import java.util.LinkedList;
 import java.util.Map;
 
 /**
- * Soft reference cache that uses a HashMap to cache objects. The objects will be releases by the 
+ * Soft reference cache that uses a HashMap to cache objects. The objects will be releases by the
  * garbage collector via soft references.
  * 
  * @author Joerg Ortmann
  */
-public class SoftReferenceCache implements ICache {
+public class SoftReferenceCache<T> implements ICache<T> {
     // the number of hard references to hold internally,
     // these last recent used entries are not removed by the garbage collector
     private final int HARD_REFERENCE_SIZE;
-    
+
     // the internal hash that holds all SoftReferences
-    private Map<Object, SoftValue> hash;
+    private Map<Object, SoftValue<T>> hash;
+
     // ReferenceQueue used to clear SoftReferences
-    private final ReferenceQueue<Object> queue = new ReferenceQueue<Object>();
-    
+    private final ReferenceQueue<T> queue = new ReferenceQueue<T>();
+
     // the list of hard references, order of last access
     private final LinkedList<Object> hardCache = new LinkedList<Object>();
-    
+
     /*
-     * Inner subclass of SoftReference which contains additional the
-     * key to make it easier to find the entry in the map, after it has been garbage collected
+     * Inner subclass of SoftReference which contains additional the key to make it easier to find
+     * the entry in the map, after it has been garbage collected
      */
-    private static class SoftValue extends SoftReference<Object> {
+    private static class SoftValue<T> extends SoftReference<T> {
         private final Object key;
-        private SoftValue(Object referent, Object key, ReferenceQueue<Object> q){
+
+        private SoftValue(T referent, Object key, ReferenceQueue<T> q) {
             super(referent, q);
             this.key = key;
         }
@@ -55,22 +57,22 @@ public class SoftReferenceCache implements ICache {
      * Created a new SoftReferencesCache with the given initial size.
      */
     public SoftReferenceCache(int initialSize) {
-        hash = new HashMap<Object, SoftValue>(initialSize);
+        hash = new HashMap<Object, SoftValue<T>>(initialSize);
         this.HARD_REFERENCE_SIZE = 0;
     }
-    
+
     /**
      * Creates a new SoftReferencesCache with the given initial size and hard reference size. The
      * hard reference size ensures that the last recent entries are not removed by the garbage
      * collector, thus all other entries in the cache will be removed first.<br>
      * If the hard refence size is 0 then no hard references are stored, and therefore all entries
-     * could be removed by the garbage collector in undefined order. If the size is -1 then the 
+     * could be removed by the garbage collector in undefined order. If the size is -1 then the
      * garbage collector never removes entries from the cache. If the size is greater than 0 then
-     * this size of entries will be stored as hard reference (order of last access) 
-     * and never deleted by the garbage collector
+     * this size of entries will be stored as hard reference (order of last access) and never
+     * deleted by the garbage collector
      */
     public SoftReferenceCache(int initialSize, int hardReferenceSize) {
-        hash = new HashMap<Object, SoftValue>(initialSize);
+        hash = new HashMap<Object, SoftValue<T>>(initialSize);
         this.HARD_REFERENCE_SIZE = hardReferenceSize;
     }
 
@@ -78,12 +80,13 @@ public class SoftReferenceCache implements ICache {
      * Go through the ReferenceQueue and remove garbage collected SoftValue objects.
      */
     private void processQueue() {
-        SoftValue sv;
-        while ((sv = (SoftValue)queue.poll()) != null){
+        Reference<? extends T> ref = queue.poll();
+        while ((ref = queue.poll()) != null) {
+            SoftValue sv = (SoftValue)ref;
             hash.remove(sv.key);
         }
     }
-    
+
     /**
      * {@inheritDoc}
      */
@@ -96,9 +99,9 @@ public class SoftReferenceCache implements ICache {
     /**
      * {@inheritDoc}
      */
-    public Object getObject(Object key) {
-        Object result = null;
-        Reference<Object> ref = hash.get(key);
+    public T getObject(Object key) {
+        T result = null;
+        Reference<T> ref = hash.get(key);
         if (ref != null) {
             // check if the value from the soft reference isn't null
             result = ref.get();
@@ -106,7 +109,7 @@ public class SoftReferenceCache implements ICache {
                 // the value has been garbage collected, remove the entry from the map
                 hash.remove(key);
             } else {
-                if (HARD_REFERENCE_SIZE != 0){
+                if (HARD_REFERENCE_SIZE != 0) {
                     hardCache.addFirst(result);
                     if (HARD_REFERENCE_SIZE > 0 && hardCache.size() > HARD_REFERENCE_SIZE) {
                         hardCache.removeLast();
@@ -116,12 +119,12 @@ public class SoftReferenceCache implements ICache {
         }
         return result;
     }
-    
+
     /**
      * {@inheritDoc}
      */
-    public void put(Object key, Object value) {
+    public void put(Object key, T value) {
         processQueue();
-        hash.put(key, new SoftValue(value, key, queue));
+        hash.put(key, new SoftValue<T>(value, key, queue));
     }
 }
