1ca2e31177100bd72fabcf9794c73c5a5fe61c17
ortmann <ortmann>
2009-12-30 14:51:05 +0000

    Verwendung von typsicheren Collections

--- org.faktorips.devtools.core/src/org/faktorips/devtools/core/internal/model/ValidationResultCache.java
+++ org.faktorips.devtools.core/src/org/faktorips/devtools/core/internal/model/ValidationResultCache.java
@@ -3,7 +3,7 @@
  * 
  * Alle Rechte vorbehalten.
  * 
- * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen, 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
  * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
  * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
  * http://www.faktorzehn.org/f10-org:lizenzen:community eingesehen werden kann.
@@ -26,65 +26,65 @@ import org.faktorips.util.message.MessageList;
  */
 public class ValidationResultCache {
 
-	private HashMap data = new HashMap(1000);
-	
-	public ValidationResultCache() {
-		super();
-	}
-	
-	/**
-	 * Puts a copy of the given the validation result for the given ips object part container 
-	 * into the chache. Overwrittes any old data for the given container. If result is <code>null</code>,
-	 * any cached data for the container is removed. 
-	 * 
-	 * @param container  The container to that the result belongs
-	 * @param result	 The validation result to put into the cache.
-	 * 
-	 * @throws NullPointerException if container is <code>null</code>.
-	 */
-	synchronized public void putResult(IIpsObjectPartContainer container, MessageList result) {
-		if (result==null) {
-			data.remove(container);
-			return;
-		}
-		// cache a defensive copy
-		MessageList copy = new MessageList(); 
-		copy.add(result);
-		data.put(container, copy);
-	}
+    private HashMap<IIpsObjectPartContainer, MessageList> data = new HashMap<IIpsObjectPartContainer, MessageList>(1000);
 
-	/**
-	 * Returns the cached validation result for the given container or <code>null</code>
-	 * if the cache does not contain a result for the container.
-	 */
+    public ValidationResultCache() {
+        super();
+    }
+
+    /**
+     * Puts a copy of the given the validation result for the given ips object part container into
+     * the chache. Overwrittes any old data for the given container. If result is <code>null</code>,
+     * any cached data for the container is removed.
+     * 
+     * @param container The container to that the result belongs
+     * @param result The validation result to put into the cache.
+     * 
+     * @throws NullPointerException if container is <code>null</code>.
+     */
+    synchronized public void putResult(IIpsObjectPartContainer container, MessageList result) {
+        if (result == null) {
+            data.remove(container);
+            return;
+        }
+        // cache a defensive copy
+        MessageList copy = new MessageList();
+        copy.add(result);
+        data.put(container, copy);
+    }
+
+    /**
+     * Returns the cached validation result for the given container or <code>null</code> if the
+     * cache does not contain a result for the container.
+     */
     synchronized public MessageList getResult(IIpsObjectPartContainer c) {
-		MessageList cached = (MessageList )data.get(c);
-		if (cached==null) {
-			return null;
-		}
-		// return a defensive copy
-		MessageList result = new MessageList(); 
-		result.add(cached);
-		return result;
-	}
-	
-	/**
-	 * Removes the data from the cache that is stale because the given ips source file
-	 * has changed. Does nothing if the given file is <code>null</code>.
-	 * <p>
-	 * Implementation note: At the moment we clear the whole cache if a file
-	 * changes as due to the dependencies between objects the validation 
-	 * result of other objects can also change if one object is changed. We might use
-	 * the exact dependencies between objects to solve this more efficiently.  
-	 */
+        MessageList cached = data.get(c);
+        if (cached == null) {
+            return null;
+        }
+        // return a defensive copy
+        MessageList result = new MessageList();
+        result.add(cached);
+        return result;
+    }
+
+    /**
+     * Removes the data from the cache that is stale because the given ips source file has changed.
+     * Does nothing if the given file is <code>null</code>.
+     * <p>
+     * Implementation note: At the moment we clear the whole cache if a file changes as due to the
+     * dependencies between objects the validation result of other objects can also change if one
+     * object is changed. We might use the exact dependencies between objects to solve this more
+     * efficiently.
+     */
     synchronized public void removeStaleData(IIpsSrcFile file) {
         data.clear();
-	}
+    }
 
-	/**
-	 * Clears the whole cache.
-	 */
-    synchronized public void clear(){
-		data.clear();
-	}
+    /**
+     * Clears the whole cache.
+     */
+    synchronized public void clear() {
+        data.clear();
+    }
 }
