d3d43d835d4c04a986896aee29c7b1b337e43314
gitconvert <gitconvert>
2010-01-07 08:31:29 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.21.2'.

--- org.faktorips.devtools.htmlexport/src/java/org/faktorips/devtools/htmlexport/pages/elements/TextType.java
+++
@@ -1,5 +0,0 @@
-package org.faktorips.devtools.htmlexport.pages.elements;
-
-public enum TextType {
-    HEADING_1, HEADING_2, HEADING_3, HEADING_4, HEADING_5, HEADING_6, BLOCK, INLINE, WITHOUT_TYPE
-}
