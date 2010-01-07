8b1599fc99aa749d2a18f85010f3d87f18b134ea
gitconvert <gitconvert>
2010-01-07 08:31:41 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.9.2'.

--- org.faktorips.devtools.htmlexport/src/java/org/faktorips/devtools/htmlexport/pages/helper/PageElementCreator.java
+++
@@ -1,7 +0,0 @@
-package org.faktorips.devtools.htmlexport.pages.helper;
-
-import org.faktorips.devtools.htmlexport.pages.elements.PageElement;
-
-public interface PageElementCreator {
-    public PageElement create(Object object);
-}
