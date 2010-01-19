591063bda3e6f10aa79c75ffc210ce12da88e99f
dirmeier <dirmeier>
2010-01-19 09:34:01 +0000

    fixed Strg-Shift Bug

--- org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/LinkDropListener.java
+++ org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/LinkDropListener.java
@@ -74,7 +74,7 @@ public class LinkDropListener extends ViewerDropAdapter {
 
     @Override
     public void dragEnter(DropTargetEvent event) {
-        if (event.detail == DND.DROP_DEFAULT) {
+        if ((event.detail & DND.DROP_DEFAULT) == DND.DROP_DEFAULT || (event.detail & DND.DROP_NONE) == DND.DROP_NONE) {
             event.detail = DND.DROP_LINK;
         }
         super.dragEnter(event);
@@ -82,9 +82,6 @@ public class LinkDropListener extends ViewerDropAdapter {
 
     @Override
     public boolean validateDrop(Object target, int operation, TransferData transferType) {
-        if (operation != DND.DROP_LINK) {
-            return false;
-        }
         List<IProductCmpt> draggedCmpts = getTransferElements(transferType);
         if (draggedCmpts == null) {
             return false;
@@ -119,6 +116,9 @@ public class LinkDropListener extends ViewerDropAdapter {
 
     @Override
     public boolean performDrop(Object data) {
+        if (getCurrentOperation() != DND.DROP_LINK) {
+            return false;
+        }
         if (!(data instanceof String[])) {
             return false;
         }
