5d76a7e8901bb59c242a5a70ceb0c4e58bdb1877
gitconvert <gitconvert>
2010-02-02 12:08:16 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.113.2'.

--- org.faktorips.devtools.htmlexport/src/java/org/faktorips/devtools/htmlexport/pages/elements/core/ICompositePageElement.java
+++
@@ -1,7 +0,0 @@
-package org.faktorips.devtools.htmlexport.pages.elements.core;
-
-import org.faktorips.devtools.htmlexport.generators.ILayouter;
-
-public interface ICompositePageElement {
-    public void visitSubElements(ILayouter layouter);
-}
