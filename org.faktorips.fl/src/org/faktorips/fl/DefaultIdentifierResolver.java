4398d3c97e32ac9f934a51e82fbe074e1d90e78f
ortmann2 <ortmann2>
2010-02-04 09:51:50 +0000

    FS#1732 fix Enumwerte nicht als Formel Parameter

--- org.faktorips.fl/src/org/faktorips/fl/DefaultIdentifierResolver.java
+++ org.faktorips.fl/src/org/faktorips/fl/DefaultIdentifierResolver.java
@@ -3,7 +3,7 @@
  * 
  * Alle Rechte vorbehalten.
  * 
- * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen, 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
  * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
  * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
  * http://www.faktorzehn.org/f10-org:lizenzen:community eingesehen werden kann.
@@ -21,29 +21,27 @@ import org.faktorips.codegen.JavaCodeFragment;
 import org.faktorips.datatype.Datatype;
 import org.faktorips.util.message.Message;
 
-
 /**
- * A default implementation of <code>IdentifierResolver</code> that allows to register
- * constant strings as identifiers together with a JavaCodeFragment and Datatype
- * that are used to create a <code>CompilationResult</code> if the resolver is
- * requested to compile the identifier.
+ * A default implementation of <code>IdentifierResolver</code> that allows to register constant
+ * strings as identifiers together with a JavaCodeFragment and Datatype that are used to create a
+ * <code>CompilationResult</code> if the resolver is requested to compile the identifier.
  */
 public class DefaultIdentifierResolver implements IdentifierResolver {
-    
-    // map with (String) identifiers as keys and FragmentDatatypeWrapper instances as values. 
+
+    // map with (String) identifiers as keys and FragmentDatatypeWrapper instances as values.
     private Map identifiers = new HashMap();
 
     /**
-     * Creates a new resolver. 
+     * Creates a new resolver.
      */
     public DefaultIdentifierResolver() {
     }
-    
+
     /**
      * Register the identifier.
      */
     public void register(String identifier, JavaCodeFragment fragment, Datatype datatype) {
-        JavaCodeFragment defensiveCopy = new JavaCodeFragment(fragment); 
+        JavaCodeFragment defensiveCopy = new JavaCodeFragment(fragment);
         identifiers.put(identifier, new FragmentDatatypeWrapper(defensiveCopy, datatype));
     }
 
@@ -52,20 +50,35 @@ public class DefaultIdentifierResolver implements IdentifierResolver {
      */
     public CompilationResult compile(String identifier, ExprCompiler exprCompiler, Locale locale) {
         FragmentDatatypeWrapper wrapper = (FragmentDatatypeWrapper)identifiers.get(identifier);
-        if (wrapper!=null) {
-            return new CompilationResultImpl(new JavaCodeFragment(wrapper.fragment), wrapper.datatype);
-        } 
+        if (wrapper != null) {
+            CompilationResultImpl compilationResult = new CompilationResultImpl(new JavaCodeFragment(wrapper.fragment),
+                    wrapper.datatype);
+            addCurrentIdentifer(compilationResult, identifier);
+            return compilationResult;
+        }
         String text = ExprCompiler.localizedStrings.getString(ExprCompiler.UNDEFINED_IDENTIFIER, locale, identifier);
-        return new CompilationResultImpl(Message.newError(ExprCompiler.UNDEFINED_IDENTIFIER, text));
+        CompilationResultImpl compilationResult = new CompilationResultImpl(Message.newError(
+                ExprCompiler.UNDEFINED_IDENTIFIER, text));
+        addCurrentIdentifer(compilationResult, identifier);
+        return compilationResult;
     }
-    
+
+    /*
+     * Adds the given identifier candidate to the compilation result
+     */
+    private void addCurrentIdentifer(CompilationResult result, String identifierCandidate) {
+        if (result instanceof CompilationResultImpl) {
+            ((CompilationResultImpl)result).addIdentifierUsed(identifierCandidate);
+        }
+    }
+
     static class FragmentDatatypeWrapper {
-        
+
         FragmentDatatypeWrapper(JavaCodeFragment fragment, Datatype datatype) {
             this.fragment = fragment;
             this.datatype = datatype;
         }
-        
+
         private JavaCodeFragment fragment;
         private Datatype datatype;
     }
--- org.faktorips.fl/test/org/faktorips/fl/functions/IfTest.java
+++ org.faktorips.fl/test/org/faktorips/fl/functions/IfTest.java
@@ -3,7 +3,7 @@
  * 
  * Alle Rechte vorbehalten.
  * 
- * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen, 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
  * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
  * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
  * http://www.faktorzehn.org/f10-org:lizenzen:community eingesehen werden kann.
@@ -28,12 +28,13 @@ import org.faktorips.values.Money;
  *
  */
 public class IfTest extends FunctionAbstractTest {
-    
+
+    @Override
     protected void setUp() throws Exception {
         super.setUp();
         registerFunction(new If("IF", ""));
     }
-    
+
     public void testBooleanCondition() throws Exception {
         registerFunction(new BooleanFct("TRUEOBJ", Boolean.TRUE));
         registerFunction(new BooleanFct("FALSEOBJ", Boolean.FALSE));
@@ -41,7 +42,7 @@ public class IfTest extends FunctionAbstractTest {
         execAndTestSuccessfull("IF(TRUEOBJ(); 2; 3)", new Integer(2), Datatype.INTEGER);
         execAndTestSuccessfull("IF(FALSEOBJ(); 2; 3)", new Integer(3), Datatype.INTEGER);
     }
-    
+
     public void testDifferentDatatypes() throws Exception {
         execAndTestFail("IF(1=2; 10EUR; 1)", If.ERROR_MESSAGE_CODE);
     }
@@ -50,56 +51,58 @@ public class IfTest extends FunctionAbstractTest {
         execAndTestSuccessfull("IF(1=1; 2.1; 3.2)", Decimal.valueOf("2.1"), Datatype.DECIMAL);
         execAndTestSuccessfull("IF(1=2; 2.1; 3.2)", Decimal.valueOf("3.2"), Datatype.DECIMAL);
     }
-    
+
     public void testDecimalInt() throws Exception {
         execAndTestSuccessfull("IF(1=1; 2.1; 3)", Decimal.valueOf("2.1"), Datatype.DECIMAL);
         execAndTestSuccessfull("IF(1=2; 2.1; 3)", Decimal.valueOf("3"), Datatype.DECIMAL);
     }
-    
+
     public void testIntDecimal() throws Exception {
         execAndTestSuccessfull("IF(1=1; 2; 3.1)", Decimal.valueOf("2"), Datatype.DECIMAL);
         execAndTestSuccessfull("IF(1=2; 2; 3.1)", Decimal.valueOf("3.1"), Datatype.DECIMAL);
     }
-    
+
     public void testInt() throws Exception {
         execAndTestSuccessfull("IF(1=1; 2; 3)", new Integer(2), Datatype.INTEGER);
         execAndTestSuccessfull("IF(1=2; 2; 3)", new Integer(3), Datatype.INTEGER);
     }
-    
+
     public void testInteger() throws Exception {
         execAndTestSuccessfull("IF(1=1; 2; 3)", new Integer(2), Datatype.INTEGER);
         execAndTestSuccessfull("IF(1=2; 2; 3)", new Integer(3), Datatype.INTEGER);
     }
-    
+
     public void testMoney() throws Exception {
         execAndTestSuccessfull("IF(1=1; 10EUR; 20EUR)", Money.euro(10, 0), Datatype.MONEY);
         execAndTestSuccessfull("IF(1=2; 10EUR; 20EUR)", Money.euro(20, 0), Datatype.MONEY);
     }
-    
+
     public void testString() throws Exception {
         execAndTestSuccessfull("IF(1=1; \"a\"; \"b\")", "a", Datatype.STRING);
         execAndTestSuccessfull("IF(1=2; \"a\"; \"b\")", "b", Datatype.STRING);
     }
-    
+
     public void testCombinations() throws Exception {
         execAndTestSuccessfull("IF(1=1; 2; 3) + 1", new Integer(3), Datatype.INTEGER);
         execAndTestSuccessfull("IF(1=1; 2.1 + 1; 3) + 2", Decimal.valueOf("5.1"), Datatype.DECIMAL);
         execAndTestSuccessfull("IF(1=1; 2; 3) + 10 + IF(1=2; 2; 3) ", new Integer(15), Datatype.INTEGER);
         execAndTestSuccessfull("IF(1=1; IF(1=2; 2; 30); 3) + 1", new Integer(31), Datatype.INTEGER);
     }
-    
-    public void testIdentifierInFunction() throws Exception{
+
+    public void testIdentifierInFunction() throws Exception {
         DefaultIdentifierResolver resolver = new DefaultIdentifierResolver();
         compiler.setIdentifierResolver(resolver);
         resolver.register("param1", new JavaCodeFragment(StringEscapeUtils.escapeJava("1")), Datatype.PRIMITIVE_INT);
         resolver.register("param2", new JavaCodeFragment(StringEscapeUtils.escapeJava("2")), Datatype.PRIMITIVE_INT);
         resolver.register("param3", new JavaCodeFragment(StringEscapeUtils.escapeJava("3")), Datatype.PRIMITIVE_INT);
         resolver.register("param4", new JavaCodeFragment(StringEscapeUtils.escapeJava("4")), Datatype.PRIMITIVE_INT);
-        CompilationResult result = execAndTestSuccessfull("IF(param1=param2; param3; param4)", new Integer(4), Datatype.INTEGER);
+        CompilationResult result = execAndTestSuccessfull("IF(param1=param2; param3; param4)", new Integer(4),
+                Datatype.INTEGER);
         List identifier = Arrays.asList(result.getResolvedIdentifiers());
+        assertEquals(4, identifier.size());
         assertTrue(identifier.contains("param1"));
         assertTrue(identifier.contains("param2"));
         assertTrue(identifier.contains("param3"));
-        assertTrue(identifier.contains("param4"));        
+        assertTrue(identifier.contains("param4"));
     }
 }
