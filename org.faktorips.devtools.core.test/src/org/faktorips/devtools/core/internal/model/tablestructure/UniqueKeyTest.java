cca154cf4cff30392a76a516b990de30aade3669
dirmeier <dirmeier>
2010-02-11 12:27:44 +0000

    FS#1781 refactored IpsObjectPart-ID to String

--- org.faktorips.devtools.core.test/src/org/faktorips/devtools/core/internal/model/tablestructure/UniqueKeyTest.java
+++ org.faktorips.devtools.core.test/src/org/faktorips/devtools/core/internal/model/tablestructure/UniqueKeyTest.java
@@ -3,7 +3,7 @@
  * 
  * Alle Rechte vorbehalten.
  * 
- * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen, 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
  * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
  * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
  * http://www.faktorzehn.org/f10-org:lizenzen:community eingesehen werden kann.
@@ -26,7 +26,6 @@ import org.faktorips.devtools.core.model.tablestructure.IKeyItem;
 import org.faktorips.util.message.MessageList;
 import org.w3c.dom.Element;
 
-
 /**
  *
  */
@@ -36,7 +35,8 @@ public class UniqueKeyTest extends AbstractIpsPluginTest {
     private IIpsSrcFile ipsSrcFile;
     private TableStructure table;
     private UniqueKey key;
-    
+
+    @Override
     protected void setUp() throws Exception {
         super.setUp();
         project = newIpsProject();
@@ -45,24 +45,24 @@ public class UniqueKeyTest extends AbstractIpsPluginTest {
         key = (UniqueKey)table.newUniqueKey();
         ipsSrcFile.save(true, null);
     }
-    
+
     public void testRemove() {
         key.delete();
         assertEquals(0, table.getNumOfUniqueKeys());
         assertTrue(ipsSrcFile.isDirty());
     }
-    
+
     public void testGetName() {
         assertEquals("", key.getName());
-        key.setKeyItems(new String[] {"age"});
+        key.setKeyItems(new String[] { "age" });
         assertEquals("age", key.getName());
-        key.setKeyItems(new String[] {"age", "gender"});
+        key.setKeyItems(new String[] { "age", "gender" });
         assertEquals("age, gender", key.getName());
     }
 
     public void testGetKeyItemNames() {
         assertEquals(0, key.getKeyItemNames().length);
-        String[] items = new String[] {"age", "gender"};
+        String[] items = new String[] { "age", "gender" };
         key.setKeyItems(items);
         assertNotSame(items, key.getKeyItemNames()); // defensive copy should be made
         assertEquals(2, key.getKeyItemNames().length);
@@ -78,8 +78,8 @@ public class UniqueKeyTest extends AbstractIpsPluginTest {
         c1.setName("c1");
         IColumnRange range = table.newRange();
         range.setColumnRangeType(ColumnRangeType.ONE_COLUMN_RANGE_FROM);
-        
-        key.setKeyItems(new String[] {"c0", range.getName(), "unknown"});
+
+        key.setKeyItems(new String[] { "c0", range.getName(), "unknown" });
         IKeyItem[] keyItems = key.getKeyItems();
         assertEquals(2, keyItems.length);
         assertEquals(c0, keyItems[0]);
@@ -87,95 +87,95 @@ public class UniqueKeyTest extends AbstractIpsPluginTest {
     }
 
     public void testSetKeyItems() {
-        String[] items = new String[] {"age", "gender"};
+        String[] items = new String[] { "age", "gender" };
         key.setKeyItems(items);
         assertTrue(ipsSrcFile.isDirty());
     }
 
-    public void testGetKeyItemAt(){
+    public void testGetKeyItemAt() {
         IColumn c0 = table.newColumn();
         c0.setName("c0");
         IColumn c1 = table.newColumn();
         c1.setName("c1");
         IColumn c2 = table.newColumn();
         c2.setName("c2");
-        key.setKeyItems(new String[] {"c0", "c1"});
+        key.setKeyItems(new String[] { "c0", "c1" });
         IKeyItem[] keyItems = key.getKeyItems();
         assertEquals(2, keyItems.length);
         IKeyItem item = key.getKeyItemAt(1);
         assertEquals("c1", item.getName());
-        
-        try{
+
+        try {
             key.getKeyItemAt(2);
             fail();
-        } catch(IndexOutOfBoundsException e){
-            //do nothing
+        } catch (IndexOutOfBoundsException e) {
+            // do nothing
         }
-        
+
     }
-    
-    public void testGetNameOfKeyItemAt(){
+
+    public void testGetNameOfKeyItemAt() {
         IColumn c0 = table.newColumn();
         c0.setName("c0");
         IColumn c1 = table.newColumn();
         c1.setName("c1");
         IColumn c2 = table.newColumn();
         c2.setName("c2");
-        key.setKeyItems(new String[] {"c0", "c1"});
+        key.setKeyItems(new String[] { "c0", "c1" });
         String itemName = key.getNameOfKeyItemAt(1);
         assertEquals("c1", itemName);
-        
-        try{
+
+        try {
             key.getNameOfKeyItemAt(3);
             fail();
-        } catch(IndexOutOfBoundsException e){
-            //do nothing
+        } catch (IndexOutOfBoundsException e) {
+            // do nothing
         }
-        
+
     }
-    
-    public void testGetIndexForKeyItem() throws Exception{
+
+    public void testGetIndexForKeyItem() throws Exception {
         IColumn c0 = table.newColumn();
         c0.setName("c0");
         IColumn c1 = table.newColumn();
         c1.setName("c1");
         IColumn c2 = table.newColumn();
         c2.setName("c2");
-        key.setKeyItems(new String[] {"c0", "c1"});
+        key.setKeyItems(new String[] { "c0", "c1" });
         int index = key.getIndexForKeyItem(key.getKeyItems()[1]);
         assertEquals(1, index);
-        
+
         TableStructure table2 = (TableStructure)newIpsObject(project, IpsObjectType.TABLE_STRUCTURE, "TestTable2");
         IColumn column = table2.newColumn();
         column.setName("t2c0");
-        try{
+        try {
             key.getIndexForKeyItem(column);
             fail();
-        } catch(IllegalArgumentException e){
-            //do nothing
+        } catch (IllegalArgumentException e) {
+            // do nothing
         }
-        
+
     }
-    
-    public void testGetIndexForKeyItemName(){
+
+    public void testGetIndexForKeyItemName() {
         IColumn c0 = table.newColumn();
         c0.setName("c0");
         IColumn c1 = table.newColumn();
         c1.setName("c1");
         IColumn c2 = table.newColumn();
         c2.setName("c2");
-        key.setKeyItems(new String[] {"c0", "c1"});
+        key.setKeyItems(new String[] { "c0", "c1" });
         int index = key.getIndexForKeyItemName("c1");
         assertEquals(1, index);
-        
-        try{
+
+        try {
             key.getIndexForKeyItemName("c3");
             fail();
-        } catch(IllegalArgumentException e){
-            //do nothing
+        } catch (IllegalArgumentException e) {
+            // do nothing
         }
     }
-    
+
     public void testGetItemCandidates() {
         assertEquals(0, key.getItemCandidates().length);
         IColumn columnGender = table.newColumn();
@@ -185,13 +185,13 @@ public class UniqueKeyTest extends AbstractIpsPluginTest {
         IColumnRange range = table.newRange();
         range.setFromColumn("ageFrom");
         range.setToColumn("ageTo");
-        
+
         assertEquals(3, key.getItemCandidates().length);
         assertEquals(columnGender, key.getItemCandidates()[0]);
         assertEquals(columnSmoker, key.getItemCandidates()[1]);
         assertEquals(range, key.getItemCandidates()[2]);
-        
-        String[] items = new String[] {"gender", range.getName()};
+
+        String[] items = new String[] { "gender", range.getName() };
         key.setKeyItems(items);
         assertEquals(1, key.getItemCandidates().length);
         assertEquals(columnSmoker, key.getItemCandidates()[0]);
@@ -199,12 +199,12 @@ public class UniqueKeyTest extends AbstractIpsPluginTest {
 
     public void testToXml() {
         key = (UniqueKey)table.newUniqueKey();
-        String[] items = new String[] {"age", "gender"};
+        String[] items = new String[] { "age", "gender" };
         key.setKeyItems(items);
         Element element = key.toXml(newDocument());
         UniqueKey copy = new UniqueKey();
         copy.initFromXml(element);
-        assertEquals(1, copy.getId());
+        assertEquals(key.getId(), copy.getId());
         assertEquals(2, copy.getNumOfKeyItems());
         assertEquals("age", copy.getKeyItemNames()[0]);
         assertEquals("gender", copy.getKeyItemNames()[1]);
@@ -212,30 +212,31 @@ public class UniqueKeyTest extends AbstractIpsPluginTest {
 
     public void testInitFromXml() {
         key.initFromXml(getTestDocument().getDocumentElement());
-        assertEquals(42, key.getId());
+        assertEquals("42", key.getId());
         assertEquals(2, key.getNumOfKeyItems());
         assertEquals("age", key.getKeyItemNames()[0]);
         assertEquals("gender", key.getKeyItemNames()[1]);
     }
 
     /**
-     * Tests for the correct type of exception to be thrown - no part of any type could ever be created.
+     * Tests for the correct type of exception to be thrown - no part of any type could ever be
+     * created.
      */
     public void testNewPart() {
-    	try {
-			key.newPart(IPolicyCmptTypeAttribute.class);
-			fail();
-		} catch (IllegalArgumentException e) {
-			//nothing to do :-)
-		}
+        try {
+            key.newPart(IPolicyCmptTypeAttribute.class);
+            fail();
+        } catch (IllegalArgumentException e) {
+            // nothing to do :-)
+        }
     }
-    
-    public void testContainsTwoColumnRanges(){
+
+    public void testContainsTwoColumnRanges() {
         key.getTableStructure().newColumn().setName("a");
         key.getTableStructure().newColumn().setName("b");
-        
+
         assertFalse(key.containsTwoColumnRanges());
-        
+
         IColumnRange range = key.getTableStructure().newRange();
         range.setColumnRangeType(ColumnRangeType.ONE_COLUMN_RANGE_FROM);
         range.setFromColumn("a");
@@ -247,12 +248,12 @@ public class UniqueKeyTest extends AbstractIpsPluginTest {
         range.setToColumn("b");
         key.addKeyItem(range.getName());
         assertTrue(key.containsTwoColumnRanges());
-        
+
         key.removeKeyItem(range.getName());
         assertFalse(key.containsTwoColumnRanges());
     }
-    
-    public void testValidateThis() throws Exception{
+
+    public void testValidateThis() throws Exception {
         assertEquals(0, key.getKeyItems().length);
         IColumn c0 = table.newColumn();
         c0.setName("c0");
@@ -263,18 +264,18 @@ public class UniqueKeyTest extends AbstractIpsPluginTest {
         IColumnRange range = table.newRange();
         range.setFromColumn(c2.getName());
         range.setColumnRangeType(ColumnRangeType.ONE_COLUMN_RANGE_FROM);
-        key.setKeyItems(new String[] {"c0", range.getName()});
-        MessageList msgList = key.validate(project); 
+        key.setKeyItems(new String[] { "c0", range.getName() });
+        MessageList msgList = key.validate(project);
         assertTrue(msgList.isEmpty());
-        
-        key.setKeyItems(new String[] {range.getName(), "c0"});
+
+        key.setKeyItems(new String[] { range.getName(), "c0" });
         msgList = key.validate(project);
         assertFalse(msgList.isEmpty());
         msgList = msgList.getMessagesFor(key, IKey.PROPERTY_KEY_ITEMS);
         assertFalse(msgList.isEmpty());
-        
+
         // test correct index (column) of message
-        key.setKeyItems(new String[] {"c0", range.getName(), "c1"});
+        key.setKeyItems(new String[] { "c0", range.getName(), "c1" });
         msgList = key.validate(project);
         assertFalse(msgList.isEmpty());
         msgList = msgList.getMessagesFor(key, IKey.PROPERTY_KEY_ITEMS, 1);
