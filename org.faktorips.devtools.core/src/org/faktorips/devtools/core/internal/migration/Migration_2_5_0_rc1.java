72c08a7da4062c9220480d5fb53748b9a3d6fbd1
ortmann2 <ortmann2>
2010-02-19 14:46:40 +0000

    M#62 - Umstellung Modellierung und Codegenerierung fuer Composites

---
+++ org.faktorips.devtools.core/src/org/faktorips/devtools/core/internal/migration/Migration_2_5_0_rc1.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * Copyright (c) 2005-2009 Faktor Zehn AG und andere.
+ * 
+ * Alle Rechte vorbehalten.
+ * 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
+ * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
+ * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
+ * http://www.faktorzehn.org/f10-org:lizenzen:community eingesehen werden kann.
+ * 
+ * Mitwirkende: Faktor Zehn AG - initial API and implementation - http://www.faktorzehn.de
+ *******************************************************************************/
+
+package org.faktorips.devtools.core.internal.migration;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.faktorips.devtools.core.model.ipsproject.IIpsProject;
+import org.faktorips.devtools.core.model.versionmanager.AbstractIpsProjectMigrationOperation;
+import org.faktorips.util.message.MessageList;
+
+/**
+ * Migration from version 2.5.0.rc1 to version 2.6.0.rc1
+ * 
+ * @author Peter Kuntz
+ */
+public class Migration_2_5_0_rc1 extends AbstractIpsProjectMigrationOperation {
+
+    public Migration_2_5_0_rc1(IIpsProject projectToMigrate, String featureId) {
+        super(projectToMigrate, featureId);
+    }
+
+    @Override
+    public String getDescription() {
+        return "WARNING do not run this migration: in progress, executing this migration will result in a runtime exception!\n"
+                + "The modeling of composition has changed.\n"
+                + "Now the child model objects becomes a concrete parent model object variable for each parent they belongs to";
+    }
+
+    @Override
+    public String getTargetVersion() {
+        return "2.6.0.rc1"; //$NON-NLS-1$
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public MessageList migrate(IProgressMonitor monitor) throws CoreException, InvocationTargetException,
+            InterruptedException {
+        throw new RuntimeException("TODO: not yet implemented!");
+    }
+}
