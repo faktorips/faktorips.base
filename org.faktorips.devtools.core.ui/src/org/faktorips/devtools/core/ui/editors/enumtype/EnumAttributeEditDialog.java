81ee07d5fceb8b2c8018df89cb7505975e512959
weickmann <weickmann>
2010-03-01 21:09:33 +0000

    *** empty log message ***

--- org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/editors/enumtype/EnumAttributeEditDialog.java
+++ org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/editors/enumtype/EnumAttributeEditDialog.java
@@ -298,7 +298,6 @@ public class EnumAttributeEditDialog extends IpsPartEditDialog2 {
             bindingContext.bindEnabled(datatypeControl, enumAttribute, IEnumAttribute.PROPERTY_INHERITED, false);
             bindingContext.bindEnabled(uniqueCheckbox, enumAttribute, IEnumAttribute.PROPERTY_INHERITED, false);
             bindingContext.bindEnabled(identifierCheckbox, enumAttribute, IEnumAttribute.PROPERTY_INHERITED, false);
-            bindingContext.bindEnabled(displayNameCheckbox, enumAttribute, IEnumAttribute.PROPERTY_INHERITED, false);
 
             // Obtain the properties from the super EnumAttribute.
             try {
