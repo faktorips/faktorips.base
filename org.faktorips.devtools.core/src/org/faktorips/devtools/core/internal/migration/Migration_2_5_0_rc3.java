f63d16713a8b4e30722c1431f29ceacc13ecf824
dirmeier <dirmeier>
2010-03-15 10:25:14 +0000

    migration 2.5.0.rc3 --> 2.5.0.rfinal

---
+++ org.faktorips.devtools.core/src/org/faktorips/devtools/core/internal/migration/Migration_2_5_0_rc3.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * Copyright (c) 2005-2009 Faktor Zehn AG und andere.
+ * 
+ * Alle Rechte vorbehalten.
+ * 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
+ * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
+ * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
+ * http://www.faktorzehn.org/f10-org:lizenzen:community eingesehen werden kann.
+ * 
+ * Mitwirkende: Faktor Zehn AG - initial API and implementation - http://www.faktorzehn.de
+ *******************************************************************************/
+
+package org.faktorips.devtools.core.internal.migration;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.faktorips.devtools.core.model.ipsproject.IIpsProject;
+import org.faktorips.devtools.core.model.versionmanager.AbstractIpsProjectMigrationOperation;
+import org.faktorips.util.message.MessageList;
+
+/**
+ * Migration from version 2.5.0.rc2 to version 2.5.0.rc3
+ * 
+ * @author Peter Kuntz
+ */
+public class Migration_2_5_0_rc3 extends AbstractIpsProjectMigrationOperation {
+
+    public Migration_2_5_0_rc3(IIpsProject projectToMigrate, String featureId) {
+        super(projectToMigrate, featureId);
+    }
+
+    @Override
+    public String getDescription() {
+        return "The generated method newCopy() in policy component types has changed to recognice"
+                + "associated copied components. This migration does not change the code or model but"
+                + "triggers a full build.";
+    }
+
+    @Override
+    public String getTargetVersion() {
+        return "2.5.0.rfinal"; //$NON-NLS-1$
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public MessageList migrate(IProgressMonitor monitor) throws CoreException, InvocationTargetException,
+            InterruptedException {
+        return new MessageList();
+    }
+}
