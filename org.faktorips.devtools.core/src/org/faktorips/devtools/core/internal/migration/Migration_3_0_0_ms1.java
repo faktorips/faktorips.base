dc762cc7c7f129d97187baad238012f8b83a9e44
ortmann2 <ortmann2>
2010-03-25 10:32:18 +0000

    *** empty log message ***

---
+++ org.faktorips.devtools.core/src/org/faktorips/devtools/core/internal/migration/Migration_3_0_0_ms1.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) 2005-2009 Faktor Zehn AG und andere.
+ * 
+ * Alle Rechte vorbehalten.
+ * 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
+ * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
+ * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
+ * http://www.faktorzehn.org/f10-org:lizenzen:community eingesehen werden kann.
+ * 
+ * Mitwirkende: Faktor Zehn AG - initial API and implementation - http://www.faktorzehn.de
+ *******************************************************************************/
+
+package org.faktorips.devtools.core.internal.migration;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.faktorips.devtools.core.model.ipsproject.IIpsProject;
+import org.faktorips.devtools.core.model.versionmanager.AbstractIpsProjectMigrationOperation;
+import org.faktorips.util.message.MessageList;
+
+/**
+ * Migration from version 3.0.0.ms1 to version 3.0.0.ms1b1.
+ * 
+ * @author Joerg Ortmann
+ */
+public class Migration_3_0_0_ms1 extends AbstractIpsProjectMigrationOperation {
+
+    public Migration_3_0_0_ms1(IIpsProject projectToMigrate, String featureId) {
+        super(projectToMigrate, featureId);
+    }
+
+    @Override
+    public String getDescription() {
+        return "";
+    }
+
+    @Override
+    public String getTargetVersion() {
+        return "3.0.0.ms1b1"; //$NON-NLS-1$
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return true;
+    }
+
+    @Override
+    public MessageList migrate(IProgressMonitor monitor) throws CoreException, InvocationTargetException,
+            InterruptedException {
+        return new MessageList();
+    }
+}
