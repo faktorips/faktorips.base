c0cdf3399d50362fa0af98f7c7723987791420db
gitconvert <gitconvert>
2010-03-31 09:23:11 +0000

    This commit was manufactured by cvs2svn to create tag 'v3_0_0_ms1c'.

--- org.faktorips.devtools.htmlexport/src/java/org/faktorips/devtools/htmlexport/pages/elements/core/ICompositePageElement.java
+++
@@ -1,9 +0,0 @@
-package org.faktorips.devtools.htmlexport.pages.elements.core;
-
-import org.faktorips.devtools.htmlexport.generators.ILayouter;
-
-public interface ICompositePageElement extends PageElement {
-	public void visitSubElements(ILayouter layouter);
-
-	public ICompositePageElement addPageElements(PageElement... pageElements);
-}
