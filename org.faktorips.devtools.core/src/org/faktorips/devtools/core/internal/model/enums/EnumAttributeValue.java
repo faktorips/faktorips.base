3a262eb37f222d631dfd3799aca972e1dbdc2c76
dirmeier <dirmeier>
2010-07-22 15:39:16 +0000

    Update copyright link

--- org.faktorips.devtools.core/src/org/faktorips/devtools/core/internal/model/enums/EnumAttributeValue.java
+++ org.faktorips.devtools.core/src/org/faktorips/devtools/core/internal/model/enums/EnumAttributeValue.java
@@ -1,16 +1,15 @@
 /*******************************************************************************
- * Copyright (c) 2005-2010 Faktor Zehn AG und andere.
+ * Copyright © 2005-2010 Faktor Zehn AG und andere.
  * 
  * Alle Rechte vorbehalten.
  * 
- * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen, 
  * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
  * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
- * http://www.faktorzehn.org/f10-org:lizenzen:community eingesehen werden kann.
+ * http://www.faktorzehn.org/fips:lizenz eingesehen werden kann.
  * 
  * Mitwirkende: Faktor Zehn AG - initial API and implementation - http://www.faktorzehn.de
- *******************************************************************************/
-
+ ******************************************************************************/
 package org.faktorips.devtools.core.internal.model.enums;
 
 import java.util.List;
--- org.faktorips.devtools.core/src/org/faktorips/devtools/core/model/pctype/Messages.java
+++ org.faktorips.devtools.core/src/org/faktorips/devtools/core/model/pctype/Messages.java
@@ -1,18 +1,15 @@
 /*******************************************************************************
- * Copyright (c) 2005,2006 Faktor Zehn GmbH und andere.
+ * Copyright © 2005-2010 Faktor Zehn AG und andere.
  * 
  * Alle Rechte vorbehalten.
  * 
- *  * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele,  *
- * Konfigurationen, etc.) duerfen nur unter den Bedingungen der  * Faktor-Zehn-Community
- * Lizenzvereinbarung - Version 0.1 (vor Gruendung Community)  * genutzt werden, die Bestandteil der
- * Auslieferung ist und auch unter  *   http://www.faktorips.org/legal/cl-v01.html  * eingesehen
- * werden kann.
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen, 
+ * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
+ * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
+ * http://www.faktorzehn.org/fips:lizenz eingesehen werden kann.
  * 
- * Mitwirkende: Faktor Zehn GmbH - initial API and implementation
- * 
- *******************************************************************************/
-
+ * Mitwirkende: Faktor Zehn AG - initial API and implementation - http://www.faktorzehn.de
+ ******************************************************************************/
 package org.faktorips.devtools.core.model.pctype;
 
 import org.eclipse.osgi.util.NLS;
