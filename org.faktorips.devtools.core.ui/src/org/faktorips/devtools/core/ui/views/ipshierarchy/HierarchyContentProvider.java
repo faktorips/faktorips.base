574049f74e851f31508bb3e2d14ea617c609533c
stoll <stoll>
2010-08-25 15:52:19 +0000

    *** empty log message ***

---
+++ org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/views/ipshierarchy/HierarchyContentProvider.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * Copyright (c) 2005-2010 Faktor Zehn AG und andere.
+ * 
+ * Alle Rechte vorbehalten.
+ * 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
+ * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
+ * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
+ * http://www.faktorzehn.org/f10-org:lizenzen:community eingesehen werden kann.
+ * 
+ * Mitwirkende: Faktor Zehn AG - initial API and implementation - http://www.faktorzehn.de
+ *******************************************************************************/
+
+package org.faktorips.devtools.core.ui.views.ipshierarchy;
+
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.Viewer;
+
+public class HierarchyContentProvider implements ITreeContentProvider {
+
+    @Override
+    public Object[] getElements(Object inputElement) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void dispose() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public Object[] getChildren(Object parentElement) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object getParent(Object element) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean hasChildren(Object element) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
