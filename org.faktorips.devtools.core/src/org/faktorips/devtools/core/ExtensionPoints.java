4a1fb1724ed9fe9b7344dee663728050976a45b9
ortmann <ortmann>
2010-08-26 19:19:11 +0000

    added more helper methods

--- org.faktorips.devtools.core/src/org/faktorips/devtools/core/ExtensionPoints.java
+++ org.faktorips.devtools.core/src/org/faktorips/devtools/core/ExtensionPoints.java
@@ -13,6 +13,9 @@
 
 package org.faktorips.devtools.core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExtension;
@@ -29,6 +32,20 @@ import org.faktorips.util.ArgumentCheck;
  */
 public class ExtensionPoints {
 
+    /**
+     * IpsPlugin relative id of the extension point for IpsObjectTypes.
+     * 
+     * @see IpsObjectType
+     */
+    public final static String IPS_OBJECT_TYPE = "ipsobjecttype"; //$NON-NLS-1$
+
+    /**
+     * IpsPlugin relative id of the extension point for custom validations.
+     * 
+     * @see IpsObjectType
+     */
+    public final static String CUSTOM_VALIDATION = "customValidation"; //$NON-NLS-1$
+
     private IExtensionRegistry registry;
     private String nameSpace;
 
@@ -43,13 +60,6 @@ public class ExtensionPoints {
     public ExtensionPoints(String nameSpace) {
         this(Platform.getExtensionRegistry(), nameSpace);
     }
-
-    /**
-     * IpsPlugin relative id of the extension point for IpsObjectTypes.
-     * 
-     * @see IpsObjectType
-     */
-    public final static String IPS_OBJECT_TYPE = "ipsobjecttype"; //$NON-NLS-1$
 
     /**
      * Returns all extensions defined for the given point. The point id should be one of the
@@ -69,9 +79,92 @@ public class ExtensionPoints {
     }
 
     /**
+     * Creates executable extensions for all extensions defined for the given point. The point id
+     * should be one of the constants defined in this class.
+     * 
+     * @param pointId The id of the extension point.
+     * @param elementName Name of the config element that contains a property that specifies the
+     *            qualified name of the class to instantiate.
+     * @param propertyName Name of the property that contains the qualified name of the class to
+     *            instantiate.
+     * @param expectedType The expected class/type of the instance.
+     * 
+     * @throws NullPointerException if any of the arguments is <code>null</code>.
+     * @throws IllegalArgumentException if no extension point with id pointId exists.
+     * 
+     * @see IConfigurationElement#createExecutableExtension(String)
+     */
+    public final <T> List<T> createExecutableExtensions(String pointId,
+            String elementName,
+            String propertyName,
+            Class<T> expectedType) {
+
+        ArgumentCheck.notNull(pointId);
+        IExtensionPoint point = registry.getExtensionPoint(nameSpace, pointId);
+        if (point == null) {
+            IpsPlugin.log(new IpsStatus("ExtensionPoint " + pointId + " not found!")); //$NON-NLS-1$ //$NON-NLS-2$
+            throw new IllegalArgumentException("Unknown extension point " + pointId); //$NON-NLS-1$
+        }
+        IExtension[] extensions = point.getExtensions();
+        List<T> execExtensions = new ArrayList<T>(extensions.length);
+        for (int i = 0; i < extensions.length; i++) {
+            T newExt = createExecutableExtension(extensions[i], elementName, propertyName, expectedType);
+            if (newExt != null) {
+                execExtensions.add(newExt);
+            }
+        }
+        return execExtensions;
+
+    }
+
+    /**
      * Wrapper around IConfigurationElement.createExecutableExtension(propertyName) with detailed
      * logging. If the executable extension couldn't be created, the reason is logged, no exception
      * is thrown. The returned object is of the expected type.
+     * 
+     * @param extension The extension.
+     * @param elementName Name of the config element that contains a property that specifies the
+     *            qualified name of the class to instantiate.
+     * @param propertyName Name of the property that contains the qualified name of the class to
+     *            instantiate.
+     * @param expectedType The expected class/type of the instance.
+     * 
+     * @see IConfigurationElement#createExecutableExtension(String)
+     * 
+     * @throws NullPointerException if any of the arguments is <code>null</code>.
+     */
+    public final static <T> T createExecutableExtension(IExtension extension,
+            String elementName,
+            String propertyName,
+            Class<T> expectedType) {
+
+        IConfigurationElement configElement = null;
+        IConfigurationElement[] configElements = extension.getConfigurationElements();
+        for (int i = 0; i < configElements.length; i++) {
+            if (elementName.equalsIgnoreCase(configElements[i].getName())) {
+                configElement = configElements[i];
+            }
+        }
+        if (configElement == null) {
+            String text = "Can't cerate executable extension " //$NON-NLS-1$
+                    + extension.getUniqueIdentifier() + ". No config element with name " + elementName + " found."; //$NON-NLS-1$ //$NON-NLS-2$
+            IpsPlugin.log(new IpsStatus(text));
+            return null;
+        }
+        return createExecutableExtension(extension, configElement, propertyName, expectedType);
+    }
+
+    /**
+     * Wrapper around IConfigurationElement.createExecutableExtension(propertyName) with detailed
+     * logging. If the executable extension couldn't be created, the reason is logged, no exception
+     * is thrown. The returned object is of the expected type.
+     * 
+     * @param extension The extension.
+     * @param element A config element of the extension that contains a property that specifies the
+     *            qualified name of the class to instantiate.
+     * @param propertyName Name of the property that contains the qualified name of the class to
+     *            instantiate.
+     * @param expectedType The expected class/type of the instance.
      * 
      * @see IConfigurationElement#createExecutableExtension(String)
      */
