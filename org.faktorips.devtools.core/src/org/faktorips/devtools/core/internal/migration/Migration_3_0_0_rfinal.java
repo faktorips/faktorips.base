7b60a0dbe5c31ae9639b41e8508394b79afeba11
dirmeier <dirmeier>
2010-10-01 11:36:29 +0000

    no migration needed. The ipsproject file is extended with new attributes automatically when safed next time

--- org.faktorips.devtools.core/src/org/faktorips/devtools/core/internal/migration/Migration_3_0_0_rfinal.java
+++ org.faktorips.devtools.core/src/org/faktorips/devtools/core/internal/migration/Migration_3_0_0_rfinal.java
@@ -13,38 +13,17 @@
 
 package org.faktorips.devtools.core.internal.migration;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.faktorips.devtools.core.IpsStatus;
 import org.faktorips.devtools.core.model.ipsproject.IIpsProject;
-import org.faktorips.devtools.core.model.ipsproject.IPersistenceOptions;
-import org.faktorips.devtools.core.model.versionmanager.AbstractIpsProjectMigrationOperation;
-import org.faktorips.devtools.core.util.XmlUtil;
-import org.faktorips.util.message.MessageList;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
 
 /**
  * Migration from Faktor-IPS Version 3.0.0.rfinal to 3.0.1.rfinal
  * 
  * @author Joerg Ortmann
  */
-public class Migration_3_0_0_rfinal extends AbstractIpsProjectMigrationOperation {
+public class Migration_3_0_0_rfinal extends EmptyMigration {
 
     public Migration_3_0_0_rfinal(IIpsProject projectToMigrate, String featureId) {
         super(projectToMigrate, featureId);
-    }
-
-    @Override
-    public String getDescription() {
-        return "The content of the .ipsproject file has changed: new PersistenceOptions attributes. All other Faktor-IPS files remain unchanged."; //$NON-NLS-1$
     }
 
     @Override
@@ -52,48 +31,4 @@ public class Migration_3_0_0_rfinal extends AbstractIpsProjectMigrationOperation
         return "3.0.1.rfinal"; //$NON-NLS-1$
     }
 
-    @Override
-    public boolean isEmpty() {
-        return false;
-    }
-
-    private void migrateIpsProjectProperties(IProgressMonitor monitor) throws CoreException {
-        IFile propertiesFile = getIpsProject().getIpsProjectPropertiesFile();
-        InputStream is = null;
-        try {
-            is = propertiesFile.getContents();
-            Document doc = XmlUtil.parseDocument(is);
-
-            // Adding the attributes "maxTableColumnScale", "maxTableColumnPrecision",
-            // "maxTableColumnSize" to the Tag <PersistenceOptions>
-            NodeList ipsProjectNl = doc.getElementsByTagName(IPersistenceOptions.XML_TAG_NAME);
-            if (ipsProjectNl.getLength() == 1) {
-                Element projectEl = (Element)ipsProjectNl.item(0);
-                projectEl.setAttribute(IPersistenceOptions.MAX_TABLE_COLUMN_SCALE, "255"); //$NON-NLS-1$
-                projectEl.setAttribute(IPersistenceOptions.MAX_TABLE_COLUMN_PRECISION, "255"); //$NON-NLS-1$
-                projectEl.setAttribute(IPersistenceOptions.MAX_TABLE_COLUMN_SIZE, "255"); //$NON-NLS-1$
-            }
-
-            ByteArrayOutputStream bos = new ByteArrayOutputStream();
-            XmlUtil.writeXMLtoStream(bos, doc, null, 2, getIpsProject().getXmlFileCharset());
-            ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());
-            propertiesFile.setContents(bis, true, false, monitor);
-        } catch (Exception e) {
-            throw new CoreException(new IpsStatus(e));
-        } finally {
-            if (is != null) {
-                try {
-                    is.close();
-                } catch (IOException e) {
-                    throw new CoreException(new IpsStatus(e));
-                }
-            }
-        }
-    }
-
-    @Override
-    public MessageList migrate(IProgressMonitor monitor) throws CoreException {
-        migrateIpsProjectProperties(monitor);
-        return new MessageList();
-    }
 }
