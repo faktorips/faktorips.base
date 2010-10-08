b56c69639b3a4d6803125fb7b54bedc22451d3f4
dirmeier <dirmeier>
2010-10-08 12:25:27 +0000

    test release for 3.0.2

---
+++ org.faktorips.devtools.core/src/org/faktorips/devtools/core/internal/migration/Migration_3_0_1_rfinal.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2005-2010 Faktor Zehn AG und andere.
+ * 
+ * Alle Rechte vorbehalten.
+ * 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
+ * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
+ * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
+ * http://www.faktorzehn.org/fips:lizenz eingesehen werden kann.
+ * 
+ * Mitwirkende: Faktor Zehn AG - initial API and implementation - http://www.faktorzehn.de
+ *******************************************************************************/
+
+package org.faktorips.devtools.core.internal.migration;
+
+import org.faktorips.devtools.core.model.ipsproject.IIpsProject;
+
+/**
+ * Migration from Faktor-IPS Version 3.0.0.rfinal to 3.0.1.rfinal
+ * 
+ * @author Joerg Ortmann
+ */
+public class Migration_3_0_1_rfinal extends EmptyMigration {
+
+    public Migration_3_0_1_rfinal(IIpsProject projectToMigrate, String featureId) {
+        super(projectToMigrate, featureId);
+    }
+
+    @Override
+    public String getTargetVersion() {
+        return "3.0.2.test"; //$NON-NLS-1$
+    }
+
+}
