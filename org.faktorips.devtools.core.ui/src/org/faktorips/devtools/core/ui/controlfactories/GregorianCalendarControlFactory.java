22c1423b2160d1872e68e412aa9792f0341afe2f
widmaier <widmaier>
2010-11-26 14:00:48 +0000

    Aufräumen

--- org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/controlfactories/GregorianCalendarControlFactory.java
+++ org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/controlfactories/GregorianCalendarControlFactory.java
@@ -18,7 +18,6 @@ import org.eclipse.nebula.jface.gridviewer.GridTableViewer;
 import org.eclipse.nebula.jface.gridviewer.GridTreeViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
@@ -87,93 +86,6 @@ public class GregorianCalendarControlFactory extends ValueDatatypeControlFactory
         button.setImage(IpsUIPlugin.getImageHandling().getSharedImage("Calendar.png", true)); //$NON-NLS-1$
         return button;
     }
-
-    protected Composite createComposite(Composite parent) {
-
-        // Composite calendarComposite = createComposite(parent);
-        // Text text = (Text)createControl(toolkit, calendarComposite, datatype, valueSet,
-        // ipsProject);
-        // GridData textGridData = new GridData(SWT.FILL, SWT.FILL, true, false);
-        // text.setLayoutData(textGridData);
-        // Button button = createButton(toolkit, calendarComposite);
-
-        // Composite calendarComposite = toolkit.createGridComposite(parent, 2, false, true);
-        Composite calendarComposite = new Composite(parent, SWT.NONE);
-        GridData compositeGridData = new GridData(SWT.FILL, SWT.FILL, false, false);
-        calendarComposite.setLayoutData(compositeGridData);
-        GridLayout gridLayout = new GridLayout(2, false);
-        gridLayout.marginTop = 2;
-        gridLayout.marginBottom = 2;
-        gridLayout.marginLeft = 1;
-        gridLayout.marginHeight = 0;
-        gridLayout.marginWidth = 0;
-        calendarComposite.setLayout(gridLayout);
-        return calendarComposite;
-    }
-
-    // private void createListeners(final FormattingTextField formatField, final Button button) {
-    // button.addSelectionListener(new SelectionListener() {
-    // @Override
-    // public void widgetSelected(SelectionEvent e) {
-    // showClendarShell(formatField, button);
-    // }
-    //
-    // @Override
-    // public void widgetDefaultSelected(SelectionEvent e) {
-    // // nothing to do
-    // }
-    // });
-    // }
-
-    // Was sagt Jan eigentlich zu einem Button mit Kalenderanzeige?
-
-    // TODO Regexp localabhängig machen, sonst kann man doch wieder ungültige Zeichen eingeben.
-    // Wie soll das gehen? -> Sample Value: Format formatieren lassen und String-Chars auf
-    // contains() prüfen.
-    // -> langsam, doof zu programmieren
-    // -> man ist nie sicher alle Sonderzeichen erwischt zu haben. zB E26 bei Double
-
-    // ValidierungsMeldungen haben anderes Format als angezeigt
-
-    /*
-     * Diskrepanz zwischen angezeigtem Wert und Modell-wert: Wenn man Tippt, wird nicht immer das
-     * Modell geändert. Das liegt daran, dass der Inhalt nicht immer geparst werden kann. Dann wird
-     * null ins Modell geschrieben. Falls dort schon Null stand, ändert sich aus Sicht FIPS nichts
-     * und somit gibt es auch kein Dirty *.
-     * 
-     * Unterbinden kann man dies nur auf zwei Wegen:
-     * 
-     * - man macht ein Control, das immer eine gültige Eingabe erzwingt.
-     * 
-     * - man lässt alles ins Modell schreiben und validieren.
-     * 
-     * Ersteres ist schwer zu implementieren, zweiteres benötigt unterschiedliche Methoden für
-     * parsable und getValue. :/
-     */
-
-    /*
-     * GregorianCalendarField:
-     * 
-     * Wird benötigt um validFrom/to(als GregorianCalendar) per UIController/PropertyMapping direkt
-     * in eine ProdCmpt zu schreiben.
-     * 
-     * Leider gibt es dabei keine Formatunterstützung wie beim FormattingEditField
-     * 
-     * -> vielleicht sollte man das GregorCalFormat parametrisieren, damit es auch einen
-     * GregorianCalendar, statt eines ISO Date Strings zurückgeben kann.
-     * 
-     * -> Leider würde dann in der alternativen GUI der KalenderButton fehlen...
-     * 
-     * -> Button+Text auslagern in eigenes Control zB TextButtonControl
-     * 
-     * Offene Punkte dann:
-     * 
-     * - wer instanziiert das FormattingEditField?
-     * 
-     * - Auf welchem Weg soll das Datum ins Modell gelangen (ISO-String bzw. GregCal)? ->
-     * Text#setText() mit medium DateFormat funktioniert, ist aber nicht schön. Alternativen?
-     * Problem ist natürlich, dass es zwei Formate gibt ...
-     */
 
     /**
      * @deprecated use
