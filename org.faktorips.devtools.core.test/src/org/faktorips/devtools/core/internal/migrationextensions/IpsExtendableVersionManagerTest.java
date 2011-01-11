054101f563fb094a93c6a41b8120e51d364734b8
dirmeier <dirmeier>
2011-01-11 10:03:15 +0000

    enabling mockito tests

--- org.faktorips.devtools.core.test/src/org/faktorips/devtools/core/internal/migrationextensions/IpsExtendableVersionManagerTest.java
+++ org.faktorips.devtools.core.test/src/org/faktorips/devtools/core/internal/migrationextensions/IpsExtendableVersionManagerTest.java
@@ -13,10 +13,19 @@
 
 package org.faktorips.devtools.core.internal.migrationextensions;
 
-// import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.withSettings;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import org.faktorips.abstracttest.AbstractIpsPluginTest;
+import org.faktorips.devtools.core.internal.model.ipsproject.IpsProject;
+import org.faktorips.devtools.core.model.ipsproject.IIpsProject;
 import org.faktorips.devtools.core.model.versionmanager.AbstractIpsProjectMigrationOperation;
+import org.faktorips.devtools.core.model.versionmanager.IIpsProjectMigrationOperationFactory;
 import org.osgi.framework.Version;
 
 public class IpsExtendableVersionManagerTest extends AbstractIpsPluginTest {
@@ -42,67 +51,62 @@ public class IpsExtendableVersionManagerTest extends AbstractIpsPluginTest {
     }
 
     public void testGetMigrationOperations() throws Exception {
-        // mockMigrationOperations();
-        //
-        // IpsProject ipsProject = mock(IpsProject.class,
-        // withSettings().defaultAnswer(RETURNS_DEEP_STUBS));
-        // when(ipsProject.getProperties().getMinRequiredVersionNumber("org.faktorips.feature")).thenReturn(
-        // currentVersion.toString());
-        //
-        // AbstractIpsProjectMigrationOperation[] migrationOperations = ipsExtendableVersionManager
-        // .getMigrationOperations(ipsProject);
-        //
-        // assertEquals(0, migrationOperations.length);
-        //
-        // when(ipsProject.getProperties().getMinRequiredVersionNumber("org.faktorips.feature")).thenReturn("1.0.0");
-        // migrationOperations = ipsExtendableVersionManager.getMigrationOperations(ipsProject);
-        // assertEquals(0, migrationOperations.length);
-        //
-        // when(ipsProject.getProperties().getMinRequiredVersionNumber("org.faktorips.feature")).thenReturn("0.0.1");
-        // migrationOperations = ipsExtendableVersionManager.getMigrationOperations(ipsProject);
-        // assertEquals(1, migrationOperations.length);
-        // assertEquals(ipsProjectMigrationOperation1, migrationOperations[0]);
+        mockMigrationOperations();
+
+        IpsProject ipsProject = mock(IpsProject.class, withSettings().defaultAnswer(RETURNS_DEEP_STUBS));
+        when(ipsProject.getProperties().getMinRequiredVersionNumber("org.faktorips.feature")).thenReturn(
+                currentVersion.toString());
+
+        AbstractIpsProjectMigrationOperation[] migrationOperations = ipsExtendableVersionManager
+                .getMigrationOperations(ipsProject);
+
+        assertEquals(0, migrationOperations.length);
+
+        when(ipsProject.getProperties().getMinRequiredVersionNumber("org.faktorips.feature")).thenReturn("1.0.0");
+        migrationOperations = ipsExtendableVersionManager.getMigrationOperations(ipsProject);
+        assertEquals(0, migrationOperations.length);
+
+        when(ipsProject.getProperties().getMinRequiredVersionNumber("org.faktorips.feature")).thenReturn("0.0.1");
+        migrationOperations = ipsExtendableVersionManager.getMigrationOperations(ipsProject);
+        assertEquals(1, migrationOperations.length);
+        assertEquals(ipsProjectMigrationOperation1, migrationOperations[0]);
     }
 
     public void testIsCurrentVersionCompatibleWith() throws Exception {
-        // mockMigrationOperations();
-        //
-        // assertTrue(ipsExtendableVersionManager.isCurrentVersionCompatibleWith(currentVersion.toString()));
-        // assertTrue(ipsExtendableVersionManager.isCurrentVersionCompatibleWith("1.0.0"));
-        //
-        // assertFalse(ipsExtendableVersionManager.isCurrentVersionCompatibleWith("0.0.1"));
-        // assertFalse(ipsExtendableVersionManager.isCurrentVersionCompatibleWith(currentVersion +
-        // "zzz"));
+        mockMigrationOperations();
+
+        assertTrue(ipsExtendableVersionManager.isCurrentVersionCompatibleWith(currentVersion.toString()));
+        assertTrue(ipsExtendableVersionManager.isCurrentVersionCompatibleWith("1.0.0"));
+
+        assertFalse(ipsExtendableVersionManager.isCurrentVersionCompatibleWith("0.0.1"));
+        assertFalse(ipsExtendableVersionManager.isCurrentVersionCompatibleWith(currentVersion + "zzz"));
     }
 
-    // private void mockMigrationOperations() {
-    // ipsProjectMigrationOperation1 = mock(AbstractIpsProjectMigrationOperation.class);
-    // when(ipsProjectMigrationOperation1.getTargetVersion()).thenReturn("0.1.0");
-    //
-    // ipsProjectMigrationOperation2 = mock(AbstractIpsProjectMigrationOperation.class);
-    // when(ipsProjectMigrationOperation2.getTargetVersion()).thenReturn(currentVersion + "zzz");
-    //
-    // Map<Version, IIpsProjectMigrationOperationFactory> registeredMigrations = new
-    // HashMap<Version, IIpsProjectMigrationOperationFactory>();
-    // registeredMigrations.put(new Version(ipsProjectMigrationOperation1.getTargetVersion()),
-    // new IIpsProjectMigrationOperationFactory() {
-    // @Override
-    // public AbstractIpsProjectMigrationOperation createIpsProjectMigrationOpertation(IIpsProject
-    // ipsProject,
-    // String featureId) {
-    // return ipsProjectMigrationOperation1;
-    // }
-    // });
-    // registeredMigrations.put(new Version(ipsProjectMigrationOperation2.getTargetVersion()),
-    // new IIpsProjectMigrationOperationFactory() {
-    //
-    // @Override
-    // public AbstractIpsProjectMigrationOperation createIpsProjectMigrationOpertation(IIpsProject
-    // ipsProject,
-    // String featureId) {
-    // return ipsProjectMigrationOperation2;
-    // }
-    // });
-    // ipsExtendableVersionManager.setRegisteredMigrations(registeredMigrations);
-    // }
+    private void mockMigrationOperations() {
+        ipsProjectMigrationOperation1 = mock(AbstractIpsProjectMigrationOperation.class);
+        when(ipsProjectMigrationOperation1.getTargetVersion()).thenReturn("0.1.0");
+
+        ipsProjectMigrationOperation2 = mock(AbstractIpsProjectMigrationOperation.class);
+        when(ipsProjectMigrationOperation2.getTargetVersion()).thenReturn(currentVersion + "zzz");
+
+        Map<Version, IIpsProjectMigrationOperationFactory> registeredMigrations = new HashMap<Version, IIpsProjectMigrationOperationFactory>();
+        registeredMigrations.put(new Version(ipsProjectMigrationOperation1.getTargetVersion()),
+                new IIpsProjectMigrationOperationFactory() {
+                    @Override
+                    public AbstractIpsProjectMigrationOperation createIpsProjectMigrationOpertation(IIpsProject ipsProject,
+                            String featureId) {
+                        return ipsProjectMigrationOperation1;
+                    }
+                });
+        registeredMigrations.put(new Version(ipsProjectMigrationOperation2.getTargetVersion()),
+                new IIpsProjectMigrationOperationFactory() {
+
+                    @Override
+                    public AbstractIpsProjectMigrationOperation createIpsProjectMigrationOpertation(IIpsProject ipsProject,
+                            String featureId) {
+                        return ipsProjectMigrationOperation2;
+                    }
+                });
+        ipsExtendableVersionManager.setRegisteredMigrations(registeredMigrations);
+    }
 }
--- org.faktorips.devtools.core.test/src/org/faktorips/devtools/core/internal/productrelease/ProductReleaseProcessorTest.java
+++ org.faktorips.devtools.core.test/src/org/faktorips/devtools/core/internal/productrelease/ProductReleaseProcessorTest.java
@@ -13,18 +13,30 @@
 
 package org.faktorips.devtools.core.internal.productrelease;
 
-// import static org.mockito.Matchers.any;
-// import static org.mockito.Matchers.eq;
-// import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
-// import static org.mockito.Mockito.mock;
-// import static org.mockito.Mockito.spy;
-// import static org.mockito.Mockito.verify;
-// import static org.mockito.Mockito.when;
-// import static org.mockito.Mockito.withSettings;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.withSettings;
 
+import java.util.ArrayList;
+import java.util.Observable;
+import java.util.Observer;
+
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtensionRegistry;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.faktorips.abstracttest.AbstractIpsPluginTest;
+import org.faktorips.devtools.core.model.ipsproject.IIpsPackageFragmentRoot;
 import org.faktorips.devtools.core.model.ipsproject.IIpsProject;
 import org.faktorips.devtools.core.productrelease.IReleaseAndDeploymentOperation;
+import org.faktorips.devtools.core.productrelease.ITargetSystem;
+import org.faktorips.devtools.core.productrelease.ObservableProgressMessages;
+import org.faktorips.util.message.Message;
 import org.faktorips.util.message.MessageList;
 
 public class ProductReleaseProcessorTest extends AbstractIpsPluginTest {
@@ -36,73 +48,67 @@ public class ProductReleaseProcessorTest extends AbstractIpsPluginTest {
 
     @Override
     protected void setUp() throws Exception {
-        // super.setUp();
-        //
-        // ipsProject = mock(IIpsProject.class, withSettings().defaultAnswer(RETURNS_DEEP_STUBS));
-        //
-        // when(ipsProject.getProperties().getReleaseExtensionId()).thenReturn("releaseExtensionId");
-        // when(ipsProject.getIpsPackageFragmentRoots()).thenReturn(new IIpsPackageFragmentRoot[0]);
-        //
-        // IConfigurationElement configElement = mock(IConfigurationElement.class);
-        // when(configElement.getAttribute("id")).thenReturn("releaseExtensionId");
-        //
-        // IExtensionRegistry extensionRegistry = mock(IExtensionRegistry.class);
-        // when(extensionRegistry.getConfigurationElementsFor(null, null)).thenReturn(
-        // new IConfigurationElement[] { configElement });
-        //
-        // ObservableProgressMessages observableMessages = new ObservableProgressMessages();
-        // productReleaseProcessor = spy(new ProductReleaseProcessor(ipsProject,
-        // observableMessages));
-        //
-        // messageList = new MessageList();
-        // Observer observer = new Observer() {
-        //
-        // @Override
-        // public void update(Observable o, Object arg) {
-        // messageList.add((Message)arg);
-        // }
-        // };
-        // observableMessages.addObserver(observer);
+        super.setUp();
+
+        ipsProject = mock(IIpsProject.class, withSettings().defaultAnswer(RETURNS_DEEP_STUBS));
+
+        when(ipsProject.getProperties().getReleaseExtensionId()).thenReturn("releaseExtensionId");
+        when(ipsProject.getIpsPackageFragmentRoots()).thenReturn(new IIpsPackageFragmentRoot[0]);
+
+        IConfigurationElement configElement = mock(IConfigurationElement.class);
+        when(configElement.getAttribute("id")).thenReturn("releaseExtensionId");
+
+        IExtensionRegistry extensionRegistry = mock(IExtensionRegistry.class);
+        when(extensionRegistry.getConfigurationElementsFor(null, null)).thenReturn(
+                new IConfigurationElement[] { configElement });
+
+        ObservableProgressMessages observableMessages = new ObservableProgressMessages();
+        productReleaseProcessor = spy(new ProductReleaseProcessor(ipsProject, observableMessages));
+
+        messageList = new MessageList();
+        Observer observer = new Observer() {
+
+            @Override
+            public void update(Observable o, Object arg) {
+                messageList.add((Message)arg);
+            }
+        };
+        observableMessages.addObserver(observer);
 
     }
-    //
-    // public void testStartReleaseBuilder() throws Exception {
-    // productReleaseProcessor.startReleaseBuilder("abc", null, new NullProgressMonitor());
-    // assertTrue(messageList.containsErrorMsg());
-    // assertEquals(1, messageList.size());
-    // assertEquals(Messages.ReleaseAndDeploymentOperation_exception_noDeploymentExtension,
-    // messageList.getMessage(0)
-    // .getText());
-    //
-    // releaseAndDeploymentOperation = mock(IReleaseAndDeploymentOperation.class);
-    // when(productReleaseProcessor.getReleaseAndDeploymentOperation()).thenReturn(releaseAndDeploymentOperation);
-    //
-    // try {
-    // productReleaseProcessor.startReleaseBuilder("abc", null, new NullProgressMonitor());
-    // } catch (InterruptedException e) {
-    // assertEquals(Messages.ProductReleaseProcessor_error_custom_validation_failed,
-    // e.getMessage());
-    // }
-    //
-    // releaseAndDeploymentOperation = mock(IReleaseAndDeploymentOperation.class);
-    // when(productReleaseProcessor.getReleaseAndDeploymentOperation()).thenReturn(releaseAndDeploymentOperation);
-    // when(releaseAndDeploymentOperation.customReleaseSettings(any(IIpsProject.class),
-    // any(IProgressMonitor.class)))
-    // .thenReturn(true);
-    //
-    // ArrayList<ITargetSystem> targetSystems = new ArrayList<ITargetSystem>();
-    // targetSystems.add(new DefaultTargetSystem("test123"));
-    //
-    // productReleaseProcessor.startReleaseBuilder("abc", targetSystems, new NullProgressMonitor());
-    //
-    // verify(releaseAndDeploymentOperation).customReleaseSettings(eq(ipsProject),
-    // any(IProgressMonitor.class));
-    //
-    // verify(releaseAndDeploymentOperation).buildReleaseAndDeployment(eq(ipsProject), eq("abc"),
-    // eq(targetSystems),
-    // any(IProgressMonitor.class));
-    //
-    // verify(releaseAndDeploymentOperation).additionalResourcesToCommit(ipsProject);
-    //
-    // }
+
+    public void testStartReleaseBuilder() throws Exception {
+        productReleaseProcessor.startReleaseBuilder("abc", null, new NullProgressMonitor());
+        assertTrue(messageList.containsErrorMsg());
+        assertEquals(1, messageList.size());
+        assertEquals(Messages.ReleaseAndDeploymentOperation_exception_noDeploymentExtension, messageList.getMessage(0)
+                .getText());
+
+        releaseAndDeploymentOperation = mock(IReleaseAndDeploymentOperation.class);
+        when(productReleaseProcessor.getReleaseAndDeploymentOperation()).thenReturn(releaseAndDeploymentOperation);
+
+        try {
+            productReleaseProcessor.startReleaseBuilder("abc", null, new NullProgressMonitor());
+        } catch (InterruptedException e) {
+            assertEquals(Messages.ProductReleaseProcessor_error_custom_validation_failed, e.getMessage());
+        }
+
+        releaseAndDeploymentOperation = mock(IReleaseAndDeploymentOperation.class);
+        when(productReleaseProcessor.getReleaseAndDeploymentOperation()).thenReturn(releaseAndDeploymentOperation);
+        when(releaseAndDeploymentOperation.customReleaseSettings(any(IIpsProject.class), any(IProgressMonitor.class)))
+                .thenReturn(true);
+
+        ArrayList<ITargetSystem> targetSystems = new ArrayList<ITargetSystem>();
+        targetSystems.add(new DefaultTargetSystem("test123"));
+
+        productReleaseProcessor.startReleaseBuilder("abc", targetSystems, new NullProgressMonitor());
+
+        verify(releaseAndDeploymentOperation).customReleaseSettings(eq(ipsProject), any(IProgressMonitor.class));
+
+        verify(releaseAndDeploymentOperation).buildReleaseAndDeployment(eq(ipsProject), eq("abc"), eq(targetSystems),
+                any(IProgressMonitor.class));
+
+        verify(releaseAndDeploymentOperation).additionalResourcesToCommit(ipsProject);
+
+    }
 }
