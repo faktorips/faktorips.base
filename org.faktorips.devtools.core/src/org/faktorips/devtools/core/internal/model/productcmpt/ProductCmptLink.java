6e3360300b1f4ad71e9577007b8d7d69dc83a4c3
dirmeier <dirmeier>
2011-01-21 12:34:10 +0000

    correct validation message

--- org.faktorips.devtools.core/src/org/faktorips/devtools/core/internal/model/productcmpt/ProductCmptLink.java
+++ org.faktorips.devtools.core/src/org/faktorips/devtools/core/internal/model/productcmpt/ProductCmptLink.java
@@ -228,7 +228,7 @@ public class ProductCmptLink extends AtomicIpsObjectPart implements IProductCmpt
                 }
                 if (sumMinCardinality > maxType) {
                     String text = NLS.bind(Messages.ProductCmptLink_msgMaxCardinalityExceedsModelMax,
-                            Integer.toString(sumMinCardinality), Integer.toString(maxType));
+                            this.getMaxCardinality(), Integer.toString(maxType));
                     list.add(new Message(MSGCODE_MAX_CARDINALITY_EXCEEDS_MODEL_MAX, text, Message.ERROR, this,
                             PROPERTY_MAX_CARDINALITY));
                 }
@@ -250,7 +250,7 @@ public class ProductCmptLink extends AtomicIpsObjectPart implements IProductCmpt
             }
             if (sumMaxCardinality < minType) {
                 String text = NLS.bind(Messages.ProductCmptLink_msgMinCardinalityExceedsModelMin,
-                        Integer.toString(sumMaxCardinality), Integer.toString(minType));
+                        this.getMinCardinality(), Integer.toString(minType));
                 list.add(new Message(MSGCODE_MIN_CARDINALITY_FALLS_BELOW_MODEL_MIN, text, Message.ERROR, this,
                         PROPERTY_MIN_CARDINALITY));
             }
