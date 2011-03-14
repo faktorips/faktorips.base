5469ca2b8c932d90a6e5e91ed91fba8295da0946
weickmann <weickmann>
2011-03-14 16:38:08 +0000

    fixed an nls warning

--- org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/editors/productcmpttype/Messages.java
+++ org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/editors/productcmpttype/Messages.java
@@ -42,7 +42,6 @@ public class Messages extends NLS {
     public static String AssociationEditDialog_targetLabel;
     public static String AssociationEditDialog_title;
     public static String AssociationEditDialog_typeLabel;
-    public static String AssociationsSection_menuOpenTargetInNewEditor;
     public static String AttributeEditDialog_datatypeLabel;
     public static String AttributeEditDialog_defaultvalueLabel;
     public static String AttributeEditDialog_general;
