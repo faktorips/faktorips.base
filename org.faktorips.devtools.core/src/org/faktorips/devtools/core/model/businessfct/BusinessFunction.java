93095e0c911900c0424b681421e174014dbbf612
ortmann <ortmann>
2011-05-20 15:00:11 +0000

    *** empty log message ***

--- org.faktorips.devtools.core/src/org/faktorips/devtools/core/model/businessfct/BusinessFunction.java
+++ org.faktorips.devtools.core/src/org/faktorips/devtools/core/model/businessfct/BusinessFunction.java
@@ -13,8 +13,21 @@
 
 package org.faktorips.devtools.core.model.businessfct;
 
+import org.faktorips.devtools.core.model.bf.IBusinessFunction;
 import org.faktorips.devtools.core.model.ipsobject.IIpsObject;
+import org.faktorips.devtools.core.model.pctype.IValidationRule;
 
+/**
+ * These kind of business functions are used to specify that validation rules are only applicable in
+ * a specific business context. The only have a name like 'Offer' oder 'Proposal' or 'NewBusiness'.
+ * There is no user interface in Faktor-IPS to create this kind of business functions as we want to
+ * re-think how validation rules can be applied in different business contexts. These kind of
+ * business functions will be removed in future versions.
+ * <p>
+ * There is another business function concept which is defined by {@link IBusinessFunction}.
+ * 
+ * @see IValidationRule#getBusinessFunctions()
+ */
 public interface BusinessFunction extends IIpsObject {
-
+    // see documentation above
 }
