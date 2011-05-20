99209b3c987edcac7253057b1b404d83aea56413
dicker <dicker>
2011-05-20 21:10:21 +0000

    menueintrag suche (noch nicht endgueltig gefixt

--- org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/commands/OpenModelSearchPageHandler.java
+++ org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/commands/OpenModelSearchPageHandler.java
@@ -11,45 +11,33 @@
  * Mitwirkende: Faktor Zehn AG - initial API and implementation - http://www.faktorzehn.de
  *******************************************************************************/
 
-package org.faktorips.devtools.core.ui.actions;
+package org.faktorips.devtools.core.ui.commands;
 
-import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.search.ui.NewSearchUI;
 import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 import org.faktorips.devtools.core.IpsPlugin;
 import org.faktorips.devtools.core.IpsStatus;
+import org.faktorips.devtools.core.ui.IpsUIPlugin;
 
-public class OpenModelSearchPageAction implements IWorkbenchWindowActionDelegate {
+public class OpenModelSearchPageHandler extends AbstractHandler {
 
     private static final String MODEL_SEARCH_PAGE_ID = "org.faktorips.devtools.core.ui.search.model.ModelSearchPage"; //$NON-NLS-1$
-
-    private IWorkbenchWindow window;
+    public static final String CONTRIBUTION_ID = "org.faktorips.devtools.core.ui.search.model"; //$NON-NLS-1$
 
     @Override
-    public void run(IAction action) {
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+
+        IWorkbenchWindow window = IpsUIPlugin.getDefault().getWorkbench().getActiveWorkbenchWindow();
+
         if (window == null || window.getActivePage() == null) {
             IpsPlugin.log(new IpsStatus("No window handle for opening the Faktor-IPS Model Search.")); //$NON-NLS-1$
-            return;
+            return null;
         }
+
         NewSearchUI.openSearchDialog(window, MODEL_SEARCH_PAGE_ID);
+        return null;
     }
-
-    @Override
-    public void selectionChanged(IAction action, ISelection selection) {
-        // nothing to do
-    }
-
-    @Override
-    public void dispose() {
-        window = null;
-    }
-
-    @Override
-    public void init(IWorkbenchWindow window) {
-        this.window = window;
-
-    }
-
 }
