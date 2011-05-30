4316b5baedcaf7744dc122fdc7091bacbc184986
dirmeier <dirmeier>
2011-05-30 16:44:05 +0000

    added adapter factory for ProductCmptLink to handle the target not the enclosing resource/object

---
+++ org.faktorips.devtools.core/src/org/faktorips/devtools/core/internal/model/adapter/ProductCmptLinkAdapterFactory.java
@@ -0,0 +1,69 @@
+/*******************************************************************************
+ * Copyright (c) 2005-2011 Faktor Zehn AG und andere.
+ * 
+ * Alle Rechte vorbehalten.
+ * 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
+ * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
+ * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
+ * http://www.faktorzehn.org/f10-org:lizenzen:community eingesehen werden kann.
+ * 
+ * Mitwirkende: Faktor Zehn AG - initial API and implementation - http://www.faktorzehn.de
+ *******************************************************************************/
+
+package org.faktorips.devtools.core.internal.model.adapter;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.faktorips.devtools.core.IpsPlugin;
+import org.faktorips.devtools.core.model.IIpsElement;
+import org.faktorips.devtools.core.model.ipsobject.IIpsObject;
+import org.faktorips.devtools.core.model.ipsobject.IIpsSrcFile;
+import org.faktorips.devtools.core.model.productcmpt.IProductCmpt;
+import org.faktorips.devtools.core.model.productcmpt.IProductCmptLink;
+
+/**
+ * This is a adapter factory to adapt from {@link IProductCmptLink} to {@link IResource} or
+ * {@link IFile}. This is necessary because in case of {@link IProductCmptLink} we do not want to
+ * use the enclosing resource like in nearly every other {@link IIpsElement} (@see
+ * {@link IpsElementAdapterFactory}) but we want to get the resource of the target.
+ * 
+ * @author dirmeier
+ */
+public class ProductCmptLinkAdapterFactory extends AbstractIpsAdapterFactory {
+
+    @Override
+    // eclipse adapters are not type safe
+    public Object getAdapter(Object adaptableObject, @SuppressWarnings("rawtypes") Class adapterType) {
+        if (!(adaptableObject instanceof IProductCmptLink)) {
+            return null;
+        }
+
+        IProductCmptLink link = (IProductCmptLink)adaptableObject;
+
+        try {
+            IProductCmpt target = link.findTarget(link.getIpsProject());
+            if (IIpsObject.class.isAssignableFrom(adapterType)) {
+                return target;
+            }
+            if (IIpsSrcFile.class.isAssignableFrom(adapterType)) {
+                return target.getIpsSrcFile();
+            }
+            IResource enclosingResource = target.getEnclosingResource();
+            if (adapterType.isInstance(enclosingResource)) {
+                return enclosingResource;
+            }
+        } catch (Exception e) {
+            IpsPlugin.log(e);
+        }
+        return null;
+    }
+
+    @SuppressWarnings("rawtypes")
+    // eclipse adapters are not type safe
+    @Override
+    public Class[] getAdapterList() {
+        return new Class[] { IResource.class, IFile.class };
+    }
+
+}
