5fbb905e3f9ef0e7d27bbf5282cce1629762ac22
weickmann <weickmann>
2011-10-17 11:27:05 +0000

    FIPS-726: https://faktorzehn.org/jira/browse/FIPS-726 set version to 3.6.0.rc1, added migration to version 3.6.0

---
+++ org.faktorips.devtools.core/src/org/faktorips/devtools/core/internal/migrationextensions/Messages.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2005-2011 Faktor Zehn AG und andere.
+ * 
+ * Alle Rechte vorbehalten.
+ * 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
+ * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
+ * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
+ * http://www.faktorzehn.org/f10-org:lizenzen:community eingesehen werden kann.
+ * 
+ * Mitwirkende: Faktor Zehn AG - initial API and implementation - http://www.faktorzehn.de
+ *******************************************************************************/
+
+package org.faktorips.devtools.core.internal.migrationextensions;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+
+    private static final String BUNDLE_NAME = "org.faktorips.devtools.core.internal.migrationextensions.messages"; //$NON-NLS-1$
+
+    static {
+        NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+    }
+
+    private Messages() {
+        // Messages bundles shall not be initialized.
+    }
+
+    public static String Migration_3_6_0_nameDefaultCategoryAttributes;
+    public static String Migration_3_6_0_nameDefaultCategoryTablesAndFormulas;
+    public static String Migration_3_6_0_nameDefaultCategoryValidationRules;
+    public static String Migration_3_6_0_nameDefaultCategoryDefaultsAndValueSets;
+
+}
---
+++ org.faktorips.devtools.core/src/org/faktorips/devtools/core/internal/migrationextensions/Migration_3_6_0.java
@@ -0,0 +1,95 @@
+/*******************************************************************************
+ * Copyright (c) 2005-2011 Faktor Zehn AG und andere.
+ * 
+ * Alle Rechte vorbehalten.
+ * 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
+ * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
+ * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
+ * http://www.faktorzehn.org/f10-org:lizenzen:community eingesehen werden kann.
+ * 
+ * Mitwirkende: Faktor Zehn AG - initial API and implementation - http://www.faktorzehn.de
+ *******************************************************************************/
+
+package org.faktorips.devtools.core.internal.migrationextensions;
+
+import org.eclipse.core.runtime.CoreException;
+import org.faktorips.devtools.core.internal.migration.DefaultMigration;
+import org.faktorips.devtools.core.model.ipsobject.IIpsSrcFile;
+import org.faktorips.devtools.core.model.ipsobject.IpsObjectType;
+import org.faktorips.devtools.core.model.ipsproject.IIpsProject;
+import org.faktorips.devtools.core.model.productcmpttype.IProductCmptCategory;
+import org.faktorips.devtools.core.model.productcmpttype.IProductCmptCategory.Position;
+import org.faktorips.devtools.core.model.productcmpttype.IProductCmptType;
+import org.faktorips.devtools.core.model.versionmanager.AbstractIpsProjectMigrationOperation;
+import org.faktorips.devtools.core.model.versionmanager.IIpsProjectMigrationOperationFactory;
+
+/**
+ * Migration to version 3.6.0.
+ * <p>
+ * Searches for all {@link IProductCmptType}s that have no supertype. For each such
+ * {@link IProductCmptType}, default {@link IProductCmptCategory}s are created. The categories that
+ * are created correspond to the UI sections as they are set up prior to version 3.6.0.
+ * 
+ * @author Alexander Weickmann
+ */
+public class Migration_3_6_0 extends DefaultMigration {
+
+    public Migration_3_6_0(IIpsProject projectToMigrate, String featureId) {
+        super(projectToMigrate, featureId);
+    }
+
+    @Override
+    protected void migrate(IIpsSrcFile srcFile) throws CoreException {
+        if (!srcFile.getIpsObjectType().equals(IpsObjectType.PRODUCT_CMPT_TYPE)) {
+            return;
+        }
+
+        IProductCmptType productCmptType = (IProductCmptType)srcFile.getIpsObject();
+        if (productCmptType.hasSupertype()) {
+            return;
+        }
+
+        IProductCmptCategory attributes = productCmptType
+                .newProductCmptCategory(Messages.Migration_3_6_0_nameDefaultCategoryAttributes);
+        attributes.setDefaultForProductCmptTypeAttributes(true);
+        IProductCmptCategory tablesAndFormulas = productCmptType
+                .newProductCmptCategory(Messages.Migration_3_6_0_nameDefaultCategoryTablesAndFormulas);
+        tablesAndFormulas.setDefaultForTableStructureUsages(true);
+        tablesAndFormulas.setDefaultForFormulaSignatureDefinitions(true);
+        IProductCmptCategory validationRules = productCmptType
+                .newProductCmptCategory(Messages.Migration_3_6_0_nameDefaultCategoryValidationRules);
+        validationRules.setDefaultForValidationRules(true);
+        IProductCmptCategory defaultsAndValueSets = productCmptType
+                .newProductCmptCategory(Messages.Migration_3_6_0_nameDefaultCategoryDefaultsAndValueSets);
+        defaultsAndValueSets.setDefaultForPolicyCmptTypeAttributes(true);
+        defaultsAndValueSets.setPosition(Position.RIGHT);
+    }
+
+    @Override
+    public String getTargetVersion() {
+        return "3.6.0"; //$NON-NLS-1$
+    }
+
+    @Override
+    public String getDescription() {
+        return "Product component properties (product relevant policy component type attributes" //$NON-NLS-1$
+                + ", configurable validation rules, product component type attributes, formula " //$NON-NLS-1$
+                + "signature definitions and table structure usages) are now all assigned to " //$NON-NLS-1$
+                + "categories in the corresponding product component type. For all product component " //$NON-NLS-1$
+                + "types without a superclass, the default categories are created. The default" //$NON-NLS-1$
+                + " categories correspond to the sorting as it is prior to version 3.6.0."; //$NON-NLS-1$
+    }
+
+    public static class Factory implements IIpsProjectMigrationOperationFactory {
+
+        @Override
+        public AbstractIpsProjectMigrationOperation createIpsProjectMigrationOpertation(IIpsProject ipsProject,
+                String featureId) {
+
+            return new Migration_3_6_0(ipsProject, featureId);
+        }
+
+    }
+
+}
