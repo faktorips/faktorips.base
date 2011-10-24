6ccc452254a81d83b23a0c28cfb5b7b4c6831579
dirmeier <dirmeier>
2011-10-24 14:39:44 +0000

    fixing test for new junit4 version (int not equals long)

--- org.faktorips.valuetypes.java5.test/src/org/faktorips/values/DecimalTest.java
+++ org.faktorips.valuetypes.java5.test/src/org/faktorips/values/DecimalTest.java
@@ -47,10 +47,10 @@ public class DecimalTest {
     @Test
     public void testLongValue() {
         Decimal d = Decimal.valueOf("56");
-        assertEquals(56, d.longValue());
+        assertEquals(56L, d.longValue());
 
         d = Decimal.valueOf("65.77");
-        assertEquals(65, d.longValue());
+        assertEquals(65L, d.longValue());
     }
 
     @Test
