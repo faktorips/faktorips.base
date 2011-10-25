814409471dd6b4d9d660875905575422d11f312f
weickmann <weickmann>
2011-10-25 07:03:29 +0000

    show checkboxes fully at their bottom

--- org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/controls/AbstractCheckbox.java
+++ org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/controls/AbstractCheckbox.java
@@ -39,7 +39,7 @@ public abstract class AbstractCheckbox extends ControlComposite {
         GridLayout layout = new GridLayout();
         layout.horizontalSpacing = 0;
         layout.verticalSpacing = 0;
-        layout.marginHeight = 4;
+        layout.marginHeight = 0;
         layout.marginWidth = 0;
         setLayout(layout);
         if (toolkit.getFormToolkit() != null) {
