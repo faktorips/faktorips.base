4414444968a8cb01ae0b9807f60ea917dc440729
schwering <schwering>
2011-11-29 09:04:37 +0000

    should be internal only

--- org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/internal/ContentProposal.java
+++ org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/internal/ContentProposal.java
@@ -11,7 +11,7 @@
  * Mitwirkende: Faktor Zehn AG - initial API and implementation - http://www.faktorzehn.de
  *******************************************************************************/
 
-package org.faktorips.devtools.core.ui;
+package org.faktorips.devtools.core.ui.internal;
 
 import org.eclipse.jface.fieldassist.IContentProposal;
 
