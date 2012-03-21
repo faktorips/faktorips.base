f5244557a048a832628596c1048711a59f69be98
dirmeier <dirmeier>
2012-03-21 08:42:38 +0000

    FIPS-1018

---
+++ org.faktorips.runtime.java5/src/org/faktorips/runtime/internal/MultiValueXmlHelper.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2005-2012 Faktor Zehn AG und andere.
+ * 
+ * Alle Rechte vorbehalten.
+ * 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
+ * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
+ * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
+ * http://www.faktorzehn.org/f10-org:lizenzen:community eingesehen werden kann.
+ * 
+ * Mitwirkende: Faktor Zehn AG - initial API and implementation - http://www.faktorzehn.de
+ *******************************************************************************/
+
+package org.faktorips.runtime.internal;
+
+/**
+ * A helper class to load and save XML elements representing multi values as they are used in
+ * attribute values.
+ * <p>
+ * This class should only contains static utility method mainly called by generated runtime code.
+ * 
+ * @author dirmeier
+ */
+public final class MultiValueXmlHelper {
+
+    public static final String XML_TYPE_NAME = "MultiValue"; //$NON-NLS-1$
+
+    /**
+     * Helper class should not be instantiated
+     */
+    private MultiValueXmlHelper() {
+        // do not instantiate
+    }
+
+}
