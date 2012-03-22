5dadf7b8528388cdf062f377605d2e6e27dfe3a3
widmaier <widmaier>
2012-03-22 13:37:47 +0000

    FIPS-1021: MulitValue-Attribute: Codegenerierung

---
+++ org.faktorips.runtime.java5.test/src/org/faktorips/runtime/internal/MultiValueXmlHelperTest.java
@@ -0,0 +1,82 @@
+/*******************************************************************************
+ * Copyright (c) 2005-2012 Faktor Zehn AG und andere.
+ * 
+ * Alle Rechte vorbehalten.
+ * 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
+ * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
+ * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
+ * http://www.faktorzehn.org/fips:lizenz eingesehen werden kann.
+ * 
+ * Mitwirkende: Faktor Zehn AG - initial API and implementation - http://www.faktorzehn.de
+ *******************************************************************************/
+
+package org.faktorips.runtime.internal;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.faktorips.runtime.XmlAbstractTestCase;
+import org.junit.Test;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+public class MultiValueXmlHelperTest extends XmlAbstractTestCase {
+
+    @Test
+    public void readValuesFromXML() {
+        Element configElement = getConfigElement(0);
+
+        List<String> valuesFromXML = MultiValueXmlHelper.getValuesFromXML(configElement);
+        assertEquals(3, valuesFromXML.size());
+        assertEquals("foo", valuesFromXML.get(0));
+        assertNull(valuesFromXML.get(1));
+        assertEquals("bar", valuesFromXML.get(2));
+    }
+
+    private Element getConfigElement(int index) {
+        return (Element)getTestDocument().getDocumentElement().getElementsByTagName("ConfigElement").item(index);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void readValuesFromXML_missingOuterValueTag() {
+        Element configElement = getConfigElement(1);
+        MultiValueXmlHelper.getValuesFromXML(configElement);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void readValuesFromXML_missingMultiValueTag() {
+        Element configElement = getConfigElement(2);
+        MultiValueXmlHelper.getValuesFromXML(configElement);
+    }
+
+    @Test
+    public void addValuesToElement() {
+        List<String> stringList = new ArrayList<String>();
+        stringList.add("foo");
+        stringList.add(null);
+        stringList.add("bar");
+        stringList.add("4711");
+
+        Element attrValueElement = getTestDocument().createElement("AttributeValue");
+        MultiValueXmlHelper.addValuesToElement(attrValueElement, stringList);
+
+        NodeList outerValueElementNodeList = attrValueElement.getChildNodes();
+        assertEquals(1, outerValueElementNodeList.getLength());
+        Element outerValueElement = (Element)outerValueElementNodeList.item(0);
+
+        NodeList multiValueElementNodeList = outerValueElement.getChildNodes();
+        assertEquals(1, multiValueElementNodeList.getLength());
+        Element multiValueElement = (Element)multiValueElementNodeList.item(0);
+
+        NodeList valueElementNodeList = multiValueElement.getChildNodes();
+        assertEquals(4, valueElementNodeList.getLength());
+        assertEquals("foo", ValueToXmlHelper.getValueFromElement((Element)valueElementNodeList.item(0)));
+        assertNull(ValueToXmlHelper.getValueFromElement((Element)valueElementNodeList.item(1)));
+        assertEquals("bar", ValueToXmlHelper.getValueFromElement((Element)valueElementNodeList.item(2)));
+        assertEquals("4711", ValueToXmlHelper.getValueFromElement((Element)valueElementNodeList.item(3)));
+    }
+}
--- org.faktorips.runtime.java5/src/org/faktorips/runtime/internal/MultiValueXmlHelper.java
+++ org.faktorips.runtime.java5/src/org/faktorips/runtime/internal/MultiValueXmlHelper.java
@@ -13,6 +13,12 @@
 
 package org.faktorips.runtime.internal;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
 /**
  * A helper class to load and save XML elements representing multi values as they are used in
  * attribute values.
@@ -20,10 +26,12 @@ package org.faktorips.runtime.internal;
  * This class should only contains static utility method mainly called by generated runtime code.
  * 
  * @author dirmeier
+ * @author widmaier
  */
 public final class MultiValueXmlHelper {
 
-    public static final String XML_TYPE_NAME = "MultiValue"; //$NON-NLS-1$
+    public static final String XML_TAG_MULTIVALUE = "MultiValue"; //$NON-NLS-1$
+    public static final String XML_TAG_VALUE = "Value"; //$NON-NLS-1$
 
     /**
      * Helper class should not be instantiated
@@ -32,4 +40,78 @@ public final class MultiValueXmlHelper {
         // do not instantiate
     }
 
+    /**
+     * Reads String values from the a XML structure. Given the below XML (AttributeValue-Element
+     * expected), this method will return a List with three elements: {null, "foo", "bar"}.
+     * 
+     * <pre>
+     * &lt;AttributeValue&gt;
+     *      &lt;Value&gt;
+     *          &lt;MultiValue&gt;
+     *              &lt;Value isNull="true"/&gt;
+     *              &lt;Value isNull="false"/&gt; foo &lt;/Value&gt;
+     *              &lt;Value isNull="false"/&gt; bar &lt;/Value&gt;
+     *          &lt;/MultiValue&gt;
+     *      &lt;/Value&gt;
+     * &lt;/AttributeValue&gt;
+     * </pre>
+     * 
+     * @param attrValueElement the element to extract multiple values from
+     * @return a list containing all values in the given element as strings
+     * @throws NullPointerException if the outer value-tag or the MultiValue-Tag can not be found
+     */
+    public static List<String> getValuesFromXML(Element attrValueElement) {
+        Element value = XmlUtil.getFirstElement(attrValueElement, XML_TAG_VALUE);
+        assertElementExists(value, XML_TAG_VALUE);
+        Element multiValueElement = XmlUtil.getFirstElement(value, XML_TAG_MULTIVALUE);
+        assertElementExists(multiValueElement, XML_TAG_MULTIVALUE);
+
+        ArrayList<String> list = new ArrayList<String>();
+        NodeList valueNodeList = multiValueElement.getElementsByTagName(XML_TAG_VALUE);
+        for (int i = 0; i < valueNodeList.getLength(); i++) {
+            Element valueElement = (Element)valueNodeList.item(i);
+            list.add(ValueToXmlHelper.getValueFromElement(valueElement));
+        }
+        return list;
+    }
+
+    /**
+     * Throws a {@link NullPointerException} in case the given element is <code>null</code>.
+     * 
+     * @param element the element to check
+     * @param xmlTagName the tag name that was expected
+     */
+    private static void assertElementExists(Element element, String xmlTagName) {
+        if (element == null) {
+            throw new NullPointerException("Inconsistent XML content. Element \"" + xmlTagName
+                    + "\" was expected, but could not be found found.");
+        }
+    }
+
+    /**
+     * Adds all values in the given list as a multi-value element to the given element. A list
+     * containing the values {"foo", null, "bar"} will add the following elements to the given
+     * element.
+     * 
+     * <pre>
+     * &lt;Value&gt;
+     *     &lt;MultiValue&gt;
+     *         &lt;Value isNull="true"/&gt;
+     *         &lt;Value isNull="false"/&gt; foo &lt;/Value&gt;
+     *         &lt;Value isNull="false"/&gt; bar &lt;/Value&gt;
+     *     &lt;/MultiValue&gt;
+     * &lt;/Value&gt;
+     * </pre>
+     */
+    public static void addValuesToElement(Element element, List<String> stringList) {
+        Element outerValueElement = element.getOwnerDocument().createElement(XML_TAG_VALUE);
+        element.appendChild(outerValueElement);
+
+        Element multiValueElement = outerValueElement.getOwnerDocument().createElement(XML_TAG_MULTIVALUE);
+        outerValueElement.appendChild(multiValueElement);
+
+        for (String stringValue : stringList) {
+            ValueToXmlHelper.addValueToElement(stringValue, multiValueElement, XML_TAG_VALUE);
+        }
+    }
 }
