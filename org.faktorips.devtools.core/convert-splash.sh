a53d703611ee293e5a8f9b0c73034295b0c0649c
gitconvert <gitconvert>
2012-04-26 08:35:00 +0000

    This commit was manufactured by cvs2svn to create tag 'Root_v3_6_pa_patches_externalize_enums'.

--- org.faktorips.devtools.core/convert-splash.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Note: you have to install inkscape and imagemagick to run this skript
-
-inkscape -z -e=splash.png splash.svg
-convert splash.png -verbose bmp3:splash.bmp
-rm splash.png
