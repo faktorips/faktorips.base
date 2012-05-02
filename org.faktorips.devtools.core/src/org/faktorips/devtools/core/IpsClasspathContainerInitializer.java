94467ba2ffafc9baf81fb0e440e8d4a76bf1137f
dirmeier <dirmeier>
2012-05-02 11:55:27 +0000

    FIPS-1010

--- org.faktorips.devtools.core/src/org/faktorips/devtools/core/IpsClasspathContainerInitializer.java
+++ org.faktorips.devtools.core/src/org/faktorips/devtools/core/IpsClasspathContainerInitializer.java
@@ -15,6 +15,7 @@ package org.faktorips.devtools.core;
 
 import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
 
 import org.apache.commons.lang.StringUtils;
 import org.eclipse.core.runtime.CoreException;
@@ -53,62 +54,6 @@ public class IpsClasspathContainerInitializer extends ClasspathContainerInitiali
 
     }
 
-    private IPath getBundlePath(String pluginId, boolean sources) {
-        Bundle bundle = Platform.getBundle(pluginId);
-        if (bundle == null) {
-            IpsPlugin
-                    .log(new IpsStatus(
-                            "Error initializing " + (sources ? "source for " : "") + "classpath container. Bundle " + pluginId + " not found.")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$
-            return null;
-        }
-
-        URL installLocation;
-        if (sources) {
-            try {
-                installLocation = bundle.getEntry("src"); //$NON-NLS-1$ 
-                String fullPath;
-                if (installLocation != null) {
-                    URL local = FileLocator.toFileURL(installLocation);
-                    fullPath = new File(local.getPath()).getAbsolutePath();
-                } else {
-                    fullPath = FileLocator.getBundleFile(bundle).getAbsolutePath();
-                    String[] split = fullPath.split(NAME_VERSION_SEP);
-                    if (split.length < 2) {
-                        return null;
-                    }
-                    split[split.length - 2] = split[split.length - 2] + ".source"; //$NON-NLS-1$
-                    fullPath = StringUtils.EMPTY;
-                    for (String string : split) {
-                        if (string != split[split.length - 1]) {
-                            fullPath += string + NAME_VERSION_SEP;
-                        } else {
-                            fullPath += string;
-                        }
-                    }
-                }
-                return Path.fromOSString(fullPath);
-            } catch (Exception e) {
-                IpsPlugin.log(new IpsStatus("Error initializing classpath container for source bundle " + pluginId, e)); //$NON-NLS-1$ 
-                return null;
-            }
-        } else {
-            try {
-                installLocation = bundle.getEntry("bin"); //$NON-NLS-1$ 
-                String fullPath;
-                if (installLocation != null) {
-                    URL local = FileLocator.toFileURL(installLocation);
-                    fullPath = new File(local.getPath()).getAbsolutePath();
-                } else {
-                    fullPath = FileLocator.getBundleFile(bundle).getAbsolutePath();
-                }
-                return Path.fromOSString(fullPath);
-            } catch (Exception e) {
-                IpsPlugin.log(new IpsStatus("Error initializing classpath container for bundle " + pluginId, e)); //$NON-NLS-1$ 
-                return null;
-            }
-        }
-    }
-
     class IpsClasspathContainer implements IClasspathContainer {
 
         private final IPath containerPath;
@@ -116,11 +61,26 @@ public class IpsClasspathContainerInitializer extends ClasspathContainerInitiali
 
         public IpsClasspathContainer(IPath containerPath) {
             this.containerPath = containerPath;
+            ArrayList<IClasspathEntry> entryList = new ArrayList<IClasspathEntry>();
+
             IClasspathEntry runtime = JavaCore.newLibraryEntry(getBundlePath(RUNTIME_BUNDLE, false),
                     getBundlePath(RUNTIME_BUNDLE, true), null);
             IClasspathEntry valuetypes = JavaCore.newLibraryEntry(getBundlePath(VALUETYPES_BUNDLE, false),
                     getBundlePath(VALUETYPES_BUNDLE, true), null);
-            entries = new IClasspathEntry[] { runtime, valuetypes };
+
+            entryList.add(runtime);
+            entryList.add(valuetypes);
+
+            if (containerPath.segmentCount() == 2 && !containerPath.lastSegment().isEmpty()) {
+                String lastSegment = containerPath.lastSegment();
+                String[] addEntries = lastSegment.split(","); //$NON-NLS-1$
+                for (String additionalEntry : addEntries) {
+                    IClasspathEntry addEntry = JavaCore.newLibraryEntry(getBundlePath(additionalEntry, false),
+                            getBundlePath(VALUETYPES_BUNDLE, true), null);
+                    entryList.add(addEntry);
+                }
+            }
+            entries = entryList.toArray(new IClasspathEntry[entryList.size()]);
         }
 
         @Override
@@ -141,6 +101,63 @@ public class IpsClasspathContainerInitializer extends ClasspathContainerInitiali
         @Override
         public IPath getPath() {
             return containerPath;
+        }
+
+        private IPath getBundlePath(String pluginId, boolean sources) {
+            Bundle bundle = Platform.getBundle(pluginId);
+            if (bundle == null) {
+                IpsPlugin
+                        .log(new IpsStatus(
+                                "Error initializing " + (sources ? "source for " : "") + "classpath container. Bundle " + pluginId + " not found.")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$
+                return null;
+            }
+
+            URL installLocation;
+            if (sources) {
+                try {
+                    installLocation = bundle.getEntry("src"); //$NON-NLS-1$ 
+                    String fullPath;
+                    if (installLocation != null) {
+                        URL local = FileLocator.toFileURL(installLocation);
+                        fullPath = new File(local.getPath()).getAbsolutePath();
+                    } else {
+                        fullPath = FileLocator.getBundleFile(bundle).getAbsolutePath();
+                        String[] split = fullPath.split(NAME_VERSION_SEP);
+                        if (split.length < 2) {
+                            return null;
+                        }
+                        split[split.length - 2] = split[split.length - 2] + ".source"; //$NON-NLS-1$
+                        fullPath = StringUtils.EMPTY;
+                        for (String string : split) {
+                            if (string != split[split.length - 1]) {
+                                fullPath += string + NAME_VERSION_SEP;
+                            } else {
+                                fullPath += string;
+                            }
+                        }
+                    }
+                    return Path.fromOSString(fullPath);
+                } catch (Exception e) {
+                    IpsPlugin.log(new IpsStatus(
+                            "Error initializing classpath container for source bundle " + pluginId, e)); //$NON-NLS-1$ 
+                    return null;
+                }
+            } else {
+                try {
+                    installLocation = bundle.getEntry("bin"); //$NON-NLS-1$ 
+                    String fullPath;
+                    if (installLocation != null) {
+                        URL local = FileLocator.toFileURL(installLocation);
+                        fullPath = new File(local.getPath()).getAbsolutePath();
+                    } else {
+                        fullPath = FileLocator.getBundleFile(bundle).getAbsolutePath();
+                    }
+                    return Path.fromOSString(fullPath);
+                } catch (Exception e) {
+                    IpsPlugin.log(new IpsStatus("Error initializing classpath container for bundle " + pluginId, e)); //$NON-NLS-1$ 
+                    return null;
+                }
+            }
         }
 
     }
