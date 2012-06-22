bd399ceb5126d4c6b8e5da81eccacd4f12b6ed5f
dirmeier <dirmeier@faktorzehn.de>
2012-06-23 01:59:36 +0200

    update commit command for setting new version

--- org.faktorips.build/buildRelease.sh
+++ org.faktorips.build/buildRelease.sh
@@ -122,7 +122,7 @@ setVersion()
 {
 	echo -e "\nSetting new Version ${NEW_VERSION}\n"
 	$MAVEN_CMD -f ${BUILD_POM} org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=${NEW_VERSION}-SNAPSHOT
-	git commit */pom.xml */META-INF/MANIFEST.MF -m "Setting new version ${NEW_VERSION}"
+	git commit -m "Setting new version ${NEW_VERSION}" -- */pom.xml */META-INF/MANIFEST.MF
 	git push
 }
 
