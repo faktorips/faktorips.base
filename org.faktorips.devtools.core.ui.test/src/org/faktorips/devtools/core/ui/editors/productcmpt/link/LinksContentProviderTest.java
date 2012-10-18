ce86a452acfaae70e956c52ce5100bb823685274
widmaier <stefan.widmaier@faktorzehn.de>
2012-10-18 13:09:05 +0200

    Fips-989: cleanup LinkContentProviderTest

--- org.faktorips.devtools.core.ui.test/src/org/faktorips/devtools/core/ui/editors/productcmpt/link/LinksContentProviderTest.java
+++ org.faktorips.devtools.core.ui.test/src/org/faktorips/devtools/core/ui/editors/productcmpt/link/LinksContentProviderTest.java
@@ -14,8 +14,6 @@
 package org.faktorips.devtools.core.ui.editors.productcmpt.link;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.CALLS_REAL_METHODS;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
@@ -24,15 +22,11 @@ import static org.mockito.Mockito.when;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.eclipse.core.runtime.CoreException;
 import org.faktorips.devtools.core.internal.model.productcmpt.ProductCmpt;
 import org.faktorips.devtools.core.internal.model.productcmpt.ProductCmptGeneration;
 import org.faktorips.devtools.core.internal.model.productcmpt.ProductCmptLink;
-import org.faktorips.devtools.core.model.ipsproject.IIpsProject;
 import org.faktorips.devtools.core.model.productcmpt.IProductCmpt;
 import org.faktorips.devtools.core.model.productcmpt.IProductCmptGeneration;
-import org.faktorips.devtools.core.model.productcmpttype.IProductCmptType;
-import org.faktorips.devtools.core.model.productcmpttype.IProductCmptTypeAssociation;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -55,30 +49,16 @@ public class LinksContentProviderTest {
     @Mock
     private ProductCmptLink staticLink2;
 
-    @Mock
-    private IProductCmptType pcType;
-
-    @Mock
-    private IProductCmptTypeAssociation association;
-    @Mock
-    private IProductCmptTypeAssociation staticAssociation;
-
     private LinksContentProvider provider;
     private List<ProductCmptLink> links;
     private List<ProductCmptLink> staticLinks;
 
     @Before
-    public void setUp() throws CoreException {
+    public void setUp() {
         gen = mock(ProductCmptGeneration.class, CALLS_REAL_METHODS);
         prodCmpt = mock(ProductCmpt.class, CALLS_REAL_METHODS);
 
         provider = new LinksContentProvider();
-        provider.inputChanged(null, null, gen);
-
-        doReturn(pcType).when(prodCmpt).findProductCmptType(any(IIpsProject.class));
-        when(pcType.findAssociation(eq("dummyAssociation"), any(IIpsProject.class))).thenReturn(association);
-        when(pcType.findAssociation(eq("staticDummyAssociation"), any(IIpsProject.class)))
-                .thenReturn(staticAssociation);
 
         when(link1.getAssociation()).thenReturn("dummyAssociation");
         when(link2.getAssociation()).thenReturn("dummyAssociation");
@@ -96,11 +76,6 @@ public class LinksContentProviderTest {
         when(gen.getProductCmpt()).thenReturn(prodCmpt);
         doReturn(links).when(gen).getLinksAsList("dummyAssociation");
         doReturn(staticLinks).when(prodCmpt).getLinksAsList("staticDummyAssociation");
-
-        when(association.isChangingOverTime()).thenReturn(true);
-        when(association.getName()).thenReturn("dummyAssociation");
-        when(staticAssociation.isChangingOverTime()).thenReturn(false);
-        when(staticAssociation.getName()).thenReturn("staticDummyAssociation");
     }
 
     @Test
