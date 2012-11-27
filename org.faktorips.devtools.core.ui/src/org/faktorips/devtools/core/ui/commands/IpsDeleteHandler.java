131bb9c4847700587022158ec4b1315725c4041c
dirmeier <dirmeier@faktorzehn.de>
2012-11-27 11:40:51 +0100

    OPEN - issue FIPS-1125: Kontextmenü des Modell Explorers auch im Instance Explorer  https://jira.faktorzehn.de/browse/FIPS-1125 :: fix delete handler to work with objects that are not adaptable to IResource

--- org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/commands/IpsDeleteHandler.java
+++ org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/commands/IpsDeleteHandler.java
@@ -13,17 +13,21 @@
 
 package org.faktorips.devtools.core.ui.commands;
 
+import java.util.ArrayList;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.window.IShellProvider;
+import org.eclipse.ltk.ui.refactoring.RefactoringWizardOpenOperation;
+import org.eclipse.ltk.ui.refactoring.resource.DeleteResourcesWizard;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.actions.DeleteResourceAction;
 import org.eclipse.ui.handlers.HandlerUtil;
+import org.faktorips.devtools.core.model.IIpsElement;
 import org.faktorips.devtools.core.model.ipsobject.IIpsObjectPart;
+import org.faktorips.devtools.core.ui.util.TypedSelection;
 import org.faktorips.util.ArgumentCheck;
 
 public class IpsDeleteHandler extends AbstractHandler {
@@ -32,32 +36,47 @@ public class IpsDeleteHandler extends AbstractHandler {
     public Object execute(ExecutionEvent event) throws ExecutionException {
         final Shell shell = HandlerUtil.getActiveShell(event);
         ArgumentCheck.notNull(shell, this);
-        DeleteResourceAction deleteAction = new DeleteResourceAction(new IShellProvider() {
-
-            @Override
-            public Shell getShell() {
-                return shell;
-            }
-        });
         ISelection selection = HandlerUtil.getCurrentSelection(event);
-        if (selection instanceof IStructuredSelection) {
-            deleteAction.selectionChanged((IStructuredSelection)selection);
-        }
-        if (canDelete((IStructuredSelection)selection)) {
-            deleteAction.run();
+        IResource[] selectedResources = getSelectedResources(selection);
+        if (selectedResources.length > 0) {
+            DeleteResourcesWizard refactoringWizard = new DeleteResourcesWizard(selectedResources);
+            RefactoringWizardOpenOperation op = new RefactoringWizardOpenOperation(refactoringWizard);
+            try {
+                op.run(shell, Messages.IpsDeleteHandler_deleteResources);
+            } catch (InterruptedException e) {
+                // do nothing
+            }
         }
         return null;
     }
 
-    private boolean canDelete(IStructuredSelection selection) {
-        Object[] items = selection.toArray();
-        boolean canDelete = true;
-        for (Object item : items) {
-            if (item instanceof IAdaptable && ((IAdaptable)item).getAdapter(IIpsObjectPart.class) != null) {
-                canDelete = false;
+    private IResource[] getSelectedResources(ISelection sel) {
+        ArrayList<IResource> result = new ArrayList<IResource>();
+        TypedSelection<IAdaptable> typedSelection = new TypedSelection<IAdaptable>(IAdaptable.class, sel);
+        if (typedSelection.isValid()) {
+            for (IAdaptable adaptable : typedSelection.getElements()) {
+                IResource resource = getAdaptedResource(adaptable);
+                if (resource != null) {
+                    result.add(resource);
+                }
             }
         }
-        return canDelete;
+        return result.toArray(new IResource[result.size()]);
+    }
+
+    private IResource getAdaptedResource(IAdaptable adaptable) {
+        if (adaptable instanceof IResource) {
+            return (IResource)adaptable;
+        }
+        IResource resource = (IResource)adaptable.getAdapter(IResource.class);
+        if (resource != null) {
+            return resource;
+        }
+        IIpsElement ipsElement = (IIpsElement)adaptable.getAdapter(IIpsElement.class);
+        if (ipsElement != null && !(ipsElement instanceof IIpsObjectPart)) {
+            return getAdaptedResource(ipsElement);
+        }
+        return null;
     }
 
 }
