d2dd4bbfa9609de9260d7d229392a48369910cf6
dirmeier <dirmeier@faktorzehn.de>
2013-02-13 12:33:09 +0100

    FIPS-1511 :: fix release build  * move product build to separate module (currently not linked in aggreator, only manual build)  * deploy repository to deploy.server using apache-wagon (scp) instead of file-deploy  * rename unzipAndBuildComposite.sh to buildComposite.sh  * No need of unzipping the repository because the new scp deployment simply compies all the files in the repository instead of the zip

--- org.faktorips.p2repository/buildComposites.sh
+++ org.faktorips.p2repository/buildComposites.sh
@@ -47,29 +47,12 @@ printf "$footer" >> $artifactFile
 set -e
 set -x
 
-#First unzip and remove the zip file given via second parameter
-BASEDIR=$1
-ZIPFILE=$2
-if [ -z $BASEDIR ] || [ -z $ZIPFILE ]
-then
-	echo "Decompress a ziped update site and create composite repositories for every directory up to a given base directory."
-	echo "The composite repositories reference every sub directory."
-	echo "The script overwrites existing composite repository without asking."
-	echo "Usage:"
-	echo "First parameter set the base directory for the composite repository"
-	echo "Second parameter set the update site zip file to unzip. Zipfile must be located in a subdirectory of the base directory"
-	echo "e.g. unzipAndBuildComposite /var/www/update.faktorzehn.org/faktorips /var/www/update.faktorzehn.org/faktorips/v3_7/3.7.0-x/v3_7-3.7.0.x.zip"
-	exit 1
-fi
-ZIPDIR=$(dirname $ZIPFILE)
-unzip -d $ZIPDIR $ZIPFILE
-#rm $ZIPFILE
-
-#second create composites for every directory until first parameter
-COMPOSITE_DIR=$ZIPDIR
+BASEDIR=`readlink -f $1`
+REPOSITORYDIR=`readlink -f $2`
+COMPOSITE_DIR=`readlink -f $REPOSITORYDIR`
 until [ $COMPOSITE_DIR = $BASEDIR ] || [ $COMPOSITE_DIR = "/" ]
 do
-	COMPOSITE_DIR=$(dirname $COMPOSITE_DIR)
+	COMPOSITE_DIR=`dirname $COMPOSITE_DIR`
 	createComposite
 done
 
