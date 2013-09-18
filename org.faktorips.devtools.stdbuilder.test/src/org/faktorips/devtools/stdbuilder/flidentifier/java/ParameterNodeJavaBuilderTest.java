0559946123939ee5a3d792bd626caf79f1d15acb
ifrank <Ilja.Frank@faktorzehn.de>
2013-09-18 06:29:44 +0200

    FIPS-2103/FIPS-2185 :: new ParameterNodeJavaBuilder

---
+++ org.faktorips.devtools.stdbuilder.test/src/org/faktorips/devtools/stdbuilder/flidentifier/java/ParameterNodeJavaBuilderTest.java
@@ -0,0 +1,95 @@
+/*******************************************************************************
+ * Copyright (c) 2005-2012 Faktor Zehn AG und andere.
+ * 
+ * Alle Rechte vorbehalten.
+ * 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
+ * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
+ * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
+ * http://www.faktorzehn.org/f10-org:lizenzen:community eingesehen werden kann.
+ * 
+ * Mitwirkende: Faktor Zehn AG - initial API and implementation - http://www.faktorzehn.de
+ *******************************************************************************/
+
+package org.faktorips.devtools.stdbuilder.flidentifier.java;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+
+import org.faktorips.codegen.CodeFragment;
+import org.faktorips.codegen.JavaCodeFragment;
+import org.faktorips.datatype.Datatype;
+import org.faktorips.devtools.core.builder.flidentifier.IdentifierNodeBuilderFactory;
+import org.faktorips.devtools.core.builder.flidentifier.ast.InvalidIdentifierNode;
+import org.faktorips.devtools.core.builder.flidentifier.ast.ParameterNode;
+import org.faktorips.devtools.core.model.ipsproject.IIpsProject;
+import org.faktorips.devtools.core.model.method.IParameter;
+import org.faktorips.devtools.stdbuilder.StandardBuilderSet;
+import org.faktorips.fl.CompilationResult;
+import org.faktorips.util.message.Message;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ParameterNodeJavaBuilderTest {
+
+    @Mock
+    private IdentifierNodeBuilderFactory<JavaCodeFragment> factory;
+
+    @Mock
+    private StandardBuilderSet builderSet;
+
+    @Mock
+    private IIpsProject ipsProject;
+
+    private ParameterNodeJavaBuilder parameterNodeJavaBuilder;
+
+    private ParameterNode parameterNode;
+
+    @Before
+    public void createParameterNodeJavaBuilder() throws Exception {
+        parameterNodeJavaBuilder = new ParameterNodeJavaBuilder(factory, builderSet);
+    }
+
+    private void setUpParameterNode() throws Exception {
+        IParameter parameter = mock(IParameter.class);
+        when(parameter.findDatatype(ipsProject)).thenReturn(Datatype.STRING);
+        when(parameter.getName()).thenReturn("ParamName");
+        parameterNode = new ParameterNode(parameter, ipsProject);
+    }
+
+    @Test
+    public void testGetCompilationResult() throws Exception {
+        setUpParameterNode();
+        CompilationResult<JavaCodeFragment> compilationResult = parameterNodeJavaBuilder.getCompilationResult(
+                parameterNode, null);
+        assertNotNull(compilationResult);
+        assertNotNull(compilationResult.getCodeFragment());
+        assertEquals("ParamName", compilationResult.getCodeFragment().getSourcecode());
+        assertEquals(Datatype.STRING, compilationResult.getDatatype());
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testGetCompilationResult_NoInteractionToContextCompilationResult() throws Exception {
+        setUpParameterNode();
+        CompilationResult<CodeFragment> contextCompilationResult = mock(CompilationResult.class);
+        parameterNodeJavaBuilder.getCompilationResult(parameterNode, contextCompilationResult);
+        verifyZeroInteractions(contextCompilationResult);
+    }
+
+    @Test
+    public void testGetErrorCompilationResult() throws Exception {
+        Message errorMessage = new Message("code", "errorMessage", Message.ERROR);
+        InvalidIdentifierNode invalidIdentifierNode = new InvalidIdentifierNode(errorMessage);
+        CompilationResult<JavaCodeFragment> errorCompilationResult = parameterNodeJavaBuilder
+                .getErrorCompilationResult(invalidIdentifierNode);
+        assertEquals(errorMessage, errorCompilationResult.getMessages().getMessageByCode("code"));
+    }
+}
---
+++ org.faktorips.devtools.stdbuilder/src/org/faktorips/devtools/stdbuilder/flidentifier/java/ParameterNodeJavaBuilder.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) 2005-2012 Faktor Zehn AG und andere.
+ * 
+ * Alle Rechte vorbehalten.
+ * 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
+ * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
+ * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
+ * http://www.faktorzehn.org/f10-org:lizenzen:community eingesehen werden kann.
+ * 
+ * Mitwirkende: Faktor Zehn AG - initial API and implementation - http://www.faktorzehn.de
+ *******************************************************************************/
+
+package org.faktorips.devtools.stdbuilder.flidentifier.java;
+
+import org.faktorips.codegen.CodeFragment;
+import org.faktorips.codegen.JavaCodeFragment;
+import org.faktorips.datatype.Datatype;
+import org.faktorips.devtools.core.builder.flidentifier.IdentifierNodeBuilderFactory;
+import org.faktorips.devtools.core.builder.flidentifier.ast.IdentifierNode;
+import org.faktorips.devtools.core.builder.flidentifier.ast.InvalidIdentifierNode;
+import org.faktorips.devtools.core.builder.flidentifier.ast.ParameterNode;
+import org.faktorips.devtools.core.model.method.IParameter;
+import org.faktorips.devtools.stdbuilder.StandardBuilderSet;
+import org.faktorips.devtools.stdbuilder.flidentifier.AbstractIdentifierJavaBuilder;
+import org.faktorips.fl.CompilationResult;
+import org.faktorips.fl.CompilationResultImpl;
+
+/**
+ * JavaBuilder for a {@link ParameterNode}
+ * 
+ * @author frank
+ * @since 3.11.0
+ */
+public class ParameterNodeJavaBuilder extends AbstractIdentifierJavaBuilder<JavaCodeFragment> {
+
+    public ParameterNodeJavaBuilder(IdentifierNodeBuilderFactory<JavaCodeFragment> factory,
+            StandardBuilderSet builderSet) {
+        super(factory, builderSet);
+    }
+
+    @Override
+    protected CompilationResult<JavaCodeFragment> getCompilationResult(IdentifierNode identifierNode,
+            CompilationResult<CodeFragment> contextCompilationResult) {
+        ParameterNode parameterNode = (ParameterNode)identifierNode;
+        Datatype datatype = parameterNode.getDatatype();
+        IParameter parameter = parameterNode.getParameter();
+        return new CompilationResultImpl(parameter.getName(), datatype);
+    }
+
+    @Override
+    protected CompilationResult<JavaCodeFragment> getErrorCompilationResult(InvalidIdentifierNode invalidIdentifierNode) {
+        return new CompilationResultImpl(invalidIdentifierNode.getMessage());
+    }
+}
