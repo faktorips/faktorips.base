0c4ddbd0f0ac4b1d7aafaed9a3870ee5b7c28c8a
ifrank <Ilja.Frank@faktorzehn.de>
2013-09-19 16:13:57 +0200

    FIPS-2103/FIPS-2189 :: new IndexBasedAssociationNodeGenerator

---
+++ org.faktorips.devtools.stdbuilder.test/src/org/faktorips/devtools/stdbuilder/flidentifier/IndexBasedAssociationNodeGeneratorTest.java
@@ -0,0 +1,90 @@
+/*******************************************************************************
+ * Copyright (c) 2005-2012 Faktor Zehn AG und andere.
+ * 
+ * Alle Rechte vorbehalten.
+ * 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
+ * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
+ * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
+ * http://www.faktorzehn.org/f10-org:lizenzen:community eingesehen werden kann.
+ * 
+ * Mitwirkende: Faktor Zehn AG - initial API and implementation - http://www.faktorzehn.de
+ *******************************************************************************/
+
+package org.faktorips.devtools.stdbuilder.flidentifier;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+
+import org.faktorips.codegen.JavaCodeFragment;
+import org.faktorips.devtools.core.builder.flidentifier.IdentifierNodeGeneratorFactory;
+import org.faktorips.devtools.core.builder.flidentifier.ast.IdentifierNodeFactory;
+import org.faktorips.devtools.core.builder.flidentifier.ast.IndexBasedAssociationNode;
+import org.faktorips.devtools.core.internal.model.type.Association;
+import org.faktorips.devtools.core.model.ipsproject.IIpsProject;
+import org.faktorips.devtools.stdbuilder.StandardBuilderSet;
+import org.faktorips.devtools.stdbuilder.xpand.policycmpt.model.XPolicyAssociation;
+import org.faktorips.fl.CompilationResult;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class IndexBasedAssociationNodeGeneratorTest {
+
+    @Mock
+    private IdentifierNodeGeneratorFactory<JavaCodeFragment> factory;
+
+    @Mock
+    private StandardBuilderSet builderSet;
+
+    @Mock
+    private IIpsProject ipsProject;
+
+    @Mock
+    private CompilationResult<JavaCodeFragment> contextCompilationResult;
+
+    @Mock
+    Association association;
+
+    private IndexBasedAssociationNodeGenerator indexBasedAssociationNodeGenerator;
+
+    private IndexBasedAssociationNode indexBasedAssociationNode;
+
+    private IdentifierNodeFactory nodeFactory;
+
+    @Before
+    public void createIndexBasedAssociationNodeGenerator() throws Exception {
+        nodeFactory = new IdentifierNodeFactory("IndexBasedAssociationNodeGeneratorTest", ipsProject);
+        indexBasedAssociationNodeGenerator = new IndexBasedAssociationNodeGenerator(factory, builderSet);
+    }
+
+    private IndexBasedAssociationNode createIndexBasedAssociationNode(int index) {
+        return (IndexBasedAssociationNode)nodeFactory.createIndexBasedAssociationNode(association, index);
+    }
+
+    @Test
+    public void testGetCompilationResult() throws Exception {
+        JavaCodeFragment javaCodeFragment = spy(new JavaCodeFragment("vertrag"));
+        XPolicyAssociation xPolicyAssociation = mock(XPolicyAssociation.class);
+        when(contextCompilationResult.getCodeFragment()).thenReturn(javaCodeFragment);
+        when(builderSet.getModelNode(association, XPolicyAssociation.class)).thenReturn(xPolicyAssociation);
+        when(xPolicyAssociation.getMethodNameGetSingle()).thenReturn("getDeckungen");
+        indexBasedAssociationNode = createIndexBasedAssociationNode(1);
+
+        CompilationResult<JavaCodeFragment> compilationResult = indexBasedAssociationNodeGenerator
+                .getCompilationResult(indexBasedAssociationNode, contextCompilationResult);
+
+        assertFalse(compilationResult.failed());
+        assertEquals("vertrag.getDeckungen(1)", compilationResult.getCodeFragment().getSourcecode());
+        verify(javaCodeFragment).getImportDeclaration();
+        verifyZeroInteractions(javaCodeFragment);
+    }
+}
---
+++ org.faktorips.devtools.stdbuilder/src/org/faktorips/devtools/stdbuilder/flidentifier/IndexBasedAssociationNodeGenerator.java
@@ -0,0 +1,62 @@
+/*******************************************************************************
+ * Copyright (c) 2005-2012 Faktor Zehn AG und andere.
+ * 
+ * Alle Rechte vorbehalten.
+ * 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
+ * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
+ * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
+ * http://www.faktorzehn.org/f10-org:lizenzen:community eingesehen werden kann.
+ * 
+ * Mitwirkende: Faktor Zehn AG - initial API and implementation - http://www.faktorzehn.de
+ *******************************************************************************/
+
+package org.faktorips.devtools.stdbuilder.flidentifier;
+
+import org.faktorips.codegen.JavaCodeFragment;
+import org.faktorips.devtools.core.builder.flidentifier.IdentifierNodeGeneratorFactory;
+import org.faktorips.devtools.core.builder.flidentifier.ast.IdentifierNode;
+import org.faktorips.devtools.core.builder.flidentifier.ast.IndexBasedAssociationNode;
+import org.faktorips.devtools.core.model.type.IAssociation;
+import org.faktorips.devtools.stdbuilder.StandardBuilderSet;
+import org.faktorips.devtools.stdbuilder.xpand.policycmpt.model.XPolicyAssociation;
+import org.faktorips.fl.CompilationResult;
+import org.faktorips.fl.CompilationResultImpl;
+
+/**
+ * Generator for {@link IndexBasedAssociationNode}
+ * 
+ * @author frank
+ * @since 3.11.0
+ */
+public class IndexBasedAssociationNodeGenerator extends AbstractIdentifierGenerator {
+
+    public IndexBasedAssociationNodeGenerator(IdentifierNodeGeneratorFactory<JavaCodeFragment> factory,
+            StandardBuilderSet builderSet) {
+        super(factory, builderSet);
+    }
+
+    @Override
+    protected CompilationResult<JavaCodeFragment> getCompilationResult(IdentifierNode identifierNode,
+            CompilationResult<JavaCodeFragment> contextCompilationResult) {
+        IndexBasedAssociationNode node = (IndexBasedAssociationNode)identifierNode;
+        JavaCodeFragment codeFragement = createAssociationGetterWithIndex(
+                contextCompilationResult.getCodeFragment(), node);
+        return new CompilationResultImpl(codeFragement, node.getDatatype());
+    }
+
+    private JavaCodeFragment createAssociationGetterWithIndex(JavaCodeFragment contextCodeFragment,
+            IndexBasedAssociationNode node) {
+        JavaCodeFragment newJavaCodeFragment = new JavaCodeFragment();
+        newJavaCodeFragment.append(contextCodeFragment);
+        newJavaCodeFragment.append('.');
+        newJavaCodeFragment.append(getAssociationTargetAtIndexGetterName(node.getAssociation()));
+        newJavaCodeFragment.append("(" + node.getIndex() + ")"); //$NON-NLS-1$ //$NON-NLS-2$
+        return newJavaCodeFragment;
+    }
+
+    private String getAssociationTargetAtIndexGetterName(IAssociation association) {
+        XPolicyAssociation xPolicyAssociation = getModelNode(association, XPolicyAssociation.class);
+        return xPolicyAssociation.getMethodNameGetSingle();
+    }
+}
