a5bbdb870c7a2418e5775d809586ac9fbef6a083
dirmeier <Cornelius.Dirmeier@faktorzehn.de>
2013-09-30 13:30:05 +0200

    FIPS-2227 :: set ClassLoader from builder class in Thread-Context-ClassLoader because maven does overwrite the correct one

--- org.faktorips.devtools.stdbuilder.test/src/org/faktorips/devtools/stdbuilder/xpand/XpandBuilderTest.java
+++ org.faktorips.devtools.stdbuilder.test/src/org/faktorips/devtools/stdbuilder/xpand/XpandBuilderTest.java
@@ -15,6 +15,7 @@ package org.faktorips.devtools.stdbuilder.xpand;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
 import static org.mockito.Mockito.when;
 
 import org.faktorips.devtools.core.model.ipsproject.IIpsProject;
@@ -36,6 +37,9 @@ public class XpandBuilderTest {
     @Mock
     private GeneratorModelContext generatorModelContext;
 
+    @Mock
+    private ClassLoader myUselessClassLoader;
+
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
@@ -51,7 +55,8 @@ public class XpandBuilderTest {
 
     @Test
     public void testBeforeBuildProcess() throws Exception {
-        when(generatorModelContext.getResourceManager()).thenReturn(new OptimizedResourceManager());
+        OptimizedResourceManager resourceManager = new OptimizedResourceManager();
+        when(generatorModelContext.getResourceManager()).thenReturn(resourceManager);
         // Using PolicyCmptImlClassBuilder as concrete instance knowing that this also tests this
         // other class
         PolicyCmptClassBuilder policyCmptClassBuilder = new PolicyCmptClassBuilder(false, builderSet,
@@ -60,4 +65,25 @@ public class XpandBuilderTest {
         assertNotNull(policyCmptClassBuilder.getOut());
         assertNotNull(policyCmptClassBuilder.getTemplateDefinition());
     }
+
+    /**
+     * FIPS-2227
+     */
+    @Test
+    public void testBuild_correctContextClassloader() throws Exception {
+        when(generatorModelContext.getResourceManager()).thenReturn(new OptimizedResourceManager());
+        PolicyCmptClassBuilder policyCmptClassBuilder = new PolicyCmptClassBuilder(false, builderSet,
+                generatorModelContext, null);
+        ClassLoader oldClassLoader = Thread.currentThread().getContextClassLoader();
+        Thread.currentThread().setContextClassLoader(myUselessClassLoader);
+
+        policyCmptClassBuilder.beforeBuildProcess(ipsProject, 0);
+        policyCmptClassBuilder.afterBuildProcess(ipsProject, 0);
+
+        assertNotNull(policyCmptClassBuilder.getTemplateDefinition());
+        assertSame(myUselessClassLoader, Thread.currentThread().getContextClassLoader());
+
+        Thread.currentThread().setContextClassLoader(oldClassLoader);
+    }
+
 }
