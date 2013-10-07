eb16c160327a49efc559422d5b954583cbffc5fb
ifrank <Ilja.Frank@faktorzehn.de>
2013-10-07 06:26:03 +0200

    FIPS-1498/FIPS-2251 :: show overlay icon on constrain associations

---
+++ org.faktorips.devtools.core.ui.test/src/org/faktorips/devtools/core/ui/workbenchadapters/AssociationWorkbenchAdapterTest.java
@@ -0,0 +1,201 @@
+/*******************************************************************************
+ * Copyright (c) 2005-2012 Faktor Zehn AG und andere.
+ * 
+ * Alle Rechte vorbehalten.
+ * 
+ * Dieses Programm und alle mitgelieferten Sachen (Dokumentationen, Beispiele, Konfigurationen,
+ * etc.) duerfen nur unter den Bedingungen der Faktor-Zehn-Community Lizenzvereinbarung - Version
+ * 0.1 (vor Gruendung Community) genutzt werden, die Bestandteil der Auslieferung ist und auch unter
+ * http://www.faktorzehn.org/f10-org:lizenzen:community eingesehen werden kann.
+ * 
+ * Mitwirkende: Faktor Zehn AG - initial API and implementation - http://www.faktorzehn.de
+ *******************************************************************************/
+
+package org.faktorips.devtools.core.ui.workbenchadapters;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.faktorips.abstracttest.AbstractIpsPluginTest;
+import org.faktorips.devtools.core.model.ipsproject.IIpsProject;
+import org.faktorips.devtools.core.model.pctype.IPolicyCmptTypeAssociation;
+import org.faktorips.devtools.core.model.pctype.IPolicyCmptTypeAttribute;
+import org.faktorips.devtools.core.model.productcmpttype.IProductCmptTypeAssociation;
+import org.faktorips.devtools.core.model.type.AssociationType;
+import org.faktorips.devtools.core.ui.IpsUIPlugin;
+import org.faktorips.devtools.core.ui.OverlayIcons;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AssociationWorkbenchAdapterTest extends AbstractIpsPluginTest {
+
+    private AssociationWorkbenchAdapter workbenchAdapter;
+
+    @Mock
+    private IProductCmptTypeAssociation aProductAssociation;
+
+    @Mock
+    private IPolicyCmptTypeAssociation aPolicyAssociation;
+
+    @Mock
+    private IIpsProject ipsProject;
+
+    @Mock
+    private IPolicyCmptTypeAttribute aPolicyCmptAttribute;
+
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        workbenchAdapter = new AssociationWorkbenchAdapter();
+        when(aProductAssociation.isChangingOverTime()).thenReturn(true);
+        when(aProductAssociation.isConstrains()).thenReturn(false);
+    }
+
+    @Test
+    public void testGetImageDescriptor_BaseNameAggregation() throws Exception {
+        when(aProductAssociation.getAssociationType()).thenReturn(AssociationType.AGGREGATION);
+
+        ImageDescriptor imageDescriptor = workbenchAdapter.getImageDescriptor(aProductAssociation);
+
+        assertNotNull(imageDescriptor);
+        assertTrue(createImageDescriptor(AssociationWorkbenchAdapter.ASSOCIATION_TYPE_AGGREGATION_IMAGE).equals(
+                imageDescriptor));
+    }
+
+    @Test
+    public void testGetImageDescriptor_BaseNameAssociation() throws Exception {
+        when(aProductAssociation.getAssociationType()).thenReturn(AssociationType.ASSOCIATION);
+
+        ImageDescriptor imageDescriptor = workbenchAdapter.getImageDescriptor(aProductAssociation);
+
+        assertNotNull(imageDescriptor);
+        assertTrue(createImageDescriptor(AssociationWorkbenchAdapter.ASSOCIATION_TYPE_ASSOCIATION_IMAGE).equals(
+                imageDescriptor));
+    }
+
+    @Test
+    public void testGetImageDescriptor_BaseNameCompositionDetailToMaster() throws Exception {
+        when(aProductAssociation.getAssociationType()).thenReturn(AssociationType.COMPOSITION_DETAIL_TO_MASTER);
+
+        ImageDescriptor imageDescriptor = workbenchAdapter.getImageDescriptor(aProductAssociation);
+
+        assertNotNull(imageDescriptor);
+        assertTrue(createImageDescriptor(
+                AssociationWorkbenchAdapter.ASSOCIATION_TYPE_COMPOSITION_DETAIL_TO_MASTER_IMAGE)
+                .equals(imageDescriptor));
+    }
+
+    @Test
+    public void testGetImageDescriptor_BaseNameCompositionMasterToDetail() throws Exception {
+        when(aProductAssociation.getAssociationType()).thenReturn(AssociationType.COMPOSITION_MASTER_TO_DETAIL);
+
+        ImageDescriptor imageDescriptor = workbenchAdapter.getImageDescriptor(aProductAssociation);
+
+        assertNotNull(imageDescriptor);
+        assertTrue(createImageDescriptor(AssociationWorkbenchAdapter.ASSOCIATION_TYPE_COMPOSITION_IMAGE).equals(
+                imageDescriptor));
+    }
+
+    @Test
+    public void testGetImageDescriptor_OverlayChangeOverTime() {
+        when(aProductAssociation.getAssociationType()).thenReturn(AssociationType.ASSOCIATION);
+        when(aProductAssociation.isChangingOverTime()).thenReturn(false);
+
+        ImageDescriptor imageDescriptor = workbenchAdapter.getImageDescriptor(aProductAssociation);
+
+        assertNotNull(imageDescriptor);
+        assertTrue(createImageDescriptorWithChangeOvertime(
+                AssociationWorkbenchAdapter.ASSOCIATION_TYPE_ASSOCIATION_IMAGE).equals(imageDescriptor));
+    }
+
+    @Test
+    public void testGetImageDescriptor_OverlayProductRelevant() throws Exception {
+        when(aPolicyAssociation.getAssociationType()).thenReturn(AssociationType.ASSOCIATION);
+        when(aPolicyAssociation.isConfigured()).thenReturn(true);
+        when(aPolicyAssociation.getIpsProject()).thenReturn(ipsProject);
+        when(aPolicyAssociation.isConstrainedByProductStructure(ipsProject)).thenReturn(true);
+
+        ImageDescriptor imageDescriptor = workbenchAdapter.getImageDescriptor(aPolicyAssociation);
+
+        assertNotNull(imageDescriptor);
+        assertTrue(createImageDescriptorWithProductRelevant(
+                AssociationWorkbenchAdapter.ASSOCIATION_TYPE_ASSOCIATION_IMAGE).equals(imageDescriptor));
+    }
+
+    @Test
+    public void testGetImageDescriptor_OverlayConstrains() {
+        when(aProductAssociation.getAssociationType()).thenReturn(AssociationType.ASSOCIATION);
+        when(aProductAssociation.isConstrains()).thenReturn(true);
+
+        ImageDescriptor imageDescriptor = workbenchAdapter.getImageDescriptor(aProductAssociation);
+
+        assertNotNull(imageDescriptor);
+        assertTrue(createImageDescriptorWithConstrains(AssociationWorkbenchAdapter.ASSOCIATION_TYPE_ASSOCIATION_IMAGE)
+                .equals(imageDescriptor));
+    }
+
+    private ImageDescriptor createImageDescriptor(String baseName) {
+        return createImageDescriptorWithOverwrittenOverlay(baseName, false, false, false);
+    }
+
+    private ImageDescriptor createImageDescriptorWithChangeOvertime(String baseName) {
+        return createImageDescriptorWithOverwrittenOverlay(baseName, false, false, true);
+    }
+
+    private ImageDescriptor createImageDescriptorWithProductRelevant(String baseName) {
+        return createImageDescriptorWithOverwrittenOverlay(baseName, false, true, false);
+    }
+
+    private ImageDescriptor createImageDescriptorWithConstrains(String baseName) {
+        return createImageDescriptorWithOverwrittenOverlay(baseName, true, false, false);
+    }
+
+    private ImageDescriptor createImageDescriptorWithOverwrittenOverlay(String baseName,
+            boolean isOverride,
+            boolean isProductRelevant,
+            boolean noChangeOverTime) {
+        String[] overlays = new String[4];
+        if (isOverride) {
+            overlays[0] = OverlayIcons.OVERRIDE_OVR;
+        }
+        if (isProductRelevant) {
+            overlays[1] = OverlayIcons.PRODUCT_OVR;
+        }
+        if (noChangeOverTime) {
+            overlays[3] = OverlayIcons.NOT_CHANGEOVERTIME_OVR;
+        }
+        return IpsUIPlugin.getImageHandling().getSharedOverlayImage(baseName, overlays);
+    }
+
+    @Test
+    public void testGetLabel() throws Exception {
+        when(aProductAssociation.getTargetRolePlural()).thenReturn("plural");
+        when(aProductAssociation.getTargetRoleSingular()).thenReturn("singular");
+        when(aPolicyCmptAttribute.getName()).thenReturn("name");
+        when(aProductAssociation.is1ToMany()).thenReturn(true);
+
+        assertEquals("plural", workbenchAdapter.getLabel(aProductAssociation));
+
+        when(aProductAssociation.is1ToMany()).thenReturn(false);
+
+        assertEquals("singular", workbenchAdapter.getLabel(aProductAssociation));
+        assertEquals("name", workbenchAdapter.getLabel(aPolicyCmptAttribute));
+    }
+
+    @Test
+    public void testGetDefaultImageDescriptor() {
+        ImageDescriptor sharedImageDescriptor = IpsUIPlugin.getImageHandling().getSharedImageDescriptor(
+                AssociationWorkbenchAdapter.ASSOCIATION_TYPE_ASSOCIATION_IMAGE, true);
+        assertEquals(sharedImageDescriptor, workbenchAdapter.getDefaultImageDescriptor());
+
+    }
+
+}
--- org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/workbenchadapters/AssociationWorkbenchAdapter.java
+++ org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/workbenchadapters/AssociationWorkbenchAdapter.java
@@ -27,47 +27,66 @@ import org.faktorips.devtools.core.ui.OverlayIcons;
 
 public class AssociationWorkbenchAdapter extends IpsObjectPartWorkbenchAdapter {
 
+    public static final String ASSOCIATION_TYPE_COMPOSITION_IMAGE = "AssociationType-Composition.gif"; //$NON-NLS-1$
+    public static final String ASSOCIATION_TYPE_COMPOSITION_DETAIL_TO_MASTER_IMAGE = "AssociationType-CompositionDetailToMaster.gif"; //$NON-NLS-1$
+    public static final String ASSOCIATION_TYPE_ASSOCIATION_IMAGE = "AssociationType-Association.gif"; //$NON-NLS-1$
+    public static final String ASSOCIATION_TYPE_AGGREGATION_IMAGE = "AssociationType-Aggregation.gif"; //$NON-NLS-1$
+
     @Override
     protected ImageDescriptor getImageDescriptor(IIpsObjectPart ipsObjectPart) {
         if (ipsObjectPart instanceof IAssociation) {
             IAssociation association = (IAssociation)ipsObjectPart;
-            String baseName = ""; //$NON-NLS-1$
-            if (association.getAssociationType() == AssociationType.AGGREGATION) {
-                baseName = "AssociationType-Aggregation.gif"; //$NON-NLS-1$
-            } else if (association.getAssociationType() == AssociationType.ASSOCIATION) {
-                baseName = "AssociationType-Association.gif"; //$NON-NLS-1$
-            } else if (association.getAssociationType() == AssociationType.COMPOSITION_DETAIL_TO_MASTER) {
-                baseName = "AssociationType-CompositionDetailToMaster.gif"; //$NON-NLS-1$
-            } else if (association.getAssociationType() == AssociationType.COMPOSITION_MASTER_TO_DETAIL) {
-                baseName = "AssociationType-Composition.gif"; //$NON-NLS-1$
-            }
-            if (association instanceof IPolicyCmptTypeAssociation) {
-                try {
-                    IPolicyCmptTypeAssociation polAssociation = (IPolicyCmptTypeAssociation)association;
-                    if (polAssociation.isConfigured()
-                            && polAssociation.isConstrainedByProductStructure(association.getIpsProject())) {
-                        return IpsUIPlugin.getImageHandling().getSharedOverlayImage(baseName, OverlayIcons.PRODUCT_OVR,
-                                IDecoration.TOP_RIGHT);
-                    }
-                } catch (CoreException e) {
-                    IpsPlugin.log(e);
-                }
-            }
-            if (association instanceof IProductCmptTypeAssociation) {
-                IProductCmptTypeAssociation productAssociation = (IProductCmptTypeAssociation)association;
-                if (!productAssociation.isChangingOverTime()) {
-                    return IpsUIPlugin.getImageHandling().getSharedOverlayImage(baseName,
-                            OverlayIcons.NOT_CHANGEOVERTIME_OVR, IDecoration.BOTTOM_RIGHT);
-                }
-            }
-            return IpsUIPlugin.getImageHandling().getSharedImageDescriptor(baseName, true);
+            String baseName = getImageBaseName(association);
+            String[] overlays = getImageOverlays(association);
+            return IpsUIPlugin.getImageHandling().getSharedOverlayImage(baseName, overlays);
         }
         return null;
     }
 
+    private String getImageBaseName(IAssociation association) {
+        String baseName = ""; //$NON-NLS-1$
+        if (association.getAssociationType() == AssociationType.AGGREGATION) {
+            baseName = ASSOCIATION_TYPE_AGGREGATION_IMAGE;
+        } else if (association.getAssociationType() == AssociationType.ASSOCIATION) {
+            baseName = ASSOCIATION_TYPE_ASSOCIATION_IMAGE;
+        } else if (association.getAssociationType() == AssociationType.COMPOSITION_DETAIL_TO_MASTER) {
+            baseName = ASSOCIATION_TYPE_COMPOSITION_DETAIL_TO_MASTER_IMAGE;
+        } else if (association.getAssociationType() == AssociationType.COMPOSITION_MASTER_TO_DETAIL) {
+            baseName = ASSOCIATION_TYPE_COMPOSITION_IMAGE;
+        }
+        return baseName;
+    }
+
+    private String[] getImageOverlays(IAssociation association) {
+        String[] overlays = new String[4];
+
+        if (association instanceof IPolicyCmptTypeAssociation) {
+            try {
+                IPolicyCmptTypeAssociation polAssociation = (IPolicyCmptTypeAssociation)association;
+                if (polAssociation.isConfigured()
+                        && polAssociation.isConstrainedByProductStructure(association.getIpsProject())) {
+                    overlays[IDecoration.TOP_RIGHT] = OverlayIcons.PRODUCT_OVR;
+                }
+            } catch (CoreException e) {
+                IpsPlugin.log(e);
+            }
+        }
+        if (association instanceof IProductCmptTypeAssociation) {
+            IProductCmptTypeAssociation productAssociation = (IProductCmptTypeAssociation)association;
+            if (!productAssociation.isChangingOverTime()) {
+                overlays[IDecoration.BOTTOM_RIGHT] = OverlayIcons.NOT_CHANGEOVERTIME_OVR;
+            }
+        }
+
+        if (association.isConstrains()) {
+            overlays[IDecoration.TOP_LEFT] = OverlayIcons.OVERRIDE_OVR;
+        }
+        return overlays;
+    }
+
     @Override
     public ImageDescriptor getDefaultImageDescriptor() {
-        return IpsUIPlugin.getImageHandling().getSharedImageDescriptor("AssociationType-Association.gif", true); //$NON-NLS-1$
+        return IpsUIPlugin.getImageHandling().getSharedImageDescriptor(ASSOCIATION_TYPE_ASSOCIATION_IMAGE, true);
     }
 
     @Override
