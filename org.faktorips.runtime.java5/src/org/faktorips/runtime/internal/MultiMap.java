3a69d0c66958def5271d0843affa5805e0d0f2f5
Ines Maazi <ines.maazi@faktorzehn.de>
2013-11-25 10:25:09 +0100

    FIPS-2383/FIPS-2407 :: MultiMap refactored and it returns now an unmodifiable Set

--- org.faktorips.runtime.java5/src/org/faktorips/runtime/internal/MultiMap.java
+++ org.faktorips.runtime.java5/src/org/faktorips/runtime/internal/MultiMap.java
@@ -13,13 +13,14 @@
 
 package org.faktorips.runtime.internal;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 /**
- * The MultiMap manages a Map, which maps keys to a Set of values.
+ * A Map which maps keys to sets of values.
  * 
  */
 
@@ -27,24 +28,43 @@ public class MultiMap<K, V> {
 
     private Map<K, Set<V>> internalHashMap = new HashMap<K, Set<V>>();
 
-    protected Set<V> get(K key) {
-        getOrCreateSetFor(key);
-        return internalHashMap.get(key);
-    }
+    private final Set<V> emptySet = new HashSet<V>();
 
+    /**
+     * Associates the specified value with the specified key in this map. If the map previously
+     * contained a mapping for the key, the set will be extended by the specified value.
+     * 
+     * @param key with which the specified value is to be associated
+     * @param value value to be associated with the specified key
+     */
     protected void put(K key, V value) {
-        Set<V> setInMap = getOrCreateSetFor(key);
-        setInMap = internalHashMap.get(key);
-        setInMap.add(value);
+        Set<V> newSet = new HashSet<V>();
+        if (internalHashMap.containsKey(key)) {
+            newSet = internalHashMap.get(key);
+        } else {
+            internalHashMap.put(key, newSet);
+        }
+        newSet.add(value);
     }
 
-    private Set<V> getOrCreateSetFor(K key) {
-        Set<V> set = internalHashMap.get(key);
-        if (set != null) {
-            return set;
+    /**
+     * Returns the according set of a key of the internalHashMap. If there is no set for a key, an
+     * empty set will be returned.
+     * 
+     * @param key the key whose associated set of values will be returned
+     * @return set of values to which the specified key is mapped or an empty Set if the map
+     *         contains no mapping for the key
+     */
+    protected Set<V> get(K key) {
+        Set<V> set = checkAndGetRightSet(key);
+        return Collections.unmodifiableSet(set);
+    }
+
+    private Set<V> checkAndGetRightSet(K key) {
+        if (internalHashMap.containsKey(key)) {
+            return internalHashMap.get(key);
         }
-        Set<V> newSet = new HashSet<V>();
-        return internalHashMap.put(key, newSet);
+        return emptySet;
     }
 
     public Map<K, Set<V>> getInternalMap() {
