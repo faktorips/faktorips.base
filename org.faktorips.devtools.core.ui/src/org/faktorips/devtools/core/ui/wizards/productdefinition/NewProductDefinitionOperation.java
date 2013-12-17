d387f3d24f0618c2c8723fe5376f1e3ee1f9fb0f
Daniel Schwering <Daniel.Schwering@faktorzehn.de>
2013-12-17 12:01:43 +0100

    FIPS-2489 :: introduce extension point for new product definition operation participants

--- org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/wizards/productdefinition/NewProductDefinitionOperation.java
+++ org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/wizards/productdefinition/NewProductDefinitionOperation.java
@@ -14,14 +14,21 @@
 package org.faktorips.devtools.core.ui.wizards.productdefinition;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IExtension;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.ui.actions.WorkspaceModifyOperation;
+import org.faktorips.devtools.core.ExtensionPoints;
+import org.faktorips.devtools.core.IpsPlugin;
+import org.faktorips.devtools.core.model.INewProductDefinitionOperationParticipant;
 
 public class NewProductDefinitionOperation extends WorkspaceModifyOperation {
 
     private final NewProductDefinitionPMO pmo;
+    private List<INewProductDefinitionOperationParticipant> participants;
 
     public NewProductDefinitionOperation(NewProductDefinitionPMO pmo) {
         this.pmo = pmo;
@@ -31,6 +38,31 @@ public class NewProductDefinitionOperation extends WorkspaceModifyOperation {
     protected void execute(IProgressMonitor monitor) throws CoreException, InvocationTargetException,
             InterruptedException {
 
+    }
+
+    /**
+     * Returns the {@link INewProductDefinitionOperationParticipant
+     * INewProductDefinitionOperationParticipants} defined in the extension point
+     * {@code org.faktorips.devtools.core.newProductDefinitionOperation}.
+     * 
+     * @return the {@link INewProductDefinitionOperationParticipant
+     *         INewProductDefinitionOperationParticipants}
+     */
+    private List<INewProductDefinitionOperationParticipant> getParticipants() {
+        if (participants == null) {
+            ExtensionPoints extensionPoints = new ExtensionPoints(IpsPlugin.getDefault().getExtensionRegistry(),
+                    IpsPlugin.PLUGIN_ID);
+            IExtension[] extensions = extensionPoints
+                    .getExtension(INewProductDefinitionOperationParticipant.EXTENSION_POINT_ID_NEW_PRODUCT_DEFINITION_OPERATION);
+            participants = new ArrayList<INewProductDefinitionOperationParticipant>();
+            for (IExtension extension : extensions) {
+                participants.addAll(ExtensionPoints.createExecutableExtensions(extension,
+                        INewProductDefinitionOperationParticipant.CONFIG_ELEMENT_ID_PARTICIPANT,
+                        INewProductDefinitionOperationParticipant.CONFIG_ELEMENT_ATTRIBUTE_CLASS,
+                        INewProductDefinitionOperationParticipant.class));
+            }
+        }
+        return participants;
     }
 
 }
