9bfb842b28922a8e2da05127061738578f387d33
dirmeier <Cornelius.Dirmeier@faktorzehn.de>
2014-01-23 11:35:37 +0100

    FIPS-2208/FIPS-2622 :: refactor ValueSetType to java 5 enum

--- org.faktorips.devtools.core/src/org/faktorips/devtools/core/model/valueset/ValueSetType.java
+++ org.faktorips.devtools.core/src/org/faktorips/devtools/core/model/valueset/ValueSetType.java
@@ -11,13 +11,9 @@
 
 package org.faktorips.devtools.core.model.valueset;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import org.faktorips.devtools.core.enums.DefaultEnumType;
-import org.faktorips.devtools.core.enums.DefaultEnumValue;
-import org.faktorips.devtools.core.enums.EnumType;
-import org.faktorips.devtools.core.enums.EnumValue;
 import org.faktorips.devtools.core.internal.model.ipsobject.DescriptionHelper;
 import org.faktorips.devtools.core.internal.model.valueset.EnumValueSet;
 import org.faktorips.devtools.core.internal.model.valueset.RangeValueSet;
@@ -29,96 +25,52 @@ import org.w3c.dom.Element;
  * 
  * @author Jan Ortmann
  */
-public class ValueSetType extends DefaultEnumValue {
+public enum ValueSetType {
 
     /**
      * Defines the value set type that does not restrict the values in the set. All values allowed
      * by the data type are allowed.
      */
-    public final static ValueSetType UNRESTRICTED;
-
-    /**
-     * Defines the value set type range.
-     */
-    public final static ValueSetType RANGE;
+    UNRESTRICTED("allValues", Messages.ValueSetType__allValues) { //$NON-NLS-1$
+        @Override
+        public IValueSet newValueSet(IValueSetOwner parent, String id) {
+            return new UnrestrictedValueSet(parent, id);
+        }
+    },
 
     /**
      * Defines the value set type enumeration.
      */
-    public final static ValueSetType ENUM;
-
-    /**
-     * Defines a value set type that has yet to be specified (on the product side) as either
-     * enumeration or range.
-     */
-    private final static DefaultEnumType enumType;
-
-    static {
-        enumType = new DefaultEnumType("ValueSetType", ValueSetType.class); //$NON-NLS-1$
-        UNRESTRICTED = new ValueSetType(enumType, "allValues", Messages.ValueSetType__allValues); //$NON-NLS-1$
-        ENUM = new ValueSetType(enumType, "enum", Messages.ValueSetType_enumeration); //$NON-NLS-1$ 
-        RANGE = new ValueSetType(enumType, "range", Messages.ValueSetType_range); //$NON-NLS-1$ 
-    }
-
-    public final static EnumType getEnumType() {
-        return enumType;
-    }
-
-    /**
-     * Returns the value set type identified by the id. Returns <code>null</code> if the id does not
-     * identify a value set type.
-     */
-    public final static ValueSetType getValueSetType(String id) {
-        return (ValueSetType)enumType.getEnumValue(id);
-    }
-
-    /**
-     * Returns the value set type identified by the name. Returns <code>null</code> if the name does
-     * not identify a value set type.
-     */
-    public final static ValueSetType getValueSetTypeByName(String name) {
-        ValueSetType[] types = getValueSetTypes();
-        for (ValueSetType type2 : types) {
-            if (type2.getName().equals(name)) {
-                return type2;
-            }
+    ENUM("enum", Messages.ValueSetType_enumeration) { //$NON-NLS-1$
+        @Override
+        public IValueSet newValueSet(IValueSetOwner parent, String id) {
+            return new EnumValueSet(parent, id);
         }
-        return null;
-    }
+    },
 
-    public final static ValueSetType[] getValueSetTypes() {
-        EnumValue[] values = getEnumType().getValues();
-        ValueSetType[] types = new ValueSetType[values.length];
-        System.arraycopy(values, 0, types, 0, values.length);
-        return types;
-    }
-
-    public final static List<ValueSetType> getValueSetTypesAsList() {
-        EnumValue[] values = getEnumType().getValues();
-        List<ValueSetType> types = new ArrayList<ValueSetType>();
-        for (EnumValue value : values) {
-            types.add((ValueSetType)value);
+    /**
+     * Defines the value set type range.
+     */
+    RANGE("range", Messages.ValueSetType_range) { //$NON-NLS-1$
+        @Override
+        public IValueSet newValueSet(IValueSetOwner parent, String id) {
+            return new RangeValueSet(parent, id);
         }
-        return types;
-    }
+    };
 
-    private ValueSetType(DefaultEnumType type, String id, String name) {
-        super(type, id, name);
+    private final String id;
+
+    private final String name;
+
+    private ValueSetType(String id, String name) {
+        this.id = id;
+        this.name = name;
     }
 
     /**
      * Creates a new value set of the type this method is invoked on.
      */
-    public IValueSet newValueSet(IValueSetOwner parent, String id) {
-        if (this == UNRESTRICTED) {
-            return new UnrestrictedValueSet(parent, id);
-        } else if (this == ENUM) {
-            return new EnumValueSet(parent, id);
-        } else if (this == RANGE) {
-            return new RangeValueSet(parent, id);
-        }
-        return null;
-    }
+    public abstract IValueSet newValueSet(IValueSetOwner parent, String id);
 
     /**
      * Creates a new <tt>IValueSet</tt> - the type of the value set is derived from the given XML
@@ -144,6 +96,19 @@ public class ValueSetType extends DefaultEnumValue {
         return null;
     }
 
+    public static List<ValueSetType> getValueSetTypesAsList() {
+        return Arrays.asList(values());
+    }
+
+    public static ValueSetType getValueSetTypeByName(String name) {
+        for (ValueSetType type : values()) {
+            if (type.getName().equals(name)) {
+                return type;
+            }
+        }
+        return null;
+    }
+
     public boolean isUnrestricted() {
         return this == ValueSetType.UNRESTRICTED;
     }
@@ -154,6 +119,14 @@ public class ValueSetType extends DefaultEnumValue {
 
     public boolean isEnum() {
         return this == ValueSetType.ENUM;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
     }
 
 }
