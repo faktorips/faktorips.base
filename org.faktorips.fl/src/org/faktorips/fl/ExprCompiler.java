bc2b1c4cf7cccc583a2e0fecc50c715b21cd95b0
dirmeier <Cornelius.Dirmeier@faktorzehn.de>
2014-01-24 08:56:16 +0100

    FIPS-2591 :: respect product attributes on policy components
    Change-Id: I4a2794a41ebe61143a5390505793289bc9781c50

--- org.faktorips.fl/src/org/faktorips/fl/ExprCompiler.java
+++ org.faktorips.fl/src/org/faktorips/fl/ExprCompiler.java
@@ -480,6 +480,7 @@ public abstract class ExprCompiler<T extends CodeFragment> {
             // CSOFF: IllegalCatch
         } catch (Exception pe) {
             // CSON: IllegalCatch
+            pe.printStackTrace();
             return newCompilationResultImpl(Message.newError(INTERNAL_ERROR,
                     LOCALIZED_STRINGS.getString(INTERNAL_ERROR, getLocale())));
         }
@@ -499,6 +500,7 @@ public abstract class ExprCompiler<T extends CodeFragment> {
             // CSOFF: IllegalCatch
         } catch (RuntimeException pe) {
             // CSON: IllegalCatch
+            pe.printStackTrace();
             return newCompilationResultImpl(Message.newError(INTERNAL_ERROR,
                     LOCALIZED_STRINGS.getString(INTERNAL_ERROR, getLocale())));
         }
