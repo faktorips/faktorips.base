1ddddb603b738bae6bb1412806454afdb5c6a74f
dirmeier <Cornelius.Dirmeier@faktorzehn.de>
2014-01-30 13:12:43 +0100

    FIPS-2582 :: validation: Non-productrelevant attributes are not allowed to have default values of extensible enum types
    Change-Id: I2ea8cda569d9eb2fa4a78f7c5e47a2316714799f

--- org.faktorips.devtools.core/src/org/faktorips/devtools/core/internal/model/type/Attribute.java
+++ org.faktorips.devtools.core/src/org/faktorips/devtools/core/internal/model/type/Attribute.java
@@ -151,7 +151,8 @@ public abstract class Attribute extends TypePart implements IAttribute {
             } else {
                 if (!getValueSet().isDetailedSpecificationOf(superAttr.getValueSet())) {
                     String text = Messages.Attribute_ValueSet_not_SubValueSet_of_the_overridden_attribute;
-                    result.add(new Message(MSGCODE_OVERWRITTEN_ATTRIBUTE_INCOMPAIBLE_VALUESET, text, Message.ERROR, getValueSet()));
+                    result.add(new Message(MSGCODE_OVERWRITTEN_ATTRIBUTE_INCOMPAIBLE_VALUESET, text, Message.ERROR,
+                            getValueSet()));
                 }
                 if (!getDatatype().equals(superAttr.getDatatype())) {
                     result.add(new Message(MSGCODE_OVERWRITTEN_ATTRIBUTE_HAS_DIFFERENT_DATATYPE,
@@ -180,7 +181,7 @@ public abstract class Attribute extends TypePart implements IAttribute {
         return candidate;
     }
 
-    private void validateDefaultValue(ValueDatatype valueDatatype, MessageList result, IIpsProject ipsProject)
+    protected void validateDefaultValue(ValueDatatype valueDatatype, MessageList result, IIpsProject ipsProject)
             throws CoreException {
 
         if (!valueDatatype.isParsable(defaultValue)) {
