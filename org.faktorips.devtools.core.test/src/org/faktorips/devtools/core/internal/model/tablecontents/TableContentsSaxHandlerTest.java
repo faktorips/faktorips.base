cdfe12c64978cc0d672036ee82585aae41ee498b
dirmeier <Cornelius.Dirmeier@faktorzehn.de>
2014-02-17 14:26:21 +0100

    FIPS-2759/FIPS-2762 :: write empty xml element for empty extension property values. Fix NPE.

---
+++ org.faktorips.devtools.core.test/src/org/faktorips/devtools/core/internal/model/tablecontents/TableContentsSaxHandlerTest.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Copyright (c) Faktor Zehn AG. <http://www.faktorzehn.org>
+ * 
+ * This source code is available under the terms of the AGPL Affero General Public License version
+ * 3.
+ * 
+ * Please see LICENSE.txt for full license terms, including the additional permissions and
+ * restrictions as well as the possibility of alternative license terms.
+ *******************************************************************************/
+package org.faktorips.devtools.core.internal.model.tablecontents;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.faktorips.devtools.core.model.tablecontents.ITableContents;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.xml.sax.Attributes;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TableContentsSaxHandlerTest {
+
+    private static final String MY_ID = "myID";
+
+    @Mock
+    private ITableContents tableContents;
+
+    private TableContentsSaxHandler tableContentsSaxHandler;
+
+    @Before
+    public void createTableContentsSaxHandler() throws Exception {
+        tableContentsSaxHandler = new TableContentsSaxHandler(tableContents);
+    }
+
+    @Test
+    public void testEndElement_extensionProperty() throws Exception {
+        tableContentsSaxHandler.startElement("", "", TableContentsGeneration.getXmlExtPropertiesElementName(), null);
+        Attributes attributes = mock(Attributes.class);
+        when(attributes.getValue(TableContentsGeneration.getXmlAttributeExtpropertyid())).thenReturn(MY_ID);
+        tableContentsSaxHandler.startElement("", "", TableContentsGeneration.getXmlValueElement(), attributes);
+
+        tableContentsSaxHandler.endElement("", "", TableContentsGeneration.getXmlValueElement());
+
+        verify(tableContents).addExtensionProperty(MY_ID, "");
+    }
+
+}
