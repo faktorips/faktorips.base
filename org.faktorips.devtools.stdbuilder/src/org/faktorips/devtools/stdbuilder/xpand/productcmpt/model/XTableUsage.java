66d7406e4d7bec91cbbda879673c8f0d78778da6
dirmeier <Cornelius.Dirmeier@faktorzehn.de>
2014-04-14 14:53:41 +0200

    FIPS-1958/FIPS-3024 :: split doInitTableUsage and fix unnecessary cast

--- org.faktorips.devtools.stdbuilder/src/org/faktorips/devtools/stdbuilder/xpand/productcmpt/model/XTableUsage.java
+++ org.faktorips.devtools.stdbuilder/src/org/faktorips/devtools/stdbuilder/xpand/productcmpt/model/XTableUsage.java
@@ -10,6 +10,7 @@
 
 package org.faktorips.devtools.stdbuilder.xpand.productcmpt.model;
 
+import org.apache.commons.lang.StringUtils;
 import org.eclipse.core.runtime.CoreException;
 import org.faktorips.devtools.core.builder.naming.BuilderAspect;
 import org.faktorips.devtools.core.exception.CoreRuntimeException;
@@ -49,6 +50,10 @@ public class XTableUsage extends AbstractGeneratorModelNode {
 
     public String getMethodNameGetter() {
         return getJavaNamingConvention().getGetterMethodName(getName());
+    }
+
+    public String getMethodNameDoInitFromXml() {
+        return "doInit" + StringUtils.capitalize(getName());
     }
 
     public String getTableClassName() {
