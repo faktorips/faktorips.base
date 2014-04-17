af3e793be864f942ec53397cb35ca920c174a25a
swidmaier <Stefan.Widmaier@faktorzehn.de>
2014-04-17 15:38:04 +0200

    FIPS-2788/FIPS-3050 :: Change UI for new DeltaEntry

--- org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/editors/productcmpt/deltapresentation/DeltaLabelProvider.java
+++ org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/editors/productcmpt/deltapresentation/DeltaLabelProvider.java
@@ -28,7 +28,6 @@ import org.faktorips.devtools.core.ui.editors.deltapresentation.DeltaCompositeIc
 
 /**
  * 
- * @author Jan Ortmann
  */
 public class DeltaLabelProvider extends LabelProvider {
     private WorkbenchLabelProvider workbenchLabelProvider;
@@ -46,63 +45,14 @@ public class DeltaLabelProvider extends LabelProvider {
         if (element instanceof DeltaTypeWrapper) {
             descriptor = getBaseImage(((DeltaTypeWrapper)element).getDeltaType());
         } else if (element instanceof IDeltaEntryForProperty) {
-            IDeltaEntryForProperty entry = (IDeltaEntryForProperty)element;
-            Image baseImage = getBaseImage(entry.getPropertyType());
-            if (entry.getDeltaType() == DeltaType.MISSING_PROPERTY_VALUE) {
-                descriptor = DeltaCompositeIcon.createAddImage(baseImage);
-            } else if (entry.getDeltaType() == DeltaType.VALUE_WITHOUT_PROPERTY) {
-                descriptor = DeltaCompositeIcon.createDeleteImage(baseImage);
-            } else {
-                descriptor = DeltaCompositeIcon.createModifyImage(baseImage);
-            }
+            descriptor = getImageDescriptorForDeltaEntryForProperty((IDeltaEntryForProperty)element);
         } else if (element instanceof IDeltaEntry) {
             descriptor = getImageDescriptorForDeltaEntry((IDeltaEntry)element);
         }
-        if (descriptor == null) {
-            if (element instanceof ProductCmptGenerationsDeltaViewItem) {
-                return workbenchLabelProvider.getImage(((ProductCmptGenerationsDeltaViewItem)element).getDelta());
-            }
-            return workbenchLabelProvider.getImage(element);
-        }
-        return (Image)resourceManager.get(descriptor);
-    }
-
-    private ImageDescriptor getImageDescriptorForDeltaEntry(IDeltaEntry deltaEntry) {
-        ImageDescriptor baseImageDescriptor = getBaseImage(deltaEntry.getDeltaType());
-        if (deltaEntry.getDeltaType() == DeltaType.LINK_CHANGING_OVER_TIME_MISMATCH) {
-            LinkChangingOverTimeMismatchEntry mismatchEntry = (LinkChangingOverTimeMismatchEntry)deltaEntry;
-            Image baseImage = (Image)resourceManager.get(baseImageDescriptor);
-            if (mismatchEntry.isMovingLink()) {
-                return DeltaCompositeIcon.createModifyImage(baseImage);
-            } else {
-                return DeltaCompositeIcon.createDeleteImage(baseImage);
-            }
-        }
-        return baseImageDescriptor;
-    }
-
-    private Image getBaseImage(ProductCmptPropertyType propertyType) {
-        if (propertyType == ProductCmptPropertyType.PRODUCT_CMPT_TYPE_ATTRIBUTE) {
-            return getImageForName("ProductAttribute.gif"); //$NON-NLS-1$
-        } else if (propertyType == ProductCmptPropertyType.TABLE_STRUCTURE_USAGE) {
-            return getImageForName("TableContentsUsage.gif"); //$NON-NLS-1$
-        } else if (propertyType == ProductCmptPropertyType.FORMULA_SIGNATURE_DEFINITION) {
-            return getImageForName("Formula.gif"); //$NON-NLS-1$
-        } else if (propertyType == ProductCmptPropertyType.POLICY_CMPT_TYPE_ATTRIBUTE) {
-            return getImageForName("PolicyAttribute.gif"); //$NON-NLS-1$
-        } else if (propertyType == ProductCmptPropertyType.VALIDATION_RULE) {
-            return getImageForName("ValidationRuleDef.gif"); //$NON-NLS-1$
-        } else {
-            return null;
-        }
-    }
-
-    private Image getImageForName(String fileName) {
-        return (Image)resourceManager.get(IpsUIPlugin.getImageHandling().createImageDescriptor(fileName));
+        return getImageForDescriptor(element, descriptor);
     }
 
     private ImageDescriptor getBaseImage(DeltaType deltaType) {
-        // TODO SW 17.5.11 use part adapters for base images
         if (deltaType == DeltaType.MISSING_PROPERTY_VALUE) {
             return IpsUIPlugin.getImageHandling().createImageDescriptor("DeltaTypeMissingPropertyValue.gif"); //$NON-NLS-1$
         } else if (deltaType == DeltaType.VALUE_WITHOUT_PROPERTY) {
@@ -117,9 +67,70 @@ public class DeltaLabelProvider extends LabelProvider {
             return IpsUIPlugin.getImageHandling().getDefaultImageDescriptor(ProductCmptTypeAssociation.class);
         } else if (deltaType == DeltaType.VALUE_HOLDER_MISMATCH) {
             return IpsUIPlugin.getImageHandling().createImageDescriptor("DeltaTypeMissingPropertyValue.gif"); //$NON-NLS-1$
+        } else if (deltaType == DeltaType.HIDDEN_ATTRIBUTE_MISMATCH) {
+            return IpsUIPlugin.getImageHandling().createImageDescriptor("DeltaTypeHiddenAttributeMismatch.gif"); //$NON-NLS-1$
         } else {
             return null;
         }
+    }
+
+    private ImageDescriptor getImageDescriptorForDeltaEntryForProperty(IDeltaEntryForProperty entry) {
+        ImageDescriptor descriptor;
+        Image baseImage = getBaseImage(entry.getPropertyType());
+        if (entry.getDeltaType() == DeltaType.MISSING_PROPERTY_VALUE) {
+            descriptor = DeltaCompositeIcon.createAddImage(baseImage);
+        } else if (entry.getDeltaType() == DeltaType.VALUE_WITHOUT_PROPERTY) {
+            descriptor = DeltaCompositeIcon.createDeleteImage(baseImage);
+        } else {
+            descriptor = DeltaCompositeIcon.createModifyImage(baseImage);
+        }
+        return descriptor;
+    }
+
+    private Image getBaseImage(ProductCmptPropertyType propertyType) {
+        return propertyType == null ? null : IpsUIPlugin.getImageHandling().getDefaultImage(
+                propertyType.getValueImplementationClass());
+    }
+
+    private ImageDescriptor getImageDescriptorForDeltaEntry(IDeltaEntry deltaEntry) {
+        ImageDescriptor baseImageDescriptor = getBaseImage(deltaEntry.getDeltaType());
+        baseImageDescriptor = getChaningOvertimeDecorationIfRequired(deltaEntry, baseImageDescriptor);
+        return baseImageDescriptor;
+    }
+
+    private ImageDescriptor getChaningOvertimeDecorationIfRequired(IDeltaEntry deltaEntry,
+            ImageDescriptor baseImageDescriptor) {
+        ImageDescriptor resultDescriptor = baseImageDescriptor;
+        if (deltaEntry.getDeltaType() == DeltaType.LINK_CHANGING_OVER_TIME_MISMATCH) {
+            LinkChangingOverTimeMismatchEntry mismatchEntry = (LinkChangingOverTimeMismatchEntry)deltaEntry;
+            resultDescriptor = getChanginOverTimeMismatchDescriptor(mismatchEntry, baseImageDescriptor);
+        }
+        return resultDescriptor;
+    }
+
+    private ImageDescriptor getChanginOverTimeMismatchDescriptor(LinkChangingOverTimeMismatchEntry mismatchEntry,
+            ImageDescriptor baseImageDescriptor) {
+        Image baseImage = (Image)resourceManager.get(baseImageDescriptor);
+        if (mismatchEntry.isMovingLink()) {
+            return DeltaCompositeIcon.createModifyImage(baseImage);
+        } else {
+            return DeltaCompositeIcon.createDeleteImage(baseImage);
+        }
+    }
+
+    private Image getImageForDescriptor(Object element, ImageDescriptor descriptor) {
+        if (descriptor == null) {
+            return getFallbackImage(element);
+        } else {
+            return (Image)resourceManager.get(descriptor);
+        }
+    }
+
+    private Image getFallbackImage(Object element) {
+        if (element instanceof ProductCmptGenerationsDeltaViewItem) {
+            return workbenchLabelProvider.getImage(((ProductCmptGenerationsDeltaViewItem)element).getDelta());
+        }
+        return workbenchLabelProvider.getImage(element);
     }
 
     @Override
