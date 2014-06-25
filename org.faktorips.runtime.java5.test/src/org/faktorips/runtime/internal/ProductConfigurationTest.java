5092ff1d29a1b353f8c8dbfff77d9e40798cee45
dirmeier <Cornelius.Dirmeier@faktorzehn.de>
2014-06-25 22:40:09 +0200

    FIPS-3041/FIPS-3299 :: Serializable ProductConfiguration
    Change-Id: I29ea923f62da40c87b51cc3c22899b7601eabf3f

--- org.faktorips.runtime.java5.test/src/org/faktorips/runtime/internal/ProductConfigurationTest.java
+++ org.faktorips.runtime.java5.test/src/org/faktorips/runtime/internal/ProductConfigurationTest.java
@@ -15,11 +15,18 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.when;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.ObjectStreamClass;
 import java.util.Calendar;
 
 import org.faktorips.runtime.IProductComponent;
 import org.faktorips.runtime.IProductComponentGeneration;
 import org.faktorips.runtime.IRuntimeRepository;
+import org.faktorips.runtime.IRuntimeRepositoryLookup;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -31,24 +38,38 @@ import org.w3c.dom.Element;
 @RunWith(MockitoJUnitRunner.class)
 public class ProductConfigurationTest {
 
+    private static final String PODUCT_COMPONENT_ID = "My_PC-ID";
+
+    /**
+     * Needs to be static to have access from the {@link TestRuntimeRepositoryLookup}
+     */
+    @Mock
+    private static IRuntimeRepository repository;
+
     @Mock
     private IProductComponent productCmpt;
+
     @Mock
     private IProductComponentGeneration productCmptGeneration;
-    @Mock
-    private IRuntimeRepository repository;
+
     @Mock(answer = Answers.RETURNS_DEEP_STUBS)
     private Element element;
-    @Mock
-    private Calendar calendar;
+
+    private final Calendar calendar = Calendar.getInstance(ProductConfiguration.TIME_ZONE);
 
     @Before
     public void createAbstractConfigurableModelObject() throws Exception {
+        when(productCmpt.getId()).thenReturn(PODUCT_COMPONENT_ID);
+        when(productCmpt.getRepository()).thenReturn(repository);
         when(productCmpt.getGenerationBase(calendar)).thenReturn(productCmptGeneration);
         when(productCmptGeneration.getProductComponent()).thenReturn(productCmpt);
+        when(productCmptGeneration.getValidFrom(ProductConfiguration.TIME_ZONE)).thenReturn(calendar.getTime());
 
-        when(element.getAttribute("productCmpt")).thenReturn("PC-ID");
-        when(repository.getExistingProductComponent("PC-ID")).thenReturn(productCmpt);
+        when(element.getAttribute("productCmpt")).thenReturn(PODUCT_COMPONENT_ID);
+        when(repository.getExistingProductComponent(PODUCT_COMPONENT_ID)).thenReturn(productCmpt);
+
+        when(repository.getProductComponent(PODUCT_COMPONENT_ID)).thenReturn(productCmpt);
+        when(repository.getProductComponentGeneration(PODUCT_COMPONENT_ID, calendar)).thenReturn(productCmptGeneration);
     }
 
     @Test
@@ -149,5 +170,119 @@ public class ProductConfigurationTest {
 
         productConfiguration.resetProductCmptGeneration();
         assertNotNull(productConfiguration.getProductCmptGeneration(calendar));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testSerialization_noLookup() throws Exception {
+        ProductConfiguration productConfiguration = initForSerialization();
+
+        serializeProductConfiguration(productConfiguration);
+    }
+
+    @Test
+    public void testSerialization() throws Exception {
+        when(repository.getRuntimeRepositoryLookup()).thenReturn(new TestRuntimeRepositoryLookup());
+        ProductConfiguration productConfiguration = initForSerialization();
+
+        byte[] serialize = serializeProductConfiguration(productConfiguration);
+        ProductConfiguration deserializedProductConfiguration = deserializeProductConfiguration(serialize);
+
+        assertEquals(productConfiguration.getProductComponent(), deserializedProductConfiguration.getProductComponent());
+        assertEquals(productConfiguration.getProductCmptGeneration(null),
+                deserializedProductConfiguration.getProductCmptGeneration(null));
+    }
+
+    @Test
+    public void testSerialization_noGeneration() throws Exception {
+        when(repository.getRuntimeRepositoryLookup()).thenReturn(new TestRuntimeRepositoryLookup());
+        ProductConfiguration productConfiguration = new ProductConfiguration();
+        productConfiguration.setProductComponent(productCmpt);
+
+        byte[] serialize = serializeProductConfiguration(productConfiguration);
+        ProductConfiguration deserializedProductConfiguration = deserializeProductConfiguration(serialize);
+
+        assertEquals(productConfiguration.getProductComponent(), deserializedProductConfiguration.getProductComponent());
+        assertNull(deserializedProductConfiguration.getProductCmptGeneration(null));
+    }
+
+    @Test
+    public void testSerialization_noProductCmpt() throws Exception {
+        when(repository.getRuntimeRepositoryLookup()).thenReturn(new TestRuntimeRepositoryLookup());
+        ProductConfiguration productConfiguration = new ProductConfiguration();
+
+        byte[] serialize = serializeProductConfiguration(productConfiguration);
+        ProductConfiguration deserializedProductConfiguration = deserializeProductConfiguration(serialize);
+
+        assertNull(deserializedProductConfiguration.getProductComponent());
+        assertNull(deserializedProductConfiguration.getProductCmptGeneration(null));
+    }
+
+    private ProductConfiguration initForSerialization() {
+        ProductConfiguration productConfiguration = new ProductConfiguration();
+        productConfiguration.setProductCmptGeneration(productCmptGeneration);
+        return productConfiguration;
+    }
+
+    private byte[] serializeProductConfiguration(ProductConfiguration productConfiguration) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream(512);
+
+        ObjectOutputStream out = new ObjectOutputStream(baos);
+        out.writeObject(productConfiguration);
+
+        return baos.toByteArray();
+    }
+
+    private ProductConfiguration deserializeProductConfiguration(byte[] byteArray) throws IOException,
+            ClassNotFoundException {
+        OsgiObjectInputStream osgiCompatibleInputStream = new OsgiObjectInputStream(byteArray);
+        try {
+            ProductConfiguration deserializedProductConfiguration = (ProductConfiguration)osgiCompatibleInputStream
+                    .readObject();
+            return deserializedProductConfiguration;
+        } finally {
+            osgiCompatibleInputStream.close();
+        }
+
+    }
+
+    private static class TestRuntimeRepositoryLookup implements IRuntimeRepositoryLookup {
+
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        public IRuntimeRepository getRuntimeRepository() {
+            return repository;
+        }
+
+    }
+
+    /**
+     * Serialization in OSGi context is a bit tricky. The modularization mechanisms of OSGi leads to
+     * {@link ClassNotFoundException} when a class that should be deserialized is part of another
+     * plug-In. When this test cases are executed as Plug-In-Tests we have such a scenario.
+     * <p>
+     * This implementation of {@link ObjectInputStream} solves this problem by always loading
+     * classes using the current thread's context class loader. Using the equinox platform the
+     * context classloader is designed to load classes from different depending bundles.
+     */
+    private static class OsgiObjectInputStream extends ObjectInputStream {
+
+        public OsgiObjectInputStream(byte[] byteArray) throws IOException {
+            super(new ByteArrayInputStream(byteArray));
+        }
+
+        @Override
+        protected Class<?> resolveClass(ObjectStreamClass desc) throws ClassNotFoundException, IOException {
+            ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+            try {
+                Class<?> c = contextClassLoader.loadClass(desc.getName());
+                if (c != null) {
+                    return c;
+                }
+            } catch (Exception e) {
+                return super.resolveClass(desc);
+            }
+            return super.resolveClass(desc);
+        }
     }
 }
--- org.faktorips.runtime.java5/src/org/faktorips/runtime/internal/ProductConfiguration.java
+++ org.faktorips.runtime.java5/src/org/faktorips/runtime/internal/ProductConfiguration.java
@@ -9,25 +9,48 @@
  *******************************************************************************/
 package org.faktorips.runtime.internal;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
 
 import org.faktorips.runtime.IProductComponent;
 import org.faktorips.runtime.IProductComponentGeneration;
 import org.faktorips.runtime.IRuntimeRepository;
+import org.faktorips.runtime.IRuntimeRepositoryLookup;
 import org.w3c.dom.Element;
 
 /**
  * Manages a product component and the corresponding generation for use with configurable model
  * objects. Used by all configurable policy component classes to manage their product configuration.
+ * <p>
+ * 
+ * @serial This class is serialized using by writing/reading the product component runtime ID and
+ *         the effective date of the current generation. To deserialize the product product
+ *         component we also serialize the {@link IRuntimeRepositoryLookup} that is provided by the
+ *         {@link IRuntimeRepository} of the current product component. The serialization would
+ *         throw an {@link IllegalStateException} if the runtime repository of the current product
+ *         component has no {@link IRuntimeRepositoryLookup} configured.
+ * 
  */
-public class ProductConfiguration {
+public class ProductConfiguration implements Serializable {
+
+    static final TimeZone TIME_ZONE = TimeZone.getTimeZone("UTC");
+
+    /**
+     * Comment for <code>serialVersionUID</code>
+     */
+    private static final long serialVersionUID = 2579369976921239373L;
 
     private static final String XML_ATTRIBUTE_PRODUCT_CMPT = "productCmpt";
 
     /** The product component this configurable model object is based on. */
-    private IProductComponent productCmpt;
+    private transient IProductComponent productCmpt;
 
-    private IProductComponentGeneration productCmptGeneration;
+    private transient IProductComponentGeneration productCmptGeneration;
 
     public ProductConfiguration() {
         super();
@@ -111,8 +134,8 @@ public class ProductConfiguration {
     }
 
     /**
-     * Loads the product component this {@link ProductConfiguration} manages. Does nothing, if
-     * no product component qualified name can be found in the XML element.
+     * Loads the product component this {@link ProductConfiguration} manages. Does nothing, if no
+     * product component qualified name can be found in the XML element.
      * 
      * @param objectEl the XML element containing the product component qualified name
      * @param productRepository the {@link IRuntimeRepository} to load the product component from
@@ -122,6 +145,97 @@ public class ProductConfiguration {
         if (!IpsStringUtils.isEmpty(productCmptId)) {
             IProductComponent existingProductCmpt = productRepository.getExistingProductComponent(productCmptId);
             setProductComponent(existingProductCmpt);
+        }
+    }
+
+    /**
+     * Serialize this {@link ProductConfiguration} instance.
+     * 
+     * @throws IllegalStateException if there is no {@link IRuntimeRepositoryLookup} configured in
+     *             the product component's runtime repository.
+     * 
+     * @serialData First the runtime ID of the product component followed by the effective date of
+     *             the current generation. At least the {@link IRuntimeRepositoryLookup} that is
+     *             retrieved by the {@link IRuntimeRepository} of the product component.
+     * 
+     */
+    private void writeObject(ObjectOutputStream s) throws IOException {
+        s.defaultWriteObject();
+        s.writeObject(getRuntimeId());
+        s.writeObject(getValidFrom());
+        s.writeObject(getRepositoryLookup());
+    }
+
+    private String getRuntimeId() {
+        if (productCmpt != null) {
+            return productCmpt.getId();
+        } else {
+            return null;
+        }
+    }
+
+    private IRuntimeRepositoryLookup getRepositoryLookup() {
+        if (productCmpt != null) {
+            IRuntimeRepositoryLookup runtimeRepositoryLookup = productCmpt.getRepository().getRuntimeRepositoryLookup();
+            checkRuntimeRepositoryLookup(runtimeRepositoryLookup);
+            return runtimeRepositoryLookup;
+        } else {
+            return null;
+        }
+    }
+
+    private void checkRuntimeRepositoryLookup(IRuntimeRepositoryLookup runtimeRepositoryLookup) {
+        if (runtimeRepositoryLookup == null) {
+            throw new IllegalStateException(
+                    "For serialization of policy component classes you need to set a IRuntimeRepositoryLookup in your runtime repository.");
+        }
+    }
+
+    private Date getValidFrom() {
+        if (productCmptGeneration != null) {
+            return productCmptGeneration.getValidFrom(TIME_ZONE);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Reads a serialized instance of {@link ProductConfiguration}.
+     * 
+     * @serialData First the runtime ID of the product component followed by the effective date of
+     *             the current generation. At least the {@link IRuntimeRepositoryLookup} that is
+     *             retrieved by the {@link IRuntimeRepository} of the product component.
+     * 
+     */
+    private void readObject(ObjectInputStream s) throws IOException, ClassNotFoundException {
+        s.defaultReadObject();
+        String runtimeId = (String)s.readObject();
+        Date validFromDate = (Date)s.readObject();
+        IRuntimeRepositoryLookup runtimeRepositoryLookup = (IRuntimeRepositoryLookup)s.readObject();
+        if (runtimeRepositoryLookup != null) {
+            initProductConfiguration(runtimeId, validFromDate, runtimeRepositoryLookup);
+        }
+    }
+
+    private void initProductConfiguration(String runtimeId,
+            Date validFromDate,
+            IRuntimeRepositoryLookup runtimeRepositoryLookup) {
+        IRuntimeRepository runtimeRepository = runtimeRepositoryLookup.getRuntimeRepository();
+        initProductCmpt(runtimeId, runtimeRepository);
+        initGeneration(runtimeId, validFromDate, runtimeRepository);
+    }
+
+    private void initProductCmpt(String runtimeId, IRuntimeRepository runtimeRepository) {
+        if (runtimeId != null) {
+            productCmpt = runtimeRepository.getProductComponent(runtimeId);
+        }
+    }
+
+    private void initGeneration(String runtimeId, Date validFromDate, IRuntimeRepository runtimeRepository) {
+        if (validFromDate != null) {
+            Calendar validFrom = Calendar.getInstance(TIME_ZONE);
+            validFrom.setTime(validFromDate);
+            productCmptGeneration = runtimeRepository.getProductComponentGeneration(runtimeId, validFrom);
         }
     }
 
