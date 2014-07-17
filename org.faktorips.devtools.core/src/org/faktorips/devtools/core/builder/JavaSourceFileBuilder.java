3be9964c3a493e7b416ee2d4a0ad3d342d8f4951
Ines Maazi <ines.maazi@faktorzehn.de>
2014-07-17 10:21:45 +0200

    FIPS-2767 :: Adapted Javadoc

--- org.faktorips.devtools.core/src/org/faktorips/devtools/core/builder/JavaSourceFileBuilder.java
+++ org.faktorips.devtools.core/src/org/faktorips/devtools/core/builder/JavaSourceFileBuilder.java
@@ -1154,8 +1154,8 @@ public abstract class JavaSourceFileBuilder extends AbstractArtefactBuilder {
      * {@inheritDoc}
      * <p>
      * Returns <code>true</code> if the generated artifact is not published and we do generate
-     * published interfaces. If we do not generate published interfaces, there are not internal
-     * artifacts at all.
+     * published interfaces. If we do not generate published interfaces, there are no internal
+     * artifacts except for the generated artifact of EnumXmlAdapterBuilder.
      * 
      * @see #isBuildingPublishedSourceFile()
      */
