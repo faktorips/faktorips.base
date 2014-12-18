5265302fdbc59fcb8234153d2e12a9a6bd8ce9c7
dirmeier <Cornelius.Dirmeier@faktorzehn.de>
2014-12-18 13:19:29 +0100

    FIPS-3966 :: Dummy test to avoid tycho build fails

---
+++ org.faktorips.devtools.stdbuilder.ui.test/src/org/faktorips/devtools/stdbuilder/ui/test/DummyTest.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) Faktor Zehn AG. <http://www.faktorzehn.org>
+ * 
+ * This source code is available under the terms of the AGPL Affero General Public License version
+ * 3.
+ * 
+ * Please see LICENSE.txt for full license terms, including the additional permissions and
+ * restrictions as well as the possibility of alternative license terms.
+ *******************************************************************************/
+package org.faktorips.devtools.stdbuilder.ui.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Delete this dummy test if there is at least one real test case. The build job needs to find at
+ * least one test result otherwise it assumes a configuration error.
+ * 
+ */
+public class DummyTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+
+}
