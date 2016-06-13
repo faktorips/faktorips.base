8ae7f31eef4308f806ec16fd32643879774cba7f
Daniel Schwering <daniel.schwering@faktorzehn.de>
2016-06-13 11:44:36 +0200

    FIPS-4844/FIPS-4944 migration adding uniqueQualifier to ipsSrcFolders
    Change-Id: Ib9175dc0760f8e5e6fc2088120ceff895296d0d1

---
+++ org.faktorips.devtools.core/src/org/faktorips/devtools/core/internal/migrationextensions/Migration_3_18_0.java
@@ -0,0 +1,75 @@
+/*******************************************************************************
+ * Copyright (c) Faktor Zehn AG. <http://www.faktorzehn.org>
+ * 
+ * This source code is available under the terms of the AGPL Affero General Public License version
+ * 3.
+ * 
+ * Please see LICENSE.txt for full license terms, including the additional permissions and
+ * restrictions as well as the possibility of alternative license terms.
+ *******************************************************************************/
+package org.faktorips.devtools.core.internal.migrationextensions;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.faktorips.devtools.core.internal.migration.DefaultMigration;
+import org.faktorips.devtools.core.internal.model.ipsproject.IpsSrcFolderEntry;
+import org.faktorips.devtools.core.model.ipsobject.IIpsSrcFile;
+import org.faktorips.devtools.core.model.ipsproject.IIpsObjectPath;
+import org.faktorips.devtools.core.model.ipsproject.IIpsProject;
+import org.faktorips.devtools.core.model.ipsproject.IIpsProjectProperties;
+import org.faktorips.devtools.core.model.ipsproject.IIpsSrcFolderEntry;
+import org.faktorips.devtools.core.model.versionmanager.AbstractIpsProjectMigrationOperation;
+import org.faktorips.devtools.core.model.versionmanager.IIpsProjectMigrationOperationFactory;
+import org.faktorips.util.message.MessageList;
+
+/**
+ * Adds {@link IpsSrcFolderEntry#setUniqueQualifier(String)} if necessary.
+ */
+public class Migration_3_18_0 extends DefaultMigration {
+
+    private Migration_3_18_0(IIpsProject projectToMigrate, String featureId) {
+        super(projectToMigrate, featureId);
+    }
+
+    @Override
+    protected void migrate(IIpsSrcFile srcFile) throws CoreException {
+        srcFile.markAsDirty();
+    }
+
+    @Override
+    public String getTargetVersion() {
+        return "3.18.0"; //$NON-NLS-1$
+    }
+
+    @Override
+    public String getDescription() {
+        return Messages.Migration_3_18_0_description;
+    }
+
+    @Override
+    public MessageList migrate(IProgressMonitor monitor) throws CoreException, InvocationTargetException {
+        MessageList messageList = super.migrate(monitor);
+        IIpsProject ipsProject = getIpsProject();
+        IIpsProjectProperties ipsProjectProperties = ipsProject.getProperties();
+        IIpsObjectPath ipsObjectPath = ipsProjectProperties.getIpsObjectPath();
+        IIpsSrcFolderEntry[] sourceFolderEntries = ipsObjectPath.getSourceFolderEntries();
+        if (sourceFolderEntries.length > 1) {
+            for (int i = 1; i < sourceFolderEntries.length; i++) {
+                IpsSrcFolderEntry ipsSrcFolderEntry = (IpsSrcFolderEntry)sourceFolderEntries[i];
+                ipsSrcFolderEntry.setUniqueQualifier(ipsSrcFolderEntry.getIpsPackageFragmentRootName());
+            }
+        }
+        ipsProject.setProperties(ipsProjectProperties);
+        return messageList;
+    }
+
+    public static class Factory implements IIpsProjectMigrationOperationFactory {
+        @Override
+        public AbstractIpsProjectMigrationOperation createIpsProjectMigrationOpertation(IIpsProject ipsProject,
+                String featureId) {
+            return new Migration_3_18_0(ipsProject, featureId);
+        }
+    }
+}
