d39da68a17bb2b1ecbf827da567278e497258517
Marc Fritzenwenger <marc.fritzenwenger@faktorzehn.de>
2016-06-27 18:22:37 +0200

    FIPS-4847/FIPS-4932 :: Annotation Klassen anlegen
    Change-Id: I7f03548c7bd1d3d2337f9fd36f314849ca033ed3

---
+++ org.faktorips.runtime/src/org/faktorips/runtime/model/annotation/IpsEnum.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) Faktor Zehn AG. <http://www.faktorzehn.org>
+ * 
+ * This source code is available under the terms of the AGPL Affero General Public License version
+ * 3.
+ * 
+ * Please see LICENSE.txt for full license terms, including the additional permissions and
+ * restrictions as well as the possibility of alternative license terms.
+ *******************************************************************************/
+package org.faktorips.runtime.model.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Preserves design time information about an {@link Enum} at runtime
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE })
+public @interface IpsEnum {
+    String name();
+
+    String[] attributeNames();
+}
