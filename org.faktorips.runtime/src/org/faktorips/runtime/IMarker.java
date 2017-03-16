3742b48200581d6973270438816d6088a365b4f8
Daniel Schwering <daniel.schwering@faktorzehn.de>
2017-03-16 11:24:18 +0100

    FIPS-4964 :: add missing information and technical constraint options to marker
    Change-Id: I30786c8c72c6555412c68fc0115b549a3df744e9

--- org.faktorips.runtime/src/org/faktorips/runtime/IMarker.java
+++ org.faktorips.runtime/src/org/faktorips/runtime/IMarker.java
@@ -7,17 +7,38 @@
  * Please see LICENSE.txt for full license terms, including the additional permissions and
  * restrictions as well as the possibility of alternative license terms.
  *******************************************************************************/
-
 package org.faktorips.runtime;
 
 /**
  * The implementation of this interface is used to provide additional information for an
  * {@link Message}.
  * <p>
- * Currently this interface is just an empty marker interface, used in {@link Message}. The
- * interface may be populated with some methods or useful constants in the next versions.
- * Implementers will be informed properly.
+ * Currently there are two standard situations markers are expected to be used in (see
+ * {@link #isRequiredInformationMissing()} and {@link #isTechnicalConstraintViolated()}).
+ * Implementers are free to use markers for other purposes.
  */
 public interface IMarker {
-    // currently only a marker interface
+
+    /**
+     * Returns whether the marked {@link Message} is stating that required information is missing.
+     * In most cases the missing information is a value for an attribute or an association's target
+     * and should be discernible from the {@link Message#getInvalidObjectProperties() Message's
+     * InvalidObjectProperties}.
+     * 
+     * @return <code>true</code> if the marked message indicates that required information is
+     *         missing, otherwise <code>false</code>
+     */
+    boolean isRequiredInformationMissing();
+
+    /**
+     * Returns whether the marked {@link Message} is stating that a technical constraint (like for
+     * example a database column length) is violated. The property the constraint applies to should
+     * be discernible from the {@link Message#getInvalidObjectProperties() Message's
+     * InvalidObjectProperties}.
+     * 
+     * @return <code>true</code> if the marked message indicates that some technical constraints are
+     *         violated, otherwise <code>false</code>
+     */
+    boolean isTechnicalConstraintViolated();
+
 }
