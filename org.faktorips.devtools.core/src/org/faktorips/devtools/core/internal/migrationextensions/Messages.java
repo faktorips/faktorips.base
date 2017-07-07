ae7772b8575a02a326adbcc7587ec160443292bd
swidmaier <Stefan.Widmaier@faktorzehn.de>
2017-07-07 14:46:11 +0200

    FIPS-5526/FIPS-5527 :: Migration 3.21
    Change-Id: I4f1902d5a136cd879e3e97655b4697dd3ebae9b7

--- org.faktorips.devtools.core/src/org/faktorips/devtools/core/internal/migrationextensions/Messages.java
+++ org.faktorips.devtools.core/src/org/faktorips/devtools/core/internal/migrationextensions/Messages.java
@@ -26,6 +26,7 @@ public class Messages extends NLS {
     public static String Migration_3_18_0_description;
     public static String Migration_3_19_0_description;
     public static String Migration_3_20_0_description;
+    public static String Migration_3_21_0_description;
 
     static {
         // initialize resource bundle
