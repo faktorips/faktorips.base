3e64c0f8b4cf38d3a7927bd9c23a58cc102b9f40
Daniel Schwering <daniel.schwering@faktorzehn.de>
2018-10-09 10:14:52 +0200

    FIPS-5989/FIPS-6225 :: Use IAttributeValue.PROPERTY_VALUE_HOLDER instead of IAttributeValue.PROPERTY_VALUE for attribute validation messages
    Change-Id: I7223c4f004b5a0298a3858017f2d2828c32ae5e7

--- org.faktorips.devtools.core/src/org/faktorips/devtools/core/internal/model/ipsobject/SimpleCustomValidationForProductCmptAttributeValue.java
+++ org.faktorips.devtools.core/src/org/faktorips/devtools/core/internal/model/ipsobject/SimpleCustomValidationForProductCmptAttributeValue.java
@@ -18,29 +18,29 @@ import org.faktorips.util.message.Message;
 import org.faktorips.util.message.MessageList;
 
 /**
- * A class that allows to implement simple custom validations for attribute values defined in
- * product component generations. By simple we mean that the value of a single attribute is
- * validated. E.g. the value of the attribute "short name" must be at least 20 characters long. For
- * more sophisticated checks that involves for example dependencies between attributes (e.g. if
- * 'SumInsuredType' is 'Percentage' then 'sumInsured' must be set), you should inherit from
- * {@link AbstractCustomValidation}.
+ * Base class for the implementation (by way of implementing {@link #validate(String, IIpsProject)})
+ * of simple custom validations for attribute values defined in product components / generations.
+ * "Simple" here means that the value of a single attribute is validated. For example the value of
+ * the attribute "short name" must be at most 20 characters long. For more sophisticated checks that
+ * involve for example dependencies between attributes (e.g. if 'SumInsuredType' is 'Percentage'
+ * then 'sumInsured' must be set), you should inherit from {@link AbstractCustomValidation}.
  * 
  * @author Jan Ortmann
  */
-public abstract class SimpleCustomValidationForProductCmptAttributeValue extends
-        AbstractCustomValidation<IAttributeValue> {
+public abstract class SimpleCustomValidationForProductCmptAttributeValue
+        extends AbstractCustomValidation<IAttributeValue> {
 
     private String productCmptTypeName;
     private String attributeName;
 
     /**
      * Creates a simple validation for the product component attribute with the given name. Note
-     * that if severall types have an attribute with the given name, the validation is applied to
-     * all of them.
+     * that if several types have an attribute with the given name, the validation is applied to all
+     * of them.
      * 
-     * @param attributeName The name of the attribute.
+     * @param attributeName The name of the attribute
      * 
-     * @throws NullPointerException if attributeName is <code>null</code>.
+     * @throws NullPointerException if {@code attributeName} is <code>null</code>
      */
     public SimpleCustomValidationForProductCmptAttributeValue(String attributeName) {
         this(null, attributeName);
@@ -50,10 +50,10 @@ public abstract class SimpleCustomValidationForProductCmptAttributeValue extends
      * Creates a simple validation for the given product component attribute defined by product
      * component type and attribute name.
      * 
-     * @param productCmptTypeName The qualified name of the product component type.
-     * @param attributeName The name of the attribute.
+     * @param productCmptTypeName The qualified name of the product component type
+     * @param attributeName The name of the attribute
      * 
-     * @throws NullPointerException if attributeName is <code>null</code>.
+     * @throws NullPointerException if {@code attributeName} is <code>null</code>.
      */
     public SimpleCustomValidationForProductCmptAttributeValue(String productCmptTypeName, String attributeName) {
         super(IAttributeValue.class);
@@ -79,11 +79,11 @@ public abstract class SimpleCustomValidationForProductCmptAttributeValue extends
             return null;
         }
         try {
-            ValidationResult result = validate(attribute.getValue(), ipsProject);
+            ValidationResult result = validate(attribute.getPropertyValue(), ipsProject);
             if (result != null) {
                 MessageList messages = new MessageList();
                 Message msg = new Message(result.msgCode, result.text, result.severity, attribute,
-                        IAttributeValue.PROPERTY_VALUE);
+                        IAttributeValue.PROPERTY_VALUE_HOLDER);
                 messages.add(msg);
                 return messages;
             }
@@ -99,6 +99,15 @@ public abstract class SimpleCustomValidationForProductCmptAttributeValue extends
         return "Custom validation for " + type + ". " + attributeName; //$NON-NLS-1$ //$NON-NLS-2$
     }
 
+    /**
+     * Validates the attribute's value.
+     * 
+     * @param value the value of the attribute
+     * @param ipsProject the project containing the attribute's product component
+     * @return a {@link ValidationResult} containing a message text and code as well as an error
+     *         level or {@code null} if the validation finds no problems.
+     * @throws CoreException if an error occurs while validating the attribute
+     */
     public abstract ValidationResult validate(String value, IIpsProject ipsProject) throws CoreException;
 
     public static ValidationResult newError(String code, String text) {
@@ -113,6 +122,9 @@ public abstract class SimpleCustomValidationForProductCmptAttributeValue extends
         return new ValidationResult(Message.INFO, text, code);
     }
 
+    /**
+     * A simplified collection of values that will be used to create a {@link Message}.
+     */
     public static class ValidationResult {
 
         private int severity;
