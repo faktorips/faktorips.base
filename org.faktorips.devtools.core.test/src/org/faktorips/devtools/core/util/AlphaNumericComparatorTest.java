ad6cc08a08168bf1296bb0868732d7fcf703c82e
Daniel Schwering <daniel.schwering@faktorzehn.de>
2020-05-25 09:26:11 +0200

    FIPS-6881/FIPS-6883 :: Compare alphanumeric values with unlimited numeric length
    Change-Id: I1dda277dafb682e34884a9cf3826afa39daf2db3

--- org.faktorips.devtools.core.test/src/org/faktorips/devtools/core/util/AlphaNumericComparatorTest.java
+++ org.faktorips.devtools.core.test/src/org/faktorips/devtools/core/util/AlphaNumericComparatorTest.java
@@ -24,76 +24,82 @@ public class AlphaNumericComparatorTest {
     private AlphaNumericComparator comparator;
 
     @Test
-    public void testCompare_equalString() throws Exception {
+    public void testCompare_EqualString() throws Exception {
         assertEquals(0, comparator.compare("a", "a"));
     }
 
     @Test
-    public void testCompare_simpleString() throws Exception {
+    public void testCompare_SimpleString() throws Exception {
         assertTrue(comparator.compare("a", "b") < 0);
         assertTrue(comparator.compare("b", "a") > 0);
     }
 
     @Test
-    public void testCompare_equalNum() throws Exception {
+    public void testCompare_EqualNum() throws Exception {
         assertEquals(0, comparator.compare("1234", "1234"));
     }
 
     @Test
-    public void testCompare_simpleNum() throws Exception {
+    public void testCompare_SimpleNum() throws Exception {
         assertTrue(comparator.compare("3", "1234") < 0);
         assertTrue(comparator.compare("1234", "3") > 0);
     }
 
     @Test
-    public void testCompare_equalAlphaNum() throws Exception {
+    public void testCompare_EqualAlphaNum() throws Exception {
         assertEquals(0, comparator.compare("1.1.abc", "1.1.abc"));
     }
 
     @Test
-    public void testCompare_alphaNum1() throws Exception {
+    public void testCompare_AlphaNum1() throws Exception {
         assertTrue(comparator.compare("1.3.xxx", "1.10.xxx") < 0);
         assertTrue(comparator.compare("1.10.xxx", "1.3.xxx") > 0);
     }
 
     @Test
-    public void testCompare_alphaNum2() throws Exception {
+    public void testCompare_AlphaNum2() throws Exception {
         assertTrue(comparator.compare("1.3.aaa", "1.3.xxx") < 0);
         assertTrue(comparator.compare("1.3.xxx", "1.3.aaa") > 0);
     }
 
     @Test
-    public void testCompare_alphaNum3() throws Exception {
+    public void testCompare_AlphaNum3() throws Exception {
         assertTrue(comparator.compare("a3", "aa3") < 0);
         assertTrue(comparator.compare("aa3", "a3") > 0);
     }
 
     @Test
-    public void testCompare_alphaNum4() throws Exception {
+    public void testCompare_AlphaNum4() throws Exception {
         assertTrue(comparator.compare("a+3", "a3") < 0);
         assertTrue(comparator.compare("a3", "a+3") > 0);
     }
 
     @Test
-    public void testCompare_alphaNum5() throws Exception {
+    public void testCompare_AlphaNum5() throws Exception {
         assertTrue(comparator.compare("a 3", "aa3") < 0);
         assertTrue(comparator.compare("aa3", "a 3") > 0);
     }
 
     @Test
-    public void testCompare_alphaNum6() throws Exception {
+    public void testCompare_AlphaNum6() throws Exception {
         assertTrue(comparator.compare("1.12.xxx", "1.4.xxx") > 0);
         assertTrue(comparator.compare("1.4.xxx", "1.12.xxx") < 0);
     }
 
     @Test
-    public void testCompare_alphaNum7() throws Exception {
+    public void testCompare_AlphaNum7() throws Exception {
         assertTrue(comparator.compare("abcasdfasf sadfasdf asdf asd 12340", "abcasdfasf sadfasdf asdf asd 1234") > 0);
         assertTrue(comparator.compare("abcasdfasf sadfasdf asdf asd 1234", "abcasdfasf sadfasdf asdf asd 12340") < 0);
     }
 
     @Test
-    public void testCompare_leadingZeroNum1() throws Exception {
+    public void testCompare_LongAlphaNum() throws Exception {
+        assertTrue(comparator.compare("12017100002", "12017100001") > 0);
+        assertTrue(comparator.compare("A9223372036854775807B", "A9223372036854775808B") < 0);
+    }
+
+    @Test
+    public void testCompare_LeadingZeroNum1() throws Exception {
         assertTrue(comparator.compare("a01a", "a1b") < 0);
         assertTrue(comparator.compare("a1b", "a01a") > 0);
         assertTrue(comparator.compare("a01c", "a1b") > 0);
@@ -101,7 +107,7 @@ public class AlphaNumericComparatorTest {
     }
 
     @Test
-    public void testCompare_leadingZeroNum2() throws Exception {
+    public void testCompare_LeadingZeroNum2() throws Exception {
         assertTrue(comparator.compare("a01", "a1") < 0);
         assertTrue(comparator.compare("a1", "a01") > 0);
         assertTrue(comparator.compare("a1xxx0001", "a001xxx1") < 0);
