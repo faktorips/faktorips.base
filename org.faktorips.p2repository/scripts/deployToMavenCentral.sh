4d30054770f7a6906cc2485b857fe417b9ae377e
Daniel Hooker <daniel.hooker@faktorzehn.at>
2021-04-19 16:48:11 +0200

    FIPS-7644/FIPS-7645 :: Poms and plugin.xml with correct version
    Change-Id: Iba2c00bc11f0ce12f598321111e6f7586418877e

--- org.faktorips.p2repository/scripts/deployToMavenCentral.sh
+++ org.faktorips.p2repository/scripts/deployToMavenCentral.sh
@@ -16,6 +16,11 @@ IS_RELEASE=$2
 BASE_DIRECTORY=$3
 VERSION_KIND=$4
 
+# check additional tools
+command -v xmlstarlet --help >/dev/null 2>&1 || { echo >&2 "xmlstarlet is not installed.  Aborting."; exit 1; }
+command -v unzip >/dev/null 2>&1 || { echo >&2 "unzip is not installed.  Aborting."; exit 1; }
+command -v zip >/dev/null 2>&1 || { echo >&2 "zip is not installed.  Aborting."; exit 1; }
+
 if [ "$IS_RELEASE" = "true" ] 
 then
 	REPO_ID=ossrh    
@@ -115,6 +120,12 @@ echo $RUNTIME_LIB_SOURCE_JAR
 echo $RUNTIME_LIB_JAVADOC_JAR
 echo $RUNTIME_LIB_POM
 echo "********"
+echo "INFO Runtime Client"
+echo $RUNTIME_CLIENT_LIB_JAR
+echo $RUNTIME_CLIENT_LIB_SOURCE_JAR
+echo $RUNTIME_CLIENT_LIB_JAVADOC_JAR
+echo $RUNTIME_CLIENT_LIB_POM
+echo "********"
 echo "INFO Valuetypes"
 echo $VALUETYPES_LIB_JAR
 echo $VALUETYPES_LIB_SOURCE_JAR
@@ -149,18 +160,82 @@ echo "No Javadoc as the project is not a Java classpath-capable package"
 echo $MAVEN_ARCHETYPE_POM
 
 echo "########################"
-echo "upload Runtime"
-mvn gpg:sign-and-deploy-file -Durl=$REPO_URL -DrepositoryId=$REPO_ID -Dfile=$RUNTIME_LIB_JAR -Dsources=$RUNTIME_LIB_SOURCE_JAR -Djavadoc=$RUNTIME_LIB_JAVADOC_JAR -Dversion=$RELEASE_VERSION -DpomFile=$RUNTIME_LIB_POM
-echo "upload Runtime Client"
-mvn gpg:sign-and-deploy-file -Durl=$REPO_URL -DrepositoryId=$REPO_ID -Dfile=$RUNTIME_CLIENT_LIB_JAR -Dsources=$RUNTIME_CLIENT_LIB_SOURCE_JAR -Djavadoc=$RUNTIME_CLIENT_LIB_JAVADOC_JAR -Dversion=$RELEASE_VERSION -DpomFile=$RUNTIME_CLIENT_LIB_POM
-echo "upload Valuetypes"
-mvn gpg:sign-and-deploy-file -Durl=$REPO_URL -DrepositoryId=$REPO_ID -Dfile=$VALUETYPES_LIB_JAR -Dsources=$VALUETYPES_LIB_SOURCE_JAR -Djavadoc=$VALUETYPES_LIB_JAVADOC_JAR -Dversion=$RELEASE_VERSION -DpomFile=$VALUETYPES_LIB_POM
-echo "upload Valuetypes Joda"
-mvn gpg:sign-and-deploy-file -Durl=$REPO_URL -DrepositoryId=$REPO_ID -Dfile=$VALUETYPES_JODA_LIB_JAR -Dsources=$VALUETYPES_JODA_LIB_SOURCE_JAR -Djavadoc=$VALUETYPES_JODA_LIB_JAVADOC_JAR -Dversion=$RELEASE_VERSION -DpomFile=$VALUETYPES_JODA_LIB_POM
-echo "upload Groovy"
-mvn gpg:sign-and-deploy-file -Durl=$REPO_URL -DrepositoryId=$REPO_ID -Dfile=$GROOVY_LIB_JAR -Dsources=$GROOVY_LIB_SOURCE_JAR -Djavadoc=$GROOVY_LIB_JAVADOC_JAR -Dversion=$RELEASE_VERSION -DpomFile=$GROOVY_LIB_POM
-echo "upload Maven Plugin"
-mvn gpg:sign-and-deploy-file -Durl=$REPO_URL -DrepositoryId=$REPO_ID -Dfile=$MAVEN_PLUGIN_JAR -Dsources=$MAVEN_PLUGIN_SOURCE_JAR -Djavadoc=$MAVEN_PLUGIN_JAVADOC_JAR -Dversion=$RELEASE_VERSION -DpomFile=$MAVEN_PLUGIN_POM
-echo "upload Maven Archetype"
-mvn gpg:sign-and-deploy-file -Durl=$REPO_URL -DrepositoryId=$REPO_ID -Dfile=$MAVEN_ARCHETYPE_JAR -Dsources=$MAVEN_ARCHETYPE_SOURCE_JAR -Dversion=$RELEASE_VERSION -DpomFile=$MAVEN_ARCHETYPE_POM
+echo "replace dev Poms with relase Poms"
 
+# $1  Project Name
+# $2  JAR File
+# $3  POM file
+function replacePom {
+    echo "working on $1"
+    unzip $2 META-INF/maven/*
+ 
+    JAR_POM=$(find META-INF/maven -name pom.xml)
+    cp $3 $JAR_POM
+ 
+    JAR_PROP=$(find META-INF/maven -name pom.properties)
+    sed -i "s/version=.*/version=$RELEASE_VERSION/g" $JAR_PROP
+ 
+    zip $2 $JAR_POM $JAR_PROP
+    rm -rf META-INF
+}
+
+# $1  Project Name
+# $2  JAR File
+# $3  POM file
+function replacePomsAndPluginXml {
+    echo "working on $1"
+    unzip $2 META-INF/maven/*
+ 
+    JAR_POM=$(find META-INF/maven -name pom.xml)
+    cp $3 $JAR_POM
+ 
+    JAR_PROP=$(find META-INF/maven -name pom.properties)
+    sed -i "s/version=.*/version=$RELEASE_VERSION/g" $JAR_PROP
+ 
+    JAR_PLUGIN=$(find META-INF/maven -name plugin.xml)
+    mv $JAR_PLUGIN $JAR_PLUGIN.old
+    xmlstarlet edit --update "/plugin/version" --value "$RELEASE_VERSION" $JAR_PLUGIN.old > $JAR_PLUGIN
+ 
+    JAR_PLUGIN_HELP=$(find META-INF/maven -name plugin-help.xml)
+    mv $JAR_PLUGIN_HELP $JAR_PLUGIN_HELP.old
+    xmlstarlet edit --update "/plugin/version" --value "$RELEASE_VERSION" $JAR_PLUGIN_HELP.old > $JAR_PLUGIN_HELP
+ 
+    zip $2 $JAR_POM $JAR_PROP $JAR_PLUGIN $JAR_PLUGIN_HELP
+    rm -rf META-INF
+}
+
+replacePom $RUNTIME_LIB $RUNTIME_LIB_JAR $RUNTIME_LIB_POM
+replacePom $VALUETYPES_LIB $VALUETYPES_LIB_JAR $VALUETYPES_LIB_POM
+replacePom $VALUETYPES_JODA_LIB $VALUETYPES_JODA_LIB_JAR $VALUETYPES_JODA_LIB_POM
+replacePom $GROOVY_LIB $GROOVY_LIB_JAR $GROOVY_LIB_POM
+replacePom $MAVEN_ARCHETYPE $MAVEN_ARCHETYPE_JAR $MAVEN_ARCHETYPE_POM
+replacePomsAndPluginXml $MAVEN_PLUGIN $MAVEN_PLUGIN_JAR $MAVEN_PLUGIN_POM
+
+# $1  Project Name
+# $2  JAR File
+# $3  Source File
+# $4  Javadoc file 
+# $5  POM file 
+function signAndDeploy {
+  echo "signing and deploying $1"
+  mvn gpg:sign-and-deploy-file -Durl=$REPO_URL -DrepositoryId=$REPO_ID -Dfile=$2 -Dsources=$3 -Djavadoc=$4 -Dversion=$RELEASE_VERSION -DpomFile=$5
+}
+
+# $1  Project Name
+# $2  JAR File
+# $3  Source File
+# $4  POM file 
+function signAndDeployWithoutJavadoc {
+  echo "signing and deploying $1"
+  mvn gpg:sign-and-deploy-file -Durl=$REPO_URL -DrepositoryId=$REPO_ID -Dfile=$2 -Dsources=$3 -Dversion=$RELEASE_VERSION -DpomFile=$4
+}
+
+echo "########################"
+echo "sign and deploy"
+signAndDeploy $RUNTIME_LIB $RUNTIME_LIB_JAR $RUNTIME_LIB_SOURCE_JAR $RUNTIME_LIB_JAVADOC_JAR $RUNTIME_LIB_POM
+signAndDeploy $RUNTIME_CLIENT_LIB $RUNTIME_CLIENT_LIB_JAR $RUNTIME_CLIENT_LIB_SOURCE_JAR $RUNTIME_CLIENT_LIB_JAVADOC_JAR $RUNTIME_CLIENT_LIB_POM
+signAndDeploy $VALUETYPES_LIB $VALUETYPES_LIB_JAR $VALUETYPES_LIB_SOURCE_JAR $VALUETYPES_LIB_JAVADOC_JAR $VALUETYPES_LIB_POM
+signAndDeploy $VALUETYPES_JODA_LIB $VALUETYPES_JODA_LIB_JAR $VALUETYPES_JODA_LIB_SOURCE_JAR $VALUETYPES_JODA_LIB_JAVADOC_JAR $VALUETYPES_JODA_LIB_POM
+signAndDeploy $GROOVY_LIB $GROOVY_LIB_JAR $GROOVY_LIB_SOURCE_JAR $GROOVY_LIB_JAVADOC_JAR $GROOVY_LIB_POM
+signAndDeploy $MAVEN_PLUGIN $MAVEN_PLUGIN_JAR $MAVEN_PLUGIN_SOURCE_JAR $MAVEN_PLUGIN_JAVADOC_JAR $MAVEN_PLUGIN_POM
+signAndDeployWithoutJavadoc $MAVEN_ARCHETYPE $MAVEN_ARCHETYPE_JAR $MAVEN_ARCHETYPE_SOURCE_JAR $MAVEN_ARCHETYPE_POM
