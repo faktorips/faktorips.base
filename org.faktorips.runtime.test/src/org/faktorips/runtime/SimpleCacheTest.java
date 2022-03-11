1392a1776dd4a67a02bfb0b11a6e7316a0e8f3cd
Lara Stork <lara.stork@faktorzehn.de>
2022-03-11 19:10:32 +0100

    FIPS-8676/FIPS-8706 :: Fix SimpleCache and add test case
    Change-Id: I05cc0f0f45793cb2e2d73f9f95df03c9bcdfa68e

---
+++ org.faktorips.runtime.test/src/org/faktorips/runtime/SimpleCacheTest.java
@@ -0,0 +1,30 @@
+package org.faktorips.runtime;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.faktorips.runtime.caching.IComputable;
+import org.junit.Test;
+
+public class SimpleCacheTest {
+
+    int number = 1;
+
+    @Test
+    public void testCompute() throws InterruptedException {
+        IComputable<Object, Object> computable = IComputable.of(Object.class, this::getNotCachedNumber);
+        SimpleCache cache = new SimpleCache(computable);
+        Object computedObjectOne = cache.compute("One");
+        assertThat(1, is(computedObjectOne));
+        Object computedObjectTwo = cache.compute("Two");
+        assertThat(2, is(computedObjectTwo));
+        Object computedObjectThree = cache.compute("Three");
+        assertThat(3, is(computedObjectThree));
+        Object computedObjectOneAgain = cache.compute("One");
+        assertThat(1, is(computedObjectOneAgain));
+    }
+
+    private Object getNotCachedNumber(Object key) {
+        return number++;
+    }
+}
