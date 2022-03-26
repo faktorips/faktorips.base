98f2bf30adf58a603b8b51b952ea0581ba986ebd
Christoph Läubrich <christoph@laeubi-soft.de>
2022-03-26 07:07:20 +0100

    FIPS-8557/FIPS-8735 :: Issue #21 : Convert org.faktorips.runtime
    Change-Id: Iae4b5a11fd265810eb8d9a303c5bbefb7c7012b9

---
+++ org.faktorips.runtime/src/test/java/org/faktorips/runtime/internal/AbstractClassLoadingRuntimeRepositoryTest.java
@@ -0,0 +1,242 @@
+/*******************************************************************************
+ * Copyright (c) Faktor Zehn GmbH - faktorzehn.org
+ * 
+ * This source code is available under the terms of the AGPL Affero General Public License version
+ * 3.
+ * 
+ * Please see LICENSE.txt for full license terms, including the additional permissions and
+ * restrictions as well as the possibility of alternative license terms.
+ *******************************************************************************/
+
+package org.faktorips.runtime.internal;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.RETURNS_SMART_NULLS;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.withSettings;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.faktorips.runtime.ClassloaderRuntimeRepository;
+import org.faktorips.runtime.IRuntimeRepository;
+import org.faktorips.runtime.ITable;
+import org.faktorips.runtime.internal.productvariant.ProductVariantRuntimeHelper;
+import org.faktorips.runtime.internal.toc.EnumContentTocEntry;
+import org.faktorips.runtime.internal.toc.GenerationTocEntry;
+import org.faktorips.runtime.internal.toc.ProductCmptTocEntry;
+import org.faktorips.runtime.internal.toc.TableContentTocEntry;
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+public class AbstractClassLoadingRuntimeRepositoryTest {
+
+    private AbstractClassLoadingRuntimeRepository repo;
+    private ProductComponent loadedProdCmpt;
+
+    @Before
+    public void setUp() throws Exception {
+        loadedProdCmpt = mock(ProductComponent.class);
+        repo = new ClassloaderRuntimeRepository(getClass().getClassLoader(), "",
+                "org/faktorips/runtime/internal/ClassLoaderRuntimeRepoToc.xml");
+        repo = spy(repo);
+        doReturn(loadedProdCmpt).when(repo).getProductComponent(anyString());
+        doNothing().when(repo).initialize();
+    }
+
+    @Test
+    public void shouldLoadNormalProductComponentNormally() {
+        ProductCmptTocEntry entry = mock(ProductCmptTocEntry.class, withSettings().defaultAnswer(RETURNS_SMART_NULLS));
+        Element element = mock(Element.class);
+        ProductComponent newProdCmptInstance = mock(ProductComponent.class);
+
+        doReturn(element).when(repo).getDocumentElement(entry);
+        when(element.hasAttribute(anyString())).thenReturn(false);
+        doReturn(newProdCmptInstance).when(repo).createProductComponentInstance(anyString(), anyString(), anyString(),
+                anyString());
+
+        repo.createProductCmpt(entry);
+
+        verify(newProdCmptInstance).initFromXml(element);
+        verifyNoMoreInteractions(loadedProdCmpt);
+    }
+
+    @Test
+    public void shouldLoadVariedProductComponent() {
+        ProductCmptTocEntry entry = mock(ProductCmptTocEntry.class, withSettings().defaultAnswer(RETURNS_SMART_NULLS));
+        Element element = mock(Element.class, withSettings().defaultAnswer(RETURNS_SMART_NULLS));
+        Element copyElement = mock(Element.class);
+        Document docMock = mock(Document.class);
+        ProductComponent newProdCmptInstance = mock(ProductComponent.class);
+
+        when(element.getOwnerDocument()).thenReturn(docMock);
+        when(docMock.cloneNode(anyBoolean())).thenReturn(docMock);
+        doReturn(element).when(repo).getDocumentElement(entry);
+        when(element.hasAttribute(anyString())).thenReturn(true);
+        doReturn(copyElement).when(loadedProdCmpt).toXml(any(Document.class));
+        doReturn(newProdCmptInstance).when(repo).createProductComponentInstance(anyString(), anyString(), anyString(),
+                anyString());
+
+        repo.createProductCmpt(entry);
+
+        verify(newProdCmptInstance).initFromXml(element);
+        verify(newProdCmptInstance).initFromXml(copyElement);
+        verifyNoMoreInteractions(newProdCmptInstance);
+    }
+
+    @Test
+    public void shouldLoadNormalGenerationNormally() {
+        GenerationTocEntry genEntry = mock(GenerationTocEntry.class);
+        ProductCmptTocEntry parentEntry = mock(ProductCmptTocEntry.class, withSettings().defaultAnswer(RETURNS_SMART_NULLS));
+        Element element = mock(Element.class);
+        Element prodCmptElement = mock(Element.class);
+        ProductComponentGeneration gen = mock(ProductComponentGeneration.class);
+
+        doReturn(parentEntry).when(genEntry).getParent();
+        doReturn(element).when(repo).getDocumentElement(genEntry);
+        doReturn(prodCmptElement).when(repo).getDocumentElement(parentEntry);
+        when(prodCmptElement.hasAttribute(anyString())).thenReturn(false);
+        doReturn(gen).when(repo).createProductComponentGenerationInstance(genEntry, loadedProdCmpt);
+
+        repo.createProductCmptGeneration(genEntry);
+
+        verify(gen).initFromXml(element);
+    }
+
+    @Test
+    public void shouldLoadVariedGeneration() {
+        GenerationTocEntry genEntry = mock(GenerationTocEntry.class);
+        Element genElement = mock(Element.class);
+        ProductVariantRuntimeHelper helper = mock(ProductVariantRuntimeHelper.class);
+
+        doReturn(genElement).when(repo).getDocumentElement(genEntry);
+        when(genElement.hasAttribute(anyString())).thenReturn(true);
+        doReturn(helper).when(repo).getProductVariantHelper();
+
+        when(helper.isProductVariantXML(genElement)).thenReturn(true);
+
+        repo.createProductCmptGeneration(genEntry);
+
+        verify(helper).initProductComponentGenerationVariation(repo, genEntry, genElement);
+    }
+
+    @Test
+    public void testCreateEnumValues_NoXmlResourceName() throws Exception {
+        EnumContentTocEntry tocEntry = mock(EnumContentTocEntry.class);
+        // in a DetachedContentRuntimeRepository, the XML resource name will be empty, but an XML
+        // input stream may be created nonetheless
+        doReturn(EnumSaxHandlerTest.class.getClassLoader()
+                .getResourceAsStream("org/faktorips/runtime/internal/EnumSaxHandlerTest.xml")).when(repo)
+                        .getXmlAsStream(tocEntry);
+        List<TestEnum> enumValues = repo.createEnumValues(tocEntry, TestEnum.class);
+
+        assertThat(enumValues.size(), is(3));
+        for (int i = 0; i < enumValues.size(); i++) {
+            // repo.createEnumValues only creates extend values, the two static values are not
+            // included, but the index must start with 2 instead of 0
+            assertThat(enumValues.get(i).index, is(i + 2));
+        }
+    }
+
+    @Test
+    public void testCreateEnumValues_NoXml() throws Exception {
+        EnumContentTocEntry tocEntry = mock(EnumContentTocEntry.class);
+
+        List<Object> enumValues = repo.createEnumValues(tocEntry, Object.class);
+
+        assertTrue(enumValues.isEmpty());
+    }
+
+    @Test
+    public void testCreateEnumValues_Empty() throws Exception {
+        EnumContentTocEntry tocEntry = mock(EnumContentTocEntry.class);
+        when(tocEntry.getXmlResourceName()).thenReturn("org/faktorips/runtime/internal/EmptyEnum.xml");
+
+        List<Object> enumValues = repo.createEnumValues(tocEntry, Object.class);
+
+        assertTrue(enumValues.isEmpty());
+    }
+
+    @Test
+    public void testCreateEnumValues_WithCorrectIndex() throws Exception {
+        EnumContentTocEntry tocEntry = mock(EnumContentTocEntry.class);
+        when(tocEntry.getXmlResourceName()).thenReturn("org/faktorips/runtime/internal/EnumSaxHandlerTest.xml");
+
+        List<TestEnum> enumValues = repo.createEnumValues(tocEntry, TestEnum.class);
+
+        assertThat(enumValues.size(), is(3));
+        for (int i = 0; i < enumValues.size(); i++) {
+            // repo.createEnumValues only creates extend values, the two static values are not
+            // included, but the index must start with 2 instead of 0
+            assertThat(enumValues.get(i).index, is(i + 2));
+        }
+    }
+
+    @Test
+    public void testCreateTable() {
+        String ipsObjectId = "qualifiedTableName";
+        String ipsObjectQualifiedName = "qualifiedTableName";
+        String xmlResourceName = "org/faktorips/runtime/internal/TableTest.xml";
+        String implementationClassName = "org.faktorips.runtime.internal.TestTable";
+        TableContentTocEntry tocEntry = new TableContentTocEntry(ipsObjectId, ipsObjectQualifiedName, xmlResourceName,
+                implementationClassName);
+
+        ITable<?> table = repo.createTable(tocEntry);
+
+        assertNotNull(table.getName());
+        assertEquals(tocEntry.getIpsObjectId(), table.getName());
+    }
+
+    public static class TestEnum {
+
+        public static final List<TestEnum> VALUES = Arrays.asList(new TestEnum(0, "A", "A", null),
+                new TestEnum(1, "B", "B", null));
+
+        private final int index;
+
+        private final String id;
+
+        private final String name;
+
+        protected TestEnum(int index, String id, String name,
+                @SuppressWarnings("unused") IRuntimeRepository runtimeRepository) {
+            this.index = index;
+            this.id = id;
+            this.name = name;
+        }
+
+        public int getIndex() {
+            return index;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public String toString() {
+            return "TestEnum [index=" + index + ", id=" + id + ", name=" + name + "]";
+        }
+
+    }
+
+}
---
+++ org.faktorips.runtime/src/test/java/org/faktorips/runtime/internal/productvariant/ProductVariantRuntimeHelperTest.java
@@ -0,0 +1,118 @@
+/*******************************************************************************
+ * Copyright (c) Faktor Zehn GmbH - faktorzehn.org
+ * 
+ * This source code is available under the terms of the AGPL Affero General Public License version
+ * 3.
+ * 
+ * Please see LICENSE.txt for full license terms, including the additional permissions and
+ * restrictions as well as the possibility of alternative license terms.
+ *******************************************************************************/
+
+package org.faktorips.runtime.internal.productvariant;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import java.util.Calendar;
+
+import org.faktorips.runtime.IClRepositoryObject;
+import org.faktorips.runtime.IRuntimeRepository;
+import org.faktorips.runtime.internal.DateTime;
+import org.faktorips.runtime.internal.IXmlPersistenceSupport;
+import org.faktorips.runtime.internal.ProductComponent;
+import org.faktorips.runtime.internal.ProductComponentGeneration;
+import org.faktorips.runtime.internal.toc.GenerationTocEntry;
+import org.faktorips.runtime.internal.toc.ProductCmptTocEntry;
+import org.junit.Test;
+import org.mockito.InOrder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+public class ProductVariantRuntimeHelperTest {
+
+    @Test
+    public void testIsProductVariantXML() {
+        Element element = mock(Element.class);
+        ProductVariantRuntimeHelper helper = new ProductVariantRuntimeHelper();
+
+        when(element.hasAttribute(ProductVariantRuntimeHelper.ATTRIBUTE_NAME_VARIED_PRODUCT_CMPT)).thenReturn(true);
+        assertTrue(helper.isProductVariantXML(element));
+
+        when(element.hasAttribute(ProductVariantRuntimeHelper.ATTRIBUTE_NAME_VARIED_PRODUCT_CMPT)).thenReturn(false);
+        assertFalse(helper.isProductVariantXML(element));
+    }
+
+    @Test
+    public void returnFalseForProductVariantXML() {
+        Element element = mock(Element.class);
+        when(element.hasAttribute(ProductVariantRuntimeHelper.ATTRIBUTE_NAME_VARIED_PRODUCT_CMPT)).thenReturn(false);
+        ProductVariantRuntimeHelper helper = new ProductVariantRuntimeHelper();
+
+        assertFalse(helper.isProductVariantXML(element));
+    }
+
+    @Test
+    public void initGeneration() {
+        IRuntimeRepository runtimeRepository = mock(IRuntimeRepository.class);
+        GenerationTocEntry genEntry = mock(GenerationTocEntry.class);
+        ProductCmptTocEntry parentEntry = mock(ProductCmptTocEntry.class);
+
+        ProductComponent originalProductCmpt = mock(ProductComponent.class);
+        ProductComponent variedProdCmpt = mock(ProductComponent.class);
+        ProductComponentGeneration originalProductCmptGen = mock(ProductComponentGeneration.class);
+        ProductComponentGeneration productCmptGenToInitialize = mock(ProductComponentGeneration.class);
+        Element genVariationElement = mock(Element.class);
+
+        ProductVariantRuntimeHelper helper = spy(new ProductVariantRuntimeHelper());
+        doNothing().when(helper).loadAndVary(originalProductCmptGen, genVariationElement, productCmptGenToInitialize);
+        doReturn(originalProductCmpt).when(helper).getOriginalProdCmpt(runtimeRepository, genVariationElement);
+        when(originalProductCmpt.getGenerationBase(any(Calendar.class))).thenReturn(originalProductCmptGen);
+        when(runtimeRepository.getProductComponent(anyString())).thenReturn(variedProdCmpt);
+        doReturn(productCmptGenToInitialize).when(helper).createNewInstance(originalProductCmptGen, variedProdCmpt);
+        when(genEntry.getValidFrom()).thenReturn(new DateTime(2010, 1, 1));
+        doReturn(parentEntry).when(genEntry).getParent();
+        doReturn("ParentID").when(parentEntry).getIpsObjectId();
+
+        helper.initProductComponentGenerationVariation(runtimeRepository, genEntry, genVariationElement);
+
+        verify(helper).loadAndVary(originalProductCmptGen, genVariationElement, productCmptGenToInitialize);
+    }
+
+    @Test
+    public void initWithVariation() {
+        Element variationElement = mock(Element.class);
+        Document docMock = mock(Document.class);
+        when(variationElement.getOwnerDocument()).thenReturn(docMock);
+        when(docMock.cloneNode(false)).thenReturn(docMock);
+
+        Element originalElement = mock(Element.class);
+        IXmlPersistenceSupport originalObject = mock(IXmlPersistenceSupport.class);
+        when(originalObject.toXml(docMock)).thenReturn(originalElement);
+
+        IClRepositoryObject objectToInitialize = mock(IClRepositoryObject.class);
+
+        ProductVariantRuntimeHelper helper = new ProductVariantRuntimeHelper();
+        helper.loadAndVary(originalObject, variationElement, objectToInitialize);
+
+        InOrder order = inOrder(objectToInitialize);
+        order.verify(objectToInitialize).initFromXml(originalElement);
+        order.verify(objectToInitialize).initFromXml(variationElement);
+        verifyNoMoreInteractions(objectToInitialize);
+    }
+
+    @Test
+    public void initProdCmptGeneration() {
+
+    }
+
+}
