cb45c287ea0dfd60e3f588ceeb93afba4f1fea8c
Daniel Hooker <daniel.hooker@faktorzehn.at>
2022-04-04 13:51:32 +0200

    FIPS-7548/FIPS-8620 :: added interface INamedDatatype
    Change-Id: I68ce4722121781a5b4925bfaaeb617ee34963871

---
+++ org.faktorips.dtflcommon/src/org/faktorips/datatype/NamedDatatype.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) Faktor Zehn GmbH - faktorzehn.org
+ * 
+ * This source code is available under the terms of the AGPL Affero General Public License version
+ * 3.
+ * 
+ * Please see LICENSE.txt for full license terms, including the additional permissions and
+ * restrictions as well as the possibility of alternative license terms.
+ *******************************************************************************/
+package org.faktorips.datatype;
+
+/**
+ * A {@link ValueDatatype} that can have a human readable name (for example currency symbols like $
+ * and € or full names like "annual payment" or "monthly payment") for each of its values. That name
+ * can be displayed in addition to or instead of the actual value (like USD or EUR or a payment mode
+ * 1 or 12), which is used as the technical ID and to persist values of the datatype.
+ * <p>
+ * Note that due to the inheritance hierarchy of datatypes, presence of this interface is no
+ * guarantee that the implementation actually supports names - only those that return {@code true}
+ * from their {@link #isSupportingNames()} method do.
+ */
+public interface NamedDatatype extends ValueDatatype {
+    /**
+     * Returns a short description of the value of this datatype specified by the ID.
+     * 
+     * @throws IllegalArgumentException if the given ID is not a valid ID of this datatype
+     * @throws IllegalStateException if the datatype {@link #isSupportingNames() does not support
+     *             names}
+     */
+    public String getValueName(String id);
+
+    /**
+     * Returns {@code true} if an implementation of this interface supports names that describe the
+     * datatype's value. E.g. an enum datatype PaymentMode might return the name "annual" for the
+     * annual payment mode with ID "1". If this method returns {@code false} a call to the
+     * {@link #getValueName(String)} method is supposed to throw an {@link IllegalStateException}.
+     */
+    public boolean isSupportingNames();
+}
--- org.faktorips.dtflcommon/src/org/faktorips/datatype/ValueDatatype.java
+++ org.faktorips.dtflcommon/src/org/faktorips/datatype/ValueDatatype.java
@@ -11,6 +11,7 @@
 package org.faktorips.datatype;
 
 import org.faktorips.runtime.MessageList;
+import org.faktorips.values.NullObject;
 
 /**
  * A datatype representing values (in contrast to reference objects).
@@ -19,45 +20,47 @@ public interface ValueDatatype extends Datatype {
 
     /**
      * If this datatype represents a primitive type, this method returns the datatype that
-     * represents the wrapper class. Returns <code>null</code> if this datatype does not represent a
+     * represents the wrapper class. Returns {@code null} if this datatype does not represent a
      * primitive.
      */
     public ValueDatatype getWrapperType();
 
     /**
-     * Returns <code>true</code> if the given string can be parsed to a value of this datatype.
-     * Returns <code>false</code> otherwise.
+     * Returns {@code true} if the given string can be parsed to a value of this datatype. Returns
+     * {@code false} otherwise.
      */
     public boolean isParsable(String value);
 
     /**
-     * Returns <code>true</code> if the given string is <code>null</code> or the representation of
-     * the NullObject (if the datatype value class makes use of the null object pattern.) Returns
-     * <code>false</code> otherwise.
+     * Returns {@code true} if the given string is {@code null} or the representation of the null
+     * object (if the datatype value class makes use of the null object pattern.) Returns
+     * {@code false} otherwise.
+     *
+     * @see NullObject
      */
     public boolean isNull(String value);
 
     /**
-     * Returns <code>true</code> if this is a mutable datatype, <code>false</code> if it is an
-     * immutable datatype.
+     * Returns {@code true} if this is a mutable datatype, {@code false} if it is an immutable
+     * datatype.
      * 
-     * @return s.above
+     * @return whether this is a mutable datatype
      */
     public boolean isMutable();
 
     /**
-     * Returns <code>true</code> if this is an immutable datatype, <code>false</code> otherwise.
+     * Returns {@code true} if this is an immutable datatype, {@code false} otherwise.
      * 
-     * @return s.above
+     * @return whether this is an immutable datatype
      */
     public boolean isImmutable();
 
     /**
      * Returns the datatype's default value. For datatypes representing objects the method returns
-     * <code>null</code>. For datatypes representing Java primitives the Java default value is
-     * returned, e.g. 0 for int.
+     * {@code null}. For datatypes representing Java primitives the Java default value is returned,
+     * e.g. 0 for int.
      * 
-     * @throws UnsupportedOperationException if this datatype is the Datatype Void.
+     * @throws UnsupportedOperationException if this datatype is the Datatype {@link Void}.
      * 
      * @see Void
      */
@@ -66,44 +69,50 @@ public interface ValueDatatype extends Datatype {
     /**
      * This method parses the given string and returns the value as an instance of the class this
      * value datatype represents.
-     * 
+     * <p>
      * Use with caution: During development time Faktor-IPS maintains all values with their string
      * representation. This allows to change the value's datatype without the need to convert the
      * value from one class to another (e.g. if the string representation is 42 you can change the
      * datatype from integer to string without converting the integer object to a string object.
+     * <p>
+     * May throw different exceptions if the given string does not represent any value, for example
+     * a {@link NumberFormatException} when {@code "twelve"} is passed to
+     * {@link PrimitiveIntegerDatatype#getValue(String)}.
      * 
-     * @param value string represenation of the value
-     * @return The value as instance of the class this datatype represents.
-     * 
-     * 
+     * @param value the string representation of a value
+     * @return the value as instance of the class this datatype represents
      */
     public Object getValue(String value);
 
     /**
-     * @return <code>true</code> if this datatype is able to compare two values.
+     * @return {@code true} if this datatype is able to compare two values.
      */
     public boolean supportsCompare();
 
     /**
+     * Compares the values created from the two given strings.
+     * 
      * @param valueA The value to compare to valueB
      * @param valueB The value to compare to valueA
      * @return A value less than 0 if valueA is less than valueB, 0 if valueA is equal to valueB and
      *         a value greater than 0 if valueA is greater than valueB.
      * @throws UnsupportedOperationException if compare is not supported by this datatype.
      * @see #supportsCompare()
+     * @see #getValue(String)
      */
     public int compare(String valueA, String valueB) throws UnsupportedOperationException;
 
     /**
-     * Returns <code>true</code> if the both given strings represent the same value defined by this
-     * datatype. The String " 1" (a blank followed by the char '1') and "1" (just the char '1') are
-     * equal if the datatype is an Integer, but will not be equal if the datatype is a String.
+     * Returns {@code true} if both given strings represent the same value defined by this datatype.
+     * The String " 1" (a blank followed by the char '1') and "1" (just the char '1') are equal if
+     * the datatype is an {@link Integer}, but will not be equal if the datatype is a
+     * {@link String}.
      * 
      * @param valueA The first parameter to compare
      * @param valueB The second parameter to compare
      * 
-     * @return <code>true</code> if the two values are equal according to the datatype, returns
-     *         <code>false</code> if they are different.
+     * @return {@code true} if the two values are equal according to the datatype, returns
+     *         {@code false} if they are different.
      * 
      * @throws IllegalArgumentException if one of the parameter values doesn't exist in the value
      *             set of this datatype.
