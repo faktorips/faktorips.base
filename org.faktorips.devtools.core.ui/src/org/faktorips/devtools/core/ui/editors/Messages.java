21cc3d198ef218dfdcdecd213b6ea2adc3dde96b
Sebastian Böhnke <Sebastian.Boehnke@faktorzehn.de>
2022-07-12 10:36:33 +0200

    FIPS-8932/FIPS-9095 :: show confirmation dialog before deleting an attribute
    Change-Id: I298362a3d3b9d3d569233d5183e83bb1eac6afcd

--- org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/editors/Messages.java
+++ org.faktorips.devtools.core.ui/src/org/faktorips/devtools/core/ui/editors/Messages.java
@@ -55,6 +55,8 @@ public class Messages extends NLS {
     public static String IpsPartsComposite_buttonUp;
     public static String IpsPartsComposite_buttonDown;
     public static String IpsPartsComposite_override;
+    public static String IpsPartsComposite_deleteAttribute;
+    public static String IpsPartsComposite_deleteAttributeConfirm;
 
     public static String UnparsableFilePage_fileContentIsNotParsable;
 
