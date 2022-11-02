8e4bfb3ae17d7af7a75f6c5bb45dc9727c918a9c
Daniel Schwering <daniel.schwering@faktorzehn.de>
2022-11-02 09:17:44 +0100

    FIPS-8935/FIPS-9341 :: Include model.eclipse in feature/repository
    Change-Id: I85d2026c2bcb23a70741bb2fed307bc8898014ed

--- devtools/eclipse/plugins/tests/org.faktorips.devtools.abstraction.eclipse.test/src/org/faktorips/devtools/abstraction/eclipse/internal/EclipseAbstractionTestSetup.java
+++ devtools/eclipse/plugins/tests/org.faktorips.devtools.abstraction.eclipse.test/src/org/faktorips/devtools/abstraction/eclipse/internal/EclipseAbstractionTestSetup.java
@@ -86,7 +86,7 @@ public class EclipseAbstractionTestSetup extends AbstractAbstractionTestSetup {
         String[] natures = description.getNatureIds();
         String[] newNatures = new String[natures.length + 1];
         System.arraycopy(natures, 0, newNatures, 1, natures.length);
-        newNatures[0] = "org.faktorips.devtools.model.ipsnature";
+        newNatures[0] = "org.faktorips.devtools.model.eclipse.ipsnature";
         description.setNatureIds(newNatures);
         return description;
     }
