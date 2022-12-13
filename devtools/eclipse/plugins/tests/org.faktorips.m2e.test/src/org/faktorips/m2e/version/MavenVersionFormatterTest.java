5341e93ef77ce2352fc2cb016bd7f4608b1aa2ca
Sebastian Böhnke <Sebastian.Boehnke@faktorzehn.de>
2022-12-13 11:30:39 +0100

    FIPS-9501/FIPS-9530 :: Fix Wrong version in pom for addNature Wizard
    Change-Id: I5662fe9f731d2f38a605c1fda1bdd2843bbe6adf
    (cherry picked from commit 190f30917fd55631548b9ffef8f0392aebcff4aa)

--- devtools/eclipse/plugins/tests/org.faktorips.m2e.test/src/org/faktorips/m2e/version/MavenVersionFormatterTest.java
+++ devtools/eclipse/plugins/tests/org.faktorips.m2e.test/src/org/faktorips/m2e/version/MavenVersionFormatterTest.java
@@ -30,6 +30,33 @@ public class MavenVersionFormatterTest {
     }
 
     @Test
+    public void testFormatVersionWithReleaseQualifier() {
+        String versionWithReleaseQualifier = "1.0.0.release";
+        assertThat(MavenVersionFormatter.formatVersion(versionWithReleaseQualifier), is("1.0.0.release"));
+    }
+
+    @Test
+    public void testFormatVersionWithRCQualifier() {
+        String versionWithRCQualifier = "20.6.0.rc01";
+        assertThat(MavenVersionFormatter.formatVersion(versionWithRCQualifier),
+                is("20.6.0.rc01"));
+    }
+
+    @Test
+    public void testFormatVersionWithMilestoneQualifier() {
+        String versionWithMilestoneQualifier = "20.6.0.m01";
+        assertThat(MavenVersionFormatter.formatVersion(versionWithMilestoneQualifier),
+                is("20.6.0.m01"));
+    }
+
+    @Test
+    public void testFormatVersionWithAlphaQualifier() {
+        String versionWithAlphaQualifier = "20.6.0.a20200513-01";
+        assertThat(MavenVersionFormatter.formatVersion(versionWithAlphaQualifier),
+                is("20.6.0.a20200513-01"));
+    }
+
+    @Test
     public void testFormatVersionWithRuntimeQualifier() {
         String versionWithQualifier = "1.0.0.randomQualifier";
         assertThat(MavenVersionFormatter.formatVersion(versionWithQualifier), is("1.0.0"));
