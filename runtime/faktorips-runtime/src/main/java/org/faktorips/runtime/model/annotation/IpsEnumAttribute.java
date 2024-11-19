0337cbd2c942aedcfa46a36c7adab47aa75a9c81
Daniel Schwering <daniel.schwering@faktorzehn.de>
2024-11-19 08:37:37 +0100

    FIPS-11148/FIPS-12375 :: Add mandatory flag to IpsEnumAttribute annotation
    Change-Id: I230914a7c810c0fb356c6987d50e830211539391

--- runtime/faktorips-runtime/src/main/java/org/faktorips/runtime/model/annotation/IpsEnumAttribute.java
+++ runtime/faktorips-runtime/src/main/java/org/faktorips/runtime/model/annotation/IpsEnumAttribute.java
@@ -1,9 +1,9 @@
 /*******************************************************************************
  * Copyright (c) Faktor Zehn GmbH - faktorzehn.org
- * 
+ *
  * This source code is available under the terms of the AGPL Affero General Public License version
  * 3.
- * 
+ *
  * Please see LICENSE.txt for full license terms, including the additional permissions and
  * restrictions as well as the possibility of alternative license terms.
  *******************************************************************************/
@@ -28,4 +28,6 @@ public @interface IpsEnumAttribute {
     boolean unique() default false;
 
     boolean displayName() default false;
+
+    boolean mandatory() default false;
 }
